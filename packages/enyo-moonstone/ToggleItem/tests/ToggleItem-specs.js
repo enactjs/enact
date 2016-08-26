c6a3a9b8583c58f5da9ce04f796ecef6761be443
Dave Freeman <sugardave@gmail.com>
2016-08-26 16:51:22 -0500

    Feature/plat 23529 (#17)
    * add ToggleItem tests
    
    * fixed 'clicked' toggle test
    
    * removed superfluous sinon import
    
    * simplify a couple ToggleItem tests
    
    * :allthethings:

---
+++ packages/enyo-moonstone/ToggleItem/tests/ToggleItem-specs.js
@@ -0,0 +1,83 @@
+
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import {ToggleItem, ToggleItemBase} from '../ToggleItem';
+
+describe('ToggleItem Specs', () => {
+
+	it('should create a \<label\> tag with an \<input\> child', function () {
+		const toggleItem = mount(
+			<ToggleItemBase>
+				Toggle Item
+			</ToggleItemBase>
+		);
+		const expected = 1;
+		const actual = toggleItem.find('label').find('input').length;
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should create \<input type="checkbox"\> by default', function () {
+		const toggleItem = mount(
+			<ToggleItemBase>
+				Toggle Item
+			</ToggleItemBase>
+		);
+		const expected = 1;
+		const actual = toggleItem.find('input[type="checkbox"]').length;
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should create \<input type="radio"\> when prop \'multi\' is false', function () {
+		const toggleItem = mount(
+			<ToggleItemBase multi={false}>
+				Toggle Item
+			</ToggleItemBase>
+		);
+		const expected = 1;
+		const actual = toggleItem.find('input[type="radio"]').length;
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should create an \<Icon\> when a non-element is passed to \'icon\'', function () {
+		const toggleItem = mount(
+			<ToggleItemBase icon="star">
+				Toggle Item
+			</ToggleItemBase>
+		);
+
+		const expected = 1;
+		const actual = toggleItem.find('Icon').length;
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should not create an \<Icon\> when an element is passed to \'icon\'', function () {
+		const icon = <span>*</span>;
+		const toggleItem = mount(
+			<ToggleItemBase icon={icon}>
+				Toggle Item
+			</ToggleItemBase>
+		);
+		const expected = 0;
+		const actual = toggleItem.find('Icon').length;
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should toggle the \'checked\' prop when clicking the item', function () {
+		const toggleItem = shallow(
+			<ToggleItem>
+				Toggle Item
+			</ToggleItem>
+		);
+		toggleItem.simulate('click');
+
+		const expected = true;
+		const actual = toggleItem.prop('checked');
+
+		expect(actual).to.equal(expected);
+	});
+});
