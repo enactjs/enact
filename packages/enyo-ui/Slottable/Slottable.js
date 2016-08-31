7d1fc85c9ec62fcd6bbc8934e4a1a7d32e616a98
Ryan Duffy <ryan.duffy@lge.com>
2016-08-31 16:09:29 -0500

    rework Slottable to redirect children to props
    Previously, Slottable required special Slot children accept a special
    slottable children to pluck off the right components. Too many special
    things ... now, thanks to @djspaceg, Slottable is a much simpler
    component that is only responsible for routing children to props of its
    wrapped child.
    
    To make it dead simple to use a slottable component, we can now
    decorate components with defaultSlot so they can land in the right
    place in a parent without adding slot="".
    
    We also add support for custom tags (e.g. <titleBelow>) to target a
    slot instead of using the slot attribute.
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

---
+++ packages/enyo-ui/Slottable/Slottable.js
@@ -0,0 +1,85 @@
+import R from 'ramda';
+import React from 'react';
+import {kind, hoc} from 'enyo-core';
+
+// ** WARNING ** This is an intentional but likely dangerous hack necessary to clone a child while
+// omitting the `slot` property. It relies on the black box structure of a React element which could
+// change breaking this code. Without it, the slot property will cascade to a DOM node causing a
+// React warning.
+const cloneElement = (child, props, omit) => {
+	if (omit && R.has(omit, child.props)) {
+		return React.createElement(child.type, R.dissoc(omit, child.props), props);
+	}
+
+	return child;
+};
+
+const distributeChild = (child, index, slots, props) => {
+	let c, slot;
+	const hasSlot = R.contains(R.__, slots);
+
+	if (!React.isValidElement(child)) {
+		return false;
+	} else if (hasSlot(slot = child.props.slot)) {
+		c = cloneElement(child, null, {key: index}, 'slot');
+	} else if (hasSlot(slot = child.type.defaultSlot)) {
+		c = child;
+	} else if (hasSlot(slot = child.type)) {
+		c = child.props.children;
+	}
+
+	if (c) {
+		let prop = props[slot];
+		if (prop) {
+			if (Array.isArray(prop)) {
+				prop.push(c);
+			} else {
+				prop = [prop, c];
+			}
+		} else {
+			prop = c;
+		}
+		props[slot] = prop;
+
+		return true;
+	}
+
+	return false;
+};
+
+const distribute = function (slots, props) {
+	if (slots) {
+		const children = [];
+		const adjusted = {...props};
+		React.Children.forEach(props.children, (child, index) => {
+			if (!distributeChild(child, index, slots, adjusted)) {
+				children.push(child);
+			}
+		});
+
+		adjusted.children = children.length > 0 ? children : null;
+		return adjusted;
+	}
+
+	return props;
+};
+
+const defaultConfig = {
+	slots: null
+};
+
+const Slottable = hoc(defaultConfig, (config, Wrapped) => {
+	const slots = config.slots;
+	return kind({
+		name: 'Slottable',
+		render: (props) => {
+			const adjusted = distribute(slots, props);
+			return (
+				<Wrapped {...adjusted} />
+			);
+		}
+	});
+});
+
+export default Slottable;
+export {Slottable};
