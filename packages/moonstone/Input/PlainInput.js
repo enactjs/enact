d3f706516d9edd40b4f7aa42a6802ad84be24980
Ryan Duffy <ryan.duffy@lge.com>
2016-09-12 14:25:10 -0500

    reorg and clean up Input
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

---
+++ packages/moonstone/Input/PlainInput.js
@@ -0,0 +1,57 @@
+import kind from 'enact-core/kind';
+import {Spottable} from 'enact-spotlight';
+import React, {PropTypes} from 'react';
+
+import css from './Input.less';
+
+const PlainInputBase = kind({
+	name: 'PlainInputBase',
+
+	propTypes: {
+		className: PropTypes.string,
+		disabled: PropTypes.bool,
+		onChange: PropTypes.func,
+		placeholder: PropTypes.string,
+		type: PropTypes.string,
+		value: PropTypes.string
+	},
+
+	defaultProps: {
+		disabled: false,
+		placeholder: '',
+		type: 'text',
+		value: ''
+	},
+
+	styles: {
+		css,
+		className: 'input'
+	},
+
+	computed: {
+		className: ({disabled, styler}) => styler.append({disabled})
+	},
+
+	render: ({inputRef, ...rest}) => {
+		delete rest.useEnterKey;
+
+		return (
+			<input {...rest} ref={inputRef} />
+		);
+	}
+});
+
+const PlainInput = Spottable(kind({
+	name: 'PlainInput',
+
+	render: (props) => {
+		delete props.spotlightDisabled;
+
+		return (
+			<PlainInput {...props} />
+		);
+	}
+}));
+
+export default PlainInput;
+export {PlainInput, PlainInputBase};
