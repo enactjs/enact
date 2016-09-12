fe668546e9a15f52b1eb1402bfccebebd8ff6d77
Derek Tor <derek.tor@lge.com>
2016-09-12 15:30:35 -0700

    Added Storybook
    Enyo-DCO-1.1-Signed-off-by: Derek Tor derek.tor@lge.com

---
+++ packages/samples/.storybook/config.js
@@ -0,0 +1,10 @@
+import { configure, setAddon } from '@kadira/storybook';
+import infoAddon from '@kadira/react-storybook-addon-info';
+
+setAddon(infoAddon);
+
+function loadStories() {
+	require('../stories/moonstone-stories/Picker.js');
+}
+
+configure(loadStories, module);
---
+++ packages/samples/.storybook/webpack.config.js
@@ -0,0 +1,15 @@
+var config = require('enyo-config/src/common-config');
+
+module.exports = config.common({
+	ri: {
+		baseSize: 24
+	}
+});
+delete module.exports.entry;
+delete module.exports.output;
+for(var i=0; i<module.exports.module.loaders.length; i++) {
+	if(module.exports.module.loaders[i].test.toString()==/\.(c|le)ss$/.toString()) {
+		module.exports.module.loaders[i].loader = 'style!css?modules&importLoaders=1!less';
+	}
+}
+module.exports.plugins.splice(module.exports.plugins.length-1, 1);
