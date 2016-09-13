28c1f9efeeaec8a79859f530fb8bfb6bb9f20c4a
Stephen Choi <stephen.choi@lge.com>
2016-09-13 16:31:51 -0700

    add LS2Request

---
+++ packages/webos/LS2Request/LS2Request.js
@@ -0,0 +1,99 @@
+let refs = {};
+
+const adjustPath = (path) => {
+	if (path.slice(-1) !== '/') {
+		path += '/';
+	}
+	return path;
+};
+
+export default class LS2Request {
+	constructor () {
+		this.bridge = null;
+		this.cancelled = false;
+		this.subscribe = false;
+	}
+
+	send ({
+		service = '',
+		method = '',
+		parameters = {},
+		onSuccess = null,
+		onFailure = null,
+		onComplete = null,
+		subscribe = false
+	}) {
+		if (!window.PalmServiceBridge) {
+			/* eslint no-unused-expressions: ["error", { "allowShortCircuit": true }]*/
+			onFailure && onFailure({errorCode:-1, errorText:'PalmServiceBridge not found.', returnValue: false});
+			onComplete && onComplete({errorCode:-1, errorText:'PalmServiceBridge not found.', returnValue: false});
+			// eslint-disable-next-line no-undef, no-console
+			console.error('PalmServiceBridge not found.');
+			return;
+		}
+
+		if (this.ts && refs[this.ts]) {
+			delete refs[this.ts];
+		}
+
+		this.subscribe = subscribe;
+		if (this.subscribe) {
+			parameters.subscribe = this.subscribe;
+		}
+		if (parameters.subscribe) {
+			this.subscribe = parameters.subscribe;
+		}
+
+		// eslint-disable-next-line no-undef
+		this.ts = performance.now();
+		refs[this.ts] = this;
+
+		// eslint-disable-next-line no-undef
+		this.bridge = new PalmServiceBridge();
+		this.bridge.onservicecallback = this.callback.bind(this, onSuccess, onFailure, onComplete);
+		this.bridge.call(adjustPath(service) + method, JSON.stringify(parameters));
+
+		return this;
+	}
+
+	callback (onSuccess, onFailure, onComplete, msg) {
+		if (this.cancelled) {
+			return;
+		}
+		let parsedMsg;
+		try {
+			parsedMsg = JSON.parse(msg);
+		} catch (e) {
+			parsedMsg = {
+				errorCode: -1,
+				errorText: msg,
+				returnValue: false
+			};
+		}
+
+		if ((parsedMsg.errorCode || parsedMsg.returnValue === false) && onFailure) {
+			onFailure(parsedMsg);
+		} else if (onSuccess) {
+			onSuccess(parsedMsg);
+		}
+
+		if (onComplete) {
+			onComplete(parsedMsg);
+		}
+		if (!this.subscribe) {
+			this.cancel();
+		}
+	}
+
+	cancel () {
+		this.cancelled = true;
+		if (this.bridge) {
+			this.bridge.cancel();
+			this.bridge = null;
+		}
+
+		if (this.ts && refs[this.ts]) {
+			delete refs[this.ts];
+		}
+	}
+}
