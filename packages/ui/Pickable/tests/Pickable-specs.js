0ec841ebfff6b06dd456900b6f465a51e28d8d6c
Dave Freeman <dave.freeman@lge.com>
2016-09-14 18:20:42 -0500

    use a real DOM event for checking assigned handler
    and switch to using shallow() instead of mount()

--- packages/ui/Pickable/tests/Pickable-specs.js
+++ packages/ui/Pickable/tests/Pickable-specs.js
@@ -2,27 +2,15 @@
 
 import React from 'react';
 import sinon from 'sinon';
-import {mount} from 'enzyme';
+import {shallow} from 'enzyme';
 import Pickable from '../Pickable';
 
 describe('Pickable Specs', () => {
 	const testValue = 3;
 
-	it('should accept a \'defaultValue\' prop', function () {
-		const Component = Pickable('div');
-		const subject = mount(
-			<Component defaultValue={testValue} />
-		);
-
-		const expected = testValue;
-		const actual = subject.prop('defaultValue');
-
-		expect(actual).to.equal(expected);
-	});
-
 	it('should pass \'defaultValue\' prop as \'value\' to the wrapped component', function () {
 		const Component = Pickable('div');
-		const subject = mount(
+		const subject = shallow(
 			<Component defaultValue={testValue} />
 		);
 		const wrapped = subject.find('div');
@@ -36,7 +24,7 @@ describe('Pickable Specs', () => {
 	it('should pass configured \'prop\' as the value\'s key to the wrapped component', function () {
 		const prop = 'id';
 		const Component = Pickable({prop: prop}, 'div');
-		const subject = mount(
+		const subject = shallow(
 			<Component defaultId={testValue} />
 		);
 		const wrapped = subject.find('div');
@@ -49,7 +37,7 @@ describe('Pickable Specs', () => {
 
 	it('should pass \'onChange\' handler to the wrapped component', function () {
 		const Component = Pickable('div');
-		const subject = mount(
+		const subject = shallow(
 			<Component />
 		);
 		const wrapped = subject.find('div');
@@ -63,7 +51,7 @@ describe('Pickable Specs', () => {
 	it('should invoke \'onChange\' handler', function() {
 		const handleChange = sinon.spy();
 		const Component = Pickable('div');
-		const subject = mount(
+		const subject = shallow(
 			<Component onChange={handleChange} />
 		);
 		subject.simulate('change', {});
@@ -75,9 +63,9 @@ describe('Pickable Specs', () => {
 	});
 
 	it('should pass configured handler to the wrapped component', function () {
-		const handle = 'onModify';
+		const handle = 'onClick';
 		const Component = Pickable({pick: handle}, 'div');
-		const subject = mount(
+		const subject = shallow(
 			<Component />
 		);
 		const wrapped = subject.find('div');
