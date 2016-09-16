676252567512f05063a3af7468417dfc913836f8
Dave Freeman <dave.freeman@lge.com>
2016-09-16 16:40:32 -0500

    update tests

--- packages/ui/resolution/tests/ResolutionDecorator-specs.js
+++ packages/ui/resolution/tests/ResolutionDecorator-specs.js
@@ -1,7 +1,7 @@
 /* globals describe, it, expect */
 
 import React from 'react';
-import {mount, shallow} from 'enzyme';
+import {shallow} from 'enzyme';
 import ResolutionDecorator from '../ResolutionDecorator';
 
 describe('ResolutionDecorator Specs', () => {
@@ -21,51 +21,48 @@ describe('ResolutionDecorator Specs', () => {
 	});
 
 	it('should allow custom screen types', function () {
-		const name = 'mhd;';
+		const name = 'mhd';
 		const screens = [
 			{name: name, pxPerRem: 36, width: 1440, height: 920, aspectRatioName: 'hdtv', base: true}
 		];
 		const Component = ResolutionDecorator({screenTypes: screens}, 'div');
-		const subject = mount(
+		const subject = shallow(
 			<Component />
 		);
-		subject.component.getInstance().handleResize();
 
-		const expected = name;
-		const actual = subject.state('screenType');
+		const expected = true;
+		const actual = subject.find('div').hasClass('enact-res-mhd');
 
 		expect(actual).to.equal(expected);
 	});
 
 	it.skip('should update the resolution when the screen is resized', function () {
-		const name = 'mhd;';
+		const name = 'mhd';
 		const screens = [
 			{name: name, pxPerRem: 36, width: 1440, height: 920, aspectRatioName: 'hdtv', base: true}
 		];
 		const Component = ResolutionDecorator({screenTypes: screens}, 'div');
-		const subject = mount(
+		const subject = shallow(
 			<Component />
 		);
-		subject.component.getInstance().handleResize();
 
-		const expected = true;
+		const expected = subject;
 		const actual = false;
 
 		expect(actual).to.equal(expected);
 	});
 
 	it.skip('should not allow dynamic resolution updates when \'dynamic\' config option is false', function () {
-		const name = 'mhd;';
+		const name = 'mhd';
 		const screens = [
 			{name: name, pxPerRem: 36, width: 1440, height: 920, aspectRatioName: 'hdtv', base: true}
 		];
 		const Component = ResolutionDecorator({screenTypes: screens, dynamic: false}, 'div');
-		const subject = mount(
+		const subject = shallow(
 			<Component />
 		);
-		subject.component.getInstance().handleResize();
 
-		const expected = true;
+		const expected = subject;
 		const actual = false;
 
 		expect(actual).to.equal(expected);
