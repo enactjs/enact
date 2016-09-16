6015246ac40bb052d243f4050a1492d637e9f37a
Blake Stephens <blake.stephens@lge.com>
2016-09-15 18:33:58 -0700

    Cleanup of sample and polish on Picker Basic structure established
    Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- packages/briefcase/.storybook/config.js
+++ packages/briefcase/.storybook/config.js
@@ -1,5 +1,13 @@
-import {configure, setAddon} from '@kadira/storybook';
+import {configure, setAddon, addDecorator} from '@kadira/storybook';
 import infoAddon from '@kadira/react-storybook-addon-info';
+import backgrounds from 'react-storybook-addon-backgrounds';
+import Moonstone from '../src/MoonstoneEnvironment';
+
+addDecorator(Moonstone);
+addDecorator(backgrounds([
+	{name: 'default', value: '#000000'},
+	{name: 'image', value: 'gray url("http://lorempixel.com/720/480/") no-repeat center/cover'}
+]));
 
 setAddon(infoAddon);
 
