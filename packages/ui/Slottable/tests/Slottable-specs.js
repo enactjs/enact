c4f412fe1ae9591d7e193843081dac2e1166c889
Dave Freeman <sugardave@users.noreply.github.com>
2016-09-20 14:00:32 -0500

    PLAT-26610 Create unit tests for Slottable (#20)
    * add tests for Slottable
    
    Enyo-DCO-1.1-Signed-off-by: Dave Freeman <dave.freeman@lge.com>
    
    * fix Slottable's cloneElement to drop slot property
    
    Slottable was calling a crufty version of internal cloneElement with
    the wrong arguments.
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * more Slottable tests
    
    * removed unnecessary imports
    
    * update tests
    
    * update test
    
    * test cleanup
    
    * Used stub to replace spy. Allows us to swallow and test warning.
    
    Enyo-DCO-1.1-Signed-off-by: Derek Tor derek.tor@lge.com
    
    
    Reviewed-By: Ryan Duffy (ryan.duffy@lge.com)
    Integrated-By: Aaron Tam (aaron.tam@lge.com)

---
+++ packages/ui/Slottable/tests/Slottable-specs.js
@@ -0,0 +1,148 @@
+/* globals console */
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
+import React from 'react';
+import {mount} from 'enzyme';
+import kind from 'enact-core/kind';
+import Slottable from '../Slottable';
+import sinon from 'sinon';
+
+describe('Slottable Specs', () => {
+
+	it('should distribute children with a \'slot\' property', function () {
+		const Component = Slottable({slots: ['a', 'b', 'c']}, ({a, b, c}) => (
+			<div>
+				{c}
+				{b}
+				{a}
+			</div>
+		));
+		const subject = mount(
+			<Component>
+				<div slot='a'>A</div>
+				<div slot='b'>B</div>
+				<div slot='c'>C</div>
+			</Component>
+		);
+
+		const expected = 'CBA';
+		const actual = subject.text();
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should distribute children with a \'type\' that matches a slot', function () {
+		const Component = Slottable({slots: ['a', 'b', 'c', 'custom']}, ({a, b, c, custom}) => (
+			<div>
+				{c}
+				{b}
+				{a}
+				{custom}
+			</div>
+		));
+		const subject = mount(
+			<Component>
+				<div slot='a'>A</div>
+				<div slot='b'>B</div>
+				<custom>D</custom>
+				<div slot='c'>C</div>
+			</Component>
+		);
+
+		const expected = 'CBAD';
+		const actual = subject.text();
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should distribute children whose \'type\' has a \'defaultSlot\' property that matches a slot', function () {
+		const Custom = kind({
+			name: 'Custom',
+			render: (props) => {
+				return <div>{props.children}</div>;
+			}
+		});
+		Custom.defaultSlot = 'a';
+
+		const Component = Slottable({slots: ['a', 'b', 'c']}, ({a, b, c}) => (
+			<div>
+				{c}
+				{b}
+				{a}
+			</div>
+		));
+		const subject = mount(
+			<Component>
+				<div slot='a'>A</div>
+				<div slot='b'>B</div>
+				<Custom>D</Custom>
+				<div slot='c'>C</div>
+			</Component>
+		);
+
+		const expected = 'CBAD';
+		const actual = subject.text();
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should distribute children with no \'slot\' property to Slottable\'s \'children\'', function () {
+		const Component = Slottable({slots: ['a', 'b']}, ({a, b, children}) => (
+			<div>
+				{children}
+				{b}
+				{a}
+			</div>
+		));
+		const subject = mount(
+			<Component>
+				<div slot='a'>A</div>
+				<div slot='b'>B</div>
+				<div>C</div>
+			</Component>
+		);
+
+		const expected = 'CBA';
+		const actual = subject.text();
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should not distribute children with an invalid \'slot\' property', function () {
+		// Remove Global Spy and replace with a stub instead
+		console.error.restore();
+		sinon.stub(console, 'error');
+
+		const Component = Slottable({slots: ['a', 'b']}, ({a, b, c}) => (
+			<div>
+				{c}
+				{b}
+				{a}
+			</div>
+		));
+
+		const subject = mount(
+			<Component>
+				<div slot='a'>A</div>
+				<div slot='b'>B</div>
+				<div slot='c'>C</div>
+			</Component>
+		);
+
+		const expected = 'BA';
+		const actual = subject.text();
+
+		expect(actual).to.equal(expected);
+
+		// Check to make sure that we only get the one expected error
+		const actualErrorsLength = console.error.args.length;
+		const expectedErrorLength = 1;
+
+		expect(actualErrorsLength).to.equal(expectedErrorLength);
+
+		const actualError = console.error.args[0][0];
+		const expectedError = 'Warning: The slot "c" specified on div does not exist';
+
+		expect(actualError).to.equal(expectedError);
+	});
+});
