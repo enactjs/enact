dcc3b65d2c5f2d7c15615a1cccbbd8ed4379c06a
Derek Tor <derekmuoktor@gmail.com>
2016-09-20 16:26:24 -0700

    Added tests for Transition (#23)
    * Added tests for Transition
    
    Enyo-DCO-1.1-Signed-off-by: Derek Tor derek.tor@lge.com
    
    * Changed tests for author classname and styles
    
    Enyo-DCO-1.1-Signed-off-by: Derek Tor derek.tor@lge.com
    
    * Removed should apply styles to inner wrapper when type == 'clip'
    
    
    Reviewed-By: Ryan Duffy (ryan.duffy@lge.com)
    Integrated-By: Aaron Tam (aaron.tam@lge.com)

---
+++ packages/ui/Transition/tests/Transistion-specs.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import Transition from '../Transition';
+import css from '../Transition.less';
+
+describe('Transition Specs', () => {
+	// NOTE: Feature not yet implemented
+	it.skip('should apply author classes', function () {
+		const className = 'classA classB';
+
+		const ChildNode = (props) => <div {...props}>Body</div>;
+
+		const wrapped = mount(
+			<Transition className={className}>
+				<ChildNode />
+			</Transition>
+		);
+
+		const expected = className;
+		const actual = wrapped.find('ChildNode').prop('className');
+
+		expect(actual).to.contain(expected);
+	});
+
+	// NOTE: Feature not yet implemented
+	it.skip('should apply author styles', function () {
+		const styles = {
+			color: '#000000',
+			backgroundColor: '#FFFFFF'
+		};
+
+		const ChildNode = (props) => <div {...props}>Body</div>;
+
+		const wrapped = mount(
+			<Transition style={styles}>
+				<ChildNode />
+			</Transition>
+		);
+
+		const expected = styles;
+		const actual = wrapped.find('ChildNode').prop('style');
+
+		expect(actual).to.equal(expected);
+	});
+
+	// Tests for prop and className combinations
+	const visibleCombinations = [
+		[css.shown, true],
+		[css.hidden, false]
+	];
+
+	const directionCombination = [
+		[css.up, 'up'],
+		[css.right, 'right'],
+		[css.down, 'down'],
+		[css.left, 'left']
+	];
+
+	const durationCombination = [
+		[css.short, 'short'],
+		[css.medium, 'medium'],
+		[css.long, 'long']
+	];
+
+	const timingFunctionCombination = [
+		[css['ease-in-out'], 'ease-in-out'],
+		[css.ease, 'ease'],
+		[css.linear, 'linear']
+	];
+
+	const propStyleCombination = [
+		['visible', visibleCombinations],
+		['duration', durationCombination],
+		['direction', directionCombination],
+		['timingFunction', timingFunctionCombination]
+	];
+
+	const propStyleMap = new Map(propStyleCombination);
+
+	propStyleMap.forEach((val, prop) => {
+		const combos = new Map(val);
+		combos.forEach((value, key) => {
+			it(`should apply classes for ${prop}={${value}}`, function () {
+				const propValue = {
+					[prop]: value
+				};
+				const wrapped = mount(
+					<Transition {...propValue}>Body</Transition>
+				);
+
+				const expected = key;
+				const actual = wrapped.childAt(0).prop('className');
+
+				expect(actual).to.contain(expected);
+			});
+		});
+	});
+});
