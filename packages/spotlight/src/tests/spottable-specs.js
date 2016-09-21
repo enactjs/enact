fe8db75f6a1cd4f42b7655770350f2703f900938
jeremy.thomas <jeremy.thomas@lge.com>
2016-09-21 17:42:03 -0400

    Add basic scaffolding for spotlight tests

---
+++ packages/spotlight/src/tests/spottable-specs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+describe('Spottable Specs', () => {
+
+	it('should evaluate 2 variables as a test', function () {
+		
+		const expected = 1;
+		const actual = 1;
+
+		expect(actual).to.equal(expected);
+	});
+});
