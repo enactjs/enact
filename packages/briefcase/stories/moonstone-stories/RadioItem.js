fa7fcae2593c5749fffad14e8b1caaf5c5386897
Stephen Choi <viodragon2@gmail.com>
2016-09-22 13:58:51 -0700

    PLAT-27353 & PLAT-27369 & PLAT-27371 & PLAT-27374: Add stories for CheckboxItem, RadioItem, SelectableItem, SwitchItem (#44)
    * Add stories for CheckboxItem, RadioItem, SelectableItem, SwitchItem
    
    * Remove unused props from sample and knobs
    
    * Restores defaultChecked prop
    
    Issue: PLAT-27338
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>
    
    
    Reviewed-By: Roy Sutton (roy.sutton@lge.com)
    Integrated-By: Aaron Tam (aaron.tam@lge.com)

---
+++ packages/briefcase/stories/moonstone-stories/RadioItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {storiesOf, action} from '@kadira/storybook';
+import {withKnobs, boolean, text} from '@kadira/storybook-addon-knobs';
+
+import {Toggleable} from 'enact-ui/Toggleable';
+import {RadioItemBase} from 'enact-moonstone/RadioItem';
+
+const RadioItem = Toggleable({prop: 'checked'}, RadioItemBase);
+RadioItem.displayName = 'RadioItem';
+RadioItem.propTypes = Object.assign({}, RadioItem.propTypes, RadioItemBase.propTypes);
+RadioItem.defaultProps = Object.assign({}, RadioItem.defaultProps, RadioItemBase.defaultProps);
+
+delete RadioItem.propTypes.checked;
+delete RadioItem.propTypes.icon;
+delete RadioItem.propTypes.iconClasses;
+
+storiesOf('RadioItem')
+	.addDecorator(withKnobs)
+	.addWithInfo(
+		'',
+		'Basic usage of RadioItem',
+		() => (
+			<RadioItem
+				disabled={boolean('disabled', false)}
+				inline={boolean('inline', false)}
+				onToggle={action('onToggle')}
+				value={text('value', 'defaultValue')}
+			>
+				Hello RadioItem
+			</RadioItem>
+		));
---
+++ packages/briefcase/stories/moonstone-stories/SelectableItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {storiesOf, action} from '@kadira/storybook';
+import {withKnobs, boolean, text} from '@kadira/storybook-addon-knobs';
+
+import {Toggleable} from 'enact-ui/Toggleable';
+import {SelectableItemBase} from 'enact-moonstone/SelectableItem';
+
+const SelectableItem = Toggleable({prop: 'checked'}, SelectableItemBase);
+SelectableItem.displayName = 'SelectableItem';
+SelectableItem.propTypes = Object.assign({}, SelectableItem.propTypes, SelectableItemBase.propTypes);
+SelectableItem.defaultProps = Object.assign({}, SelectableItem.defaultProps, SelectableItemBase.defaultProps);
+
+delete SelectableItem.propTypes.checked;
+delete SelectableItem.propTypes.icon;
+delete SelectableItem.propTypes.iconClasses;
+
+storiesOf('SelectableItem')
+	.addDecorator(withKnobs)
+	.addWithInfo(
+		'',
+		'Basic usage of SelectableItem',
+		() => (
+			<SelectableItem
+				disabled={boolean('disabled', false)}
+				inline={boolean('inline', false)}
+				onToggle={action('onToggle')}
+				value={text('value', 'defaultValue')}
+			>
+				Hello SelectableItem
+			</SelectableItem>
+		));
