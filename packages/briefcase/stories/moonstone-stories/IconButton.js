5bf2f50ba1e68396bf4cd44c50cb9735b1e0c7be
Dave Freeman <sugardave@users.noreply.github.com>
2016-09-21 19:30:09 -0500

    PLAT-27352 PLAT-27359 PLAT-27375: Create Best Practices sample for Buttons (#42)
    * Button story
    
    * IconButton story
    
    * Merge ToggleButton story
    
    * Update button stories
    
    * Minor cleanup
    
    Issue: PLAT-27352
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>
    
    
    Reviewed-By: Derek Tor (derek.tor@lge.com)
    Reviewed-By: Roy Sutton (roy.sutton@lge.com)
    Integrated-By: Aaron Tam (aaron.tam@lge.com)

---
+++ packages/briefcase/stories/moonstone-stories/IconButton.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {storiesOf, action} from '@kadira/storybook';
+import {withKnobs, boolean, select} from '@kadira/storybook-addon-knobs';
+
+import IconButton, {IconButtonBase} from 'enact-moonstone/IconButton';
+import IconList from 'enact-moonstone/Icon/IconList';
+
+const buttonStories = storiesOf('IconButton').addDecorator(withKnobs);
+
+IconButton.propTypes = Object.assign({}, IconButtonBase.propTypes, IconButton.propTypes);
+IconButton.defaultProps = Object.assign({}, IconButtonBase.defaultProps, IconButton.defaultProps);
+IconButton.displayName = 'IconButton';
+
+// Set up some defaults for info and knobs
+const prop = {
+	backgroundOpacity: {'opaque': 'opaque', 'translucent': 'translucent', 'transparent': 'transparent'}
+};
+
+const icons = Object.keys(IconList);
+
+buttonStories
+	.addWithInfo(
+		'',
+		'The basic IconButton.',
+		() => (
+			<IconButton
+				onClick={action('onClick')}
+				backgroundOpacity={select('backgroundOpacity', prop.backgroundOpacity)}
+				disabled={boolean('disabled')}
+				small={boolean('small')}
+			>{select('icon', icons, 'play')}</IconButton>
+		)
+	)
+;
---
+++ packages/briefcase/stories/moonstone-stories/ToggleButton.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {storiesOf, action} from '@kadira/storybook';
+import {withKnobs, text, boolean, select} from '@kadira/storybook-addon-knobs';
+
+import Toggleable from 'enact-ui/Toggleable';
+import ToggleButton, {ToggleButtonBase} from 'enact-moonstone/ToggleButton';
+
+const buttonStories = storiesOf('ToggleButton').addDecorator(withKnobs);
+
+const StatefulToggleButton = Toggleable(ToggleButton);
+
+StatefulToggleButton.propTypes = Object.assign({}, ToggleButtonBase.propTypes, ToggleButton.propTypes);
+StatefulToggleButton.defaultProps = Object.assign({}, ToggleButtonBase.defaultProps, ToggleButton.defaultProps);
+StatefulToggleButton.displayName = 'ToggleButton';
+
+// Set up some defaults for info and knobs
+const prop = {
+	backgroundOpacity: {'opaque': 'opaque', 'translucent': 'translucent', 'transparent': 'transparent'}
+};
+
+buttonStories
+	.addWithInfo(
+		'',
+		'The basic ToggleButton.',
+		() => (
+			<StatefulToggleButton
+				onClick={action('onClick')}
+				backgroundOpacity={select('backgroundOpacity', prop.backgroundOpacity)}
+				disabled={boolean('disabled')}
+				preserveCase={boolean('preserveCase')}
+				small={boolean('small')}
+				toggleOnLabel={text('toggleOnLabel', 'On')}
+				toggleOffLabel={text('toggleOffLabel', 'Off')}
+			/>
+		)
+	)
+;
