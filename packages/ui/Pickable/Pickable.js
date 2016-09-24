8b6c2a2ecb62e47c4a0807ecfd7542a208a3783a
Roy Sutton <pre101@pre101.com>
2016-09-23 21:09:41 -0400

    ENYO-3491 Fix prop bleed in Pickable (#34)
    * ENYO-3491 Fix prop bleed in Pickable
    
    Enyo-DCO-1.1-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Change let to const
    
    Enyo-DCO-1.1-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    
    
    Reviewed-By: Ryan Duffy (ryan.duffy@lge.com)
    Integrated-By: Aaron Tam (aaron.tam@lge.com)

--- packages/ui/Pickable/Pickable.js
+++ packages/ui/Pickable/Pickable.js
@@ -84,20 +84,19 @@ const PickableHOC = hoc(defaultConfig, (config, Wrapped) => {
 		constructor (props) {
 			super(props);
 			const key = (mutable && prop in props) ? prop : defaultPropKey;
-			const value = this.value = props[key];
+			const value = props[key];
 			this.state = {value};
 		}
 
 		componentWillReceiveProps (nextProps) {
 			if (mutable) {
-				this.value = nextProps[prop];
-			} else {
-				this.value = this.state.value;
+				const value = nextProps[prop];
+				this.setState({value});
 			}
 		}
 
 		pick = (ev) => {
-			const value = this.value = ev[config.prop];
+			const value = ev[config.prop];
 			const onPick = this.props[config.pick];
 
 			this.setState({value});
@@ -110,7 +109,7 @@ const PickableHOC = hoc(defaultConfig, (config, Wrapped) => {
 			props[config.prop] = this.state.value;
 			delete props[defaultPropKey];
 
-			return <Wrapped {...props} value={this.value} />;
+			return <Wrapped {...props} />;
 		}
 	};
 });
