889e1039617207d928a08d0d53b1c80dacd212e1
Jason Robitaille <jason.robitaille@lge.com>
2016-09-26 15:04:09 -0700

    Temporarily install local lerna as part of dependency-based bootstrap

--- scripts/npm-bootstrap.js
+++ scripts/npm-bootstrap.js
@@ -2,11 +2,23 @@
 
 'use strict';
 
-var lerna = require('lerna');
-var GitUtilities = require('lerna/lib/GitUtilities');
+var
+	cp = require('child_process'),
+	path = require('path'),
+	lObj = require(path.join(__dirname, '..', 'lerna.json'));
 
-GitUtilities.isInitialized = function() { return true; };
-GitUtilities.getTopLevelDirectory = function() { return process.cwd(); };
+cp.exec('npm install lerna@' + lObj.lerna, {env:process.env, cwd:__dirname}, function(err, stdout, stderr) {
+	var lerna = require('lerna');
+	var GitUtilities = require('lerna/lib/GitUtilities');
 
-var bootstrap = new lerna.__commands__.bootstrap('', {concurrency:1});
-bootstrap.run();
+	GitUtilities.isInitialized = function() { return true; };
+	GitUtilities.getTopLevelDirectory = function() { return process.cwd(); };
+
+	var bootstrap = new lerna.__commands__.bootstrap('', {concurrency:1});
+	bootstrap.run();
+
+	if(path.basename(path.dirname(path.dirname(path.dirname(__dirname))))==='node_modules') {
+		cp.exec('npm remove lerna', {env:process.env, cwd:__dirname});
+	}
+	
+});
