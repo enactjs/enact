0aa34a55f144ea0ef5d5d78d53a15ec02168110f
Roy Sutton <pre101@pre101.com>
2016-09-27 14:57:07 -0400

    PLAT-26630 Add documentation for i18n HOCs (#54)
    * PLAT-26630 Add documentation for i18n HOCs
    
    Enyo-DCO-1.1-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Converts to scoped modules and minor clean up
    
    Issue: PLAT-26630
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>
    
    
    Reviewed-By: Aaron Tam (aaron.tam@lge.com)
    Integrated-By: Aaron Tam (aaron.tam@lge.com)

--- packages/i18n/I18nDecorator/I18nDecorator.js
+++ packages/i18n/I18nDecorator/I18nDecorator.js
@@ -1,16 +1,46 @@
+/**
+ * Exports the {@link module:@enact/i18n/I18nDecorator~I18nDecorator} component and
+ * {@link module:@enact/i18n/I18nDecorator~contextTypes} validation rules.
+ *
+ * @module @enact/i18n/I18nDecorator
+ */
+
 import hoc from '@enact/core/hoc';
 import React from 'react';
 
 import '../src/glue';
-import {isRtl, getI18NClasses, updateLocale} from '../src/locale';
+import {isRtl, getI18nClasses, updateLocale} from '../src/locale';
 
+/**
+ * `contextTypes` is an object that exports the default context validation rules. These must be applied
+ * to any child components that wish to receive the i18n context.
+ *
+ * ```
+ * import {contextTypes} from '@enact/i18n/I18nDecorator';
+ * ...
+ * myComponent.contextTypes = contextTypes;
+ * ```
+ *
+ * @public
+ */
 const contextTypes = {
 	rtl: React.PropTypes.bool,
 	updateLocale: React.PropTypes.func
 };
 
+/**
+ * {@link module:@enact/i18n/I18nDecorator~I18nDecorator} is a Higher Order Component that is used to wrap
+ * the root element in an app. It provides an `rtl` member on the context of the wrapped component, allowing
+ * the children to check the current text directionality as well as an `updateLocale` method that can be
+ * used to update the current locale.
+ *
+ * There are no configurable options on this HOC.
+ *
+ * @class I18nDecorator
+ * @public
+ */
 const IntlHoc = hoc((config, Wrapped) => {
-	return class I18NDecorator extends React.Component {
+	return class I18nDecorator extends React.Component {
 		static childContextTypes = contextTypes
 		static propTypes = {
 			className: React.PropTypes.string
@@ -31,7 +61,7 @@ const IntlHoc = hoc((config, Wrapped) => {
 		}
 
 		render () {
-			let classes = getI18NClasses();
+			let classes = getI18nClasses();
 			if (this.props.className) {
 				classes = this.props.className + ' ' + classes;
 			}
@@ -44,4 +74,4 @@ const IntlHoc = hoc((config, Wrapped) => {
 });
 
 export default IntlHoc;
-export {IntlHoc as I18NDecorator, contextTypes};
+export {IntlHoc as I18nDecorator, contextTypes};
