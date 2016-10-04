06ab446e68b6da248b9a5b16fd5fb385bd3ca299
srirama.singeri <srirama.singeri@lge.com>
2016-10-04 19:05:51 +0530

    PLAT-28116 : Add Button qa samples into enact-qa-sampler

--- packages/sampler/stories/qa-stories/Button.js
+++ packages/sampler/stories/qa-stories/Button.js
@@ -1,20 +1,53 @@
 import Button, {ButtonBase} from '@enact/moonstone/Button';
 import React from 'react';
-import {storiesOf} from '@kadira/storybook';
-import {withKnobs} from '@kadira/storybook-addon-knobs';
+import {storiesOf, action} from '@kadira/storybook';
+import {withKnobs, boolean, select, text} from '@kadira/storybook-addon-knobs';
 
 Button.propTypes = Object.assign({}, ButtonBase.propTypes, Button.propTypes);
 Button.defaultProps = Object.assign({}, ButtonBase.defaultProps, Button.defaultProps);
 Button.displayName = 'Button';
 
+// Set up some defaults for info and knobs
+const prop = {
+	backgroundOpacity: {'opaque': 'opaque', 'translucent': 'translucent', 'transparent': 'transparent'},
+	longText:{'Loooooooooooooooooog Button': 'Loooooooooooooooooog Button', 'BUTTON   WITH   EXTRA   SPACES': 'BUTTON   WITH   EXTRA   SPACES'},
+	tallText:{'ิ้  ไั  ஒ  து': 'ิ้  ไั  ஒ  து', 'ÁÉÍÓÚÑÜ': 'ÁÉÍÓÚÑÜ', 'Bản văn': 'Bản văn'}
+};
+
 storiesOf('Button')
 	.addDecorator(withKnobs)
 	.addWithInfo(
-		' ',
-		'The basic Button',
+		'Long Text',
 		() => (
-			<Button>
-				QA Button
+			<Button
+				onClick={action('onClick')}
+				backgroundOpacity={select('backgroundOpacity', prop.backgroundOpacity)}
+				disabled={boolean('disabled')}
+				minWidth={boolean('minWidth')}
+				preserveCase={boolean('preserveCase')}
+				selected={boolean('selected')}
+				small={boolean('small')}
+
+			>
+				{select('value', prop.longText,'Loooooooooooooooooog Button')}
+			</Button>
+		)
+	)
+	.addWithInfo(
+		'Tall Characters',
+		() => (
+			<Button
+				onClick={action('onClick')}
+				backgroundOpacity={select('backgroundOpacity', prop.backgroundOpacity)}
+				disabled={boolean('disabled')}
+				minWidth={boolean('minWidth')}
+				preserveCase={boolean('preserveCase')}
+				selected={boolean('selected')}
+				small={boolean('small')}
+
+			>
+				{select('value', prop.tallText,'ิ้  ไั  ஒ  து')}
 			</Button>
 		)
 	);
+
