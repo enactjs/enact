05259ed7a816f4d087b24a1d26416655ed44ceba
Stephen Choi <stephen.choi@lge.com>
2016-10-04 16:49:19 -0700

    more cleanup

--- packages/sampler/stories/moonstone-stories/ToggleItem.js
+++ packages/sampler/stories/moonstone-stories/ToggleItem.js
@@ -8,26 +8,26 @@ import MockI18NAddon from '../../src/MockI18NAddon';
 import {icons} from '@enact/moonstone/Icon';
 const iconNames = Object.keys(icons);
 
-const StatefulToggleItem = Toggleable({prop: 'checked'}, ToggleItemBase);
-StatefulToggleItem.propTypes = Object.assign({}, ToggleItemBase.propTypes, Toggleable.propTypes);
-StatefulToggleItem.defaultProps = Object.assign({}, ToggleItemBase.defaultProps, Toggleable.defaultProps);
-StatefulToggleItem.displayName = 'StatefulToggleItem';
+const ToggleItem = Toggleable({prop: 'checked'}, ToggleItemBase);
+ToggleItem.propTypes = Object.assign({}, ToggleItemBase.propTypes, Toggleable.propTypes);
+ToggleItem.defaultProps = Object.assign({}, ToggleItemBase.defaultProps, Toggleable.defaultProps);
+ToggleItem.displayName = 'ToggleItem';
 
 storiesOf('ToggleItem')
 	.addDecorator(MockI18NAddon())
 	.addDecorator(withKnobs)
 	.addWithInfo(
-		'',
+		' ',
 		'The basic ToggleItem',
 		() => {
 			return (
-				<StatefulToggleItem
+				<ToggleItem
 					icon={select('icon', iconNames, 'play')}
 					disabled={boolean('disabled', false)}
-					onToggle={action('onToggle')}
 					inline={boolean('inline', false)}
+					onToggle={action('onToggle')}
 				>
-						Toggle Item
-				</StatefulToggleItem>
+					Toggle Item
+				</ToggleItem>
 		);}
 	);
