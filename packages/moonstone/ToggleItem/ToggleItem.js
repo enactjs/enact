bbf740396af17e8d3a7fbc5d91f9a2df23de46ba
Roy Sutton <roy.sutton@lge.com>
2016-10-06 13:49:28 -0700

    Update jsdoc comments
    Enyo-DCO-1.1-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- packages/moonstone/ToggleItem/ToggleItem.js
+++ packages/moonstone/ToggleItem/ToggleItem.js
@@ -1,6 +1,5 @@
 /**
- * Exports the {@link module:@enact/moonstone/ToggleItem~ToggleItem} and {@link module:@enact/moonstone/ToggleItem~ToggleItemBase}
- * components.  The default export is {@link module:@enact/moonstone/ToggleItem~ToggleItem}.
+ * Exports the {@link module:@enact/moonstone/ToggleItem~ToggleItem} component.
  *
  * @module @enact/moonstone/ToggleItem
  */
@@ -14,14 +13,11 @@ import Icon from '../Icon';
 import css from './ToggleItem.less';
 
 /**
- * {@link module:@enact/moonstone/ToggleItem~ToggleItemBase} is a base component
- * to make an Toggleable Item(e.g Checkbox, RadioItem). It has a customizable
- * prop for icon, so any Moonstone Icon can be used to represent the checked
- * state.
+ * {@link module:@enact/moonstone/ToggleItem~ToggleItem} is a component to make a Toggleable Item
+ * (e.g Checkbox, RadioItem). It has a customizable prop for icon, so any Moonstone Icon can be used
+ * to represent the checked state.
  *
- * {@link module:@enact/moonstone/ToggleItem~ToggleItem}
- *
- * @class ToggleItemBase
+ * @class ToggleItem
  * @ui
  * @public
  */
