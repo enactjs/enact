09bc0104c8992a92fc137ebc3fc86d4e5cd50b49
anishlg <anish.td@lge.com>
2016-10-07 13:14:03 +0530

    PLAT-28118: updated qa sample for picker component

--- packages/sampler/stories/qa-stories/Picker.js
+++ packages/sampler/stories/qa-stories/Picker.js
@@ -19,12 +19,6 @@ const prop = {
 const iconNames = Object.keys(icons);
 
 const pickerList = {
-	airports: [
-		'San Francisco Airport Terminal Gate 1',
-		'Boston Airport Terminal Gate 2',
-		'Tokyo Airport Terminal Gate 3',
-		'נמל התעופה בן גוריון טרמינל הבינלאומי'
-	],
 	tall: [
 		'नरेंद्र मोदी',
 		' ฟิ้  ไั  ஒ  து',
@@ -44,73 +38,24 @@ const pickerList = {
 		'Onion',
 		'Broccoli',
 		'Spinach'
-	],
-	countries: [
-		'IND',
-		'USA',
-		'KOR',
-		'CAN',
-		'BEL',
-		'AUS',
-		'FRA',
-		'HKG',
-		'SGP'
 	]
 };
 
 storiesOf('Picker')
 	.addDecorator(withKnobs)
 	.addWithInfo(
-		'Horizontal',
+		'Tall Texts' ,
 		() => (
 			<StatefulPicker
 				onChange={action('onChange')}
 				width={select('width', prop.width, 'large')}
-				orientation={'horizontal'}
+				orientation={select('orientation', prop.orientation, 'horizontal')}
 				wrap={boolean('wrap')}
 				joined={boolean('joined')}
 				noAnimation={boolean('noAnimation')}
 				disabled={boolean('disabled')}
-				incrementIcon={select('incrementIcon', iconNames, 'arrowlargeright')}
-				decrementIcon={select('decrementIcon', iconNames, 'arrowlargeleft')}
-			>
-				{pickerList.airports}
-			</StatefulPicker>
-		)
-	)
-
-	.addWithInfo(
-		'Vertical',
-		() => (
-			<StatefulPicker
-				onChange={action('onChange')}
-				width={select('width', prop.width, 'large')}
-				orientation={'vertical'}
-				wrap={boolean('wrap')}
-				joined={boolean('joined')}
-				noAnimation={boolean('noAnimation')}
-				disabled={boolean('disabled')}
-				incrementIcon={select('incrementIcon', iconNames, 'arrowlargeup')}
-				decrementIcon={select('decrementIcon', iconNames, 'arrowlargedown')}
-			>
-				{pickerList.airports}
-			</StatefulPicker>
-		)
-	)
-
-	.addWithInfo(
-		'Horizontal Tall',
-		() => (
-			<StatefulPicker
-				onChange={action('onChange')}
-				width={select('width', prop.width, 'large')}
-				orientation={'horizontal'}
-				wrap={boolean('wrap')}
-				joined={boolean('joined')}
-				noAnimation={boolean('noAnimation')}
-				disabled={boolean('disabled')}
-				incrementIcon={select('incrementIcon', iconNames, 'arrowlargeright')}
-				decrementIcon={select('decrementIcon', iconNames, 'arrowlargeleft')}
+				incrementIcon={select('incrementIcon', iconNames)}
+				decrementIcon={select('decrementIcon', iconNames)}
 			>
 				{pickerList.tall}
 			</StatefulPicker>
@@ -118,37 +63,18 @@ storiesOf('Picker')
 	)
 
 	.addWithInfo(
-		'Vertical Tall',
+		'Long Texts',
 		() => (
 			<StatefulPicker
 				onChange={action('onChange')}
 				width={select('width', prop.width, 'large')}
-				orientation={'vertical'}
+				orientation={select('orientation', prop.orientation, 'horizontal')}
 				wrap={boolean('wrap')}
 				joined={boolean('joined')}
 				noAnimation={boolean('noAnimation')}
 				disabled={boolean('disabled')}
-				incrementIcon={select('incrementIcon', iconNames, 'arrowlargeup')}
-				decrementIcon={select('decrementIcon', iconNames, 'arrowlargedown')}
-			>
-				{pickerList.tall}
-			</StatefulPicker>
-		)
-	)
-
-	.addWithInfo(
-		'Horizontal Long',
-		() => (
-			<StatefulPicker
-				onChange={action('onChange')}
-				width='large'
-				orientation={'horizontal'}
-				wrap
-				joined={boolean('joined')}
-				noAnimation={boolean('noAnimation')}
-				disabled={boolean('disabled')}
-				incrementIcon={select('incrementIcon', iconNames, 'arrowlargeright')}
-				decrementIcon={select('decrementIcon', iconNames, 'arrowlargeleft')}
+				incrementIcon={select('incrementIcon', iconNames)}
+				decrementIcon={select('decrementIcon', iconNames)}
 			>
 				{pickerList.long}
 			</StatefulPicker>
@@ -156,192 +82,21 @@ storiesOf('Picker')
 	)
 
 	.addWithInfo(
-		'Vertical Long',
+		'Default value',
 		() => (
 			<StatefulPicker
 				onChange={action('onChange')}
-				width='large'
-				orientation={'vertical'}
-				wrap
+				width={select('width', prop.width, 'medium')}
+				orientation={select('orientation', prop.orientation, 'horizontal')}
+				wrap={boolean('wrap')}
 				joined={boolean('joined')}
 				noAnimation={boolean('noAnimation')}
 				disabled={boolean('disabled')}
-				incrementIcon={select('incrementIcon', iconNames, 'arrowlargeup')}
-				decrementIcon={select('decrementIcon', iconNames, 'arrowlargedown')}
-			>
-				{pickerList.long}
-			</StatefulPicker>
-		)
-	)
-
-	.addWithInfo(
-		'Horizontal with defalut value',
-		() => (
-			<StatefulPicker
-				onChange={action('onChange')}
-				width='medium'
-				orientation={'horizontal'}
-				joined={boolean('joined')}
-				noAnimation={boolean('noAnimation')}
-				disabled={boolean('disabled')}
-				incrementIcon={select('incrementIcon', iconNames, 'arrowlargeright')}
-				decrementIcon={select('decrementIcon', iconNames, 'arrowlargeleft')}
-				value={2}
+				incrementIcon={select('incrementIcon', iconNames)}
+				decrementIcon={select('decrementIcon', iconNames)}
+				defaultValue={2}
 			>
 				{pickerList.vegetables}
 			</StatefulPicker>
-		)
-	)
-
-	.addWithInfo(
-		'Vertical with defalut value',
-		() => (
-			<StatefulPicker
-				onChange={action('onChange')}
-				width='medium'
-				orientation={'vertical'}
-				joined={boolean('joined')}
-				noAnimation={boolean('noAnimation')}
-				disabled={boolean('disabled')}
-				incrementIcon={select('incrementIcon', iconNames, 'arrowlargeup')}
-				decrementIcon={select('decrementIcon', iconNames, 'arrowlargedown')}
-				value={2}
-			>
-				{pickerList.vegetables}
-			</StatefulPicker>
-		)
-	)
-
-	.addWithInfo(
-		'Horizontal with null defalut value',
-		() => (
-			<StatefulPicker
-				onChange={action('onChange')}
-				width='medium'
-				orientation={'horizontal'}
-				joined={boolean('joined')}
-				noAnimation={boolean('noAnimation')}
-				disabled={boolean('disabled')}
-				incrementIcon={select('incrementIcon', iconNames, 'arrowlargeright')}
-				decrementIcon={select('decrementIcon', iconNames, 'arrowlargeleft')}
-				value=''
-			>
-				{pickerList.vegetables}
-			</StatefulPicker>
-		)
-	)
-
-	.addWithInfo(
-		'Vertical with null defalut value',
-		() => (
-			<StatefulPicker
-				onChange={action('onChange')}
-				width='medium'
-				orientation={'vertical'}
-				joined={boolean('joined')}
-				noAnimation={boolean('noAnimation')}
-				disabled={boolean('disabled')}
-				incrementIcon={text('incrementIcon')}
-				decrementIcon={text('decrementIcon')}
-				incrementIcon={select('incrementIcon', iconNames, 'arrowlargeup')}
-				decrementIcon={select('decrementIcon', iconNames, 'arrowlargedown')}
-				value=''
-			>
-				{pickerList.vegetables}
-			</StatefulPicker>
-		)
-	)
-
-	.addWithInfo(
-		'Horizontal Multiple',
-		() => (
-			<div>
-				<StatefulPicker
-					onChange={action('onChange')}
-					width={'small'}
-					orientation={'horizontal'}
-					wrap={boolean('wrap')}
-					joined
-					noAnimation={boolean('noAnimation')}
-					disabled={boolean('disabled')}
-					incrementIcon={select('incrementIcon', iconNames, 'arrowlargeright')}
-					decrementIcon={select('decrementIcon', iconNames, 'arrowlargeleft')}
-				>
-					{pickerList.countries}
-				</StatefulPicker>
-				<StatefulPicker
-					onChange={action('onChange')}
-					width={'small'}
-					orientation={'horizontal'}
-					wrap={boolean('wrap')}
-					joined
-					noAnimation={boolean('noAnimation')}
-					disabled={boolean('disabled')}
-					incrementIcon={select('incrementIcon', iconNames, 'arrowlargeright')}
-					decrementIcon={select('decrementIcon', iconNames, 'arrowlargeleft')}
-				>
-					{pickerList.countries}
-				</StatefulPicker>
-				<StatefulPicker
-					onChange={action('onChange')}
-					width={'small'}
-					orientation={'horizontal'}
-					wrap={boolean('wrap')}
-					joined
-					noAnimation={boolean('noAnimation')}
-					disabled={boolean('disabled')}
-					incrementIcon={select('incrementIcon', iconNames, 'arrowlargeright')}
-					decrementIcon={select('decrementIcon', iconNames, 'arrowlargeleft')}
-				>
-					{pickerList.countries}
-				</StatefulPicker>
-			</div>
-		)
-	)
-
-	.addWithInfo(
-		'Vertical Multiple',
-		() => (
-			<div>
-				<StatefulPicker
-					onChange={action('onChange')}
-					width={'small'}
-					orientation={'vertical'}
-					wrap={boolean('wrap')}
-					joined
-					noAnimation={boolean('noAnimation')}
-					disabled={boolean('disabled')}
-					incrementIcon={select('incrementIcon', iconNames, 'arrowlargeup')}
-					decrementIcon={select('decrementIcon', iconNames, 'arrowlargedown')}
-				>
-					{pickerList.countries}
-				</StatefulPicker>
-				<StatefulPicker
-					onChange={action('onChange')}
-					width={'small'}
-					orientation={'vertical'}
-					wrap={boolean('wrap')}
-					joined
-					noAnimation={boolean('noAnimation')}
-					disabled={boolean('disabled')}
-					incrementIcon={select('incrementIcon', iconNames, 'arrowlargeup')}
-					decrementIcon={select('decrementIcon', iconNames, 'arrowlargedown')}
-				>
-					{pickerList.countries}
-				</StatefulPicker>
-				<StatefulPicker
-					onChange={action('onChange')}
-					width={'small'}
-					orientation={'vertical'}
-					wrap={boolean('wrap')}
-					joined
-					noAnimation={boolean('noAnimation')}
-					disabled={boolean('disabled')}
-					incrementIcon={select('incrementIcon', iconNames, 'arrowlargeup')}
-					decrementIcon={select('decrementIcon', iconNames, 'arrowlargedown')}
-				>
-					{pickerList.countries}
-				</StatefulPicker>
-			</div>
 		)
 	);
