a893cb07a08212b55b8616fb2752cea7a6768d90
Madala Cholan Satyanarayana <cholanmadala1510@gmail.com>
2016-10-07 16:03:41 +0530

    PLAT-28117- Made changes as per review comments

--- packages/sampler/stories/qa-stories/Input.js
+++ packages/sampler/stories/qa-stories/Input.js
@@ -1,7 +1,6 @@
 import {kind, hoc} from '@enact/core';
 import {icons} from '@enact/moonstone/Icon';
 import Input, {InputBase} from '@enact/moonstone/Input';
-import Button, {ButtonBase} from '@enact/moonstone/Button';
 import Pickable from '@enact/ui/Pickable';
 import React from 'react';
 import {storiesOf, action} from '@kadira/storybook';
@@ -36,10 +35,10 @@ const iconNames = ['', ...Object.keys(icons)];
 
 const inputData = {
 	longText : 'Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Text',
-	tallText : ['नरेंद्र मोदी',' ฟิ้  ไั  ஒ  து', 'ÃÑÕÂÊÎÔÛÄËÏÖÜŸ'],
+	tallText : ['नरेंद्र मोदी', ' ฟิ้  ไั  ஒ  து', 'ÃÑÕÂÊÎÔÛÄËÏÖÜŸ'],
 	extraSpaceText : 'This		Text 		has			extra 		space',
 	defaultNumber : 10,
-	type: ['text','number','password'],
+	type: ['text', 'number', 'password'],
 	minValue: 0,
 	maxValue: 10
 };
@@ -69,7 +68,7 @@ storiesOf('Input')
 				disabled={boolean('disabled')}
 				iconEnd={select('iconEnd', iconNames)}
 				iconStart={select('iconStart', iconNames)}
-				placeholder={text('placeholder','Input some tall characters')}
+				placeholder={text('placeholder', 'Input some tall characters')}
 				type={select('type', inputData.type, inputData.type[0])}
 				value={select('value', inputData.tallText,  inputData.tallText[2])}
 			/>
@@ -97,47 +96,10 @@ storiesOf('Input')
 				disabled={boolean('disabled')}
 				iconEnd={select('iconEnd', iconNames)}
 				iconStart={select('iconStart', iconNames)}
-				placeholder={text('placeholder','Number')}
 				type={inputData.type[1]}
 				min={text('min value', inputData.minValue)}
 				max={text('max value', inputData.maxValue)}
 				value={text('value', inputData.minValue)}
 			/>
-		)
-	)
-	.addWithInfo(
-		'Multiple Components',
-		() => (
-			<div>
-				<Button 
-					onClick={action('onClick')}
-				>
-					QA Button
-				</Button>
-				<StatefulInput
-					onChange={action('onChange')}
-					disabled={boolean('disabled')}
-					iconEnd={select('iconEnd', iconNames)}
-					iconStart={select('iconStart', iconNames)}
-					placeholder={text('placeholder')}
-					type={select('type', inputData.type, inputData.type[0])}
-					value={text('value', 'QA INPUT')}
-				/>
-				<br />
-				<Button
-					onClick={action('onClick')}
-				>
-					QA Button
-				</Button>
-				<StatefulInput
-					onChange={action('onChange')}
-					disabled={boolean('disabled')}
-					iconEnd={select('iconEnd', iconNames)}
-					iconStart={select('iconStart', iconNames)}
-					placeholder={text('placeholder')}
-					type={select('type', inputData.type, inputData.type[0])}
-					value={text('value', 'QA INPUT')}
-				/>
-			</div>
 		)
 	);
