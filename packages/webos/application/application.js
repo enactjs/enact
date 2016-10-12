182925b159458bd9f3eb0013686316121d1d187c
Stephen Choi <stephen.choi@lge.com>
2016-10-11 18:00:21 -0700

    Add application

---
+++ packages/webos/application/application.js
@@ -0,0 +1,95 @@
+/**
+ * Fetches the appID of the caller app
+ * @returns {String} AppID of the app.
+ */
+const fetchAppId = () => {
+	if (window.PalmSystem && window.PalmSystem.identifier) {
+		// PalmSystem.identifier: <appid> <processid>
+		return window.PalmSystem.identifier.split(' ')[0];
+	}
+};
+
+/**
+ * @callback webOS~appInfoCallback
+ * @param {?object} info - JSON data object read from the app's "appinfo.json" file. Undefined if not found.
+ */
+let appInfo = {};
+/**
+ * Fetches the appinfo.json data of the caller app with a cache saved to webOS.appInfo
+ * @param {webOS~appInfoCallback} callback - The function to called upon completion
+ * @param {String} [path] - An optional relative filepath from the current document to a specific appinfo to read
+ * @returns {undefined}
+ */
+const fetchAppInfo = (callback, path) => {
+	if (Object.keys(appInfo).length === 0) {
+		const parseInfo = (err, info) => {
+			if (!err && info) {
+				try {
+					appInfo = JSON.parse(info);
+					callback && callback(appInfo);
+				} catch (e) {
+					console.error('Unable to parse appinfo.json file for ' + appID);
+					callback && callback();
+				}
+			} else {
+				callback && callback();
+			}
+		};
+		const req = new XMLHttpRequest();
+		req.onreadystatechange = function () {
+			if (req.readyState == 4) {
+				if ((req.status >= 200 && req.status < 300) || req.status === 0) {
+					parseInfo(undefined, req.responseText);
+				} else {
+					parseInfo({status:404});
+				}
+			}
+		};
+		try {
+			req.open('GET', path || 'appinfo.json', true);
+			req.send(null);
+		} catch (e) {
+			parseInfo({status:404});
+		}
+	} else {
+		callback && callback(appInfo);
+	}
+};
+
+/**
+ * Fetches the full root URI path of the caller app
+ * @returns {String} App's URI path the app is within.
+ */
+const fetchAppRootPath = () => {
+	let base = window.location.href;
+	if ('baseURI' in window.document) {
+		base = window.document.baseURI;
+	} else {
+		let baseTags = window.document.getElementsByTagName('base');
+		if (baseTags.length > 0) {
+			base = baseTags[0].href;
+		}
+	}
+	let match = base.match(new RegExp('.*:\/\/[^#]*\/'));
+	if (match) {
+		return match[0];
+	}
+	return '';
+};
+
+/**
+ * Emulate the back key to move backwards 1 level.
+ * @returns {undefined}
+ */
+const platformBack = () => {
+	if (window.PalmSystem && window.PalmSystem.platformBack) {
+		return window.PalmSystem.platformBack();
+	}
+};
+
+export {
+	fetchAppId,
+	fetchAppInfo,
+	fetchAppRootPath,
+	platformBack
+};
