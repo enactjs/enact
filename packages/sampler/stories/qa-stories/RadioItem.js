de3c23921b31cc6694fa0eb3bbd6ab1bce815972
richa.Sha <richa.shaurabh@gmail.com>
2016-10-13 12:15:16 +0530

    PLAT-28119-richa.shaurbh:Upadted RadioItem qa-stories

--- packages/sampler/stories/qa-stories/RadioItem.js
+++ packages/sampler/stories/qa-stories/RadioItem.js
@@ -2,7 +2,7 @@ import {RadioItemBase} from '@enact/moonstone/RadioItem';
 import {Toggleable} from '@enact/ui/Toggleable';
 import React from 'react';
 import {storiesOf, action} from '@kadira/storybook';
-import {withKnobs, boolean, select} from '@kadira/storybook-addon-knobs';
+import {withKnobs, boolean} from '@kadira/storybook-addon-knobs';
 
 const RadioItem = Toggleable({prop: 'checked'}, RadioItemBase);
 RadioItem.displayName = 'RadioItem';
@@ -14,10 +14,8 @@ delete RadioItem.propTypes.icon;
 delete RadioItem.propTypes.iconClasses;
 
 const radioData = {
-	checkList: ['None', 'First RadioItem', 'Second RadioItem', 'Both RadioItem'],
-	smallText: ['Text With Space', 'Text Without Space'],
-	longText1 : ['PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSISPNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSISNEUM', 'PNEUMONOUL TRAMICROSCOPICSILICO VOLCANOCONIOSISP NEUMONOULTRAMIC ROSCOPICSILICOVOLCANOCONIOSIS'],
-	longText2 : ['LOPADOTEMACHOSELACHOGALEOKRANIOLEIPSANLOPADOTEMACHOSELACHOGALEOKRANIOLEIPSAN', 'LOPADOTEMACHOSELACHOGALEOKRANIOLEIPSAN LOPADOTEMACHOSELACHOGALEOKRANIOLEIPSAN LOPADOTEMACHOSELACHOGALEOKRANIOLEIPSAN'],
+	longTextWithSpace : ['FirstLongTextWithSpace FirstLongTextWithSpace FirstLongTextWithSpace FirstLongTextWithSpace', 'SecondLongTextWithSpace SecondLongTextWithSpace SecondLongTextWithSpace SecondLongTextWithSpace'],
+	longTextWithoutSpace : ['FirstLongTextWithoutSpace_FirstLongTextWithoutSpace_FirstLongTextWithoutSpace', 'SecondLongTextWithoutSpace_SecondLongTextWithoutSpace_SecondLongTextWithoutSpace_SecondLongTextWithoutSpace'],
 	tallText : ['இந்தியா', 'ÃÑÕÂÊÎÔÛÄËÏÖÜŸ'],
 	rightToLeft : ['صباح الخير', 'مساء الخير']
 };
@@ -25,7 +23,7 @@ const radioData = {
 storiesOf('RadioItem')
 	.addDecorator(withKnobs)
 	.addWithInfo(
-		'Long Text',
+		'Long Text with space',
 		() => (
 			<div>
 				<RadioItem
@@ -33,14 +31,35 @@ storiesOf('RadioItem')
 					inline={boolean('inline', false)}
 					onToggle={action('onToggle')}
 				>
-					{select('Space', radioData.smallText, radioData.smallText[0]) === radioData.smallText[1] ? radioData.longText1[0] : radioData.longText1[1]}
+					{radioData.longTextWithSpace[0]}
 				</RadioItem>
 				<RadioItem
 					disabled={boolean('disabled', false)}
 					inline={boolean('inline', false)}
 					onToggle={action('onToggle')}
 				>
-					{select('Space', radioData.smallText, radioData.smallText[0]) === radioData.smallText[1] ? radioData.longText2[0] : radioData.longText2[1]}
+					{radioData.longTextWithSpace[1]}
+				</RadioItem>
+			</div>
+		)
+	)
+	.addWithInfo(
+		'Long Text without space',
+		() => (
+			<div>
+				<RadioItem
+					disabled={boolean('disabled', false)}
+					inline={boolean('inline', false)}
+					onToggle={action('onToggle')}
+				>
+					{radioData.longTextWithoutSpace[0]}
+				</RadioItem>
+				<RadioItem
+					disabled={boolean('disabled', false)}
+					inline={boolean('inline', false)}
+					onToggle={action('onToggle')}
+				>
+					{radioData.longTextWithoutSpace[1]}
 				</RadioItem>
 			</div>
 		)
