43bf75e72d4a3e12da93bcdf39efe73b7a34053a
Jason Robitaille <robitaille88@gmail.com>
2016-10-14 13:32:05 -0700

    Add webOS meta assets and update webpack configs for new plugins (#123)
    * Add webOS meta assets and update webpack configs for new plugins
    
    * Add comment about plugin order, alphabetize requires.

--- packages/sampler/.qa-storybook/webpack.config.js
+++ packages/sampler/.qa-storybook/webpack.config.js
@@ -1,3 +1,8 @@
-var storybookWebpack = require('../.storybook/webpack.config.js');
+var
+	path = require('path'),
+	WebOSMetaPlugin = require('webos-meta-webpack-plugin'),
+	storybookWebpack = require('../.storybook/webpack.config.js');
+
+storybookWebpack.plugins[storybookWebpack.plugins.length-1] = new WebOSMetaPlugin({path:path.join(__dirname, 'webos-meta')});
 
 module.exports = storybookWebpack;
