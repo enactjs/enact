81bef075112b390e31274c1e6df7d487f46a9bf3
Jeremy Thomas <Jeremy Thomas>
2016-10-14 01:02:46 -0400

    Implement LabeledItem

--- packages/moonstone/LabeledItem/LabeledItem.js
+++ packages/moonstone/LabeledItem/LabeledItem.js
@@ -1,3 +1,9 @@
+/**
+ * Exports the {@link module:@enact/moonstone/LabeledItem~LabeledItem} component.
+ *
+ * @module @enact/moonstone/LabeledItem
+ */
+
 import kind from '@enact/core/kind';
 import React, {PropTypes} from 'react';
 
@@ -6,12 +12,35 @@ import {MarqueeDecorator} from '../Marquee';
 
 import css from './LabeledItem.less';
 
+/**
+ * {@link module:@enact/moonstone/LabeledItem~LabeledItem} is a focusable Moonstone-styled component
+ * that combines marquee-enabled text content with a text label.
+ *
+ * @class LabeledItem
+ * @ui
+ * @public
+ */
 const LabeledItemBase = kind({
 	name: 'LabeledItem',
 
-	propTypes : {
+	propTypes: {
+		/**
+		 * The node to be displayed as the main text of the item.
+		 *
+		 * @type {React.node}
+		 * @required
+		 * @public
+		 */
 		children: PropTypes.node,
-		label: PropTypes.node
+
+		/**
+		 * The label to be displayed along with the text.
+		 *
+		 * @type {String|Number}
+		 * @default null
+		 * @public
+		 */
+		label: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 	},
 
 	styles: {
