b5a4c575bbad78a8eb9c6f8bcfb4d7e707174e10
Ryan Duffy <ryan.duffy@lge.com>
2016-10-24 12:26:57 -0500

    add stub DatePicker tests
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

---
+++ packages/moonstone/DatePicker/tests/DatePicker-specs.js
@@ -0,0 +1,29 @@
+// import React from 'react';
+// import {shallow, mount} from 'enzyme';
+// import sinon from 'sinon';
+// import DatePicker from '../DatePicker';
+
+describe.only('DatePicker', () => {
+
+	// Suite-wide setup
+
+	it('should display noneText when value is undefined', function () {
+
+	});
+
+	it('should restore the value from props after an onCancel event', function () {
+
+	});
+
+	it('should set an internal value when opened', function () {
+
+	});
+
+	it('should emit an onChange event when closed', function () {
+
+	});
+
+	it('should not emit an onChange event when cancelled', function () {
+
+	});
+});
