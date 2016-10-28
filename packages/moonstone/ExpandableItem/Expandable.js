7c20303940332a1e2552e85dbc69a990d0eb4b92
Ryan Duffy <ryan.duffy@lge.com>
2016-10-28 16:27:31 -0500

    add Expandable docs
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- packages/moonstone/ExpandableItem/Expandable.js
+++ packages/moonstone/ExpandableItem/Expandable.js
@@ -2,6 +2,14 @@ import Cancelable from '@enact/ui/Cancelable';
 import R from 'ramda';
 import Toggleable from '@enact/ui/Toggleable';
 
+/**
+ * Called by {@link module:@enact/ui/Cancelable~Cancelable} when a cancel event occurs and calls the
+ * `onClose` handler provided by the wrapping Toggleable HOC.
+ *
+ * @param  {Object} props Current props object
+ *
+ * @returns {undefined}
+ */
 const handleCancel = function (props) {
 	if (props.open) {
 		props.onClose();
@@ -11,6 +19,15 @@ const handleCancel = function (props) {
 	}
 };
 
+/**
+ * {@link module:@enact/moonstone/ExpandableItem~Expandable} manages the open state of a component
+ * and adds {@link module:@enact/ui/Cancelable~Cancelable} support to call the `onClose` handler on
+ * cancel.
+ *
+ * @class Expandable
+ * @ui
+ * @private
+ */
 const Expandable = R.compose(
 	Toggleable({activate: 'onOpen', deactivate: 'onClose', mutable: true, prop: 'open'}),
 	Cancelable({component: 'span', onCancel: handleCancel})
