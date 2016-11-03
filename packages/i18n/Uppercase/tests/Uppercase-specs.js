8707f9846c1f230c6f8cf7a7ba757c1fbc7be9a0
Ryan Duffy <ryan.duffy@lge.com>
2016-11-03 14:18:34 -0500

    add tests for toUpperCase
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- packages/i18n/Uppercase/tests/Uppercase-specs.js
+++ packages/i18n/Uppercase/tests/Uppercase-specs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {mount} from 'enzyme';
 import Uppercase from '../Uppercase';
+import {updateLocale} from '../../src';
 
 describe('Uppercase', () => {
 
@@ -69,6 +70,28 @@ describe('Uppercase', () => {
 
 		const expected = 'uppercase';
 		const actual = subject.text();
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should uppercase non-latin locales correctly', function () {
+		// This is difficult to truly unit test because we need a character set we know ilib
+		// uppercases correctly but String.prototype.toUpperCase doesn't.
+
+		updateLocale('fr-FR');
+
+		const Component = (props) => (
+			<div>{props.children}</div>
+		);
+		const Wrapped = Uppercase(Component);
+		const subject = mount(
+			<Wrapped>àçèìñòù</Wrapped>
+		);
+
+		const expected = 'ACEINOU';
+		const actual = subject.text();
+
+		updateLocale();
 
 		expect(actual).to.equal(expected);
 	});
