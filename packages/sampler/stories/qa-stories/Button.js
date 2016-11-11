0319e1d9b3ba22830a4b2435850c88d33a49748d
srirama.singeri <srirama.singeri@lge.com>
2016-11-11 20:20:47 +0530

    Added tap area for normal and small buttons

--- packages/sampler/stories/qa-stories/Button.js
+++ packages/sampler/stories/qa-stories/Button.js
@@ -2,24 +2,18 @@ import Button, {ButtonBase} from '@enact/moonstone/Button';
 import React from 'react';
 import {storiesOf, action} from '@kadira/storybook';
 import {withKnobs, boolean, select, text} from '@kadira/storybook-addon-knobs';
+import css from './Button.less';
 
 Button.propTypes = Object.assign({}, ButtonBase.propTypes, Button.propTypes);
 Button.defaultProps = Object.assign({}, ButtonBase.defaultProps, Button.defaultProps);
 Button.displayName = 'Button';
 
 // Set up some defaults for info and knobs
-const
-	prop = {
-		backgroundOpacity: {'opaque': 'opaque', 'translucent': 'translucent', 'transparent': 'transparent'},
-		longText:{'Loooooooooooooooooog Button': 'Loooooooooooooooooog Button', 'BUTTON   WITH   EXTRA   SPACES': 'BUTTON   WITH   EXTRA   SPACES'},
-		tallText:{'ิ้  ไั  ஒ  து': 'ิ้  ไั  ஒ  து', 'ÁÉÍÓÚÑÜ': 'ÁÉÍÓÚÑÜ', 'Bản văn': 'Bản văn'}
-	},
-	style = {
-		tapArea: {
-			background: 'rgba(0, 100, 200, 0.2)',
-			border: '1px solid rgba(0, 100, 200, 0.4)'
-		}
-	};
+const prop = {
+	backgroundOpacity: {'opaque': 'opaque', 'translucent': 'translucent', 'transparent': 'transparent'},
+	longText:{'Loooooooooooooooooog Button': 'Loooooooooooooooooog Button', 'BUTTON   WITH   EXTRA   SPACES': 'BUTTON   WITH   EXTRA   SPACES'},
+	tallText:{'ิ้  ไั  ஒ  து': 'ิ้  ไั  ஒ  து', 'ÁÉÍÓÚÑÜ': 'ÁÉÍÓÚÑÜ', 'Bản văn': 'Bản văn'}
+};
 
 storiesOf('Button')
 	.addDecorator(withKnobs)
@@ -74,18 +68,31 @@ storiesOf('Button')
 	.addWithInfo(
 		'with tap area displayed',
 		() => (
-			<Button
-				style={style.tapArea}
-				onClick={action('onClick')}
-				backgroundOpacity={select('backgroundOpacity', prop.backgroundOpacity)}
-				disabled={boolean('disabled')}
-				minWidth={boolean('minWidth')}
-				preserveCase={boolean('preserveCase')}
-				selected={boolean('selected')}
-				small={boolean('small')}
-			>
-				Button
-			</Button>
+			<div>
+				<Button
+					className={css.tapArea}
+					onClick={action('onClick')}
+					backgroundOpacity={select('backgroundOpacity', prop.backgroundOpacity)}
+					disabled={boolean('disabled')}
+					minWidth={boolean('minWidth')}
+					preserveCase={boolean('preserveCase')}
+					selected={boolean('selected')}
+				>
+					Normal Button
+				</Button>
+				<Button
+					className={css.tapArea}
+					onClick={action('onClick')}
+					backgroundOpacity={select('backgroundOpacity', prop.backgroundOpacity)}
+					disabled={boolean('disabled')}
+					minWidth={boolean('minWidth')}
+					preserveCase={boolean('preserveCase')}
+					selected={boolean('selected')}
+					small={true}
+				>
+					Small Button
+				</Button>
+			</div>
 		)
 	)
 	.addWithInfo(
@@ -94,7 +101,7 @@ storiesOf('Button')
 			<div>
 				<Button
 					onClick={action('onClick')}
-					backgroundOpacity={'backgroundOpacity', 'translucent'}
+					backgroundOpacity='translucent'
 					disabled={boolean('disabled')}
 					minWidth={boolean('minWidth')}
 					preserveCase={boolean('preserveCase')}
@@ -105,7 +112,7 @@ storiesOf('Button')
 				</Button>
 				<Button
 					onClick={action('onClick')}
-					backgroundOpacity={'backgroundOpacity', 'transparent'}
+					backgroundOpacity='transparent'
 					disabled={boolean('disabled')}
 					minWidth={boolean('minWidth')}
 					preserveCase={boolean('preserveCase')}
