48457fa8fcad179c88a5071057e8237a77fd4854
changgi.lee <changgi.lee@lge.com>
2016-11-14 18:18:28 +0900

    PLAT-23519 : Tooltip Component
    Initial commit of TooltipDecorator, Tooltip component

---
+++ packages/moonstone/Tooltip/Tooltip.js
@@ -0,0 +1,93 @@
+import React, {PropTypes} from 'react';
+import css from './Tooltip.less';
+
+const TooltipArrow = (props) => {
+	
+	return  <svg className={css.tooltipArrow} viewBox={props.arrowType=='full' ? '0 0 3 6' : '0 0 3 5'} >
+				<path d={props.type=='full' ? "M0,6C0,5,1,3,3,3C1,3,0,1,0,0V6Z" : "M0,5C0,3,1,0,3,0H0V5Z"} />
+			</svg>
+}
+
+class Tooltip extends React.Component {
+	static defaultProps = {
+		tooltipType: 'below-left',
+		tooltipTop: 0,
+		tooltipLeft: 0,
+		arrowType: 'half',
+		visible: 'hidden'
+	}
+
+	static propTypes = {
+		/**
+		* Message of tooltip
+		*
+		* @type {string}
+		* @default is not exist
+		* @public
+		*/
+		alt: React.PropTypes.string.isRequired,
+
+		/**
+		* Tooltip Type
+		*
+		* @type {string}
+		* @default 'below-left'
+		* @public
+		*/
+		tooltipType: React.PropTypes.string,
+
+		/**
+		* Tooltip Top Position
+		*
+		* @type {number}
+		* @default 0
+		* @public
+		*/
+		tooltipTop: React.PropTypes.number,
+
+		/**
+		* Tooltip Left Position
+		*
+		* @type {number}
+		* @default 0
+		* @public
+		*/
+		tooltipLeft: React.PropTypes.number,
+
+		/**
+		* Tooltip Arrow Type
+		*
+		* @type {string}
+		* @default 'half'
+		* @public
+		*/
+		arrowType: React.PropTypes.oneOf(['full', 'half']),
+
+		/**
+		* Tooltip Visibility
+		*
+		* @type {string}
+		* @default 'hidden'
+		* @public
+		*/
+		visible: React.PropTypes.oneOf(['visible', 'hidden'])
+	}		
+
+	render () {
+		return(
+			<div
+				className={css.tooltip + " " + css[this.props.tooltipType]}
+				style={{visibility:this.props.visible, left:this.props.tooltipLeft, top:this.props.tooltipTop}}>
+				<TooltipArrow type={this.props.arrowType} />
+				<div
+					ref={(label) => this.labelRef = label}
+					className={css.tooltipLabel}>
+					{this.props.alt}
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Tooltip;
+export {Tooltip};
---
+++ packages/moonstone/Tooltip/TooltipDecorator.js
@@ -0,0 +1,255 @@
+import React, {PropTypes} from 'react';
+import {hoc} from '@enact/core';
+import ri from '@enact/ui/resolution';
+import Portal from '@enact/ui/Portal';
+import {isRtlText} from '@enact/i18n';
+import css from './TooltipDecorator.less';
+import {Tooltip} from './Tooltip';
+
+const arrowMargin = 18;
+
+const TooltipDecorator = hoc( (config, Wrapped) => {
+	return class extends React.Component {
+		static defaultProps = {
+			uppercase: true,
+			position: 'auto'
+		}
+
+		static propTypes = {
+			/**
+			 * Message of tooltip
+			 *
+			 * @type {string}
+			 * @default is not exist
+			 * @public
+			 */
+			alt: React.PropTypes.string.isRequired,
+
+			/**
+			* When `true`, the content will have locale-safe uppercasing applied.
+			*
+			* @type {Boolean}
+			* @default true
+			* @public
+			*/
+			uppercase: React.PropTypes.bool,
+
+			/**
+			* Position of the tooltip with respect to the activating control. Valid values are
+			* `'above'`, `'below'`, `'left top'`, `'left bottom'`, `'right top'`, `'right bottom'`, and
+			* `'auto'`. The values starting with `'left`' and `'right'` place the tooltip on the side
+			* (sideways tooltip) with two additional positions available, `'top'` and `'bottom'`, which
+			* places the tooltip content toward the top or bottom, with the tooltip pointer
+			* middle-aligned to the activator.
+			*
+			* Note: The sideways tooltip does not automatically switch sides if it gets too close or
+			* overlaps with the window bounds, as this may cause undesirable layout implications,
+			* covering your other controls.
+			*
+			* @type {String}
+			* @default 'auto'
+			* @public
+			*/
+			position: React.PropTypes.oneOf(['auto', 'above', 'below', 'left top', 'left bottom', 'left-center', 'right top', 'right bottom', 'below center'])
+		}
+
+		constructor (props) {
+			super(props);
+			this.state = {
+				tooltipType: 'below-left',
+				tooltipTop: 0,
+				tooltipLeft: 0,
+				tooltipVisible: 'hidden',
+				arrowType: 'half'
+			};
+		}
+
+		componentDidMount () {
+			this.adjustPosition();
+		}
+
+		adjustPosition () {
+			const position = this.props.position;
+			let tPos; // tooltip position
+			let aPos; // arrow position
+			let r;
+			let arr;
+			let rtl = isRtlText(this.props.alt);
+
+			if ((arr = position.split(' ')).length == 2) {
+				[tPos, aPos] = arr;
+			} else if (position == 'above' || position == 'below') {
+				tPos = position;
+				aPos = 'left';
+			} else {
+				tPos = 'above';
+				aPos = 'left';
+			}
+			
+			if (rtl) {
+				if (tPos == 'above' || tPos == 'below') {
+					aPos = aPos == 'left' ? 'right' : 'left';
+				} else if (tPos == 'left' || tPos == 'right') {
+					tPos = tPos == 'left' ? 'right' : 'left';
+				}
+			}
+
+			r = this.calcPosition(tPos, aPos);
+			
+			if (tPos == 'above' || tPos == 'below') {
+				let isCalculate = false;
+
+				if (aPos == 'left' && r.tX + r.tW > window.innerWidth || aPos == 'right' && r.tX < 0) {
+					isCalculate = true;
+					aPos = aPos == 'left' ? 'right' : 'left';
+				}
+
+				if (tPos == 'below' && r.tY + r.tH > window.innerHeight ||  tPos == 'above' && r.tY < 0 ) {
+					isCalculate = true;
+					tPos = tPos == 'above' ? 'below' : 'above';
+				}
+
+				if (isCalculate) {
+					r = this.calcPosition(tPos, aPos);
+				}
+			}			
+			
+			this.setState({
+				tooltipType: tPos+"-"+aPos,
+				tooltipTop: ri.unit(r.tY, 'rem'),
+				tooltipLeft: ri.unit(r.tX, 'rem'),
+				arrowType: aPos == 'center' ? 'full' : 'half'
+			});
+		}
+
+		calcPosition (tPos, aPos) {
+			const cBound = this.clientRef.getBoundingClientRect(); // clinet bound
+			const lBound = this.tooltipRef.labelRef.getBoundingClientRect(); // label bound			
+			let tX, tY; // tooltip position
+
+			switch (tPos) {
+				case 'below':
+					tX = cBound.left + cBound.width/2;
+					tY = cBound.bottom + arrowMargin;	
+					
+					if (aPos == 'right') {
+						tX -= lBound.width;
+					} else if (aPos == 'center') {
+						tX -= lBound.width/2;
+					}					
+					break;
+				case 'above':
+					tX = cBound.left + cBound.width/2;
+					tY = cBound.top - lBound.height - arrowMargin;
+					if (aPos == 'right') {
+						tX -= lBound.width;
+					} else if (aPos == 'center') {
+						tX -= lBound.width/2;
+					}
+					break;
+				case 'left':
+					tX = cBound.left - lBound.width - arrowMargin;
+					tY = cBound.top + cBound.height/2;
+
+					if (aPos == 'top') {
+						tY -= lBound.height;
+					} else if(aPos == 'center') {
+						tY -= lBound.height/2;
+					}
+					break;
+				case 'right':
+					tX = cBound.right + arrowMargin;
+					tY = cBound.top + cBound.height/2;
+
+					if (aPos == 'top') {
+						tY -= lBound.height;
+					} else if(aPos == 'center') {
+						tY -= lBound.height/2;
+					}
+					break;
+				default:
+					tX=0;
+					tY=0;
+					break;
+			}
+
+			return {
+				tX: tX,
+				tY: tY,
+				tW: lBound.width,
+				tH: lBound.height
+			}
+		}
+
+		handleFocus () {
+			this.show();
+		}
+
+		handleBlur () {
+			this.hide();
+		}
+
+		show () {
+			this.setTimeout(() => {
+				this.adjustPosition();
+				this.setState({
+					tooltipVisible: 'visible'
+				});
+			}, 500);
+		}
+
+		hide () {
+			this.clearTimeout();
+			this.setState({
+				tooltipVisible: 'hidden'
+			});
+		}
+
+		clearTimeout () {
+			if (window) {
+				window.clearTimeout(this.timer);
+			}
+			this.timer = null;
+		}
+
+		setTimeout (fn, time) {
+			this.clearTimeout();
+			if (window) {
+				this.timer = window.setTimeout(fn, time);
+			}
+		}
+
+		render () {
+			const style = this.props.style ? this.props.style : {};
+			const props = Object.assign({}, this.props, {
+				alt: this.props.alt && this.props.uppercase ? this.props.alt.toUpperCase() : '',
+			});
+
+			delete props.style;
+
+			return(
+				<div className={css.tooltipDecorator} style={style}>
+					<div
+						onBlur={this.handleBlur.bind(this)}
+						onFocus={this.handleFocus.bind(this)}
+						ref={(client) => this.clientRef = client}>
+						<Wrapped {...props}/>
+					</div>
+					<Portal open={true}>
+						<Tooltip
+							{...props}
+							tooltipType={this.state.tooltipType}
+							tooltipTop={this.state.tooltipTop}
+							tooltipLeft={this.state.tooltipLeft}
+							arrowType={this.state.arrowType}
+							visible={this.state.tooltipVisible}
+							ref={(tooltip) => this.tooltipRef = tooltip}/>
+					</Portal>
+				</div>
+			)
+		}
+	}
+});
+
+export default TooltipDecorator;
+export {TooltipDecorator};
