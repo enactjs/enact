ea97829af186ae0044045a594b8b2d057e784cf5
richa.Sha <richa.shaurabh@gmail.com>
2016-11-15 17:01:04 +0530

    PLAT-29235:Create samples for Spinner

---
+++ packages/sampler/stories/qa-stories/Spinner.js
@@ -0,0 +1,89 @@
+import Spinner from '@enact/moonstone/Spinner';
+import Icon from '@enact/moonstone/Icon';
+import ri from '@enact/ui/resolution';
+import React from 'react';
+import {storiesOf} from '@kadira/storybook';
+import {withKnobs, text, boolean} from '@kadira/storybook-addon-knobs';
+
+// Set up some defaults for info and knobs
+const 
+	prop = {
+		longText:'SpinnerWithLongText SpinnerWithLongText SpinnerWithLongText'
+	},
+	style = {
+		spinnerDiv: {
+			height: ri.scale(15) + 'em',
+			width: '100%',
+			border: 0.01 + 'em dotted red'
+		}
+	};
+
+storiesOf('Spinner')
+	.addDecorator(withKnobs)
+	.addWithInfo(
+		'with long content',
+		() => (
+			<div style={style.spinnerDiv}>
+				<Spinner
+					transparent={boolean('transparent')}
+					middle={boolean('middle')}
+					center={boolean('center')}
+				>
+					{text('content', prop.longText)}
+				</Spinner>
+			</div>
+		)
+	)
+
+	.addWithInfo(
+		'with components inside before text',
+		() => (
+			<div style={style.spinnerDiv}>
+				<Spinner
+					transparent={boolean('transparent')}
+					middle={boolean('middle')}
+					center={boolean('center')}
+				>
+					<Icon>hollowstar</Icon>
+					<Icon>star</Icon>
+					{text('content', prop.longText)}
+				</Spinner>
+			</div>
+		)
+	)
+
+	.addWithInfo(
+		'with components inside after text ',
+		() => (
+			<div style={style.spinnerDiv}>
+				<Spinner
+					transparent={boolean('transparent')}
+					middle={boolean('middle')}
+					center={boolean('center')}
+				>
+					{text('content', prop.longText)}
+					<Icon>hollowstar</Icon>
+					<Icon>star</Icon>
+				</Spinner>
+			</div>
+		)
+	)
+
+	.addWithInfo(
+		'with components inside before and after text ',
+		() => (
+			<div style={style.spinnerDiv}>
+				<Spinner
+					transparent={boolean('transparent')}
+					middle={boolean('middle')}
+					center={boolean('center')}
+				>
+					<Icon>hollowstar</Icon>
+					<Icon>star</Icon>
+					{text('content', 'Spinner')}
+					<Icon>hollowstar</Icon>
+					<Icon>star</Icon>
+				</Spinner>
+			</div>
+		)
+	);
