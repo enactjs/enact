dd4a1810c1b2dd9d4af8b5dba90ad5b40dd4874e
Stephen Choi <stephen.choi@lge.com>
2016-11-21 17:31:33 -0800

    Add ContextualPopup sample

---
+++ packages/sampler/stories/moonstone-stories/ContextualPopup.js
@@ -0,0 +1,28 @@
+import {ContextualPopupDecorator} from '@enact/moonstone/ContextualPopup';
+import Button from '@enact/moonstone/Button';
+import React from 'react';
+import {storiesOf} from '@kadira/storybook';
+import {withKnobs, boolean, text, select} from '@kadira/storybook-addon-knobs';
+
+const ContextualButton = ContextualPopupDecorator(Button);
+ContextualButton.displayName = 'ContextualButton';
+
+storiesOf('ContextualPopup')
+	.addDecorator(withKnobs)
+	.addWithInfo(
+		' ',
+		'Basic usage of ContextualPopup',
+		() => {
+			const renderPopup = () => (
+				<div>{text('popup string', 'Hello Contextual Popup')}</div>
+			);
+			return (
+				<ContextualButton
+					direction={select('direction', ['up', 'down', 'left', 'right'], 'down')}
+					showCloseButton={boolean('showCloseButton', false)}
+					popupComponent={renderPopup}
+				>
+					{text('button string', 'Hello Contextual Button')}
+				</ContextualButton>
+			);
+		});
