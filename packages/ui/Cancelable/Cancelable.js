7ad0f5307b933992cb15427c696136685795a853
Ryan Duffy <ryan.duffy@lge.com>
2016-12-01 11:26:13 -0600

    move addModal into componentWillMount
    In order to ensure that parent modals are added before children, we
    need to use componentWillMount which is called before render and
    therefore before the children are created and mounted.
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- packages/ui/Cancelable/Cancelable.js
+++ packages/ui/Cancelable/Cancelable.js
@@ -66,7 +66,7 @@ const Cancelable = hoc(defaultConfig, (config, Wrapped) => {
 			onCancel: React.PropTypes.func
 		}
 
-		componentDidMount () {
+		componentWillMount () {
 			if (modal) {
 				addModal(this);
 			}
