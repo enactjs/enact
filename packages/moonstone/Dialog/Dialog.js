744a2482b48aa9c8d8419b10defbc89f7feaec2a
Roy Sutton <roy.sutton@lge.com>
2016-12-06 13:00:17 -0800

    Clean up Dialog and Popup props
    Enyo-DCO-1.1-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- packages/moonstone/Dialog/Dialog.js
+++ packages/moonstone/Dialog/Dialog.js
@@ -58,12 +58,14 @@ const DialogBase = kind({
 		noAnimation: PropTypes.bool,
 
 		/**
-		 * A function to be run when close button is clicked.
+		 * A function to be run when a closing action is invoked by the user. These actions include
+		 * pressing `ESC` key or clicking on the close button. It is the responsibility of the
+		 * callback to set the `open` property to `false`.
 		 *
 		 * @type {Function}
 		 * @public
 		 */
-		onCloseButtonClicked: PropTypes.func,
+		onClose: PropTypes.func,
 
 		/**
 		 * A function to be run after transition for hiding is finished.
@@ -92,6 +94,14 @@ const DialogBase = kind({
 		showCloseButton: PropTypes.bool,
 
 		/**
+		 * When `true`, a divider line separates the title from the dialog body
+		 *
+		 * @type {Boolean}
+		 * @public
+		 */
+		showDivider: PropTypes.bool,
+
+		/**
 		 * Title of the header
 		 *
 		 * @type {String}
@@ -105,15 +115,7 @@ const DialogBase = kind({
 		 * @type {String}
 		 * @public
 		 */
-		titleBelow: PropTypes.string,
-
-		/**
-		 * When `true`, a divider line separates the title from the dialog body
-		 *
-		 * @type {Boolean}
-		 * @public
-		 */
-		showDivider: PropTypes.bool
+		titleBelow: PropTypes.string
 	},
 
 	defaultProps: {
