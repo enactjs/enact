1eb2008d3021ebeb35b725a2c0508b3dd8b03000
Ryan Duffy <ryan.duffy@lge.com>
2016-12-07 15:08:41 -0600

    refactor floating layer container markup into FloatingLayer
    + drop zIndex calculations
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- packages/ui/FloatingLayer/Scrim.js
+++ packages/ui/FloatingLayer/Scrim.js
@@ -1,30 +1,21 @@
-/* //TODO: JSDOC revisit
- * Exports the {@link ui/FloatingLayer/Scrim.Scrim} component and
- * {@link ui/FloatingLayer/Scrim.ScrimBase} components. The default export is
- * {@link ui/FloatingLayer/Scrim.Scrim}.
- *
- * @module ui/FloatingLayer/Scrim
- * @private
- */
-
 import kind from '@enact/core/kind';
 import React from 'react';
 
 import css from './Scrim.less';
 
 /**
- * {@link ui/FloatingLayer/Scrim.ScrimBase} provides an overlay that will prevent taps from propagating
+ * {@link ui/FloatingLayer.Scrim} provides an overlay that will prevent taps from propagating
  * to the controls that it covers.
  *
- * @class ScrimBase
- * @memberof ui/FloatingLayer/Scrim
+ * @class Scrim
+ * @memberof ui/FloatingLayer
  * @ui
  * @private
  */
-const ScrimBase = kind({
-	name: 'ScrimBase',
+const Scrim = kind({
+	name: 'Scrim',
 
-	propTypes: /** @lends ui/FloatingLayer/Scrim.ScrimBase.prototype */ {
+	propTypes: /** @lends ui/FloatingLayer.Scrim.prototype */ {
 		/**
 		 * Types of scrim. It can be either `'transparent'` or `'translucent'`.
 		 *
@@ -45,7 +36,7 @@ const ScrimBase = kind({
 	},
 
 	computed: {
-		className: ({type, styler}) => styler.append([type])
+		className: ({type, styler}) => styler.append(type)
 	},
 
 	render: (props) => {
@@ -57,80 +48,7 @@ const ScrimBase = kind({
 	}
 });
 
-/**
- * {@link ui/FloatingLayer/Scrim.Scrim} is a layer which adds scrim behind the children.
- * Children will always have one higher z-index than scrim.
- *
- * @class Scrim
- * @memberof ui/FloatingLayer/Scrim
- * @ui
- * @private
- */
-const Scrim = kind({
-	name: 'Scrim',
 
-	propTypes: /** @lends ui/FloatingLayer/Scrim.Scrim.prototype */ {
-		/**
-		 * z-index of the layer. Scrim will have the z-index of layer, and the children will have
-		 * one higher index to display on top of Scrim.
-		 *
-		 * @type {Number}
-		 * @public
-		 */
-		zIndex: React.PropTypes.number.isRequired,
-
-		/**
-		 * Types of scrim. It can be either `'transparent'`, `'translucent'`, or `'none'`.
-		 *
-		 * @type {String}
-		 * @default `translucent`
-		 * @public
-		 */
-		type: React.PropTypes.oneOf(['transparent', 'translucent', 'none']),
-
-		/**
-		 * When `true`, scrim will appear
-		 *
-		 * @type {Boolean}
-		 * @default false
-		 * @public
-		 */
-		visible: React.PropTypes.bool
-	},
-
-	defaultProps: {
-		type: 'translucent',
-		visible: false
-	},
-
-	computed: {
-		children: ({children, zIndex, ...rest}) => {
-			delete rest.type;
-			delete rest.visible;
-
-			const style = Object.assign({}, children.props.style, {zIndex: zIndex + 1});
-			return React.cloneElement(children, {style, ...rest});
-		},
-		scrim: ({visible, zIndex, type}) => {
-			if (visible && type !== 'none') {
-				return <ScrimBase type={type} style={{zIndex}} />;
-			}
-		}
-	},
-
-	render: ({scrim, children, ...rest}) => {
-		delete rest.type;
-		delete rest.visible;
-		delete rest.zIndex;
-
-		return (
-			<div {...rest}>
-				{children}
-				{scrim}
-			</div>
-		);
-	}
-});
 
 export default Scrim;
-export {ScrimBase, Scrim};
+export {Scrim};
