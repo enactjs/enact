8ccf34d961cd2af21be4b38a012bebc560f2a748
Blake Stephens <blake.stephens@lge.com>
2016-12-27 15:39:02 -0800

    Static values need an initial scale applied before conversion to `rem`. `<b>`’s are not broken when line wrapping.
    Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- packages/sampler/stories/moonstone-stories/About.js
+++ packages/sampler/stories/moonstone-stories/About.js
@@ -13,12 +13,12 @@ const riSafe = (style) => {
 		case 'object':
 			for (let rule in style) {
 				if (typeof style[rule] === 'number') {
-					style[rule] = ri.unit(style[rule], 'rem');
+					style[rule] = ri.unit(ri.scale(style[rule]), 'rem');
 				}
 			}
 			return style;
 		default:
-			return ri.unit(style, 'rem');
+			return ri.unit(ri.scale(style), 'rem');
 	}
 };
 
