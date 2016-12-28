208d0227189a0e97b4f23084a1f3a8983ad55469
Teck <teck.liew@lge.com>
2016-12-28 15:13:36 -0800

    fix incrementSlider buttons

--- packages/moonstone/IncrementSlider/IncrementSliderButton.js
+++ packages/moonstone/IncrementSlider/IncrementSliderButton.js
@@ -23,7 +23,7 @@ const IncrementSliderButtonBase = kind({
 	},
 
 	render: (props) => (
-		<IconButton {...props} small />
+		<IconButton {...props} backgroundOpacity='transparent' small />
 	)
 });
 
