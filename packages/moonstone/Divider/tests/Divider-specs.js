c507e91992f2143063e59d5d7c09771d4bdd8eb4
Dave Freeman <sugardave@users.noreply.github.com>
2017-01-04 18:51:24 -0600

    PLAT-26617: Create unit tests for Divider (#67)
    Enyo-DCO-1.1-Signed-off-by: Dave Freeman <dave.freeman@lge.com>
    
    Reviewed-By: Ryan Duffy (ryan.duffy@lge.com)
    Integrated-By: Aaron Tam (aaron.tam@lge.com)

--- packages/moonstone/Divider/tests/Divider-specs.js
+++ packages/moonstone/Divider/tests/Divider-specs.js
@@ -4,6 +4,18 @@ import Divider from '../Divider';
 
 describe('Divider Specs', () => {
 
+	it('should capitalize its content', function () {
+		const content = 'uncapped';
+		const divider = mount(
+			<Divider>{content}</Divider>
+		);
+
+		const expected = content.charAt(0).toUpperCase();
+		const actual = divider.text().charAt(0);
+
+		expect(actual).to.equal(expected);
+	});
+
 	it('should render a Divider with content', function () {
 		const content = 'Hello Divider!';
 
