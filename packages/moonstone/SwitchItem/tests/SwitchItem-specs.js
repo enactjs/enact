6eae1f32ba321781e1710feeebb44e46546c56a9
Derek Tor <derek.tor@lge.com>
2017-01-06 17:42:05 -0800

    Added Tests for SwitchItem
    Enact-DCO-1.1-Signed-off-by: Derek Tor derek.tor@lge.com

---
+++ packages/moonstone/SwitchItem/tests/SwitchItem-specs.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import SwitchItem from '../SwitchItem';
+import SwitchCss from '../../Switch/Switch.less';
+
+describe('SwitchItem Specs', () => {
+
+	it('should contain Switch Icon', function () {
+
+		const switchItem = mount(
+			<SwitchItem>
+				SwitchItem
+			</SwitchItem>
+		);
+
+		const IconComponent = switchItem.find('Icon');
+
+		const expected = SwitchCss.icon;
+		const actual = IconComponent.prop('className');
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should pass selected Switch element', function () {
+
+		const switchItem = mount(
+			<SwitchItem selected>
+				SwitchItem
+			</SwitchItem>
+		);
+
+		const SwitchComponent = switchItem.find('Switch');
+
+		const expected = true;
+		const actual = SwitchComponent.prop('selected');
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should pass disabled Switch element', function () {
+
+		const switchItem = mount(
+			<SwitchItem disabled>
+				SwitchItem
+			</SwitchItem>
+		);
+
+		const SwitchComponent = switchItem.find('Switch');
+
+		const expected = true;
+		const actual = SwitchComponent.prop('disabled');
+
+		expect(actual).to.equal(expected);
+	});
+});
