4008fa0db5b00d70d4de7fa3844f6e30f5bab3bc
Teck <teck.liew@lge.com>
2017-01-09 11:03:56 -0800

    update test description and test default value prop

--- packages/moonstone/ExpandablePicker/tests/ExpandablePicker-specs.js
+++ packages/moonstone/ExpandablePicker/tests/ExpandablePicker-specs.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {mount} from 'enzyme';
 import sinon from 'sinon';
-import ExpandablePicker from '../ExpandablePicker';
+import ExpandablePicker, {ExpandablePickerBase} from '../ExpandablePicker';
 
 describe('ExpandablePicker Specs', () => {
 
@@ -22,7 +22,7 @@ describe('ExpandablePicker Specs', () => {
 		expect(actual).to.equal(expected);
 	});
 
-	it('should include value in onChange', function () {
+	it('should include value in onChange when value is specified', function () {
 		const value = 2;
 		const handleChange = sinon.spy();
 		const expandablePicker = mount(
@@ -40,18 +40,18 @@ describe('ExpandablePicker Specs', () => {
 		expect(actual).to.equal(expected);
 	});
 
-	it('should include value in onChange', function () {
+	it('should include default value in onChange when value is not specified', function () {
 		const handleChange = sinon.spy();
 		const expandablePicker = mount(
-			<ExpandablePicker title='Options' onChange={handleChange} open>
+			<ExpandablePickerBase title='Options' onChange={handleChange} open>
 				{['Option one', 'Option two', 'Option three']}
-			</ExpandablePicker>
+			</ExpandablePickerBase>
 		);
 
 		const checkButton = expandablePicker.find('IconButton').last();
 		checkButton.simulate('click');
 
-		const expected = 0;
+		const expected = expandablePicker.props().value;
 		const actual = handleChange.firstCall.args[0].value;
 
 		expect(actual).to.equal(expected);
