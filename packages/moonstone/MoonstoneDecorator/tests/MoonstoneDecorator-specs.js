07467f23c6d28fd6a6aadec8f20df89d710e17a7
Ryan Duffy <ryan@tiqtech.com>
2017-01-16 19:16:30 -0600

    PLAT-26622: update MoonstoneDecorator tests (#445)
    * update MoonstoneDecorator tests
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * Removed unnecessary eslint code
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

---
+++ packages/moonstone/MoonstoneDecorator/tests/MoonstoneDecorator-specs.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import MoonstoneDecorator from '../';
+import Spotlight from '@enact/spotlight';
+
+import css from '../MoonstoneDecorator.less';
+
+describe('MoonstoneDecorator', () => {
+
+	const AppRoot = (props) => <app {...props} />;
+
+	it('should add base moonstone classes to wrapped component', function () {
+		const config = {ri: false, i18n: false, spotlight: false, float: false, overlay: false};
+		const App = MoonstoneDecorator(config, AppRoot);
+		const subject = mount(
+			<App />
+		);
+
+		Spotlight.terminate();
+
+		const appRoot = subject.find('app');
+
+		const expected = true;
+		const actual = appRoot.hasClass(css.moon) && appRoot.hasClass(css.bg);
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should add author classes to wrapped component', function () {
+		const config = {ri: false, i18n: false, spotlight: false, float: false, overlay: false};
+		const App = MoonstoneDecorator(config, AppRoot);
+		const subject = mount(
+			<App className="author-class" />
+		);
+
+		Spotlight.terminate();
+
+		const appRoot = subject.find('app');
+
+		const expected = true;
+		const actual = appRoot.hasClass('author-class');
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should not add .moon class to wrapped component when float is enabled', function () {
+		const config = {ri: false, i18n: false, spotlight: false, float: true, overlay: false};
+		const App = MoonstoneDecorator(config, AppRoot);
+		const subject = mount(
+			<App />
+		);
+
+		Spotlight.terminate();
+
+		const appRoot = subject.find('app');
+
+		const expected = false;
+		const actual = appRoot.hasClass(css.moon);
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should not add .bg class to wrapped component when overlay is enabled', function () {
+		const config = {ri: false, i18n: false, spotlight: false, float: false, overlay: true};
+		const App = MoonstoneDecorator(config, AppRoot);
+		const subject = mount(
+			<App />
+		);
+
+		Spotlight.terminate();
+
+		const appRoot = subject.find('app');
+
+		const expected = false;
+		const actual = appRoot.hasClass(css.bg);
+
+		expect(actual).to.equal(expected);
+	});
+
+});
