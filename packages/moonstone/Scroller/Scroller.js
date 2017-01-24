759e11c222c47aab6fe95229daf2bfbae077e4b3
bongsub.kim <bongsub.kim@lgepartner.com>
2017-01-25 08:11:46 +0900

    ENYO-3758: Fix that scrollbar thumb is not showing (#409)
    * ENYO-3758: Fix that scrollbar thumb is not showing
    
    Add calculatePositionOnFocus function for supporting spotlight moving using 5-way keys.
    
    https://jira2.lgsvl.com/browse/ENYO-3758
    Enact-DCO-1.1-Signed-off-by: Bongsub Kim <bongsub.kim@lgepartner.com>
    
    Change-Id: If04a99cfcbf2d016dbdfd2edfe83f2b6d21e594e
    
    * ENYO-3758: Clean up code
    
    https://jira2.lgsvl.com/browse/ENYO-3758
    Enact-DCO-1.1-Signed-off-by: Bongsub Kim <bongsub.kim@lgepartner.com>
    
    Change-Id: Ib4e1f63465ed2713ade6777ffa8ae63566bf145a
    
    * ENYO-3758: Clean up
    
    https://jira2.lgsvl.com/browse/ENYO-3758
    Enact-DCO-1.0-Signed-off-by: Bongsub Kim <bongsub.kim@lgepartner.com>
    
    * Unifies position calculation function
    
    Issue: ENYO-3758
    Enact-DCO-1.0-Signed-off-by Aaron Tam <aaron.tam@lge.com>
    
    * Simplifies positioning logic
    
    Issue: ENYO-3758
    Enact-DCO-1.0-Signed-off-by Aaron Tam <aaron.tam@lge.com>
    
    * ENYO-3758: Merge ENYO-3758-aarontam and keep calculateOnFocus in Scroller
    
    https://jira2.lgsvl.com/browse/ENYO-3758
    Enact-DCO-1.0-Signed-off-by: Bongsub Kim <bongsub.kim@lgepartner.com>
    
    * Fixes permissions
    
    Issue: ENYO-3758
    Enact-DCO-1.0-Signed-off-by Aaron Tam <aaron.tam@lge.com>
    
    
    1st-Reviewed-by: YB Sung ( yb.sung@lge.com)
    Integrated-By: Aaron Tam (aaron.tam@lge.com)

--- packages/moonstone/Scroller/Scroller.js
+++ packages/moonstone/Scroller/Scroller.js
@@ -71,6 +71,11 @@ class ScrollerBase extends Component {
 		maxTop: 0
 	}
 
+	scrollPos = {
+		top: 0,
+		left: 0
+	}
+
 	getScrollBounds = () => this.scrollBounds
 
 	setScrollPosition (valX, valY) {
@@ -80,9 +85,11 @@ class ScrollerBase extends Component {
 
 		if (this.isVertical()) {
 			node.scrollTop = valY;
+			this.scrollPos.top = valY;
 		}
 		if (this.isHorizontal()) {
 			node.scrollLeft = rtl ? (this.scrollBounds.maxLeft - valX) : valX;
+			this.scrollPos.left = valX;
 		}
 	}
 
@@ -103,6 +110,31 @@ class ScrollerBase extends Component {
 		}
 
 		return bounds;
+	}
+
+	calculatePositionOnFocus = (focusedItem) => {
+		const
+			rtlDirection = this.context.rtl ? -1 : 1,
+			currentLeft = this.scrollPos.left * rtlDirection,
+			currentTop = this.scrollPos.top;
+
+		if (this.isVertical()) {
+			if (focusedItem.offsetTop + focusedItem.offsetHeight > (this.scrollBounds.clientHeight + currentTop)) {
+				this.scrollPos.top += ((focusedItem.offsetTop + focusedItem.offsetHeight) - (this.scrollBounds.clientHeight + currentTop));
+			} else if (focusedItem.offsetTop < currentTop) {
+				this.scrollPos.top += (focusedItem.offsetTop - currentTop);
+			}
+		}
+
+		if (this.isHorizontal()) {
+			if (focusedItem.offsetLeft + focusedItem.offsetWidth > (this.scrollBounds.clientWidth + currentLeft)) {
+				this.scrollPos.left += rtlDirection * ((focusedItem.offsetLeft + focusedItem.offsetWidth) - (this.scrollBounds.clientWidth + currentLeft));
+			} else if (focusedItem.offsetLeft < currentLeft) {
+				this.scrollPos.left += rtlDirection * (focusedItem.offsetLeft - currentLeft);
+			}
+		}
+
+		return this.scrollPos;
 	}
 
 	isVertical = () => (this.props.vertical !== 'hidden')
