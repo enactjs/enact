8c3e938fa11ea2b76f5fd1c211aa5e1964173526
Blake Stephens <blake.stephens@lge.com>
2017-01-26 13:25:15 -0800

    Revert "ENYO-3746: Preserve Panel Spotlight Focus (#442)"
    This reverts commit 75d7ff3d33bb4ec3b02d52ebed543c4fee9d0110.

--- packages/moonstone/Panels/Viewport.js
+++ packages/moonstone/Panels/Viewport.js
@@ -1,32 +1,20 @@
 import {findDOMNode} from 'react-dom';
-import {forward} from '@enact/core/handle';
+import {forward, withArgs as handle} from '@enact/core/handle';
 import kind from '@enact/core/kind';
 import ViewManager, {shape} from '@enact/ui/ViewManager';
 import invariant from 'invariant';
 import React from 'react';
-import Spotlight, {spottableClass} from '@enact/spotlight';
+import Spotlight from '@enact/spotlight';
 
 import css from './Panels.less';
 
-const findNode = (view) => {
+const spotPanel = ({view}) => {
 	// eslint-disable-next-line react/no-find-dom-node
-	return findDOMNode(view);
-};
-
-const spotPanel = ({view, lastFocusedIndex}) => {
-	const node = findNode(view);
-
+	const node = findDOMNode(view);
 	if (node) {
-		let lastFocusedSpottable;
-
-		if (lastFocusedIndex >= 0) {
-			const spottables = node.querySelectorAll(`.${spottableClass}`);
-			if (lastFocusedIndex < spottables.length) {
-				lastFocusedSpottable = spottables[lastFocusedIndex];
-			}
-		}
-
-		const spottable = lastFocusedSpottable || node.querySelector(`section .${spottableClass}`) || node.querySelector(`header .${spottableClass}`);
+		const body = node.querySelector('section .spottable');
+		const header = node.querySelector('header .spottable');
+		const spottable = body || header;
 
 		if (spottable) {
 			Spotlight.focus(spottable);
@@ -34,134 +22,16 @@ const spotPanel = ({view, lastFocusedIndex}) => {
 	}
 };
 
-const initialFocusedIndex = -1;
-const forwardOnAppear = forward('onAppear');
-const forwardOnEnter = forward('onEnter');
-const forwardOnTransition = forward('onTransition');
-const forwardOnWillTransition = forward('onWillTransition');
-
 /**
  * The container for a set of Panels
- *
- * @class ViewportBase
- * @private
- */
-const ViewportBase = kind({
-	name: 'ViewportBase',
-
-	propTypes: /** @lends ViewportBase.prototype */ {
-		/**
-		 * Set of functions that control how the panels are transitioned into and out of the
-		 * viewport
-		 *
-		 * @type {Arranger}
-		 */
-		arranger: shape,
-
-		/**
-		 * Panels to be rendered
-		 *
-		 * @type {Panel}
-		 */
-		children: React.PropTypes.node,
-
-		/**
-		 * Index of the active panel
-		 *
-		 * @type {Number}
-		 * @default 0
-		 */
-		index: React.PropTypes.number,
-
-		/**
-		 * Disable panel transitions
-		 *
-		 * @type {Boolean}
-		 * @default false
-		 */
-		noAnimation: React.PropTypes.bool,
-
-		/**
-		 * Called when each view is rendered during initial construction.
-		 *
-		 * @type {Function}
-		 */
-		onAppear: React.PropTypes.func,
-
-		/**
-		 * Called when each view completes its transition into the viewport.
-		 *
-		 * @type {Function}
-		 */
-		onEnter: React.PropTypes.func,
-
-		/**
-		 * Called once when all views have completed their transition.
-		 *
-		 * @type {Function}
-		 */
-		onTransition: React.PropTypes.func,
-
-		/**
-		 * Called once before views begin their transition.
-		 *
-		 * @type {Function}
-		 */
-		onWillTransition: React.PropTypes.func
-	},
-
-	defaultProps: {
-		index: 0,
-		noAnimation: false
-	},
-
-	styles: {
-		css,
-		className: 'viewport'
-	},
-
-	computed: {
-		children: ({children}) => React.Children.map(children, (child, index) => {
-			return React.cloneElement(child, {'data-index': index});
-		})
-	},
-
-	render: ({arranger, children, index, noAnimation, onAppear, onEnter, onTransition, onWillTransition, ...rest}) => {
-		const count = React.Children.count(children);
-		invariant(
-			index === 0 && count === 0 || index < count,
-			`Panels index, ${index}, is invalid for number of children, ${count}`
-		);
-
-		return (
-			<ViewManager
-				{...rest}
-				noAnimation={noAnimation}
-				arranger={arranger}
-				duration={200}
-				index={index}
-				component="main"
-				onAppear={onAppear}
-				onEnter={onEnter}
-				onTransition={onTransition}
-				onWillTransition={onWillTransition}
-			>
-				{children}
-			</ViewManager>
-		);
-	}
-});
-
-/**
- * A stateful component that helps ViewportBase by handling life-cycle and focus management tasks.
  *
  * @class Viewport
  * @private
  */
-class Viewport extends React.Component {
-	static displayName = 'Viewport';
+const ViewportBase = kind({
+	name: 'Viewport',
 
-	static propTypes = /** @lends Viewport.prototype */ {
+	propTypes: /** @lends Viewport.prototype */ {
 		/**
 		 * Set of functions that control how the panels are transitioned into and out of the
 		 * viewport
@@ -192,65 +62,53 @@ class Viewport extends React.Component {
 		 * @default false
 		 */
 		noAnimation: React.PropTypes.bool
-	}
+	},
 
-	static defaultProps = {
+	defaultProps: {
 		index: 0,
 		noAnimation: false
-	}
+	},
 
-	constructor (props) {
-		super(props);
-		this.state = {
-			lastFocusedIndices: React.Children.map(this.props.children, () => {
-				return initialFocusedIndex;
-			})
-		};
-	}
+	styles: {
+		css,
+		className: 'viewport'
+	},
 
-	componentWillReceiveProps (nextProps) {
-		const {index} = this.props;
-		const node = findNode(this);
-		const current = Spotlight.getCurrent();
+	computed: {
+		children: ({children}) => React.Children.map(children, (child, index) => {
+			return React.cloneElement(child, {'data-index': index});
+		}),
+		handleAppear: handle(forward('onAppear'), spotPanel),
+		handleEnter: handle(forward('onEnter'), spotPanel),
+		handleTransition: handle(forward('onTransition'), Spotlight.resume),
+		handleWillTransition: handle(forward('onWillTransition'), Spotlight.pause)
+	},
 
-		if (index !== nextProps.index && node.contains(current)) {
-			const {lastFocusedIndices} = this.state;
-			// we must convert our NodeList to an Array in order to find the index of current
-			lastFocusedIndices[index] = [].slice.call(node.querySelectorAll(`.${spottableClass}`)).indexOf(current);
-			this.setState({lastFocusedIndices});
-		}
-	}
-
-	handleAppear = ({view}) => {
-		spotPanel({view, lastFocusedIndex: this.state.lastFocusedIndices[this.props.index]});
-		forwardOnAppear(view, this.props);
-	}
-
-	handleEnter = ({view}) => {
-		spotPanel({view, lastFocusedIndex: this.state.lastFocusedIndices[this.props.index]});
-		forwardOnEnter(view, this.props);
-	}
-
-	handleTransition = () => {
-		Spotlight.resume();
-		forwardOnTransition(null, this.props);
-	}
-
-	handleWillTransition = () => {
-		Spotlight.pause();
-		forwardOnWillTransition(null, this.props);
-	}
-
-	render () {
-		const {children, ...rest} = this.props;
+	render: ({arranger, children, handleAppear, handleEnter, handleTransition, handleWillTransition, index, noAnimation, ...rest}) => {
+		const count = React.Children.count(children);
+		invariant(
+			index === 0 && count === 0 || index < count,
+			`Panels index, ${index}, is invalid for number of children, ${count}`
+		);
 
 		return (
-			<ViewportBase {...rest} onAppear={this.handleAppear} onEnter={this.handleEnter} onTransition={this.handleTransition} onWillTransition={this.handleWillTransition}>
+			<ViewManager
+				{...rest}
+				noAnimation={noAnimation}
+				arranger={arranger}
+				duration={200}
+				index={index}
+				component="main"
+				onAppear={handleAppear}
+				onEnter={handleEnter}
+				onTransition={handleTransition}
+				onWillTransition={handleWillTransition}
+			>
 				{children}
-			</ViewportBase>
+			</ViewManager>
 		);
 	}
-}
+});
 
-export default Viewport;
-export {Viewport, ViewportBase};
+export default ViewportBase;
+export {ViewportBase as Viewport, ViewportBase};
