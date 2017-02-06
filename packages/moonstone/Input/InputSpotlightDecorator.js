36bc7d044c211b27a3dd8f14443b0a45157fc0c3
Ryan Duffy <ryan@tiqtech.com>
2017-02-06 17:29:35 -0600

    ENYO-3817: Prevent blurring input when clicking the decorator (#485)
    * prevent blur from propagating when clicking the decorator
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * clean up InputSpotlightDecorator focused state when leaving
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    
    Reviewed-By: Roy Sutton (roy.sutton@lge.com)
    Integrated-By: Aaron Tam (aaron.tam@lge.com)

--- packages/moonstone/Input/InputSpotlightDecorator.js
+++ packages/moonstone/Input/InputSpotlightDecorator.js
@@ -87,7 +87,7 @@ const InputSpotlightDecorator = hoc((config, Wrapped) => {
 		}
 
 		componentDidUpdate (_, prevState) {
-			if (this.state.node && (this.state.node !== prevState.node)) {
+			if (this.state.node) {
 				this.state.node.focus();
 			}
 
@@ -148,13 +148,21 @@ const InputSpotlightDecorator = hoc((config, Wrapped) => {
 						this.focusDecorator(ev.currentTarget);
 						ev.stopPropagation();
 					}
-				} if (!this.state.focused === 'input') {	// Blurring decorator but not focusing input
+				} else if (!ev.currentTarget.contains(ev.relatedTarget)) {
+					// Blurring decorator but not focusing input
+					forwardBlur(ev, this.props);
 					this.blur();
 				}
 			} else if (this.state.focused === 'input' && this.state.node === ev.target) {
-				// only blur when the input should be focused and is the target of the blur
-				this.blur();
-				forwardBlur(ev, this.props);
+				if (ev.currentTarget === ev.relatedTarget) {
+					// if the focused item (ev.relatedTarget) is the current target (the decorator),
+					// prevent the blur from propagating so the input will be re-focused on update.
+					ev.stopPropagation();
+				} else {
+					// only blur when the input should be focused and is the target of the blur
+					this.blur();
+					forwardBlur(ev, this.props);
+				}
 			}
 		}
 
