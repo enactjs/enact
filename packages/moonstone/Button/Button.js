31e9bd592ded257457014f618632900e49c46adb
Blake Stephens <blake.stephens@lge.com>
2017-02-08 16:37:11 -0800

    Implements a restricted set of co-authored classes (`.bg` and `.selected`) Also finally adds an alignment to button, so they line up when placed next to each other or with other elements.
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- packages/moonstone/Button/Button.js
+++ packages/moonstone/Button/Button.js
@@ -27,112 +27,115 @@ import componentCss from './Button.less';
  * @ui
  * @public
  */
-const ButtonBaseFactory = factory({css: componentCss}, ({css}) => kind({
-	name: 'Button',
+const ButtonBaseFactory = factory({css: componentCss}, ({css}) => {
+	css = {...componentCss, bg: css.bg, selected: css.selected};
+	return kind({
+		name: 'Button',
 
-	propTypes: /** @lends moonstone/Button.ButtonBase.prototype */ {
-		children: PropTypes.node.isRequired,
+		propTypes: /** @lends moonstone/Button.ButtonBase.prototype */ {
+			children: PropTypes.node.isRequired,
 
-		/**
-		 * The background-color opacity of this button; valid values are `'opaque'`, `'translucent'`,
-		 * and `'transparent'`.
-		 *
-		 * @type {String}
-		 * @default 'opaque'
-		 * @public
-		 */
-		backgroundOpacity: PropTypes.oneOf(['opaque', 'translucent', 'transparent']),
+			/**
+			 * The background-color opacity of this button; valid values are `'opaque'`, `'translucent'`,
+			 * and `'transparent'`.
+			 *
+			 * @type {String}
+			 * @default 'opaque'
+			 * @public
+			 */
+			backgroundOpacity: PropTypes.oneOf(['opaque', 'translucent', 'transparent']),
 
-		/**
-		 * When `true`, the [button]{@glossary button} is shown as disabled and does not
-		 * generate tap [events]{@glossary event}.
-		 *
-		 * @type {Boolean}
-		 * @default false
-		 * @public
-		 */
-		disabled: PropTypes.bool,
+			/**
+			 * When `true`, the [button]{@glossary button} is shown as disabled and does not
+			 * generate tap [events]{@glossary event}.
+			 *
+			 * @type {Boolean}
+			 * @default false
+			 * @public
+			 */
+			disabled: PropTypes.bool,
 
-		/**
-		 * A boolean parameter affecting the minimum width of the button. When `true`,
-		 * the minimum width will be set to 180px (or 130px if [small]{@link moonstone/Button.Button#small}
-		 * is `true`). If `false`, the minimum width will be set to the current value of
-		 * `@moon-button-height` (thus forcing the button to be no smaller than a circle with
-		 * diameter `@moon-button-height`).
-		 *
-		 * @type {Boolean}
-		 * @default true
-		 * @public
-		 */
-		minWidth: PropTypes.bool,
+			/**
+			 * A boolean parameter affecting the minimum width of the button. When `true`,
+			 * the minimum width will be set to 180px (or 130px if [small]{@link moonstone/Button.Button#small}
+			 * is `true`). If `false`, the minimum width will be set to the current value of
+			 * `@moon-button-height` (thus forcing the button to be no smaller than a circle with
+			 * diameter `@moon-button-height`).
+			 *
+			 * @type {Boolean}
+			 * @default true
+			 * @public
+			 */
+			minWidth: PropTypes.bool,
 
-		/**
-		 * When `true`, a pressed visual effect is applied to the button
-		 *
-		 * @type {Boolean}
-		 * @default false
-		 * @public
-		 */
-		pressed: PropTypes.bool,
+			/**
+			 * When `true`, a pressed visual effect is applied to the button
+			 *
+			 * @type {Boolean}
+			 * @default false
+			 * @public
+			 */
+			pressed: PropTypes.bool,
 
-		/**
-		 * When `true`, a selected visual effect is applied to the button
-		 *
-		 * @type {Boolean}
-		 * @default false
-		 * @public
-		 */
-		selected: PropTypes.bool,
+			/**
+			 * When `true`, a selected visual effect is applied to the button
+			 *
+			 * @type {Boolean}
+			 * @default false
+			 * @public
+			 */
+			selected: PropTypes.bool,
 
-		/**
-		 * A boolean parameter affecting the size of the button. If `true`, the
-		 * button's diameter will be set to 60px. However, the button's tap target
-		 * will still have a diameter of 78px, with an invisible DOM element
-		 * wrapping the small button to provide the larger tap zone.
-		 *
-		 * @type {Boolean}
-		 * @default false
-		 * @public
-		 */
-		small: PropTypes.bool
-	},
+			/**
+			 * A boolean parameter affecting the size of the button. If `true`, the
+			 * button's diameter will be set to 60px. However, the button's tap target
+			 * will still have a diameter of 78px, with an invisible DOM element
+			 * wrapping the small button to provide the larger tap zone.
+			 *
+			 * @type {Boolean}
+			 * @default false
+			 * @public
+			 */
+			small: PropTypes.bool
+		},
 
-	defaultProps: {
-		backgroundOpacity: 'opaque',
-		disabled: false,
-		minWidth: true,
-		pressed: false,
-		selected: false,
-		small: false
-	},
+		defaultProps: {
+			backgroundOpacity: 'opaque',
+			disabled: false,
+			minWidth: true,
+			pressed: false,
+			selected: false,
+			small: false
+		},
 
-	styles: {
-		css,
-		className: 'button'
-	},
+		styles: {
+			css,
+			className: 'button'
+		},
 
-	computed: {
-		className: ({backgroundOpacity, minWidth, pressed, selected, small, styler}) => styler.append(
-			{pressed, small, minWidth, selected},
-			backgroundOpacity
-		)
-	},
+		computed: {
+			className: ({backgroundOpacity, minWidth, pressed, selected, small, styler}) => styler.append(
+				{pressed, small, minWidth, selected},
+				backgroundOpacity
+			)
+		},
 
-	render: ({children, ...rest}) => {
-		delete rest.backgroundOpacity;
-		delete rest.minWidth;
-		delete rest.pressed;
-		delete rest.selected;
-		delete rest.small;
+		render: ({children, ...rest}) => {
+			delete rest.backgroundOpacity;
+			delete rest.minWidth;
+			delete rest.pressed;
+			delete rest.selected;
+			delete rest.small;
 
-		return (
-			<div role="button" {...rest}>
-				<div className={css.bg} />
-				<div className={css.client}>{children}</div>
-			</div>
-		);
-	}
-}));
+			return (
+				<div role="button" {...rest}>
+					<div className={css.bg} />
+					<div className={css.client}>{children}</div>
+				</div>
+			);
+		}
+	});
+});
 
 /**
  * {@link moonstone/Button.Button} is a Button with Moonstone styling, Spottable and
@@ -153,7 +156,6 @@ const ButtonBaseFactory = factory({css: componentCss}, ({css}) => kind({
  * @ui
  * @public
  */
-// const Button = Uppercase(
 const ButtonFactory = factory(css => {
 	const Base = ButtonBaseFactory(css);
 	return Uppercase(
