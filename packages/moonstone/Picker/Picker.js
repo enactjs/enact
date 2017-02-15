42ecdfc341f302612aafb6a42723980e7989d012
Teck <teck.liew@lge.com>
2017-02-15 11:43:29 -0800

    PickerItem marquee on "focus" if joined and not disabled; "hover" for all other cases

--- packages/moonstone/Picker/Picker.js
+++ packages/moonstone/Picker/Picker.js
@@ -156,8 +156,16 @@ const PickerBase = kind({
 	computed: {
 		max: ({children}) => children && children.length ? children.length - 1 : 0,
 		reverse: ({orientation}) => (orientation === 'vertical'),
-		children: ({children, marqueeDisabled}) => React.Children.map(children, (child) => {
-			return <PickerItem marqueeDisabled={marqueeDisabled}>{child}</PickerItem>;
+		children: ({children, disabled, joined, marqueeDisabled}) => React.Children.map(children, (child) => {
+			return (
+				<PickerItem
+					disabled={disabled}
+					joined={joined}
+					marqueeDisabled={marqueeDisabled}
+				>
+					{child}
+				</PickerItem>
+			);
 		}),
 		value: ({value, children}) => {
 			const max = children && children.length ? children.length - 1 : 0;
--- packages/moonstone/internal/Picker/PickerItem.js
+++ packages/moonstone/internal/Picker/PickerItem.js
@@ -8,14 +8,39 @@ import css from './Picker.less';
 const PickerItemBase = kind({
 	name: 'PickerItem',
 
+	propTypes: {
+		/**
+		 * When `true`, the [button]{@glossary button} is shown as disabled and does not
+		 * generate tap [events]{@glossary event}.
+		 *
+		 * @type {Boolean}
+		 * @public
+		 */
+		disabled: React.PropTypes.bool,
+		/**
+		 * Determines the user interaction of the control. A joined picker allows the user to use
+		 * the arrow keys to adjust the picker's value. The user may no longer use those arrow keys
+		 * to navigate, while this control is focused. A split control allows full navigation,
+		 * but requires individual ENTER presses on the incrementer and decrementer buttons.
+		 * Pointer interaction is the same for both formats.
+		 *
+		 * @type {Boolean}
+		 * @public
+		 */
+		joined: React.PropTypes.bool
+	},
+
 	styles: {
 		css,
 		className: 'item'
 	},
 
-	render: (props) => (
-		<MarqueeText {...props} marqueeCentered marqueeOn="hover" />
-	)
+	render: ({disabled, joined, ...rest}) => {
+		const marqueeControl =  !disabled && joined ? 'focus' : 'hover';
+		return (
+			<MarqueeText {...rest} marqueeCentered marqueeOn={marqueeControl} />
+		);
+	}
 });
 
 export default PickerItemBase;
