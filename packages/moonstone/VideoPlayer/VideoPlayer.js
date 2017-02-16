d6ccfa0c3f697825e524f096bbf29f78b2051656
Dave Freeman <dave.freeman@lge.com>
2017-02-15 18:01:50 -0600

    modify onPlayButtonClick to still pause/play video

--- packages/moonstone/VideoPlayer/VideoPlayer.js
+++ packages/moonstone/VideoPlayer/VideoPlayer.js
@@ -892,6 +892,16 @@ const VideoPlayerBase = class extends React.Component {
 		delete rest.jumpBy;
 		delete rest.titleHideDelay;
 
+		let onPlayButtonClick = this.onPlay;
+
+		if (rest.onPlayButtonClick) {
+			onPlayButtonClick = () => {
+				rest.onPlayButtonClick();
+				this.onPlay();
+			}
+			delete rest.onPlayButtonClick;
+		}
+
 		// Handle some cases when the "more" button is pressed
 		const moreDisabled = !(this.state.more);
 
