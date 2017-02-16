6706532dd78cad246469e2b96230204223d7a36b
Blake Stephens <blake.stephens@lge.com>
2017-02-16 12:52:18 -0800

    PLAT-36932: Add factory support to IconButton
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- packages/moonstone/IconButton/IconButton.js
+++ packages/moonstone/IconButton/IconButton.js
@@ -15,7 +15,6 @@ import Icon from '../Icon';
 import componentCss from './IconButton.less';
 
 const OptimizedIcon = onlyUpdateForKeys(['small', 'children'])(Icon);
-const Button = ButtonFactory({componentCss});
 
 /**
  * {@link moonstone/IconButton.IconButton} is a {@link moonstone/Icon.Icon} that acts like a button.
@@ -34,91 +33,104 @@ const Button = ButtonFactory({componentCss});
  * @ui
  * @public
  */
-const IconButtonBaseFactory = factory({css: componentCss}, ({css}) => kind({
-	name: 'IconButton',
+const IconButtonBaseFactory = factory({css: componentCss}, ({css}) => {
+	const Button = ButtonFactory({css});
+	return kind({
+		name: 'IconButton',
 
-	propTypes: /** @lends moonstone/IconButton.IconButton.prototype */ {
-		/**
-		 * The background-color opacity of this icon button; valid values are `'opaque'`,
-		 * `'translucent'`, and `'transparent'`.
-		 *
-		 * @type {String}
-		 * @default 'opaque'
-		 * @public
-		 */
-		backgroundOpacity: PropTypes.oneOf(['opaque', 'translucent', 'transparent']),
+		propTypes: /** @lends moonstone/IconButton.IconButton.prototype */ {
+			/**
+			 * The background-color opacity of this icon button; valid values are `'opaque'`,
+			 * `'translucent'`, and `'transparent'`.
+			 *
+			 * @type {String}
+			 * @default 'opaque'
+			 * @public
+			 */
+			backgroundOpacity: PropTypes.oneOf(['opaque', 'translucent', 'transparent']),
 
-		/**
-		 * The icon displayed within the button.
-		 *
-		 * @see {@link moonstone/Icon.Icon#children}
-		 * @type {String|Object}
-		 * @public
-		 */
-		children: PropTypes.string,
+			/**
+			 * The icon displayed within the button.
+			 *
+			 * @see {@link moonstone/Icon.Icon#children}
+			 * @type {String|Object}
+			 * @public
+			 */
+			children: PropTypes.string,
 
-		/**
-		 * When `true`, the [button]{@glossary button} is shown as disabled and does not
-		 * generate tap [events]{@glossary event}.
-		 *
-		 * @type {Boolean}
-		 * @default false
-		 * @public
-		 */
-		disabled: PropTypes.bool,
+			/**
+			 * When `true`, the [button]{@glossary button} is shown as disabled and does not
+			 * generate tap [events]{@glossary event}.
+			 *
+			 * @type {Boolean}
+			 * @default false
+			 * @public
+			 */
+			disabled: PropTypes.bool,
 
-		/**
-		 * When `true`, a pressed visual effect is applied to the icon button
-		 *
-		 * @type {Boolean}
-		 * @default false
-		 * @public
-		 */
-		pressed: PropTypes.bool,
+			/**
+			 * When `true`, a pressed visual effect is applied to the icon button
+			 *
+			 * @type {Boolean}
+			 * @default false
+			 * @public
+			 */
+			pressed: PropTypes.bool,
 
-		/**
-		 * When `true`, a selected visual effect is applied to the icon button
-		 *
-		 * @type {Boolean}
-		 * @default false
-		 * @public
-		 */
-		selected: PropTypes.bool,
+			/**
+			 * When `true`, a selected visual effect is applied to the icon button
+			 *
+			 * @type {Boolean}
+			 * @default false
+			 * @public
+			 */
+			selected: PropTypes.bool,
 
-		/**
-		 * A boolean parameter affecting the size of the button. If `true`, the
-		 * button's diameter will be set to 60px. However, the button's tap target
-		 * will still have a diameter of 78px, with an invisible DOM element
-		 * wrapping the small button to provide the larger tap zone.
-		 *
-		 * @type {Boolean}
-		 * @default false
-		 * @public
-		 */
-		small: PropTypes.bool
-	},
+			/**
+			 * A boolean parameter affecting the size of the button. If `true`, the
+			 * button's diameter will be set to 60px. However, the button's tap target
+			 * will still have a diameter of 78px, with an invisible DOM element
+			 * wrapping the small button to provide the larger tap zone.
+			 *
+			 * @type {Boolean}
+			 * @default false
+			 * @public
+			 */
+			small: PropTypes.bool
+		},
 
-	defaultProps: {
-		small: false
-	},
+		defaultProps: {
+			small: false
+		},
 
-	styles: {
-		css,
-		className: 'iconButton'
-	},
+		styles: {
+			css: {
+				...componentCss,
+				/*
+				 * Allowed CSS Class Overrides
+				 *
+				 * * .bg	The background of the button, used on a child of button.
+				 * * .selected	The selected state of the button, applied to the base element.
+				 */
+				bg: css.bg,
+				selected: css.selected
+			},
+			className: 'iconButton'
+		},
 
-	computed: {
-		className: ({small, styler}) => styler.append({small})
-	},
+		computed: {
+			className: ({small, styler}) => styler.append({small})
+		},
 
-	render: ({children, small, ...rest}) => {
-		return (
-			<Button {...rest} small={small} minWidth={false} marqueeDisabled>
-				<OptimizedIcon small={small} className={css.icon}>{children}</OptimizedIcon>
-			</Button>
-		);
-	}
-}));
+		render: ({children, small, ...rest}) => {
+			return (
+				<Button {...rest} small={small} minWidth={false} marqueeDisabled>
+					<OptimizedIcon small={small} className={css.icon}>{children}</OptimizedIcon>
+				</Button>
+			);
+		}
+	});
+});
 
 const IconButtonBase = IconButtonBaseFactory();
 
