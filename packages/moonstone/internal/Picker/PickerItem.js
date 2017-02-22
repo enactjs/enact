4ca6cca5a9d71b2c8e2e4df5acfc0de2a3be410e
richa.Sha <richa.shaurabh@gmail.com>
2017-02-22 17:31:43 +0530

    ENYO-3886:In Picker RTL text in RTL Locale coming as LTR Text

--- packages/moonstone/internal/Picker/PickerItem.js
+++ packages/moonstone/internal/Picker/PickerItem.js
@@ -2,19 +2,45 @@ import kind from '@enact/core/kind';
 import React from 'react';
 
 import {MarqueeText} from '../../Marquee';
+import {isRtlText} from '@enact/i18n';
 
 import css from './Picker.less';
 
 const PickerItemBase = kind({
 	name: 'PickerItem',
 
+	propTypes: /* @lends moonstone/Picker.PickerBase.prototype */ {
+
+		/**
+		* Children from which to pick
+		*
+		* @type {Node}
+		* @public
+		*/
+		children: React.PropTypes.node.isRequired
+	},
+
 	styles: {
 		css,
 		className: 'item'
 	},
 
-	render: (props) => (
-		<MarqueeText {...props} marqueeCentered marqueeOn="hover" />
+	computed: {
+		clientStyle: ({children}) => {
+			let rtl = isRtlText(children);
+			let direction = 'inherit';
+			direction = rtl ? 'rtl' : 'ltr';
+
+			const style = {
+				direction
+			};
+
+			return style;
+		}
+	},
+
+	render: ({clientStyle, ...rest}) => (
+		<MarqueeText {...rest} style={clientStyle} marqueeCentered marqueeOn="hover" />
 	)
 });
 
