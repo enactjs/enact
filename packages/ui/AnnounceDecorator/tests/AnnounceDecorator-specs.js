d975e2fc4bc619e7af5dba927c657260c197af9e
Seungcheon Baek <sc.baek@lge.com>
2017-02-25 01:52:20 +0900

    PLAT-35472: a11y support for a page control (ScrollBar) and GridListImageItem (#547)
    * PLAT-35472: added a11y support for page controls (scrollbar buttons)
    
    When focused, it reads scrollable direction. For example) 'scroll up button'.
    When pressed by OK button, it reads scrolling direction. For example) 'UP'.
    
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)
    
    * Fixed redundant reading on page controls
    
    Issue: when pressing page controls, it reads twice.
    Solution: use aria-live only except role.
    
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)
    
    * PLAT-35472: added a11y support for GridListImageItem
    
    Added `role` and `aria-checked` when `selectionOverlayShowing is set to true.
    
    Note:
    - prehint and hint are NOT considered for now.
    - If `aria-label` is set, it reads `aria-label`. Or it reads childrens by natural.
    
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)
    
    * PLAT-35472: Add ui/AnnounceDecorator to support a11y alert requirements (#567)
    
    * refactor ScrollButton out of Scrollbar
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * wip
    
    * add AnnounceDecorator HOC
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * move classNames into ScrollButton
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * cleanup
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * further clean up
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * prevent emulating mouse events when key events are repeated
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    Integrated-By: Ryan Duffy (ryan.duffy@lge.com)

---
+++ packages/ui/AnnounceDecorator/tests/AnnounceDecorator-specs.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import AnnounceDecorator from '../AnnounceDecorator';
+
+describe.only('AnnounceDecorator', () => {
+
+	// no-op wrapper
+	const Div = () => <div />;
+
+	it('should pass a function in the announce prop', function () {
+		const Component = AnnounceDecorator(Div);
+		const subject = shallow(
+			<Component />
+		);
+
+		const expected = 'function';
+		const actual = typeof subject.prop('announce');
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should allow prop to be configured for announce function', function () {
+		const prop = '__NOTIFY__';
+		const Component = AnnounceDecorator({prop}, Div);
+		const subject = shallow(
+			<Component />
+		);
+
+		const expected = 'function';
+		const actual = typeof subject.prop(prop);
+
+		expect(actual).to.equal(expected);
+
+	});
+
+});
