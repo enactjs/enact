0195dce3b4451ae8fd6977127c9a60f388d083d0
Teck <teck.liew@lge.com>
2017-02-28 15:54:41 -0800

    update qa sample

--- packages/sampler/stories/qa-stories/Spinner.js
+++ packages/sampler/stories/qa-stories/Spinner.js
@@ -18,6 +18,8 @@ const
 		}
 	};
 
+const nullify = (v) => v === '<null>' ? null : v;
+
 storiesOf('Spinner')
 	.addDecorator(withKnobs)
 	.addWithInfo(
@@ -88,7 +90,7 @@ storiesOf('Spinner')
 		'blocking click events',
 		() => (
 			<div>
-				<div style={Object.assign({}, style.spinnerDiv, {backgroundColor: '#222'})}>
+				<div style={Object.assign({}, style.spinnerDiv, {height: '280px', backgroundColor: '#222', overflow: 'hidden'})}>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 					<Button>Button</Button>
 					<Button>Button</Button>
@@ -96,7 +98,7 @@ storiesOf('Spinner')
 					<Spinner
 						transparent={boolean('transparent', false)}
 						centered={boolean('centered', false)}
-						blockClick={select('blockClick', [null, 'container', 'screen'])}
+						blockClick={nullify(select('blockClick', ['<null>', 'container', 'screen']))}
 						scrim={boolean('scrim', true)}
 					>
 						{text('content')}
