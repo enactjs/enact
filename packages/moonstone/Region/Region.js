1ec411059b5236dbcc282aea68b7e26d840a7832
Ryan Duffy <ryan.duffy@lge.com>
2017-03-09 09:15:22 -0600

    add Region component
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

---
+++ packages/moonstone/Region/Region.js
@@ -0,0 +1,78 @@
+/**
+ * Exports the {@link moonstone/Region.Region} component.
+ *
+ * @module moonstone/Region
+ */
+
+import kind from '@enact/core/kind';
+import React from 'react';
+
+import Divider from '../Divider';
+
+/**
+ * {@link moonstone/Region.Region} provides a labeled region to group components. The `title` is
+ * wrapped by a {@link moonstone/Divider.Divider} which precedes any `children`.
+ *
+ * ```
+ * <Region title="Select an Option">
+ *   <Group childComponent={CheckboxItem} selectedProp="selected">
+ *     {items}
+ *   </Group>
+ * </Region>
+ * ```
+ *
+ * @class Region
+ * @memberof moonstone/Region
+ * @ui
+ * @public
+ */
+const RegionBase = kind({
+	name: 'Region',
+
+	propTypes: /** @lends moonstone/Region.Region.prototype */ {
+		/**
+		 * The title of the region placed within an instance of {@link moonstone/Divider.Divider}
+		 * before the children.
+		 *
+		 * @type {String}
+		 * @required
+		 * @public
+		 */
+		title: React.PropTypes.string.isRequired,
+
+		/**
+		 * Sets the aria-label for the region. If unset, it defaults to the value of `title`
+		 *
+		 * @type {String}
+		 * @public
+		 */
+		'aria-label': React.PropTypes.string,
+
+		/**
+		 * The contents of the region
+		 *
+		 * @type {Node}
+		 * @public
+		 */
+		children: React.PropTypes.node
+	},
+
+	computed: {
+		'aria-label': ({'aria-label': ariaLabel, title}) => ariaLabel || title
+	},
+
+	render: ({'aria-label': ariaLabel, children, title, ...rest}) => {
+		return (
+			<div {...rest} role="region" aria-label={ariaLabel}>
+				<Divider>{title}</Divider>
+				{children}
+			</div>
+		);
+	}
+});
+
+export default RegionBase;
+export {
+	RegionBase as Region,
+	RegionBase
+};
