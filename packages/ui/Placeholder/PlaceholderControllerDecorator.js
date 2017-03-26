c82ca5a95a652afe312500d9d7729f133e4d9065
YunBum SUNG <yb.sung@lge.com>
2017-03-27 07:22:48 +0900

    PLAT-38500: Create `PlaceholderDecorator` and `PlaceholderControllerDecorator` to render the wrapped component when it comes into view (#623)
    * Add VisibilityObserver
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Remove intersection-observer-polyfill
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Separate LazyChildDecorator and Resizable
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Replace IntersectionObserver with Viscroll based approach
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Move LazyChildFeature from Scrollable to Scroller
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Cleanup; JS Doc
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Reduce the count to notify
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Remove key handling
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Define dummy div element's style
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Fix JS hint
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Cleanup
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Update CHANGELOG.md
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * moonstone/LazyChildDecorator -> ui/PlaceholderDecorator
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Create PlaceholderContainer
    
    - Move the code related with placeholder in Scroller to
    PlaceholderContainer
    - Cache offsetTopThreshold in PlaceholderContainer
    - Pass `clientHeight` prop from App to PlaceholderContainer
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Define clientHeight as default config of PlaceHolderDecorator
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Register not Component but callback
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Update JS Doc
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * wip - ui/Placeholder
    
    * correct changelog
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * generalize placeholder behavior
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * Fixes up docs
    
    * Actually fix up docs
    
    Issue: PLAT-38500
    Enact-DCO-1.0-Signed-off-by Aaron Tam <aaron.tam@lge.com>
    
    
    1st-Reviewed-by: Seungcheon Baek (sc.baek@lge.com)
    Integrated-By: Aaron Tam (aaron.tam@lge.com)

---
+++ packages/ui/Placeholder/PlaceholderControllerDecorator.js
@@ -0,0 +1,162 @@
+import {forward, handle} from '@enact/core/handle';
+import hoc from '@enact/core/hoc';
+import {Job} from '@enact/core/util';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {contextTypes} from './PlaceholderDecorator';
+
+/**
+ * Default config for {@link ui/Placeholder.PlaceholderControllerDecorator}
+ *
+ * @memberof ui/Placeholder.PlaceholderControllerDecorator
+ * @hocconfig
+ * @public
+ */
+const defaultConfig = {
+	/**
+	 * The bounds of the container represented by an object with `height` and `width` members. If
+	 * the container is a static size, this can be specified at design-time to avoid calculating the
+	 * bounds at run-time (the default behavior).
+	 *
+	 * @type {Object}
+	 * @default null
+	 * @memberof ui/Placeholder.PlaceholderControllerDecorator.defaultConfig
+	 */
+	bounds: null,
+
+	/**
+	 * Event callback which indicates that the viewport has scrolled and placeholders should be
+	 * notified.
+	 *
+	 * @type {String}
+	 * @default onScroll
+	 * @memberof ui/Placeholder.PlaceholderControllerDecorator.defaultConfig
+	 */
+	notify: 'onScroll'
+};
+
+/**
+ * {@link ui/Placeholder.PlaceholderControllerDecorator} is a Higher-order Component that can make
+ * placeholders rendered or not rendered depending on `'scrollTop'` from the `'onScroll'`'s parameter.
+ *
+ * @class PlaceholderControllerDecorator
+ * @memberof ui/Placeholder
+ * @hoc
+ * @public
+ */
+const PlaceholderControllerDecorator = hoc(defaultConfig, (config, Wrapped) => {
+	const {bounds, notify} = config;
+
+	return class extends React.Component {
+		static displayName = 'PlaceholderControllerDecorator'
+
+		static childContextTypes = contextTypes
+
+		bounds = null
+		controlled = []
+		leftThreshold = -1
+		node = null
+		topThreshold = -1
+
+		getChildContext () {
+			return {
+				registerPlaceholder: this.handleRegister,
+				unregisterPlaceholder: this.handleUnregister
+			};
+		}
+
+		componentDidMount () {
+			this.setBounds();
+			this.setThresholds(0, 0);
+		}
+
+		componentWillUnmount () {
+			this.notifyAllJob.stop();
+		}
+
+		setBounds () {
+			if (bounds != null) {
+				this.bounds = Object.assign({}, bounds);
+			} else {
+				// Allowing findDOMNode for HOCs versus adding extra ref props
+				// eslint-disable-next-line	react/no-find-dom-node
+				this.node = ReactDOM.findDOMNode(this);
+				this.bounds = {
+					height: this.node.offsetHeight,
+					width: this.node.offsetWidth
+				};
+			}
+		}
+
+		handleRegister = (key, callback) => {
+			this.controlled.push({callback, key});
+
+			// do not notify until we've initialized the thresholds
+			if (this.topThreshold !== -1 && this.leftThreshold !== -1) {
+				this.notifyAllJob.start(this.topThreshold, this.leftThreshold);
+			}
+		}
+
+		handleUnregister = (key) => {
+			const length = this.controlled.length;
+
+			for (let i = 0; i < length; i++) {
+				if (this.controlled[i].key === key) {
+					this.controlled.splice(i, 1);
+					break;
+				}
+			}
+		}
+
+		notifyAll = (topThreshold, leftThreshold) => {
+			for (let index = this.controlled.length - 1; index >= 0; index--) {
+				const {callback} = this.controlled[index];
+
+				callback({
+					index,
+					leftThreshold,
+					topThreshold
+				});
+			}
+		}
+
+		// queue up notifications when placeholders are first created
+		notifyAllJob = new Job(this.notifyAll, 32)
+
+		setThresholds (top, left) {
+			if (this.controlled.length === 0) return;
+
+			const {height, width} = this.bounds;
+			const topThreshold = (Math.floor(top / height) + 2) * height;
+			const leftThreshold = (Math.floor(left / width) + 2) * width;
+
+			if (this.topThreshold < topThreshold || this.leftThreshold < leftThreshold) {
+				this.notifyAll(topThreshold, leftThreshold);
+				this.topThreshold = topThreshold;
+				this.leftThreshold = leftThreshold;
+			}
+		}
+
+		handle = handle.bind(this)
+
+		handleNotify = this.handle(
+			forward(notify),
+			({scrollLeft, scrollTop}) => {
+				this.setThresholds(scrollTop, scrollLeft);
+			}
+		)
+
+		render () {
+			const props = Object.assign({}, this.props);
+
+			if (notify) props[notify] = this.handleNotify;
+
+			return (
+				<Wrapped {...props} />
+			);
+		}
+	};
+});
+
+export default PlaceholderControllerDecorator;
---
+++ packages/ui/Placeholder/PlaceholderDecorator.js
@@ -0,0 +1,123 @@
+import hoc from '@enact/core/hoc';
+import React from 'react';
+
+/**
+ * Default config for {@link ui/Placeholder.PlaceholderDecorator}
+ *
+ * @memberof ui/Placeholder.PlaceholderDecorator
+ * @hocconfig
+ * @public
+ */
+const defaultConfig = {
+	/**
+	 * Configures the style of the placeholder element
+	 *
+	 * @type {Object}
+	 * @default {height: 0, width: 'auto'}
+	 * @memberof ui/Placeholder.PlaceholderDecorator.defaultConfig
+	 */
+	style: {height: 0, width: 'auto'},
+
+	/**
+	 * The component to use as a placeholder.
+	 *
+	 * @type {String}
+	 * @default 'div'
+	 * @memberof ui/Placeholder.PlaceholderDecorator.defaultConfig
+	 */
+	placeholderComponent: 'div'
+};
+
+/**
+ * The context propTypes required by `PlaceholderDecorator`. This should be set as the `childContextTypes` of a
+ * container so that the container could notify when scrolling
+ *
+ * @memberof ui/Placeholder.PlaceholderDecorator
+ * @public
+ */
+const contextTypes = {
+	registerPlaceholder: React.PropTypes.func,
+	unregisterPlaceholder: React.PropTypes.func
+};
+
+/**
+ * {@link ui/Placeholder.PlaceholderDecorator} is a Higher-order Component that can be used that
+ * a container notify the Wrapped component when scrolling.
+ *
+ * Containers must provide `registerPlaceholder` and `unregisterPlaceholder` methods via React's context in order for
+ * `PlaceholderDecorator` instances.
+ *
+ * @class PlaceholderDecorator
+ * @memberof ui/Placeholder
+ * @hoc
+ * @public
+ */
+const PlaceholderDecorator = hoc(defaultConfig, (config, Wrapped) => {
+	const {placeholderComponent: PlaceholderComponent, style} = config;
+	const placeholderStyle = Object.assign({}, defaultConfig.style, style);
+
+	return class extends React.Component {
+		static displayName = 'PlaceholderDecorator'
+
+		static contextTypes = contextTypes
+
+		constructor () {
+			super();
+
+			this.state = {
+				visible: false
+			};
+		}
+
+		componentDidMount () {
+			if (!this.state.visible) {
+				this.context.registerPlaceholder(this, this.update);
+			}
+		}
+
+		componentWillUnmount () {
+			if (!this.state.visible) {
+				this.context.unregisterPlaceholder(this);
+			}
+		}
+
+		update = ({leftThreshold, topThreshold}) => {
+			const {offsetLeft, offsetTop} = this.placeholderRef;
+
+			if (offsetTop < topThreshold && offsetLeft < leftThreshold) {
+				this.setState({visible: true});
+				this.context.unregisterPlaceholder(this);
+			}
+		}
+
+		initPlaceholderRef = (ref) => {
+			this.placeholderRef = ref;
+		}
+
+		render () {
+			const {visible} = this.state;
+
+			if (visible) {
+				return (
+					<Wrapped
+						{...this.props}
+						ref={this.initPlaceholderRef}
+					/>
+				);
+			} else {
+				return (
+					<PlaceholderComponent
+						ref={this.initPlaceholderRef}
+						style={placeholderStyle}
+					/>
+				);
+			}
+		}
+	};
+});
+
+export default PlaceholderDecorator;
+export {
+	contextTypes,
+	PlaceholderDecorator
+};
