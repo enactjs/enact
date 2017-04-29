7198b67e72cd67633fd03fb5b62d32f546460121
Aaron Tam <aaron.tam@lge.com>
2017-04-28 22:29:14 -0700

    Cleans up code
    Issue: ENYO-4064
    Enact-DCO-1.0-Signed-off-by Aaron Tam <aaron.tam@lge.com>

--- packages/moonstone/internal/DateTimeDecorator/DateTimeDecorator.js
+++ packages/moonstone/internal/DateTimeDecorator/DateTimeDecorator.js
@@ -161,8 +161,9 @@ const DateTimeDecorator = hoc((config, Wrapped) => {
 		 * @returns {Number}			Updated internal value
 		 */
 		updateValue = (value) => {
-			let maxDays = value.cal.getMonLength(value.month, value.year);
-			value.day = (value.day <= maxDays) ? value.day : maxDays;
+			const {day, month, year} = value;
+			const maxDays = value.cal.getMonLength(month, year);
+			value.day = (day <= maxDays) ? day : maxDays;
 
 			const date = DateFactory(value);
 			const newValue = date.getTimeExtended();
