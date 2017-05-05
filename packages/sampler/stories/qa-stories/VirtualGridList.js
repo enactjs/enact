59e0c72a37668ede035e889267f74568afd7e3cf
srirama.singeri <srirama.singeri@lge.com>
2017-05-05 15:13:13 +0530

    Create qa-sample for a Horizontal VirtualGridList

---
+++ packages/sampler/stories/qa-stories/VirtualGridList.js
@@ -0,0 +1,59 @@
+import {VirtualGridList} from '@enact/moonstone/VirtualList';
+import ri from '@enact/ui/resolution';
+import {VirtualListCore} from '@enact/moonstone/VirtualList/VirtualListBase';
+import GridListImageItem from '@enact/moonstone/VirtualList/GridListImageItem';
+import React from 'react';
+import {storiesOf, action} from '@kadira/storybook';
+import {number} from '@kadira/storybook-addon-knobs';
+
+import {mergeComponentMetadata} from '../../src/utils/propTables';
+
+const Config = mergeComponentMetadata('VirtualGridList', VirtualListCore, VirtualGridList);
+
+const
+	listStyle = {height: ri.scale(550) + 'px'},
+	items = [],
+	// eslint-disable-next-line enact/prop-types
+	renderItem = ({data, index, ...rest}) => {
+		const {text, subText, source} = data[index];
+
+		return (
+			<GridListImageItem
+				{...rest}
+				caption={text}
+				source={source}
+				subCaption={subText}
+			/>
+		);
+	};
+
+for (let i = 0; i < 1000; i++) {
+	const
+		count = ('00' + i).slice(-3),
+		text = `Item ${count}`,
+		subText = `SubItem ${count}`,
+		color = Math.floor((Math.random() * (0x1000000 - 0x101010)) + 0x101010).toString(16),
+		source = `http://placehold.it/300x300/${color}/ffffff&text=Image ${i}`;
+
+	items.push({text, subText, source});
+}
+
+storiesOf('VirtualList.VirtualGridList')
+	.addWithInfo(
+		' ',
+		'Horizontal VirtualGridList',
+		() => (
+			<VirtualGridList
+				component={renderItem}
+				data={items}
+				dataSize={number('dataSize', items.length)}
+				direction='horizontal'
+				itemSize={{minWidth: ri.scale(number('minWidth', 180)), minHeight: ri.scale(number('minHeight', 270))}}
+				onScrollStart={action('onScrollStart')}
+				onScrollStop={action('onScrollStop')}
+				spacing={ri.scale(number('spacing', 20))}
+				style={listStyle}
+			/>
+		),
+		{propTables: [Config]}
+	);
