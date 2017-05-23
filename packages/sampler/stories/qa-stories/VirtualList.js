8ba21ea392e1b03a1be9a09e39b4fb873d8ff5c5
srirama.singeri <srirama.singeri@lge.com>
2017-05-23 14:27:22 +0530

    Change to maintain the selected state in item

--- packages/sampler/stories/qa-stories/VirtualList.js
+++ packages/sampler/stories/qa-stories/VirtualList.js
@@ -3,10 +3,13 @@ import VirtualList from '@enact/moonstone/VirtualList';
 import {VirtualListCore} from '@enact/moonstone/VirtualList/VirtualListBase';
 import ri from '@enact/ui/resolution';
 import React from 'react';
+import PropTypes from 'prop-types';
 import {storiesOf, action} from '@kadira/storybook';
-import {number} from '@kadira/storybook-addon-knobs';
+import {boolean, number} from '@kadira/storybook-addon-knobs';
+
 
 import {mergeComponentMetadata} from '../../src/utils/propTables';
+import nullify from '../../src/utils/nullify.js';
 
 const Config = mergeComponentMetadata('VirtualList', VirtualListCore, VirtualList);
 
@@ -19,68 +22,53 @@ const
 		list: {
 			height: ri.unit(552, 'rem')
 		}
-	};
-
-class StatefulVirtualList extends React.Component {
-	constructor (props) {
-		super(props);
-		this.state = {
-			itemList: []
-		};
-	}
-
-	componentWillMount () {
-		this.initializeItems();
-	}
-
-	initializeItems = () => {
-		const initailItems = [];
-		for (let i = 0; i < 100; i++) {
-			initailItems.push({item :'Item ' + ('00' + i).slice(-3), selected:false});
-		}
-
-		this.setState({
-			itemList: initailItems
-		});
-	}
-
-	handleClick = (ev) => {
-		const changedItems = [...this.state.itemList],
-			index = ev.currentTarget.dataset.index,
-			sel = changedItems[index].selected;
-		changedItems[index] = {
-			...this.state.itemList[index],
-			selected: !sel
-		};
-
-		this.setState({
-			itemList: changedItems
-		});
-	}
-
+	},
+	items = [],
+	// eslint-disable-next-line enact/prop-types, enact/display-name
 	renderItem = (size) => ({data, index, ...rest}) => {
 		const itemStyle = {height: size + 'px', ...style.item};
 		return (
-			<SwitchItem {...rest} onClick={this.handleClick} selected={data[index].selected} style={itemStyle}>
+			<StatefulSwitchItem  index={index} style={itemStyle} {...rest}>
 				{data[index].item}
-			</SwitchItem>
+			</StatefulSwitchItem>
 		);
 	};
 
+for (let i = 0; i < 1000; i++) {
+	items.push({item :'Item ' + ('00' + i).slice(-3), selected: false});
+}
+
+class StatefulSwitchItem extends React.Component {
+	static propTypes = {
+		index: PropTypes.number
+	}
+
+	constructor (props) {
+		super(props);
+		this.state = {
+			selected: items[props.index].selected
+		};
+	}
+
+	componentWillReceiveProps (nextProps) {
+		if (this.props.index !== nextProps.index) {
+			this.setState({selected: items[nextProps.index].selected});
+		}
+	}
+
+	onToggle = () => {
+		items[this.props.index].selected = !items[this.props.index].selected;
+		this.setState({selected: !this.state.selected});
+	}
+
 	render () {
-		const itemSize = ri.scale(number('itemSize', 60));
+		const props = Object.assign({}, this.props);
+		delete props.index;
+
 		return (
-			<VirtualList
-				{...this.props}
-				component={this.renderItem(itemSize)}
-				data={this.state.itemList}
-				dataSize={number('dataSize', this.state.itemList.length)}
-				itemSize={itemSize}
-				onScrollStart={action('onScrollStart')}
-				onScrollStop={action('onScrollStop')}
-				spacing={ri.scale(number('spacing', 0))}
-				style={style.list}
-			/>
+			<SwitchItem {...props} onToggle={this.onToggle} selected={this.state.selected}>
+				{this.props.children}
+			</SwitchItem>
 		);
 	}
 }
@@ -89,8 +77,19 @@ storiesOf('VirtualList')
 	.addWithInfo(
 		'with more items',
 		() => {
+			const itemSize = ri.scale(number('itemSize', 72));
 			return (
-				<StatefulVirtualList />
+				<VirtualList
+					component={renderItem(itemSize)}
+					data={items}
+					dataSize={number('dataSize', items.length)}
+					focusableScrollbar={nullify(boolean('focusableScrollbar', false))}
+					itemSize={itemSize}
+					onScrollStart={action('onScrollStart')}
+					onScrollStop={action('onScrollStop')}
+					spacing={ri.scale(number('spacing', 0))}
+					style={style.list}
+				/>
 			);
 		},
 		{propTables: [Config]}
