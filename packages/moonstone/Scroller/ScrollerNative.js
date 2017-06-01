d4d8d24999c773b2c8f3f9fa2f85d5fabe5097c8
Seungcheon Baek <sc.baek@lge.com>
2017-06-01 15:52:56 +0900

    Update document for ScrollerNative
    Update document along with PLAT-40386.
    
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- packages/moonstone/Scroller/ScrollerNative.js
+++ packages/moonstone/Scroller/ScrollerNative.js
@@ -36,6 +36,37 @@ const ScrollerNative = SpotlightContainerDecorator(
 
 // Docs for ScrollerNative
 /**
+ * The callback function which is called for linking scrollTo function.
+ * You should specify a callback function as the value of this prop
+ * to use scrollTo feature.
+ *
+ * The scrollTo function passed to the parent component requires below as an argument.
+ * - {position: {x, y}} - You can set a pixel value for x and/or y position
+ * - {align} - You can set one of values below for align
+ *   `'left'`, `'right'`, `'top'`, `'bottom'`,
+ *   `'topleft'`, `'topright'`, `'bottomleft'`, and `'bottomright'`.
+ * - {index} - You can set an index of specific item. (`0` or positive integer)
+ *   This option is available for only VirtualList kind.
+ * - {node} - You can set a node to scroll
+ * - {animate} - When `true`, scroll occurs with animation.
+ *   Set it to `false`, if you want scrolling without animation.
+ * - {indexToFocus} - Deprecated: Use `focus` insead.
+ * - {focus} - Set it `true`, if you want the item to be focused after scroll.
+ *   This option is only valid when you scroll by `index` or `node`.
+ *
+ * @name cbScrollTo
+ * @type {Function}
+ * @memberof moonstone/Scroller.ScrollerNative
+ * @example
+ *	// If you set cbScrollTo prop like below;
+ *	cbScrollTo: (fn) => {this.scrollTo = fn;}
+ *	// You can simply call like below;
+ *	this.scrollTo({align: 'top'}); // scroll to the top
+ * @instance
+ * @public
+ */
+
+/**
  * When `true`, allows 5-way navigation to the scrollbar controls. By default, 5-way will
  * not move focus to the scrollbar controls.
  *
@@ -52,6 +83,60 @@ const ScrollerNative = SpotlightContainerDecorator(
  * @name direction
  * @type {String}
  * @default 'both'
+ * @memberof moonstone/Scroller.ScrollerNative
+ * @instance
+ * @public
+ */
+
+/**
+ * Specifies how to show horizontal scrollbar. Acceptable values are `'auto'`,
+ * `'visible'`, and `'hidden'`.
+ *
+ * @name horizontalScrollbar
+ * @type {String}
+ * @default 'auto'
+ * @memberof moonstone/Scroller.ScrollerNative
+ * @instance
+ * @public
+ */
+
+/**
+ * Called when scrolling
+ *
+ * @name onScroll
+ * @type {Function}
+ * @memberof moonstone/Scroller.ScrollerNative
+ * @instance
+ * @public
+ */
+
+/**
+ * Called when scroll starts
+ *
+ * @name onScrollStart
+ * @type {Function}
+ * @memberof moonstone/Scroller.ScrollerNative
+ * @instance
+ * @public
+ */
+
+/**
+ * Called when scroll stops
+ *
+ * @name onScrollStop
+ * @type {Function}
+ * @memberof moonstone/Scroller.ScrollerNative
+ * @instance
+ * @public
+ */
+
+/**
+ * Specifies how to show vertical scrollbar. Acceptable values are `'auto'`,
+ * `'visible'`, and `'hidden'`.
+ *
+ * @name verticalScrollbar
+ * @type {String}
+ * @default 'auto'
  * @memberof moonstone/Scroller.ScrollerNative
  * @instance
  * @public
