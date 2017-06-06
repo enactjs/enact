f2ecea624be624577a982e392262482e17388517
Dave Freeman <dave.freeman@lge.com>
2017-06-06 16:09:03 -0500

    add webos variant to platform sample

--- packages/sampler/stories/moonstone-stories/Platform.js
+++ packages/sampler/stories/moonstone-stories/Platform.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import platform from '@enact/core/platform';
+import webosPlatform from '@enact/webos/platform';
 import {storiesOf} from '@kadira/storybook';
+
+const {gesture, platformName, touch} = platform;
+const version = platform[platformName];
+
+const deviceType = () => {
+	let device = '';
+	for (let p in webosPlatform) {
+		if (webosPlatform[p]) {
+			device = p;
+		}
+	}
+	return device;
+};
 
 storiesOf('Platform')
 	.addWithInfo(
 		' ',
 		'Detection',
-		() => {
-			const {gesture, platformName: name, touch} = platform;
-			const version = platform[name];
-			return (
-				<div>
-					Detected: {name}
-					<br />
-					Version: {version}
-					<br />
-					Gesture support: {gesture ? 'true' : 'false'}
-					<br />
-					Touch support: {touch ? 'true' : 'false'}
-				</div>
-			);
-		}
+		() => (
+			<div>
+				Detected: {platformName} {platform.webos ? `(${deviceType()})` : ''}
+				<br />
+				Version: {version}
+				<br />
+				Gesture support: {gesture ? 'true' : 'false'}
+				<br />
+				Touch support: {touch ? 'true' : 'false'}
+			</div>
+		)
 	);
 
