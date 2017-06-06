2d8031eb88b3daf39da208bc34c040a90c83986f
Stephen Choi <stephen.choi@lge.com>
2017-06-05 17:13:58 -0700

    Add check for window object
    Enyo-DCO-1.1-Signed-off-by: Stephen Choi <stephen.choi@lge.com>

--- packages/ui/Cancelable/modalHandler.js
+++ packages/ui/Cancelable/modalHandler.js
@@ -29,7 +29,7 @@ const dispatchToModals = (ev) => {
  * @private
  */
 const addModal = (obj) => {
-	if (modals.push(obj) === 1) {
+	if (modals.push(obj) === 1 && typeof window === 'object') {
 		on('keyup', dispatchToModals, window);
 	}
 };
@@ -48,7 +48,7 @@ const removeModal = (obj) => {
 		modals.splice(index, 1);
 	}
 
-	if (modals.length === 0) {
+	if (modals.length === 0 && typeof window === 'object') {
 		off('keyup', dispatchToModals, window);
 	}
 };
