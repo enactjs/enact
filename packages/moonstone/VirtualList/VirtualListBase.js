2148facd85e24225269a0ca0e8611b92136c73c9
Ryan Duffy <ryan.duffy@lge.com>
2017-06-07 15:36:42 -0500

    add support for last-focused to VirtualList
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- packages/moonstone/VirtualList/VirtualListBase.js
+++ packages/moonstone/VirtualList/VirtualListBase.js
@@ -11,11 +11,14 @@ import {is} from '@enact/core/keymap';
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import Spotlight from '@enact/spotlight';
+import Spottable from '@enact/spotlight/Spottable';
 import SpotlightContainerDecorator from '@enact/spotlight/SpotlightContainerDecorator';
 
 import {dataIndexAttribute, Scrollable} from '../Scroller/Scrollable';
 
 import css from './ListItem.less';
+
+const SpotlightPlaceholder = Spottable('div');
 
 const
 	dataContainerMutedAttribute = 'data-container-muted',
@@ -107,6 +110,14 @@ class VirtualListCore extends Component {
 		 * @public
 		 */
 		data: PropTypes.any,
+
+		/**
+		 * Spotlight container Id
+		 *
+		 * @type {String}
+		 * @private
+		 */
+		'data-container-id': PropTypes.string, // eslint-disable-line react/sort-prop-types
 
 		/**
 		 * Size of the data.
@@ -221,6 +232,17 @@ class VirtualListCore extends Component {
 		} else if (hasDataChanged) {
 			this.updateStatesAndBounds(nextProps);
 		}
+
+		const placeholder = this.getPlaceholder();
+
+		if (placeholder) {
+			const index = placeholder.dataset.index;
+
+			if (index) {
+				this.lastFocusedIndex = parseInt(index);
+				this.restoreLastFocused = true;
+			}
+		}
 	}
 
 	shouldComponentUpdate (nextProps, nextState) {
@@ -229,6 +251,15 @@ class VirtualListCore extends Component {
 			return false;
 		}
 		return true;
+	}
+
+	componentDidUpdate () {
+		if (this.restoreLastFocused && !this.getPlaceholder()) {
+			const containerId = this.props['data-container-id'];
+			this.restoreLastFocused = !Spotlight.focus(
+				`[data-container-id="${containerId}"] [data-index="${this.lastFocusedIndex}"]`
+			);
+		}
 	}
 
 	componentWillUnmount () {
@@ -278,6 +309,15 @@ class VirtualListCore extends Component {
 	isVertical = () => this.isPrimaryDirectionVertical
 
 	isHorizontal = () => !this.isPrimaryDirectionVertical
+
+	getPlaceholder () {
+		const current = Spotlight.getCurrent();
+		if (current && current.dataset.vlPlaceholder && this.containerRef.contains(current)) {
+			return current;
+		}
+
+		return false;
+	}
 
 	getScrollBounds = () => this.scrollBounds
 
@@ -720,7 +760,9 @@ class VirtualListCore extends Component {
 
 		return (
 			<div {...props} ref={this.initContainerRef}>
-				{cc}
+				{cc.length ? cc : (
+					<SpotlightPlaceholder data-vl-placeholder />
+				)}
 			</div>
 		);
 	}
@@ -738,7 +780,45 @@ class VirtualListCore extends Component {
  * @ui
  * @private
  */
-const VirtualListBase = SpotlightContainerDecorator({restrict: 'self-first'}, Scrollable(VirtualListCore));
+const VirtualListBase = SpotlightContainerDecorator(
+	{
+		enterTo: 'last-focused',
+		/*
+		 * Returns the data-index as the key for last focused
+		 */
+		lastFocusedPersist: (node) => {
+			const indexed = node.dataset.index ? node : node.closest('[data-index]');
+
+			if (indexed) {
+				return {
+					container: false,
+					element: true,
+					key: indexed.dataset.index
+				};
+			}
+		},
+		/*
+		 * Restores the data-index into the placeholder if its the only element. Tries to find a
+		 * matching child otherwise.
+		 */
+		lastFocusedRestore: ({key}, all) => {
+			if (all.length === 1 && 'vlPlaceholder' in all[0].dataset) {
+				all[0].dataset.index = key;
+
+				return all[0];
+			}
+
+			return all.reduce((focused, node) => {
+				return focused || node.dataset.index === key && node;
+			}, null);
+		},
+		preserveId: true,
+		restrict: 'self-first'
+	},
+	Scrollable(
+		VirtualListCore
+	)
+);
 
 export default VirtualListBase;
 export {gridListItemSizeShape, VirtualListCore, VirtualListBase};
