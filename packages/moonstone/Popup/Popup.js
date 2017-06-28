3c4b0c484a84e7a9faec7e4d5d729ec66f25282f
Dave Freeman <dave.freeman@lge.com>
2017-06-28 18:41:04 -0500

    fix lint error
    oops!

--- packages/moonstone/Popup/Popup.js
+++ packages/moonstone/Popup/Popup.js
@@ -355,7 +355,7 @@ class Popup extends React.Component {
 	}
 
 	handleKeyDown = (ev) => {
-		const {onClose, onKeyDown, spotlightRestrict} = this.props;
+		const {onClose, spotlightRestrict} = this.props;
 		const keyCode = ev.keyCode;
 		const direction = getDirection(keyCode);
 		const spottables = Spotlight.getSpottableDescendants(this.state.containerId).length;
