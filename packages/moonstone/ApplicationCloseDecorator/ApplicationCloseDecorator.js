a2e0c44af860db7e8b66d01a3e0122a5440abefc
Stephen Choi <stephen.choi@lge.com>
2017-07-06 16:17:10 -0700

    Add hasCloseButton contextTypes
    Enyo-DCO-1.1-Signed-off-by: Stephen Choi <stephen.choi@lge.com>

--- packages/moonstone/ApplicationCloseDecorator/ApplicationCloseDecorator.js
+++ packages/moonstone/ApplicationCloseDecorator/ApplicationCloseDecorator.js
@@ -3,11 +3,27 @@
  */
 
 import hoc from '@enact/core/hoc';
-import kind from '@enact/core/kind';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import ApplicationCloseButton from './ApplicationCloseButton';
+
+/**
+ * `contextTypes` is an object that exports the default context validation rules. These must be applied
+ * to any child components that wish to receive the ApplicationCloseDecorator context.
+ *
+ * ```
+ * import {contextTypes} from '@enact/moonstone/ApplicationCloseDecorator';
+ * ...
+ * myComponent.contextTypes = contextTypes;
+ * ```
+ *
+ * @memberof moonstone/ApplicationCloseDecorator
+ * @public
+ */
+const contextTypes = {
+	hasCloseButton: PropTypes.bool
+};
 
 /**
  * Higher-order Component that adds a ApplicationCloseDecorator adjacent to wrapped component.
@@ -18,11 +34,13 @@ import ApplicationCloseButton from './ApplicationCloseButton';
  * @public
  */
 const ApplicationCloseDecorator = hoc((config, Wrapped) => {
+	return class extends React.Component {
 
-	return kind({
-		name: 'ApplicationCloseDecorator',
+		displayName: 'ApplicationCloseDecorator'
 
-		propTypes: {
+		static childContextTypes = contextTypes
+
+		static propTypes = /** @lends moonstone/ApplicationCloseDecorator.ApplicationCloseDecorator.prototype */ {
 			/**
 			 * A function to run when app close button is clicked
 			 *
@@ -30,9 +48,16 @@ const ApplicationCloseDecorator = hoc((config, Wrapped) => {
 			 * @public
 			 */
 			onApplicationClose: PropTypes.func
-		},
+		}
 
-		render: ({onApplicationClose, ...rest}) => {
+		getChildContext () {
+			return {
+				hasCloseButton: true
+			};
+		}
+
+		render () {
+			const {onApplicationClose, ...rest} = this.props;
 			return (
 				<div>
 					<Wrapped {...rest} />
@@ -40,7 +65,8 @@ const ApplicationCloseDecorator = hoc((config, Wrapped) => {
 				</div>
 			);
 		}
-	});
+	};
 });
 
 export default ApplicationCloseDecorator;
+export {ApplicationCloseDecorator, contextTypes};
