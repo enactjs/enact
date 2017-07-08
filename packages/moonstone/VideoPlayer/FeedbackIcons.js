071123c0e9b1decd18feac002d4b1f9b6da63aa8
Stephen Choi <viodragon2@gmail.com>
2017-07-07 17:44:16 -0700

    PLAT-42370: Change pause icon not to time out in VideoPlayer (#947)
    * PLAT-42370: Change pause icon not to time out
    
    Enyo-DCO-1.1-Signed-off-by: Stephen Choi <stephen.choi@lge.com>
    
    * Update CHANGELOG
    
    Enyo-DCO-1.1-Signed-off-by: Stephen Choi <stephen.choi@lge.com>

--- packages/moonstone/VideoPlayer/FeedbackIcons.js
+++ packages/moonstone/VideoPlayer/FeedbackIcons.js
@@ -3,7 +3,7 @@
 
 export default {
 	play          : {icon: 'play',           position: 'after',   allowHide: true,   message: null},
-	pause         : {icon: 'pause',          position: 'after',   allowHide: true,   message: null},
+	pause         : {icon: 'pause',          position: 'after',   allowHide: false,  message: null},
 	rewind        : {icon: 'backward',       position: 'before',  allowHide: false,  message: 'x'},
 	slowRewind    : {icon: 'pausebackward',  position: 'before',  allowHide: false,  message: 'x'},
 	fastForward   : {icon: 'forward',        position: 'after',   allowHide: false,  message: 'x'},
