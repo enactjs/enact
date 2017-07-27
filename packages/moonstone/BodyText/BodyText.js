ae3d364367e6113b08e65c79af6d96a29a1e1278
Blake Stephens <blake.stephens@lge.com>
2017-07-26 17:29:14 -0700

    BodyText
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

---
+++ packages/moonstone/BodyText/BodyText.js
@@ -0,0 +1,57 @@
+/**
+ * Exports the {@link moonstone/BodyText.BodyText} and {@link moonstone/BodyText.BodyTextBase}
+ * components.  The default export is {@link moonstone/BodyText.BodyTextBase}.
+ *
+ * @module moonstone/BodyText
+ */
+
+import factory from '@enact/core/factory';
+import {BodyTextFactory as UiBodyTextFactory} from '@enact/ui/BodyText';
+
+import Skinnable from '../Skinnable';
+
+import componentCss from './BodyText.less';
+
+/**
+ * {@link moonstone/BodyText.BodyText} is a stateless BodyText with Moonstone styling
+ * applied.
+ *
+ * @class BodyText
+ * @memberof moonstone/BodyText
+ * @ui
+ * @public
+ */
+const BodyTextBaseFactory = factory({css: componentCss}, ({css}) => {
+	// console.group('Moon BodyText');
+	// for (const key in componentCss) {
+	// 	if (componentCss[key] !== css[key]) console.log(key, ':', componentCss[key], '   VS   ', css[key]);
+	// }
+	// console.groupEnd();
+	return UiBodyTextFactory({
+	/* Replace classes in this step */
+		css: /** @lends moonstone/Button.BodyTextFactory.prototype */ {
+			...componentCss,
+			// Include the component class name so it too may be overridden.
+			bodyText: css.bodyText
+		}
+	});
+});
+
+const BodyTextBase = BodyTextBaseFactory();
+
+const BodyText = Skinnable(
+	BodyTextBase
+);
+
+const BodyTextFactory = (props) => Skinnable(
+	BodyTextBaseFactory(props)
+);
+
+
+export default BodyText;
+export {
+	BodyText,
+	// BodyTextBase,
+	BodyTextFactory,
+	BodyTextBaseFactory
+};
--- packages/ui/BodyText/BodyText.js
+++ packages/ui/BodyText/BodyText.js
@@ -5,59 +5,77 @@
  * @module moonstone/BodyText
  */
 
+import factory from '@enact/core/factory';
 import kind from '@enact/core/kind';
 import React from 'react';
 import PropTypes from 'prop-types';
-import Skinnable from '../Skinnable';
 
-import css from './BodyText.less';
+import componentCss from './BodyText.less';
 
 /**
- * {@link moonstone/BodyText.BodyText} is a stateless BodyText with Moonstone styling
- * applied.
+ * {@link ui/BodyText.BodyTextBaseFactory} is Factory wrapper around {@link ui/BodyText.BodyTextBase}
+ * that allows overriding certain classes at design time. The following are properties of the `css`
+ * member of the argument to the factory.
  *
- * @class BodyText
- * @memberof moonstone/BodyText
+ * @class BodyTextBaseFactory
+ * @memberof ui/BodyText
+ * @factory
  * @ui
  * @public
  */
-const BodyTextBase = kind({
-	name: 'BodyText',
+const BodyTextBaseFactory = factory({css: componentCss}, ({css}) => {
+	/**
+	 * {@link moonstone/BodyText.BodyText} is a stateless BodyText with Moonstone styling
+	 * applied.
+	 *
+	 * @class BodyText
+	 * @memberof moonstone/BodyText
+	 * @ui
+	 * @public
+	 */
+	return kind({
+		name: 'BodyText',
 
-	propTypes: /** @lends moonstone/BodyText.BodyText.prototype */ {
-		/**
-		 * If `true`, text content is centered; otherwise, it is left-aligned.
-		 *
-		 * @type {Boolean}
-		 * @default false
-		 * @public
-		 */
-		centered: PropTypes.bool
-	},
+		propTypes: /** @lends moonstone/BodyText.BodyText.prototype */ {
+			/**
+			 * If `true`, text content is centered; otherwise, it is left-aligned.
+			 *
+			 * @type {Boolean}
+			 * @default false
+			 * @public
+			 */
+			centered: PropTypes.bool
+		},
 
-	defaultProps: {
-		centered: false
-	},
+		defaultProps: {
+			centered: false
+		},
 
-	styles: {
-		css,
-		className: 'bodyText'
-	},
+		styles: {
+			css,
+			className: 'bodyText'
+		},
 
-	computed: {
-		className: ({centered, styler}) => styler.append({centered})
-	},
+		computed: {
+			className: ({centered, styler}) => styler.append({centered})
+		},
 
-	render: (props) => {
-		delete props.centered;
+		render: (props) => {
+			delete props.centered;
 
-		return (
-			<p {...props} />
-		);
-	}
+			return (
+				<p {...props} />
+			);
+		}
+	});
 });
 
-const BodyText = Skinnable(BodyTextBase);
+const BodyTextBase = BodyTextBaseFactory();
 
-export default BodyText;
-export {BodyText, BodyTextBase};
+export default BodyTextBase;
+export {
+	BodyTextBase as BodyText,
+	BodyTextBase,
+	BodyTextBaseFactory as BodyTextFactory,
+	BodyTextBaseFactory
+};
