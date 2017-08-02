491990587ee6b8188f76a3b25aa4e932630919b3
Ryan Duffy <ryan.duffy@lge.com>
2017-08-02 16:20:00 -0500

    allow empty string for more labels
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- packages/moonstone/VideoPlayer/MediaControls.js
+++ packages/moonstone/VideoPlayer/MediaControls.js
@@ -164,9 +164,9 @@ const MediaControls = kind({
 		moreIcon: ({showMoreComponents}) => showMoreComponents ? 'arrowshrinkleft' : 'ellipsis',
 		moreIconLabel: ({moreButtonCloseLabel, moreButtonLabel, showMoreComponents}) => {
 			if (showMoreComponents) {
-				return moreButtonCloseLabel || $L('Back');
+				return moreButtonCloseLabel === '' ? moreButtonCloseLabel : $L('Back');
 			} else {
-				return moreButtonLabel || $L('More');
+				return moreButtonLabel === '' ? moreButtonLabel : $L('More');
 			}
 		},
 		playPauseIcon: ({paused, pauseIcon, playIcon}) => (paused ? playIcon : pauseIcon),
