d338abe5671d90eecfcbef1b2052a90b8b48ec8b
Roy Sutton <roy.sutton@lge.com>
2017-08-09 16:47:15 -0400

    Fix doc parse errors
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- packages/moonstone/BodyText/BodyText.js
+++ packages/moonstone/BodyText/BodyText.js
@@ -14,6 +14,38 @@ import Skinnable from '../Skinnable';
 import componentCss from './BodyText.less';
 
 /**
+ * {@link moonstone/BodyText.BodyTextBaseFactory} is a factory for customizing BodyTextBase.
+ *
+ * @class BodyTextBaseFactory
+ * @memberof moonstone/BodyText
+ * @factory
+ * @public
+ */
+const BodyTextBaseFactory = factory({css: componentCss}, ({css}) => {
+	// diffClasses('Moon BodyText', componentCss, css);
+
+	return UiBodyTextFactory({
+		/* Replace classes in this step */
+		css: /** @lends moonstone/BodyText.BodyTextBaseFactory.prototype */ {
+			...componentCss,
+			// Include the component class name so it too may be overridden.
+			bodyText: css.bodyText
+		}
+	});
+});
+
+/**
+ * {@link moonstone/BodyText.BodyTextBase} is a stateless BodyText with Moonstone styling
+ * applied, without HOCs applied.
+ *
+ * @class BodyTextBase
+ * @memberof moonstone/BodyText
+ * @factory
+ * @public
+ */
+const BodyTextBase = BodyTextBaseFactory();
+
+/**
  * {@link moonstone/BodyText.BodyText} is a stateless BodyText with Moonstone styling
  * applied.
  *
@@ -22,25 +54,18 @@ import componentCss from './BodyText.less';
  * @ui
  * @public
  */
-const BodyTextBaseFactory = factory({css: componentCss}, ({css}) => {
-	// diffClasses('Moon BodyText', componentCss, css);
-
-	return UiBodyTextFactory({
-		/* Replace classes in this step */
-		css: /** @lends moonstone/BodyText.BodyTextFactory.prototype */ {
-			...componentCss,
-			// Include the component class name so it too may be overridden.
-			bodyText: css.bodyText
-		}
-	});
-});
-
-const BodyTextBase = BodyTextBaseFactory();
-
 const BodyText = Skinnable(
 	BodyTextBase
 );
 
+/**
+ * {@link moonstone/BodyText.BodyTextFactory} is a factory for customizing BodyText.
+ *
+ * @class BodyTextFactory
+ * @memberof moonstone/BodyText
+ * @factory
+ * @public
+ */
 const BodyTextFactory = (props) => Skinnable(
 	BodyTextBaseFactory(props)
 );
