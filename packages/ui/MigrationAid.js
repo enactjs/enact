28ff85a5ee9d0715de01d4515adaa9fa1253a6e2
Blake Stephens <blake.stephens@lge.com>
2017-08-11 18:07:54 -0700

    Don't lint warn on MigrationAid for consoles.
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- packages/ui/MigrationAid.js
+++ packages/ui/MigrationAid.js
@@ -6,15 +6,15 @@
 
 
 const diffClasses = function (name, componentClasses, combinedClasses) {
-	console.group(name);
+	console.group(name);	// eslint-disable-line no-console
 	for (const key in componentClasses) {
 		if (componentClasses[key] !== combinedClasses[key]) {
-			console.log('Component: ' + key + ':', componentClasses[key]);
-			console.log(' Combined: ' + key + ':', combinedClasses[key]);
+			console.log('Component: ' + key + ':', componentClasses[key]);	// eslint-disable-line no-console
+			console.log(' Combined: ' + key + ':', combinedClasses[key]);	// eslint-disable-line no-console
 		}
 	}
-	console.log('Original CombinedClasses:', combinedClasses);
-	console.groupEnd();
+	console.log('Original CombinedClasses:', combinedClasses);	// eslint-disable-line no-console
+	console.groupEnd();	// eslint-disable-line no-console
 };
 
 
