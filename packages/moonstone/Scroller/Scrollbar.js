enactjs/enact

commit 03ba715b7cb9b00f787c8123465fbabfbed73366

PLAT-39161: Apply CSS flexbox to two scrollbars and content in Scrollable (#903)
* flexbox] apply flexbox to Scrollable

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* flexbox] apply Flexbox.less; cleanup

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* flexbox] Cleanup

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* flexbox] Refactor Scrollbar

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* flexbox] Cleanup

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* flexbox] Add JS docs; Add setCSSVariable, toPercentage utils

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* flexbox] Cleanup

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* Cleanup and simplification of css variable :partyparrot: (#904)

* Cleanup and simplification of css variable

Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

* Defined getScrollThumbRef prop

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* Removed Flexbox.less and setCSSVariable, toPercentage in util

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* flexbox] Make ScrollThumb as simple as possible with kind

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* flexbox] cleanup

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* flexbox] delegate some CSS styles from Scrollable to Scrollbar

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* Update CHANGELOG

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* flexbox] Apply flexbox to VirtualListNative, ScrollerNative

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* flexbox] cleanup

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* flexbox] fixed margin for scrollbar margin

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

* Removed useless CSS class

Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

packages/moonstone/Scroller/Scrollbar.js
@@ -2,52 +2,49 @@ import {off, on} from '@enact/core/dispatcher';
 import {Announce} from '@enact/ui/AnnounceDecorator';
 import ApiDecorator from '@enact/core/internal/ApiDecorator';
 import classNames from 'classnames';
-import {contextTypes} from '@enact/i18n/I18nDecorator';
 import {Job} from '@enact/core/util';
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
-import ri from '@enact/ui/resolution';
+import React, {PureComponent} from 'react';
 import Spotlight from '@enact/spotlight';
+import ri from '@enact/ui/resolution';
 
 import $L from '../internal/$L';
 import DisappearSpotlightDecorator from '../internal/DisappearSpotlightDecorator';
 
 import ScrollButton from './ScrollButton';
+import ScrollThumb from './ScrollThumb';
 
 import css from './Scrollbar.less';
 
 const
-	verticalProperties = {
-		matrix: (position, scaledSize, natualSize) => (
-			'matrix3d(1, 0, 0, 0, 0,' + (scaledSize / natualSize) + ', 0, 0, 0, 0, 1, 0, 0, ' + position + ', 1, 1)'
-		),
-		scrollbarClass: css.scrollbarContainerVColumn,
-		sizeProperty: 'clientHeight',
-		thumbClass: css.scrollbarVthumb
-	},
-	horizontalProperties = {
-		matrix: (position, scaledSize, natualSize) => (
-			'matrix3d(' + (scaledSize / natualSize) + ', 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, ' + position + ', 0, 1, 1)'
-		),
-		scrollbarClass: css.scrollbarContainerHColumn,
-		sizeProperty: 'clientWidth',
-		thumbClass: css.scrollerHthumb
-	},
 	nop = () => {},
-	minThumbSize = 20,
-	prepareButton = (isPrev) => (isVertical, rtl) => {
+	minThumbSize = 18, // Size in pixels
+	prepareButton = (isPrev) => (isVertical) => {
 		let direction;
 
 		if (isVertical) {
-			direction = (isPrev) ? 'up' : 'down';
-		} else { /* if rtl is true, arrows are replaced each other. So, rtl XOR isPrev: right arrow, otherwise left arrow */
-			direction = (rtl === isPrev) ? 'right' : 'left';
+			direction = isPrev ? 'up' : 'down';
+		} else {
+			direction = isPrev ? 'left' : 'right';
 		}
 
 		return 'arrowsmall' + direction;
 	},
 	preparePrevButton = prepareButton(true),
 	prepareNextButton = prepareButton(false);
+
+/**
+ * Set CSS Varaible value.
+ *
+ * @method
+ * @memberof core/util
+ * @param {Node} element - Node.
+ * @param {String} variable - CSS Variable property.
+ * @param {String} value - CSS Variable value.
+ */
+const setCSSVariable = (element, variable, value) => {
+	element.style.setProperty(variable, value);
+};
 
 /**
  * {@link moonstone/Scroller.Scrollbar} is a Scrollbar with Moonstone styling.
@@ -58,7 +55,7 @@ const
  * @ui
  * @private
  */
-class ScrollbarBase extends Component {
+class ScrollbarBase extends PureComponent {
 	static displayName = 'Scrollbar'
 
 	static propTypes = /** @lends moonstone/Scroller.Scrollbar.prototype */ {
@@ -69,6 +66,14 @@ class ScrollbarBase extends Component {
 		 * @public
 		 */
 		announce: PropTypes.func,
+
+		/**
+		 * If `true`, add the corner between vertical and horizontal scrollbars.
+		 *
+		 * @type {Booelan}
+		 * @public
+		 */
+		corner: PropTypes.bool,
 
 		/**
 		 * Specifies to reflect scrollbar's disabled property to the paging controls.
@@ -129,9 +134,8 @@ class ScrollbarBase extends Component {
 		vertical: PropTypes.bool
 	}
 
-	static contextTypes = contextTypes
-
 	static defaultProps = {
+		corner: false,
 		onNextScroll: nop,
 		onPrevScroll: nop,
 		vertical: true
@@ -140,16 +144,10 @@ class ScrollbarBase extends Component {
 	constructor (props) {
 		super(props);
 
-		const
-			{vertical} = props,
-			{scrollbarClass, thumbClass, sizeProperty, matrix} = ((vertical) ? verticalProperties : horizontalProperties);
-
 		this.state = {
 			prevButtonDisabled: true,
 			nextButtonDisabled: true
 		};
-
-		this.scrollbarInfo = {scrollbarClass, thumbClass, sizeProperty, matrix};
 
 		this.initAnnounceRef = this.initRef('announceRef');
 		this.initContainerRef = this.initRef('containerRef');
@@ -177,14 +175,10 @@ class ScrollbarBase extends Component {
 		this.setIgnoreMode(false); // To remove event handler
 	}
 
-	autoHide = true
-	thumbSize = 0
 	minThumbSizeRatio = 0
-	trackSize = 0
 	pressed = false
 	ignoreMode = false
 	buttonToFocus = null
-
 	// component refs
 	containerRef = null
 	thumbRef = null
@@ -250,64 +244,41 @@ class ScrollbarBase extends Component {
 		}
 	}
 
-	update (bounds) {
+	update = (bounds) => {
 		const
-			{trackSize, minThumbSizeRatio} = this,
 			{vertical} = this.props,
-			{rtl} = this.context,
 			{clientWidth, clientHeight, scrollWidth, scrollHeight, scrollLeft, scrollTop} = bounds,
-			scrollLeftRtl = rtl ? (scrollWidth - clientWidth - scrollLeft) : scrollLeft,
-			thumbSizeRatioBase = vertical ?
-				Math.min(1, clientHeight / scrollHeight) :
-				Math.min(1, clientWidth / scrollWidth);
-		let
-			thumbSizeRatio = Math.max(minThumbSizeRatio, thumbSizeRatioBase),
-			thumbPositionRatio = vertical ?
-				scrollTop / (scrollHeight - clientHeight) :
-				scrollLeftRtl / (scrollWidth - clientWidth),
-			thumbSize, thumbPosition;
+			clientSize = vertical ? clientHeight : clientWidth,
+			scrollSize = vertical ? scrollHeight : scrollWidth,
+			scrollOrigin = vertical ? scrollTop : scrollLeft,
 
-		// overscroll cases
-		if (thumbPositionRatio < 0) {
-			thumbSizeRatio = Math.max(minThumbSizeRatio, thumbSizeRatio + thumbPositionRatio);
-			thumbPositionRatio = 0;
-		} else if (thumbPositionRatio > 1) {
-			thumbSizeRatio = Math.max(minThumbSizeRatio, thumbSizeRatio + (1 - thumbPositionRatio));
-			thumbPositionRatio = 1;
-		}
+			thumbSizeRatioBase = (clientSize / scrollSize),
+			scrollThumbPositionRatio = (scrollOrigin / (scrollSize - clientSize)),
+			scrollThumbSizeRatio = Math.max(this.minThumbSizeRatio, Math.min(1, thumbSizeRatioBase));
 
-		thumbSize = Math.round(thumbSizeRatio * trackSize);
-		thumbPositionRatio = (vertical || !rtl) ? (thumbPositionRatio * (1 - thumbSizeRatio)) : (thumbPositionRatio * (1 - thumbSizeRatio) - 1);
-		thumbPosition = Math.round(thumbPositionRatio * trackSize);
-
-		this.thumbRef.style.transform = this.scrollbarInfo.matrix(thumbPosition, thumbSize, this.thumbSize);
+		setCSSVariable(this.thumbRef, '--scrollbar-size-ratio', scrollThumbSizeRatio);
+		setCSSVariable(this.thumbRef, '--scrollbar-progress-ratio', scrollThumbPositionRatio);
 		this.updateButtons(bounds);
 	}
 
 	showThumb () {
 		this.hideThumbJob.stop();
 		this.thumbRef.classList.add(css.thumbShown);
-		this.thumbRef.classList.remove(css.thumbHidden);
 	}
 
 	startHidingThumb () {
-		this.hideThumbJob.stop();
-		if (this.autoHide) {
-			this.hideThumbJob.start();
-		}
+		this.hideThumbJob.start();
 	}
 
 	hideThumb = () => {
-		this.thumbRef.classList.add(css.thumbHidden);
 		this.thumbRef.classList.remove(css.thumbShown);
 	}
 
 	hideThumbJob = new Job(this.hideThumb, 200);
 
-	calculateMetrics () {
-		this.thumbSize = this.thumbRef[this.scrollbarInfo.sizeProperty];
-		this.trackSize = this.containerRef[this.scrollbarInfo.sizeProperty];
-		this.minThumbSizeRatio = ri.scale(minThumbSize) / this.trackSize;
+	calculateMetrics = () => {
+		const trackSize = this.containerRef[this.props.vertical ? 'clientHeight' : 'clientWidth'];
+		this.minThumbSizeRatio = ri.scale(minThumbSize) / trackSize;
 	}
 
 	initRef (prop) {
@@ -351,16 +322,19 @@ class ScrollbarBase extends Component {
 
 	render () {
 		const
-			{className, disabled, onNextSpotlightDisappear, onPrevSpotlightDisappear, vertical} = this.props,
+			{className, corner, disabled, onNextSpotlightDisappear, onPrevSpotlightDisappear, vertical} = this.props,
 			{prevButtonDisabled, nextButtonDisabled} = this.state,
-			{rtl} = this.context,
-			{scrollbarClass, thumbClass} = this.scrollbarInfo,
-			scrollbarClassNames = classNames(className, scrollbarClass),
-			prevIcon = preparePrevButton(vertical, rtl),
-			nextIcon = prepareNextButton(vertical, rtl);
+			containerClassName = classNames(
+				className,
+				css.scrollbar,
+				corner ? css.corner : null,
+				vertical ? css.vertical : css.horizontal
+			),
+			prevIcon = preparePrevButton(vertical),
+			nextIcon = prepareNextButton(vertical);
 
 		return (
-			<div ref={this.initContainerRef} className={scrollbarClassNames}>
+			<div ref={this.initContainerRef} className={containerClassName}>
 				<ScrollButton
 					data-scroll-button="previous"
 					direction={vertical ? 'up' : 'left'}
@@ -374,6 +348,11 @@ class ScrollbarBase extends Component {
 				>
 					{prevIcon}
 				</ScrollButton>
+				<ScrollThumb
+					className={css.scrollThumb}
+					getScrollThumbRef={this.initThumbRef}
+					vertical={vertical}
+				/>
 				<ScrollButton
 					data-scroll-button="next"
 					direction={vertical ? 'down' : 'right'}
@@ -387,7 +366,6 @@ class ScrollbarBase extends Component {
 				>
 					{nextIcon}
 				</ScrollButton>
-				<div className={thumbClass} ref={this.initThumbRef} />
 				<Announce ref={this.initAnnounceRef} />
 			</div>
 		);

packages/moonstone/Scroller/ScrollThumb.js
@@ -1,0 +1,60 @@
+import kind from '@enact/core/kind';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import css from './ScrollThumb.less';
+
+/**
+ * {@link moonstone/ScrollThumb.ScrollThumb} is a stateless ScrollThumb with Moonstone styling appied.
+ *
+ * @class ScrollThumb
+ * @memberof moonstone/ScrollThumb
+ * @ui
+ * @public
+ */
+const ScrollThumb = kind({
+	name: 'ScrollThumb',
+
+	propTypes: /** @lends moonstone/ScrollThumb.ScrollThumb.prototype */ {
+		/**
+		 * The function to pass a wrapped ref.
+		 *
+		 * @type {Function}
+		 * @public
+		 */
+		getScrollThumbRef: PropTypes.func,
+
+		/**
+		 * If `true`, the scrollbar will be oriented vertically.
+		 *
+		 * @type {Boolean}
+		 * @public
+		 */
+		vertical: PropTypes.bool
+	},
+
+	defaultProps: {
+		getScrollThumbRef: null,
+		vertical: true
+	},
+
+	styles: {
+		css,
+		className: 'scrollThumb'
+	},
+
+	computed: {
+		className: ({vertical, styler}) => styler.append({vertical})
+	},
+
+	render: ({getScrollThumbRef, ...rest}) => {
+		delete rest.vertical;
+
+		return <div {...rest} ref={getScrollThumbRef} />;
+	}
+});
+
+export default ScrollThumb;
+export {
+	ScrollThumb
+};

packages/moonstone/Scroller/ScrollThumb.less
@@ -1,0 +1,46 @@
+.scrollThumb {
+	/* Set these to reasonable default values */
+	--scrollbar-size-ratio: 1;
+	--scrollbar-progress-ratio: 0;
+	--scrollbar-size: ~'calc(100% * var(--scrollbar-size-ratio))';
+	--scrollbar-progress: ~'calc(((1 - var(--scrollbar-size-ratio)) * var(--scrollbar-progress-ratio)) * 100%)';
+
+	position: relative;
+	width: 3px;
+	height: 3px;
+	margin: auto;
+	opacity: 0;
+	flex: auto;
+
+	-webkit-transition: opacity 100ms linear;
+	transition: opacity 100ms linear;
+
+	will-change: transform, top, left, right;
+
+	&::before {
+		background: fade(#666, 75%);
+		display: block;
+		position: absolute;
+		content: '';
+		width: 100%;
+		height: 100%;
+
+		will-change: transform, top, left, right;
+	}
+
+	&.vertical {
+		&::before {
+			height: var(--scrollbar-size);
+			top: var(--scrollbar-progress);
+		}
+	}
+	&:not(.vertical) {
+		&::before {
+			width: var(--scrollbar-size);
+			left: var(--scrollbar-progress);
+			top: 50%;
+
+			transform: translateY(-50%);
+		}
+	}
+}

packages/moonstone/Scroller/Scrollable.js
@@ -913,9 +913,9 @@ const ScrollableHoC = hoc((config, Wrapped) => {
 		getHorizontalScrollbar = (isHorizontalScrollbarVisible, isVerticalScrollbarVisible) => (
 			isHorizontalScrollbarVisible ? (
 				<Scrollbar
-					className={!isVerticalScrollbarVisible ? css.onlyHorizontalScrollbarNeeded : null}
-					disabled={!isHorizontalScrollbarVisible}
 					{...this.horizontalScrollbarProps}
+					corner={isVerticalScrollbarVisible}
+					disabled={!isHorizontalScrollbarVisible}
 				/>
 			) : null
 		)
@@ -923,9 +923,8 @@ const ScrollableHoC = hoc((config, Wrapped) => {
 		getVerticalScrollbar = (isHorizontalScrollbarVisible, isVerticalScrollbarVisible) => (
 			isVerticalScrollbarVisible ? (
 				<Scrollbar
-					className={!isHorizontalScrollbarVisible ? css.onlyVerticalScrollbarNeeded : null}
-					disabled={!isVerticalScrollbarVisible}
 					{...this.verticalScrollbarProps}
+					disabled={!isVerticalScrollbarVisible}
 				/>
 			) : null
 		)
@@ -944,13 +943,7 @@ const ScrollableHoC = hoc((config, Wrapped) => {
 				{isHorizontalScrollbarVisible, isVerticalScrollbarVisible} = this.state,
 				vscrollbar = this.getVerticalScrollbar(isHorizontalScrollbarVisible, isVerticalScrollbarVisible),
 				hscrollbar = this.getHorizontalScrollbar(isHorizontalScrollbarVisible, isVerticalScrollbarVisible),
-				scrollableClasses = classNames(
-					css.scrollable,
-					!(isHorizontalScrollbarVisible || isVerticalScrollbarVisible) ? css.scrollableHiddenScrollbars : null,
-					isHorizontalScrollbarVisible ? null : css.takeAvailableSpaceForVertical,
-					isVerticalScrollbarVisible ? null : css.takeAvailableSpaceForHorizontal,
-					className
-				);
+				scrollableClasses = classNames(css.scrollable, className);
 
 			delete props.cbScrollTo;
 			delete props.className;
@@ -969,14 +962,16 @@ const ScrollableHoC = hoc((config, Wrapped) => {
 					focusableScrollbar={focusableScrollbar}
 					style={style}
 				>
-					<Wrapped
-						{...props}
-						cbScrollTo={this.scrollTo}
-						className={css.container}
-						onScroll={this.handleScroll}
-						ref={this.initChildRef}
-					/>
-					{vscrollbar}
+					<div className={css.container}>
+						<Wrapped
+							{...props}
+							cbScrollTo={this.scrollTo}
+							className={css.content}
+							onScroll={this.handleScroll}
+							ref={this.initChildRef}
+						/>
+						{vscrollbar}
+					</div>
 					{hscrollbar}
 				</ScrollableSpotlightContainer>
 			);

packages/moonstone/Scroller/Scrollable.less
@@ -9,46 +9,22 @@
 
 .scrollable {
 	.scrollableFill;
-	.container {
-		.scrollableFill;
-		background: inherit;
-		overflow: hidden;
-	}
-	.padding-start-end(0, @moon-icon-button-small-size + @moon-spotlight-outset);
 
-	padding-bottom: @moon-icon-button-small-size + @moon-spotlight-outset;
+	display: flex;
 	box-sizing: border-box;
+	flex-direction: column;
 
-	:global(.enact-text-large) & {
-		padding-bottom: @moon-icon-button-small-size-large + @moon-spotlight-outset;
-		padding-left: 0;
-		padding-right: @moon-icon-button-small-size-large + @moon-spotlight-outset;
+	.container {
+		display: flex;
+		width: 100%;
+		height: 100%;
+		flex: auto;
 	}
 
-	:global(.enact-text-large.enact-locale-right-to-left) & {
-		padding-left: @moon-icon-button-small-size-large + @moon-spotlight-outset;
-		padding-right: 0;
+	.content {
+		background: inherit;
+		position: relative;
+		overflow: hidden;
+		flex: auto;
 	}
-
-	div.onlyVerticalScrollbarNeeded {
-		bottom: 0;
-	}
-
-	div.onlyHorizontalScrollbarNeeded {
-		.position-start-end(0, 0);
-	}
-
-	&.takeAvailableSpaceForHorizontal {
-		.padding-start-end(0, 0);
-	}
-
-	&.takeAvailableSpaceForVertical {
-		padding-bottom: 0;
-	}
-}
-
-.scrollableHiddenScrollbars {
-	overflow: hidden;
-	.padding-start-end(0, 0);
-	padding-bottom: 0;
 }

packages/moonstone/Scroller/ScrollableNative.js
@@ -784,9 +784,9 @@ const ScrollableHoC = hoc((config, Wrapped) => {
 		getHorizontalScrollbar = (isHorizontalScrollbarVisible, isVerticalScrollbarVisible) => (
 			isHorizontalScrollbarVisible ? (
 				<Scrollbar
-					className={!isVerticalScrollbarVisible ? css.onlyHorizontalScrollbarNeeded : null}
-					disabled={!isHorizontalScrollbarVisible}
 					{...this.horizontalScrollbarProps}
+					corner={isVerticalScrollbarVisible}
+					disabled={!isHorizontalScrollbarVisible}
 				/>
 			) : null
 		)
@@ -794,9 +794,8 @@ const ScrollableHoC = hoc((config, Wrapped) => {
 		getVerticalScrollbar = (isHorizontalScrollbarVisible, isVerticalScrollbarVisible) => (
 			isVerticalScrollbarVisible ? (
 				<Scrollbar
-					className={!isHorizontalScrollbarVisible ? css.onlyVerticalScrollbarNeeded : null}
-					disabled={!isVerticalScrollbarVisible}
 					{...this.verticalScrollbarProps}
+					disabled={!isVerticalScrollbarVisible}
 				/>
 			) : null
 		)
@@ -808,13 +807,7 @@ const ScrollableHoC = hoc((config, Wrapped) => {
 				{isHorizontalScrollbarVisible, isVerticalScrollbarVisible} = this.state,
 				vscrollbar = this.getVerticalScrollbar(isHorizontalScrollbarVisible, isVerticalScrollbarVisible),
 				hscrollbar = this.getHorizontalScrollbar(isHorizontalScrollbarVisible, isVerticalScrollbarVisible),
-				scrollableClasses = classNames(
-					css.scrollable,
-					!(isHorizontalScrollbarVisible || isVerticalScrollbarVisible) ? css.scrollableHiddenScrollbars : null,
-					isHorizontalScrollbarVisible ? null : css.takeAvailableSpaceForVertical,
-					isVerticalScrollbarVisible ? null : css.takeAvailableSpaceForHorizontal,
-					className
-				);
+				scrollableClasses = classNames(css.scrollable, className);
 
 			delete props.cbScrollTo;
 			delete props.className;
@@ -833,8 +826,10 @@ const ScrollableHoC = hoc((config, Wrapped) => {
 					focusableScrollbar={focusableScrollbar}
 					style={style}
 				>
-					<Wrapped {...props} ref={this.initChildRef} cbScrollTo={this.scrollTo} className={css.container} />
-					{vscrollbar}
+					<div className={css.container}>
+						<Wrapped {...props} ref={this.initChildRef} cbScrollTo={this.scrollTo} className={css.content} />
+						{vscrollbar}
+					</div>
 					{hscrollbar}
 				</ScrollableSpotlightContainer>
 			);

packages/moonstone/Scroller/Scrollbar.less
@@ -1,183 +1,68 @@
-@import '../styles/mixins.less';
 @import '../styles/variables.less';
 
-.scrollbarContainerBase {
+.scrollbar {
 	background: inherit;
-	position: absolute;
+	display: flex;
 	border-color: transparent;
-	border-style: solid;
 	visibility: visible;
 	opacity: 1;
+	flex: none;
 
 	transform: translateZ(0);
 	-webkit-transform: translateZ(0);
 	transition: opacity 0.1s linear;
 	-moz-transition: opacity 0.1s linear;
 	-webkit-transition: opacity 0.1s linear;
-}
 
-.scrollbarButtonNoCompositeLayer {
-	&,
-	&:before,
-	&:after,
-	div,
-	span {
-		will-change: auto;
+	&.vertical {
+		flex-direction: column;
 	}
 
-	&:before {
-		content: none;
+	&.horizontal {
+		:global(.enact-locale-right-to-left) & {
+			// Flip the entire bar horizontally in RTL mode, and force it to remain in LTR.
+			direction: ltr;
+			transform: scaleX(-1);
+		}
 	}
 
-	div,
-	span {
-		-webkit-transform: initial;
-		transform: initial;
-	}
-}
+	&.corner {
+		-webkit-padding-end: @moon-icon-button-small-size;
 
-.scrollbarContainerVColumn,
-.scrollbarContainerHColumn {
+		:global(.enact-text-large) & {
+			-webkit-padding-end: @moon-icon-button-small-size-large;
+		}
+	}
+
+	/* ScrollButton */
+	.scrollbarButtonNoCompositeLayer {
+		&,
+		&:before,
+		&:after,
+		div,
+		span {
+			will-change: auto;
+		}
+
+		&:before {
+			content: none;
+		}
+
+		div,
+		span {
+			-webkit-transform: initial;
+			transform: initial;
+		}
+	}
+
 	.scrollButton {
 		.scrollbarButtonNoCompositeLayer;
 		background-color: transparent;
-		position: absolute;
-		width: @moon-icon-button-small-size;
-		height: @moon-icon-button-small-size;
-		padding: 0;
-		border-color: transparent;
 		margin: 0;
-
-		:global(.enact-text-large) & {
-			width: @moon-icon-button-small-size-large;
-			height: @moon-icon-button-small-size-large;
-		}
-	}
-}
-
-.scrollbarContainerVColumn {
-	.scrollbarContainerBase;
-	width: @moon-icon-button-small-size;
-	border-width: @moon-icon-button-small-size 0;
-	top: 0;
-	bottom: @moon-icon-button-small-size;
-
-	.position-start-end(auto, @moon-spotlight-outset);
-
-	:global(.enact-text-large) & {
-		width: @moon-icon-button-small-size-large;
-		border-width: @moon-icon-button-small-size-large 0;
-		bottom: @moon-icon-button-small-size-large;
 	}
 
-	.scrollbarUpButton {
-		top: -@moon-icon-button-small-size;
-
-		:global(.enact-text-large) & {
-			top: -@moon-icon-button-small-size-large;
-		}
+	/* ScrollThumb */
+	.thumbShown {
+		opacity: 1;
 	}
-
-	.scrollbarBottomButton {
-		bottom: -@moon-icon-button-small-size;
-
-		:global(.enact-text-large) & {
-			bottom: -@moon-icon-button-small-size-large;
-		}
-	}
-}
-
-.scrollbarContainerHColumn {
-	.scrollbarContainerBase;
-	height: @moon-icon-button-small-size;
-	border-width: 0 @moon-icon-button-small-size;
-	bottom: 0;
-
-	.position-start-end(0, @moon-icon-button-small-size);
-
-	:global(.enact-text-large) & {
-		height: @moon-icon-button-small-size-large;
-		border-width: 0 @moon-icon-button-small-size-large;
-		left: 0;
-		right: @moon-icon-button-small-size-large;
-	}
-
-	:global(.enact-text-large.enact-locale-right-to-left) & {
-		left: @moon-icon-button-small-size-large;
-		right: 0;
-	}
-
-	.scrollbarLeftButton {
-		.position-start-end(-@moon-icon-button-small-size, auto);
-
-		:global(.enact-text-large) & {
-			left: -@moon-icon-button-small-size-large;
-			right: auto;
-		}
-
-		:global(.enact-text-large.enact-locale-right-to-left) & {
-			left: auto;
-			right: -@moon-icon-button-small-size-large;
-		}
-	}
-
-	.scrollbarRightButton {
-		.position-start-end(auto, -@moon-icon-button-small-size);
-
-		:global(.enact-text-large) & {
-			left: auto;
-			right: -@moon-icon-button-small-size-large;
-		}
-
-		:global(.enact-text-large.enact-locale-right-to-left) & {
-			left: -@moon-icon-button-small-size-large;
-			right: auto;
-		}
-	}
-}
-
-.scrollbarVthumb {
-	background: #666;
-	position: absolute;
-	width: 3px;
-	height: 3px;
-	right: @moon-icon-button-small-size / 2 - 2;
-	opacity: 0;
-
-	-webkit-transition: opacity 0.1s linear;
-	transition: opacity 0.1s linear;
-
-	-webkit-transform-origin: 0 0 0;
-	transform-origin: 0 0 0;
-
-	:global(.enact-text-large) & {
-		right: @moon-icon-button-small-size-large / 2 - 2;
-	}
-}
-
-.scrollerHthumb {
-	background: #666;
-	position: absolute;
-	width: 3px;
-	height: 3px;
-	bottom: @moon-icon-button-small-size / 2 - 2;
-	opacity: 0;
-
-	-webkit-transition: opacity 0.1s linear;
-	transition: opacity 0.1s linear;
-
-	-webkit-transform-origin: 0 0 0;
-	transform-origin: 0 0 0;
-
-	:global(.enact-text-large) & {
-		bottom: @moon-icon-button-small-size-large / 2 - 2;
-	}
-}
-
-.thumbShown {
-	opacity: 0.75;
-}
-
-.thumbHidden {
-	opacity: 0;
 }

commit 2773a5cee85c530d8cc96ba2fd7cd78aa6bd3793

remove scrollbar button focus mgmt logic
Some crufty code was broken by #903 which reordered the DOM. That code
isn't needed any more with the changes from #919 so it can be removed.

Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

packages/moonstone/Scroller/Scrollbar.js
@@ -180,7 +180,6 @@ class ScrollbarBase extends PureComponent {
 	minThumbSizeRatio = 0
 	pressed = false
 	ignoreMode = false
-	buttonToFocus = null
 	// component refs
 	containerRef = null
 	thumbRef = null
@@ -189,10 +188,6 @@ class ScrollbarBase extends PureComponent {
 
 	setPressStatus = (isPressed) => {
 		this.pressed = isPressed;
-		if (!isPressed && this.buttonToFocus) {
-			Spotlight.focus(this.buttonToFocus);
-			this.buttonToFocus = null;
-		}
 	}
 
 	setIgnoreMode = (shouldIgnore) => {
@@ -217,7 +212,7 @@ class ScrollbarBase extends PureComponent {
 			maxPos = vertical ? bounds.maxTop : bounds.maxLeft,
 			shouldDisablePrevButton = currentPos <= 0,
 			shouldDisableNextButton = currentPos >= maxPos,
-			spotItem = window.document.activeElement;
+			spotItem = Spotlight.getCurrent();
 
 		this.setState((prevState) => {
 			const
@@ -233,16 +228,11 @@ class ScrollbarBase extends PureComponent {
 			}
 		});
 
-		if (shouldDisablePrevButton && spotItem === prevButtonNodeRef) {
-			if (this.pressed) {
-				this.setIgnoreMode(true);
-				this.buttonToFocus = nextButtonNodeRef;
-			}
-		} else if (shouldDisableNextButton && spotItem === nextButtonNodeRef) {
-			if (this.pressed) {
-				this.setIgnoreMode(true);
-				this.buttonToFocus = prevButtonNodeRef;
-			}
+		if (this.pressed && (
+			shouldDisablePrevButton && spotItem === prevButtonNodeRef ||
+			shouldDisableNextButton && spotItem === nextButtonNodeRef
+		)) {
+			this.setIgnoreMode(true);
 		}
 	}
 