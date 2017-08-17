00f348be0eaa5c4802264660362fa3711e7234c4
Seungcheon Baek <sc.baek@lge.com>
2017-08-17 20:16:35 +0900

    ENYO-4618: Paging control down button is not disabled when the list reached to the bottom
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- packages/moonstone/Scroller/Scrollbar.js
+++ packages/moonstone/Scroller/Scrollbar.js
@@ -216,7 +216,7 @@ class ScrollbarBase extends PureComponent {
 			currentPos = vertical ? bounds.scrollTop : bounds.scrollLeft,
 			maxPos = vertical ? bounds.maxTop : bounds.maxLeft,
 			shouldDisablePrevButton = currentPos <= 0,
-			shouldDisableNextButton = currentPos >= maxPos,
+			shouldDisableNextButton = currentPos >= maxPos - 1,
 			spotItem = window.document.activeElement;
 
 		this.setState((prevState) => {
