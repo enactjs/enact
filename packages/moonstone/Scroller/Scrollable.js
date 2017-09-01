31760df8805108c1aa4f8f955489c284ec92ff0e
Derek Tor <derek.tor@lge.com>
2017-09-01 16:09:52 -0700

    added event forwarding
    Enact-DCO-1.0-Signed-off-by: Derek Tor derek.tor@lge.com

--- packages/moonstone/Scroller/Scrollable.js
+++ packages/moonstone/Scroller/Scrollable.js
@@ -167,7 +167,7 @@ const ScrollableHoC = hoc((config, Wrapped) => {
 			onScroll: PropTypes.func,
 
 			/**
-			 * Called scrollbar visability changes
+			 * Called when scrollbar visability changes
 			 *
 			 * @type {Function}
 			 * @public
