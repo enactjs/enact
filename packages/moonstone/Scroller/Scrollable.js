3bdbdeee0f45bc5b37688a366a1debb2aebe6cc8
Ryan Duffy <ryan.duffy@lge.com>
2017-09-22 10:17:37 -0500

    update scrollable native
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- packages/moonstone/Scroller/Scrollable.js
+++ packages/moonstone/Scroller/Scrollable.js
@@ -1127,9 +1127,6 @@ const ScrollableHoC = hoc((config, Wrapped) => {
 			this.bounds.scrollHeight = this.getScrollBounds().scrollHeight;
 		}
 
-		// forceUpdate is a bit jarring and may interrupt other actions like animation so we'll
-		// queue it up in case we get multiple calls (e.g. when grouped expandables toggle).
-		//
 		// TODO: consider replacing forceUpdate() by storing bounds in state rather than a non-
 		// state member.
 		enqueueForceUpdate = () => {
