eff927c1e7990361b7883d5895b66eb617356e0f
Ryan Duffy <ryan.duffy@lge.com>
2017-10-05 15:46:41 -0500

    fix restarting controlled marquees after invalidation
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- packages/moonstone/Marquee/MarqueeDecorator.js
+++ packages/moonstone/Marquee/MarqueeDecorator.js
@@ -291,6 +291,9 @@ const MarqueeDecorator = hoc(defaultConfig, (config, Wrapped) => {
 			if ((!childrenEquals(this.props.children, next.children)) || (invalidateProps && didPropChange(invalidateProps, this.props, next))) {
 				this.invalidateMetrics();
 				this.cancelAnimation();
+
+				// restart marqueeOn="render" marquees or synced marquees that were animating
+				this.forceRestartMarquee = next.marqueeOn === 'render' || (this.sync && this.state.animating);
 				this.textDirectionValidated = false;
 			} else if (next.marqueeOn !== marqueeOn || next.marqueeDisabled !== marqueeDisabled || next.marqueeSpeed !== marqueeSpeed) {
 				this.cancelAnimation();
@@ -364,10 +367,8 @@ const MarqueeDecorator = hoc(defaultConfig, (config, Wrapped) => {
 		 */
 		shouldStartMarquee () {
 			return (
-				// restart un-synced marquees or marqueeOn="render" synced marquees that were
-				// cancelled due to a prop change
-				(!this.sync || this.forceRestartMarquee) && (
-					this.props.marqueeOn === 'render' ||
+				this.forceRestartMarquee ||
+				!this.sync && (
 					(this.isFocused && this.props.marqueeOn === 'focus') ||
 					(this.isHovered && this.props.marqueeOn === 'hover')
 				)
@@ -564,7 +565,6 @@ const MarqueeDecorator = hoc(defaultConfig, (config, Wrapped) => {
 		 */
 		cancelAnimation = () => {
 			if (this.sync) {
-				this.forceRestartMarquee = true;
 				this.context.cancel(this);
 			}
 
