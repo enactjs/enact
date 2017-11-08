95ef7ecf2663b2735fc01eb5535a6b08fc61c02d
Ryan Duffy <ryan.duffy@lge.com>
2017-11-08 09:24:12 -0800

    enable container on keydown in Scroller
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- packages/moonstone/Scroller/Scroller.js
+++ packages/moonstone/Scroller/Scroller.js
@@ -437,9 +437,21 @@ class ScrollerBase extends Component {
 		}
 	}
 
+	handleGlobalKeyDown = () => {
+		this.setContainerDisabled(false);
+	}
+
 	setContainerDisabled = (bool) => {
-		if (this.containerRef) {
-			this.containerRef.setAttribute(dataContainerDisabledAttribute, bool);
+		const containerNode = this.containerRef;
+
+		if (containerNode) {
+			containerNode.setAttribute(dataContainerDisabledAttribute, bool);
+
+			if (bool) {
+				document.addEventListener('keydown', this.handleGlobalKeyDown, {capture: true});
+			} else {
+				document.removeEventListener('keydown', this.handleGlobalKeyDown, {capture: true});
+			}
 		}
 	}
 
