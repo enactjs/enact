97bbdd3d12a1bb68edd6210ee0118a0cfba5cdd1
bongsub.kim <bongsub.kim@lgepartner.com>
2017-12-07 18:06:28 +0900

    ENYO-4957: revert PR #1247 to show scroll thumbs properly when scroll position reaches the top or the bottom (#1346)
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- packages/moonstone/Scroller/Scrollbar.js
+++ packages/moonstone/Scroller/Scrollbar.js
@@ -216,10 +216,6 @@ class ScrollbarBase extends PureComponent {
 			shouldDisableNextButton = maxPos - currentPos <= 1,
 			spotItem = Spotlight.getCurrent();
 
-		if (currentPos <= 0 || currentPos >= maxPos) {
-			this.startHidingThumb();
-		}
-
 		this.setState((prevState) => {
 			const
 				updatePrevButton = (prevState.prevButtonDisabled !== shouldDisablePrevButton),
