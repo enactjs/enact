52a8e213c33472fb32af8d2b9c8cfc3e67255e89
Stephen Choi <stephen.choi@lge.com>
2017-12-19 17:48:56 -0800

    add knob add-on

--- packages/samplerv3/src/configure.js
+++ packages/samplerv3/src/configure.js
@@ -1,6 +1,8 @@
-import {configure} from '@storybook/react';
+import {configure, addDecorator} from '@storybook/react';
+import {withKnobs} from '@storybook/addon-knobs';
 
 function config (stories, mod) {
+	addDecorator(withKnobs);
 
 	function loadStories () {
 		stories.keys().forEach((filename) => stories(filename));
