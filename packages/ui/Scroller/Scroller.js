eea90f0b73962442c078fd036e56cd1e42559e92
Ryan Duffy <ryan.duffy@lge.com>
2017-12-29 15:09:15 -0800

    starting ui/Scroller
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- packages/ui/Scroller/Scroller.js
+++ packages/ui/Scroller/Scroller.js
@@ -6,12 +6,15 @@
  * @module ui/Scroller
  */
 
-import classNames from 'classnames';
+import classNamesBind from 'classnames/bind';
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 
+import ComponentOverride from '../ComponentOverride';
+
 import css from './Scroller.less';
-import Scrollable from './Scrollable';
+
+const classnames = classNamesBind.bind(css);
 
 /**
  * {@link ui/Scroller.ScrollerBase} is a base component for Scroller.
@@ -27,236 +30,150 @@ class ScrollerBase extends Component {
 	static displayName = 'Scroller'
 
 	static propTypes = /** @lends ui/Scroller.ScrollerBase.prototype */ {
-		children: PropTypes.node.isRequired,
-
-		/**
-		 * Callback method of scrollTo.
-		 * Normally, `Scrollable` should set this value.
-		 *
-		 * @type {Function}
-		 * @private
-		 */
-		cbScrollTo: PropTypes.func,
-
-		/**
-		 * Direction of the scroller; valid values are `'both'`, `'horizontal'`, and `'vertical'`.
-		 *
-		 * @type {String}
-		 * @default 'both'
-		 * @public
-		 */
-		direction: PropTypes.oneOf(['both', 'horizontal', 'vertical'])
-	}
-
-	static contextTypes = {
-		rtl: PropTypes.bool
+		defaultScrollLeft: PropTypes.number,
+		defaultScrollTop: PropTypes.number,
+		horizontal: PropTypes.string,
+		vertical: PropTypes.string
 	}
 
 	static defaultProps = {
-		direction: 'both'
+		defaultScrollLeft: 0,
+		defaultScrollTop: 0,
+		horizontal: 'auto',
+		vertical: 'auto'
 	}
 
 	constructor (props) {
-		super(props);
+		super();
+
+		this.state = {
+			height: 0,
+			width: 0,
+			scrollHeight: 0,
+			scrollWidth: 0,
+			x: props.defaultScrollLeft,
+			y: props.defaultScrollTop
+		};
 	}
 
 	componentDidMount () {
 		this.calculateMetrics();
 	}
 
-	componentDidUpdate () {
-		this.calculateMetrics();
-	}
+	componentDidUpdate (prevProps, prevState) {
+		const {x, y} = this.state;
 
-	scrollBounds = {
-		clientWidth: 0,
-		clientHeight: 0,
-		scrollWidth: 0,
-		scrollHeight: 0,
-		maxLeft: 0,
-		maxTop: 0
-	}
+		if (this.containerNode) {
+			let transform = '';
+			if (x !== prevState.x) {
+				transform += ` translateX(-${x}px)`;
+			}
 
-	scrollPos = {
-		top: 0,
-		left: 0
-	}
+			if (y !== prevState.y) {
+				transform += ` translateY(-${y}px)`;
+			}
 
-	getScrollBounds = () => this.scrollBounds
-
-	getRtlPositionX = (x) => (this.context.rtl ? this.scrollBounds.maxLeft - x : x)
-
-	// for Scrollable
-	setScrollPosition (x, y) {
-		const
-			node = this.containerRef;
-
-		if (this.isVertical()) {
-			node.scrollTop = y;
-			this.scrollPos.top = y;
-		}
-		if (this.isHorizontal()) {
-			node.scrollLeft = this.getRtlPositionX(x);
-			this.scrollPos.left = x;
+			this.contentNode.style.transform = transform;
 		}
 	}
 
-	getNodePosition = (node) => {
-		const
-			{left: nodeLeft, top: nodeTop, height: nodeHeight, width: nodeWidth} = node.getBoundingClientRect(),
-			{left: containerLeft, top: containerTop} = this.containerRef.getBoundingClientRect(),
-			{scrollLeft, scrollTop} = this.containerRef,
-			left = this.isHorizontal() ? (scrollLeft + nodeLeft - containerLeft) : null,
-			top = this.isVertical() ? (scrollTop + nodeTop - containerTop) : null;
-
-		return {
-			left,
-			top,
-			width: nodeWidth,
-			height: nodeHeight
-		};
+	setContainer = (node) => {
+		this.containerNode = node;
 	}
 
-	isVertical = () => {
-		const {direction} = this.props;
-		return (direction !== 'horizontal');
-	}
-
-	isHorizontal = () => {
-		const {direction} = this.props;
-		return (direction !== 'vertical');
+	setContent = (node) => {
+		this.contentNode = node;
 	}
 
 	calculateMetrics () {
-		const
-			{scrollBounds} = this,
-			{scrollWidth, scrollHeight, clientWidth, clientHeight} = this.containerRef;
-		scrollBounds.scrollWidth = scrollWidth;
-		scrollBounds.scrollHeight = scrollHeight;
-		scrollBounds.clientWidth = clientWidth;
-		scrollBounds.clientHeight = clientHeight;
-		scrollBounds.maxLeft = Math.max(0, scrollWidth - clientWidth);
-		scrollBounds.maxTop = Math.max(0, scrollHeight - clientHeight);
+		this.setState((currentBounds) => {
+			if (this.containerNode) {
+				const {clientHeight: height, clientWidth: width, scrollHeight, scrollWidth} = this.containerNode;
+
+				if (
+					height !== currentBounds.height ||
+					width !== currentBounds.width ||
+					scrollHeight !== currentBounds.scrollHeight ||
+					scrollWidth !== currentBounds.scrollWidth
+				) {
+					return {height, width, scrollHeight, scrollWidth};
+				}
+			}
+
+			return null;
+		});
 	}
 
-	initRef = (ref) => {
-		this.containerRef = ref;
+	isHorizontalScrollbarVisible () {
+		return this.props.horizontal === 'always' || (
+			this.props.horizontal === 'auto' && this.state.scrollWidth > this.state.width
+		);
+	}
+
+	isVerticalScrollbarVisible () {
+		return this.props.vertical === 'always' || (
+			this.props.vertical === 'auto' && this.state.scrollHeight > this.state.height
+		);
+	}
+
+	handleHorizontalStep = ({value}) => {
+		this.setState(() => {
+			return {
+				x: value
+			};
+		});
+	}
+
+	handleVerticalStep = ({value}) => {
+		this.setState(() => {
+			return {
+				y: value
+			};
+		});
 	}
 
 	render () {
-		const
-			{className, style} = this.props,
-			props = Object.assign({}, this.props),
-			mergedStyle = Object.assign({}, style, {
-				overflowX: this.isHorizontal() ? 'auto' : 'hidden',
-				overflowY: this.isVertical() ? 'auto' : 'hidden'
-			});
+		const {children, className, scrollbarComponent, ...rest} = this.props;
 
-		delete props.cbScrollTo;
-		delete props.className;
-		delete props.direction;
-		delete props.style;
+		const showHorizontal = this.isHorizontalScrollbarVisible();
+		const showVertical = this.isVerticalScrollbarVisible();
+
+		const classes = classnames(
+			css.scroller,
+			className,
+			{
+				vertical: showVertical,
+				horizontal: showHorizontal
+			}
+		);
 
 		return (
-			<div
-				{...props}
-				className={classNames(className, css.hideNativeScrollbar)}
-				ref={this.initRef}
-				style={mergedStyle}
-			/>
+			<div className={classes} {...rest}>
+				<div className={css.container} ref={this.setContainer}>
+					<div ref={this.setContent} className={css.content}>
+						{children}
+					</div>
+				</div>
+				<ComponentOverride
+					component={showVertical ? scrollbarComponent : null}
+					max={this.state.scrollHeight}
+					onStep={this.handleVerticalStep}
+					orientation="vertical"
+					size={this.state.height}
+					value={this.state.y}
+				/>
+				<ComponentOverride
+					component={showHorizontal ? scrollbarComponent : null}
+					max={this.state.scrollWidth}
+					onStep={this.handleHorizontalStep}
+					orientation="horizontal"
+					size={this.state.width}
+					value={this.state.x}
+				/>
+			</div>
 		);
 	}
 }
 
-/**
- * {@link ui/Scroller.Scroller} is a Scroller with Scrollable applied.
- *
- * Usage:
- * ```
- * <Scroller>Scroll me.</Scroller>
- * ```
- *
- * @class Scroller
- * @memberof ui/Scroller
- * @mixes ui/Scroller.Scrollable
- * @see ui/Scroller.ScrollerBase
- * @ui
- * @public
- */
-const Scroller = Scrollable(ScrollerBase);
-
-// Docs for Scroller
-/**
- * The callback function which is called for linking scrollTo function.
- * You should specify a callback function as the value of this prop
- * to use scrollTo feature.
- *
- * The scrollTo function passed to the parent component requires below as an argument.
- * - {position: {x, y}} - You can set a pixel value for x and/or y position
- * - {align} - You can set one of values below for align
- *   `'left'`, `'right'`, `'top'`, `'bottom'`,
- *   `'topleft'`, `'topright'`, `'bottomleft'`, and `'bottomright'`.
- * - {index} - You can set an index of specific item. (`0` or positive integer)
- *   This option is available only for `VirtualList` kind.
- * - {node} - You can set a node to scroll
- * - {animate} - When `true`, scroll occurs with animation.
- *   Set it to `false` if you want scrolling without animation.
- *
- * Example:
- * ```
- *	// If you set cbScrollTo prop like below;
- *	cbScrollTo: (fn) => {this.scrollTo = fn;}
- *	// You can simply call like below;
- *	this.scrollTo({align: 'top'}); // scroll to the top
- * ```
- *
- * @name cbScrollTo
- * @type {Function}
- * @memberof ui/Scroller.Scroller
- * @instance
- * @public
- */
-
-/**
- * Direction of the scroller; valid values are `'both'`, `'horizontal'`, and `'vertical'`.
- *
- * @name direction
- * @type {String}
- * @default 'both'
- * @memberof ui/Scroller.Scroller
- * @instance
- * @public
- */
-
-/**
- * Called when scrolling
- *
- * @name onScroll
- * @type {Function}
- * @memberof ui/Scroller.Scroller
- * @instance
- * @public
- */
-
-/**
- * Called when scroll starts
- *
- * @name onScrollStart
- * @type {Function}
- * @memberof ui/Scroller.Scroller
- * @instance
- * @public
- */
-
-/**
- * Called when scroll stops
- *
- * @name onScrollStop
- * @type {Function}
- * @memberof ui/Scroller.Scroller
- * @instance
- * @public
- */
-
-export default Scroller;
-export {Scroller, ScrollerBase};
+export default ScrollerBase;
+export {ScrollerBase as Scroller, ScrollerBase};
