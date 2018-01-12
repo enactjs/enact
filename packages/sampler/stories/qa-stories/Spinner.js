aa04ab857258a280a5aa9f4cdaa5f69339fcaa2a
Ryan Duffy <ryan.duffy@lge.com>
2018-01-11 17:28:34 -0800

    convert border to ri
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- packages/sampler/stories/qa-stories/Spinner.js
+++ packages/sampler/stories/qa-stories/Spinner.js
@@ -20,7 +20,7 @@ storiesOf('Spinner')
 				<div
 					style={{
 						height: ri.unit(420, 'rem'),
-						border: '3px dotted red'
+						border: ri.unit(3, 'rem') + ' dotted red'
 					}}
 				>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -46,7 +46,7 @@ storiesOf('Spinner')
 				<div
 					style={{
 						height: ri.unit(420, 'rem'),
-						border: '3px dotted red'
+						border: ri.unit(3, 'rem') + ' dotted red'
 					}}
 				>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
