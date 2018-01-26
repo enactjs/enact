7b6a19e1f03cf1a8105037543d6a0f33ca4b894a
Blake Stephens <blake.stephens@lge.com>
2018-01-26 11:20:24 -0800

    Unicode ranges indented a little for readability
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- packages/moonstone/MoonstoneDecorator/fontGenerator.js
+++ packages/moonstone/MoonstoneDecorator/fontGenerator.js
@@ -50,22 +50,22 @@ const fonts = {
 	'ur': {
 		regular: 'LG Display_Urdu',
 		unicodeRange:
-		'U+600-6FF,' +
-		'U+FE70-FEFE,' +
-		'U+FB50-FDFF'
+			'U+600-6FF,' +
+			'U+FE70-FEFE,' +
+			'U+FB50-FDFF'
 	},
 	'zh-HK': {
 		regular: 'LG Display GP4_HK',
 		bold:    'LG Display GP4_HK',
 		unicodeRange:
-		'U+0-FF,' +
-		'U+2E80-2EFF,' +
-		'U+3000-303F,' +
-		'U+3200-33FF,' +
-		'U+3400-4DBF,' +
-		'U+4E00-9FFF,' +
-		'U+E000-FAFF,' +
-		'U+FF00-FFEF'
+			'U+0-FF,' +
+			'U+2E80-2EFF,' +
+			'U+3000-303F,' +
+			'U+3200-33FF,' +
+			'U+3400-4DBF,' +
+			'U+4E00-9FFF,' +
+			'U+E000-FAFF,' +
+			'U+FF00-FFEF'
 	}
 };
 
