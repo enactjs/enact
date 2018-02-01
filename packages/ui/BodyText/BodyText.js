99e4df2e686d0c2ff475c1e6b29ff546218a642f
Derek Tor <derekmuoktor@gmail.com>
2018-01-31 16:56:35 -0800

    PLAT-49058: Migrate BodyText to ui (#1377)
    * update react version
    
    * Temporarily test travis against enact-cli#feature/react16
    
    * Moved BodyText to ui
    
    Enact-DCO-1.0-Signed-off-by: Derek Tor (derek.tor@lge.com)
    
    * removed only
    
    * removed classnames
    
    * made minor doc changes, fixed typos
    
    * added more docs
    
    * fixed docs and removed css prop
    
    * removed centered proptype and fixed publicClassNames
    
    * Fixes to docs, structural bits, and exports.
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * move css prop after spread
    
    * fix BodyText import
    
    Reviewed-By: Blake Stephens (blake.stephens@lge.com)
    Integrated-By: Ryan Duffy (ryan.duffy@lge.com)

---
+++ packages/ui/BodyText/BodyText.js
@@ -0,0 +1,78 @@
+/**
+ * Provides unstyled BodyText components and behaviors to be customized by a theme or application.
+ *
+ * @module ui/BodyText
+ * @exports BodyText
+ */
+
+import kind from '@enact/core/kind';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import componentCss from './BodyText.less';
+
+/**
+ * A basic text-block component without any behaviors applied to it.
+ *
+ * @class BodyText
+ * @memberof ui/BodyText
+ * @ui
+ * @public
+ */
+const BodyText = kind({
+	name: 'ui:BodyText',
+
+	propTypes: /** @lends ui/BodyText.BodyText.prototype */ {
+		/**
+		 * Applies the `centered` CSS class to the [BodyTextBase]{@link ui/BodyText.BodyTextBase}
+		 *
+		 * @type {Boolean}
+		 * @default false
+		 * @public
+		 */
+		centered: PropTypes.bool,
+
+		/**
+		 * Customizes the component by mapping the supplied collection of CSS class names to the
+		 * corresponding internal Elements and states of this component.
+		 *
+		 * The following classes are supported:
+		 *
+		 * * `bodyText` - The root class name
+		 * * `centered` - Applied when `centered` prop is `true`
+		 *
+		 * @type {Object}
+		 * @public
+		 */
+		css: PropTypes.object
+	},
+
+	defaultProps: {
+		centered: false
+	},
+
+	styles: {
+		css: componentCss,
+		className: 'bodyText',
+		publicClassNames: true
+	},
+
+	computed: {
+		className: ({centered, styler}) => styler.append({centered})
+	},
+
+	render: (props) => {
+		delete props.centered;
+
+		return (
+			<p
+				{...props}
+			/>
+		);
+	}
+});
+
+export default BodyText;
+export {
+	BodyText
+};
