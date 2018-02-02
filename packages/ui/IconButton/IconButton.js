169eb2baeea8b29a4d90866f8b1da8f6852c754f
Teck <teck.liew@lge.com>
2018-02-02 14:41:15 -0800

    updated props buttonComponent and iconComponent as required

--- packages/ui/IconButton/IconButton.js
+++ packages/ui/IconButton/IconButton.js
@@ -36,7 +36,18 @@ const IconButtonBase = kind({
 		 * @type {Function}
 		 * @public
 		 */
-		buttonComponent: PropTypes.func,
+		buttonComponent: PropTypes.func.isRequired,
+
+		/**
+		 * The component used to render the [icon]{@link ui/IconButton.IconButtonBase.icon}.
+		 *
+		 * This component will receive the `small` property set on the IconButton as well as the
+		 * `icon` class to customize its styling.
+		 *
+		 * @type {Function}
+		 * @public
+		 */
+		iconComponent: PropTypes.func.isRequired,
 
 		/**
 		 * Additional children that follow the icon.
@@ -86,17 +97,6 @@ const IconButtonBase = kind({
 		 * @public
 		 */
 		icon: PropTypes.string,
-
-		/**
-		 * The component used to render the [icon]{@link ui/IconButton.IconButtonBase.icon}.
-		 *
-		 * This component will receive the `small` property set on the IconButton as well as the
-		 * `icon` class to customize its styling.
-		 *
-		 * @type {Function}
-		 * @public
-		 */
-		iconComponent: PropTypes.func,
 
 		/**
 		 * Applies the `pressed` CSS class to the [IconButton]{@link ui/IconButton.IconButtonBase}
