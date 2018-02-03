8f45c62655eea02a8e140b6e632968cc608cbc81
Blake Stephens <blake.stephens@lge.com>
2018-02-03 11:54:07 -0800

    Relocating tests and added touch to Item tests
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- packages/ui/Item/tests/Item-specs.js
+++ packages/ui/Item/tests/Item-specs.js
@@ -1,18 +1,93 @@
 import React from 'react';
 import {mount} from 'enzyme';
-import {ItemBase} from '../Item';
+import sinon from 'sinon';
+import Item from '../Item';
+
+const tap = (node) => {
+	node.simulate('mousedown');
+	node.simulate('mouseup');
+};
 
 describe('Item', () => {
 
 	it('should create an Item that is enabled by default', function () {
 		const item = mount(
-			<ItemBase>I am an Item</ItemBase>
+			<Item>I am an Item</Item>
 		);
 
 		const expected = 0;
 		const actual = item.find({disabled: true}).length;
 
 		expect(actual).to.equal(expected);
+	});
+
+	it('should have \'disabled\' HTML attribute when \'disabled=true\'', function () {
+		const item = mount(
+			<Item disabled>I am a disabled Item</Item>
+		);
+
+		const expected = 1;
+		const actual = item.find('div[disabled=true]').length;
+
+		expect(actual).to.equal(expected);
+	});
+
+	describe('events', () => {
+		it('should call onClick when clicked', function () {
+			const handleClick = sinon.spy();
+			const item = mount(
+				<Item onClick={handleClick}>I am a normal Item</Item>
+			);
+
+			item.simulate('click');
+
+			const expected = true;
+			const actual = handleClick.called;
+
+			expect(actual).to.equal(expected);
+		});
+
+		it('should call onTap when tapped', function () {
+			const handleClick = sinon.spy();
+			const item = mount(
+				<Item onTap={handleClick}>I am a normal Item</Item>
+			);
+
+			tap(item);
+
+			const expected = true;
+			const actual = handleClick.called;
+
+			expect(actual).to.equal(expected);
+		});
+
+		it('should not call onClick when clicked and disabled', function () {
+			const handleClick = sinon.spy();
+			const item = mount(
+				<Item disabled onClick={handleClick}>I am a disabled Item</Item>
+			);
+
+			item.simulate('click');
+
+			const expected = false;
+			const actual = handleClick.called;
+
+			expect(actual).to.equal(expected);
+		});
+
+		it('should not call onTap when tapped and disabled', function () {
+			const handleClick = sinon.spy();
+			const item = mount(
+				<Item disabled onTap={handleClick}>I am a disabled Item</Item>
+			);
+
+			tap(item);
+
+			const expected = false;
+			const actual = handleClick.called;
+
+			expect(actual).to.equal(expected);
+		});
 	});
 
 });
