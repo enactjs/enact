aecc88bd2376249f0f3e296bbbba8e927bd7965b
Teck <teck.liew@lge.com>
2018-02-12 19:10:28 -0800

    use indexOf to check for substring

--- packages/moonstone/Button/Button.js
+++ packages/moonstone/Button/Button.js
@@ -192,7 +192,7 @@ const ButtonBaseFactory = factory({css: componentCss}, ({css}) =>
 
 		computed: {
 			className: ({backgroundOpacity, className, color, minWidth, noAnimation, pressed, selected, small, styler}) => {
-				const isIconButton = className ? className.includes('IconButton') : false;
+				const isIconButton = className ? className.indexOf('IconButton') > -1 : false;
 				// Deprecate `noAnimation` except for `IconButton`, because it is used as a defaultProp.
 				if (noAnimation && !isIconButton) {
 					deprecate({name: 'noAnimation', since: '1.14.0', until: '2.0.0'});
