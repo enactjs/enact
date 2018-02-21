35f69401ad54a3ca1e7346e8f660ee3da078d4aa
yb.sung <yb.sung@lge.com>
2018-02-21 09:25:57 +0900

    Show scrollbars in edge cases
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- packages/moonstone/Scroller/Scrollable.js
+++ packages/moonstone/Scroller/Scrollable.js
@@ -137,6 +137,15 @@ const ScrollableHoC = hoc((config, Wrapped) => {
 			cbScrollTo: PropTypes.func,
 
 			/**
+			 * Size of the data.
+			 *
+			 * @type {Number}
+			 * @default 0
+			 * @public
+			 */
+			dataSize: PropTypes.number,
+
+			/**
 			 * When `true`, allows 5-way navigation to the scrollbar controls. By default, 5-way will
 			 * not move focus to the scrollbar controls.
 			 *
@@ -305,7 +314,13 @@ const ScrollableHoC = hoc((config, Wrapped) => {
 
 			this.direction = this.childRef.props.direction;
 			this.updateEventListeners();
-			this.updateScrollbars();
+
+			if (!(
+				this.props.dataSize === prevProps.dataSize &&
+				(this.state.isHorizontalScrollbarVisible && !prevState.isHorizontalScrollbarVisible || this.state.isVerticalScrollbarVisible && !prevState.isVerticalScrollbarVisible)
+			)) {
+				this.updateScrollbars();
+			}
 
 			if (this.scrollToInfo !== null) {
 				if (!this.deferScrollTo) {
