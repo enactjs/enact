f3b7c6c818a7fb139c11760a173423cb0d75dd5b
Teck <teck.liew@lge.com>
2018-03-07 18:46:15 -0800

    update unit-test 2

--- packages/moonstone/ProgressBar/tests/ProgressBar-specs.js
+++ packages/moonstone/ProgressBar/tests/ProgressBar-specs.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {mount} from 'enzyme';
 import ProgressBar from '../ProgressBar';
-import css from '../ProgressBar.less';
 
 describe('ProgressBar Specs', () => {
 	it('should only show tooltip when tooltip is true', function () {
