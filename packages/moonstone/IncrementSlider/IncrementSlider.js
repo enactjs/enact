58b88c04b8dc80015db155b8acb2d2860bb418e0
Seungho Park <seunghoh.park@lge.com>
2018-03-08 18:47:00 +0900

    Add missed modification
    Enact-DCO-1.0-Signed-off-by: Seungho Park <seunghoh.park@lge.com>

--- packages/moonstone/IncrementSlider/IncrementSlider.js
+++ packages/moonstone/IncrementSlider/IncrementSlider.js
@@ -60,23 +60,13 @@ const PrivateIncrementSliderBaseFactory = factory({css: componentCss}, ({css}) =
 			'aria-hidden': PropTypes.bool,
 
 			/**
-			 * Overrides value string to read for the slider. It is mapping to `aria-valuetext` internally.
-			 * By default, `aria-valuetext` is set to the current value. This should only be used
-			 * when the parent controls the value of the slider directly through the props.
-			 *
-			 * @type {String|Number}
-			 * @public
-			 */
-			'aria-label': PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-
-			/**
 			 * Overrides the `aria-valuetext` for the slider. By default, `aria-valuetext` is set
 			 * to the current value. This should only be used when the parent controls the value of
 			 * the slider directly through the props.
 			 *
 			 * @type {String|Number}
 			 * @memberof moonstone/IncrementSlider.IncrementSliderBase.prototype
-			 * @private
+			 * @public
 			 */
 			'aria-valuetext': PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 
