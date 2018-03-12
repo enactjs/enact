27720846f7c73649293591ca5eb02a341b9fc742
hyeok jo <hyeok.jo@lge.com>
2018-03-12 16:57:19 +0900

    Fix about review
    Enact-DCO-1.0-Signed-off-by: Hyeok Jo <hyeok.jo@lge.com>

--- packages/moonstone/PictureInGraphics/PictureInGraphics.js
+++ packages/moonstone/PictureInGraphics/PictureInGraphics.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
+import equals from 'ramda/src/equals';
 
 import Slottable from '@enact/ui/Slottable';
 import Spottable from '@enact/spotlight/Spottable';
-import ri from '@enact/ui/resolution';
 import {forward} from '@enact/core/handle';
 
 import Image from '../Image';
@@ -12,9 +12,9 @@ import Skinnable from '../Skinnable';
 import {ItemBase} from '../Item';
 import {MarqueeController, MarqueeText} from '../Marquee';
 
-import {subscribeVideoState, launchApp} from '@enact/webos/PictureInGraphics';
-
 import css from './PictureInGraphics.less';
+
+const Container = MarqueeController({marqueeOnFocus: true}, Spottable(ItemBase));
 
 const defaultPlaceholder =
 	'data:image/svg+xml;charset=utf-8;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC' +
@@ -24,26 +24,21 @@ const defaultPlaceholder =
 
 class PictureInGraphicsBase extends React.Component {
 	static propTypes = {
-		source: PropTypes.string.isRequired,
-		sourceType: PropTypes.oneOf(['broadcast', 'externalInput']).isRequired,
+		source: PropTypes.node.isRequired,
 		captionComponent: PropTypes.node,
 		imageOverlayShowing: PropTypes.bool,
 		imageOverlaySrc: PropTypes.string,
 		isIpChannel: PropTypes.bool,
-		pigHeight: PropTypes.number,
-		pigWidth: PropTypes.number,
 		placeholder: PropTypes.string,
 		spotlightDisabled: PropTypes.bool,
 		textOverlayContent: PropTypes.string
 	}
 
 	static defaultProps = {
-		imageOverlayShowing: true,
+		imageOverlayShowing: false,
 		placeholder: defaultPlaceholder,
 		imageOverlaySrc: '',
 		isIpChannel: false,
-		pigWidth: 320,
-		pigHeight: 186,
 		spotlightDisabled: false
 	}
 
@@ -57,49 +52,15 @@ class PictureInGraphicsBase extends React.Component {
 	}
 
 	componentDidUpdate (prevProps) {
-		const {sourceType, source} = this.props;
-		const {sourceType: prevSourceType, source: prevSource} = prevProps;
+		const {source} = this.props;
+		const {source: prevSource} = prevProps;
 
-		if (sourceType !== prevSourceType || source !== prevSource) {
+		if (prevSource !== source && !equals(source, prevSource)) {
 			this.video.load();
 		}
 	}
 
-	getExternalInputSource = (inputType = '') => {
-		switch (inputType) {
-			case 'HDMI_1':
-				return 'ext://hdmi:1';
-			case 'HDMI_2':
-				return 'ext://hdmi:2';
-			case 'HDMI_3':
-				return 'ext://hdmi:3';
-			case 'HDMI_4':
-				return 'ext://hdmi:4';
-			case 'COMPONENT':
-				return 'ext://comp:1';
-			case 'AV_1':
-				return 'ext://av:1';
-			case 'AV_2':
-				return 'ext://av:2';
-			default:
-				return '';
-		}
-	};
-	getBroadcastSource = (channelId = '') => {
-		return 'tv://' + channelId;
-	};
-
 	handleLoadedMetadata = (ev) => {
-		const {sourceType} = this.props;
-
-		subscribeVideoState(ev.target.mediaId, sourceType, {
-			onSuccess: (res) => {
-				forward('onReceivedVideoState', res, this.props);
-			},
-			onFailure: (res) => {
-				forward('onReceivedVideoState', res, this.props);
-			}
-		});
 		forward('onLoadedMetadata', ev, this.props);
 	}
 
@@ -107,54 +68,23 @@ class PictureInGraphicsBase extends React.Component {
 		this.video = video;
 	}
 
-	handleClick = () => {
+	handleClick = (ev) => {
 		if (!this.props.spotlightDisabled) {
-			const {sourceType, source} = this.props;
-			let id = '';
-
-			if (sourceType === 'broadcast') {
-				id = 'com.webos.app.livetv';
-			} else if (sourceType === 'externalInput') {
-				switch (source) {
-					case 'HDMI_1':
-						id = 'com.webos.app.hdmi1';
-						break;
-					case 'HDMI_2':
-						id = 'com.webos.app.hdmi2';
-						break;
-					case 'HDMI_3':
-						id = 'com.webos.app.hdmi3';
-						break;
-					case 'HDMI_4':
-						id = 'com.webos.app.hdmi4';
-						break;
-					case 'COMPONENT':
-						id = 'com.webos.app.externalinput.component';
-						break;
-					case 'AV_1':
-						id = 'com.webos.app.externalinput.av1';
-						break;
-					case 'AV_2':
-						id = 'com.webos.app.externalinput.av2';
-						break;
-				}
-			}
-			launchApp(id);
+			forward('onVideoClick', ev, this.props);
 		}
 	};
 
-	render = () => {
-		const {source, sourceType, placeholder, imageOverlayShowing, imageOverlaySrc, textOverlayContent, captionComponent, isIpChannel, pigWidth, pigHeight, spotlightDisabled, ...rest} = this.props;
+	render () {
+		const {className, source, placeholder, imageOverlayShowing, imageOverlaySrc, textOverlayContent, captionComponent, isIpChannel, spotlightDisabled, ...rest} = this.props;
+		const containerClassName = classNames(className, css['pictureInGraphics']);
 		const textOverlayClassName = classNames(css.textOverlay, isIpChannel ? css.ipChannel : null);
-		const pigStyle = {width: ri.scale(pigWidth) + 'px', height: ri.scale(pigHeight) + 'px'};
-		const containerWidth = {width: ri.scale(pigWidth) + 'px'};
 
 		delete rest.onLoadedMetadata;
 		delete rest.onReceivedVideoState;
 
 		return (
-			<div {...rest} style={containerWidth}>
-				<SpottableItem className={css['pictureInGraphics']} style={pigStyle} onClick={this.handleClick} spotlightDisabled={spotlightDisabled}>
+			<Container className={containerClassName} spotlightDisabled={spotlightDisabled}>
+				<div className={css['videoContainer']} onClick={this.handleClick} >
 					<video
 						className={css.video}
 						autoPlay
@@ -162,37 +92,27 @@ class PictureInGraphicsBase extends React.Component {
 						ref={this.setVideoRef}
 						onLoadedMetadata={this.handleLoadedMetadata}
 					>
-						{sourceType === 'broadcast' ?
-							<source type="service/webos-broadcast" src={this.getBroadcastSource(source)} /> :
-							<source type="service/webos-external" src={this.getExternalInputSource(source)} />}
+						{source}
 					</video>
 					{imageOverlayShowing ?
 						<div>
 							<Image placeholder={placeholder} className={css.image} src={imageOverlaySrc} />
-							{textOverlayContent ? (<MarqueeText alignment="center" className={textOverlayClassName} marqueeOn="focus">{textOverlayContent}</MarqueeText>) : null}
+							{textOverlayContent ? (<MarqueeText alignment="center" className={textOverlayClassName}>{textOverlayContent}</MarqueeText>) : null}
 						</div> :
 						null
 					}
-				</SpottableItem>
-				{captionComponent}
-
-			</div>
+				</div>
+				<div className={css['captionContainer']}>
+					{captionComponent}
+				</div>
+			</Container>
 		);
 	}
 }
 
-const SpottableItem =  MarqueeController(
-	{marqueeOnFocus: true},
-	Spottable(
-		Skinnable(
-			ItemBase
-		)
-	)
-);
-
 const PictureInGraphics = Slottable(
-	{slots: ['captionComponent']},
-	PictureInGraphicsBase
+	{slots: ['captionComponent', 'source']},
+	Skinnable(PictureInGraphicsBase)
 );
 
 export default PictureInGraphics;
