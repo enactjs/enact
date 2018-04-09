0afd22da24dfbfe5a97abe87b760a178362b8b0b
changgi.lee <changgi.lee@lge.com>
2018-04-09 09:20:06 +0900

    PLAT-52473: Add attribute check in VoiceControl
    When outer div is not matched prop delegate target,
    
    onVoice event is not declared property.
    
    For handle this case, data-voice-event-target is added.
    
    Enact-DCO-1.0-Signed-off-by: Changgi Lee <changgi.lee@lge.com>

--- packages/webos/VoiceControl/VoiceControl.js
+++ packages/webos/VoiceControl/VoiceControl.js
@@ -8,11 +8,15 @@ class VoiceControl extends React.Component {
 	}
 
 	componentDidMount () {
-		findDOMNode(this).addEventListener('webOSVoice', this.props.onVoice);
+		this.node = findDOMNode(this);
+		if (!(this.node.hasAttribute('data-voice-event-target') || this.node.hasAttribute('webos-voice-intent'))) {
+			this.node = this.node.querySelector('[data-voice-event-target]') || this.node.querySelector('[webos-voice-intent]');
+		}
+		if (this.node) this.node.addEventListener('webOSVoice', this.props.onVoice);
 	}
 
 	componentWillUnmount () {
-		findDOMNode(this).removeEventListener('webOSVoice', this.props.onVoice);
+		if (this.node) this.node.removeEventListener('webOSVoice', this.props.onVoice);
 	}
 
 	render = () => (this.props.children)
