8ab8153a100c232915e67403f68cf7672ded54a9
changgi.lee <changgi.lee@lge.com>
2018-04-16 09:55:52 +0900

    PLAT-52473: Change voice control structure
    Change voice control component structure to HOC
    
    Enact-DCO-1.0-Signed-off-by: Changgi Lee <changgi.lee@lge.com>

---
+++ packages/webos/VoiceControlDecorator/VoiceControlDecorator.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {findDOMNode} from 'react-dom';
+import hoc from '@enact/core/hoc';
+
+const VoiceControlDecorator = hoc({}, (config, Wrapped) => {
+	return class extends React.Component {
+		static displayName = 'VoiceControlDecorator'
+
+		static propTypes = {
+			onVoice: PropTypes.func.isRequired
+		}
+
+		componentDidMount () {
+			this.node = findDOMNode(this);
+			if (!(this.node.hasAttribute('data-voice-event-target') || this.node.hasAttribute('data-webos-voice-intent'))) {
+				this.node = this.node.querySelector('[data-voice-event-target]') || this.node.querySelector('[data-webos-voice-intent]');
+			}
+			if (this.node) this.node.addEventListener('webOSVoice', this.props.onVoice);
+		}
+
+		componentWillUnmount () {
+			if (this.node) this.node.removeEventListener('webOSVoice', this.props.onVoice);
+		}
+
+		render () {
+			const props = {...this.props};
+			delete props.onVoice;
+			return (<Wrapped {...props} />);
+		}
+    };
+});
+
+export default VoiceControlDecorator;
+export {VoiceControlDecorator};
