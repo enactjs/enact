63b940be979ab63ee13f29e731fe0b2b50f46c22
Roy Sutton <pre101@pre101.com>
2018-04-23 15:56:06 -0400

    Consolidate speech modules (#1582)
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- packages/webos/speech/VoiceControlDecorator.js
+++ packages/webos/speech/VoiceControlDecorator.js
@@ -3,16 +3,30 @@ import PropTypes from 'prop-types';
 import {findDOMNode} from 'react-dom';
 import hoc from '@enact/core/hoc';
 
+/**
+ * Adds a callback for voice events
+ *
+ * @class VoiceControlDecorator
+ * @memberof webos/speech
+ * @hoc
+ */
 const VoiceControlDecorator = hoc({}, (config, Wrapped) => {
 	return class extends React.Component {
 		static displayName = 'VoiceControlDecorator'
 
-		static propTypes = {
+		static propTypes = /** @lends webos/speech.VoiceControlDecorator.prototype */ {
+			/**
+			 * Callback to be executed when a wrapped element's `data-webos-voice-intent` is activated.
+			 *
+			 * @type {Function}
+			 * @required
+			 * @public
+			 */
 			onVoice: PropTypes.func.isRequired
 		}
 
 		componentDidMount () {
-			this.node = findDOMNode(this);
+			this.node = findDOMNode(this);	// eslint-disable-line 
 			if (!(this.node.hasAttribute('data-webos-voice-event-target') || this.node.hasAttribute('data-webos-voice-intent'))) {
 				this.node = this.node.querySelector('[data-webos-voice-event-target]') || this.node.querySelector('[data-webos-voice-intent]');
 			}
@@ -28,7 +42,7 @@ const VoiceControlDecorator = hoc({}, (config, Wrapped) => {
 			delete props.onVoice;
 			return (<Wrapped {...props} />);
 		}
-    };
+	};
 });
 
 export default VoiceControlDecorator;
