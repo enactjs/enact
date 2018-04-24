a0227c6ed3f29ecce2a13fc6e29adae4e101865b
Seungcheon Baek <sc.baek@lge.com>
2018-04-24 15:28:54 +0900

    Updated sampler to add `wrap` to knobs
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- packages/sampler/stories/moonstone-stories/VirtualList.js
+++ packages/sampler/stories/moonstone-stories/VirtualList.js
@@ -5,13 +5,19 @@ import ri from '@enact/ui/resolution';
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
-import {boolean, number} from '@storybook/addon-knobs';
+import {boolean, number, select} from '@storybook/addon-knobs';
 import {withInfo} from '@storybook/addon-info';
 
 import nullify from '../../src/utils/nullify.js';
 import {mergeComponentMetadata} from '../../src/utils/propTables';
 
 const Config = mergeComponentMetadata('VirtualList', VirtualList, VirtualListBase, UiVirtualListBase);
+
+const wrapOption = {
+	'false': false,
+	'true': true,
+	"'noAnimation'": 'noAnimation'
+};
 
 const
 	items = [],
@@ -78,6 +84,7 @@ storiesOf('Moonstone', module)
 					style={{
 						height: ri.unit(552, 'rem')
 					}}
+					wrap={wrapOption[select('wrap', ['false', 'true', "'noAnimation'"])]}
 				/>
 			);
 		})
