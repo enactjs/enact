6fc64779b367de53f5287d5beb48bce5a915f7e9
yb.sung <yb.sung@lge.com>
2018-05-04 11:19:51 +0900

    Cleanup
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- packages/moonstone/Scrollable/Scrollable.js
+++ packages/moonstone/Scrollable/Scrollable.js
@@ -342,7 +342,7 @@ class ScrollableBase extends Component {
 
 			const
 				// VirtualList and Scroller have a spotlightId on containerRef
-				spotlightId = this.uiRef.containerRef.dataset.spotlightId,
+				spotlightId = containerRef.dataset.spotlightId,
 				direction = this.getPageDirection(keyCode),
 				rDirection = reverseDirections[direction],
 				viewportBounds = containerRef.getBoundingClientRect(),
