4e732a8cccbf38f0ff5bd9baf1e903b7cd31981c
Derek Tor <derekmuoktor@gmail.com>
2018-05-11 15:19:25 -0700

    PLAT-53609 - Touchable: add support for emitting onTap for click events (#1581)
    * added ability to fire on click of Touchable
    
    Enact-DCO-1.0-Signed-off-by: Derek Tor (derek.tor@lge.com)
    
    * removed only
    
    * updated changelog
    
    * Update docs
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * added check for last mouse event
    
    * updated changelog
    
    * removed block
    
    * fixed docs
    
    * refactoring suggestion
    
    Enact-DCO-1.0-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * added clickable class
    
    * renamed to ClickAllow
    
    * using call to clean up code
    
    * use prototype functions instead of arrow functions
    
    * ensure onClick still fires in addition to onTap
    
    Enact-DCO-1.0-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * rename Touchable event types to use camel casing with the on prefix
    
    Enact-DCO-1.0-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * Reword CHANGELOG
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

---
+++ packages/ui/Touchable/ClickAllow.js
@@ -0,0 +1,21 @@
+class ClickAllow {
+	constructor () {
+		this.lastMouseUpTime = 0;
+	}
+
+	setLastMouseUp (ev) {
+		if (ev.type === 'mouseup') {
+			this.lastMouseUpTime = ev.timeStamp;
+		}
+	}
+
+	shouldAllowTap (ev) {
+		const {type, timeStamp} = ev;
+		return !(type === 'click' && this.lastMouseUpTime === timeStamp);
+	}
+}
+
+export default ClickAllow;
+export {
+	ClickAllow
+};
