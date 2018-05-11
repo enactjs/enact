d6023a3d275c76d350e04b81dd02bc7aab414227
changgi.lee <changgi.lee@lge.com>
2018-05-11 09:03:09 +0900

    PLAT-57949: Add description to VoiceControlDecorator
    Add a description of how to use VoiceControlDecorator component
    
    Enact-DCO-1.0-Signed-off-by: Changgi Lee <changgi.lee@lge.com>

--- packages/webos/speech/VoiceControlDecorator.js
+++ packages/webos/speech/VoiceControlDecorator.js
@@ -4,7 +4,47 @@ import {findDOMNode} from 'react-dom';
 import hoc from '@enact/core/hoc';
 
 /**
- * Adds a callback for voice events
+ * VoiceControlDecorator is a Higher-order Component that adds a callback for voice event
+ * to its wrapped component.
+ *
+ * By default, `onVoice` handler will be added to the node where `data-webos-voice-intent` attribute is declared.
+ * In addition, if you need to specify a target node, `data-webos-voice-event-target` attribute can be used.
+ *
+ * import React from 'react';
+ * import Item from '@enact/moonstone/Item';
+ * import {VoiceControlDecorator} from '@enact/webos/speech';
+ *
+ * const VoiceDiv = VoiceControlDecorator('div');
+ * const VoiceItem = VoiceControlDecorator(Item);
+ *
+ * class Sample extends React.Component {
+ *   handlePlayListControl = (e) => {
+ *     const {intent, control} = e.detail;
+ *     // Change the app status refer to the `control` value.
+ *   }
+ *
+ *   handlePlayContent = (e) => {
+ *     const {intent, value} = e.detail;
+ *     // Play content
+ *   }
+ *
+ *   render () {
+ *     return(
+ *       <div>
+ *         <VoiceDiv
+ *           data-webos-voice-intent='PlayListControl'
+ *           onVoice={this.handlePlayListControl}
+ *         />
+ *         <VoiceItem
+ *           data-webos-voice-intent='Select PlayContent'
+ *           onVoice={this.handlePlayContent}
+ *         >
+ *           The Dark Knight
+ *         </VoiceItem>
+ *       </div>
+ *     );
+ *   }
+ * }
  *
  * @class VoiceControlDecorator
  * @memberof webos/speech
