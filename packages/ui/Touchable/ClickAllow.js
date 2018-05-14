8f090bd3e719c01b6c1f1c274999bd81fc075773
Ryan Duffy <ryan.duffy@lge.com>
2018-05-14 16:54:51 -0700

    add an inline comment
    Enact-DCO-1.0-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- packages/ui/Touchable/ClickAllow.js
+++ packages/ui/Touchable/ClickAllow.js
@@ -11,6 +11,9 @@ class ClickAllow {
 
 	shouldAllowTap (ev) {
 		const {type, timeStamp} = ev;
+
+		// Allow the custom tap event for a “click” when it’s actually a click and it’s not from the
+		// last mouseup event which would have fired the click for us
 		return type === 'click' && this.lastMouseUpTime !== timeStamp;
 	}
 }
