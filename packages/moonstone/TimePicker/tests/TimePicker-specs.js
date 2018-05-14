f1c3f885ec5df0bbf7a8f3a5edc65c80da819716
Baekwoo Jung <baekwoo.jung@lge.com>
2018-05-14 16:10:51 +0900

    clean up
    Enact-DCO-1.0-Signed-off-by: Baekwoo Jung (baekwoo.jung@lge.com)

--- packages/moonstone/TimePicker/tests/TimePicker-specs.js
+++ packages/moonstone/TimePicker/tests/TimePicker-specs.js
@@ -22,7 +22,7 @@ describe('TimePicker', () => {
 	it('should emit an onChange event when changing a component picker', function () {
 		const handleChange = sinon.spy();
 		const subject = mount(
-			<TimePicker title="Time" value={new Date(2000, 6, 15, 3, 30)} open onChange={handleChange} />
+			<TimePicker onChange={handleChange} open title="Time" value={new Date(2000, 6, 15, 3, 30)} />
 		);
 
 		const base = subject.find('DateComponentRangePicker').first();
@@ -36,7 +36,7 @@ describe('TimePicker', () => {
 
 	it('should omit labels when noLabels is true', function () {
 		const subject = mount(
-			<TimePickerBase title="Time" hour={1} minute={1} meridiem={0} meridiems={['am', 'pm']} order={['h', 'm']} open noLabels />
+			<TimePickerBase hour={1} meridiem={0} meridiems={['am', 'pm']} minute={1} noLabels open order={['h', 'm']} title="Time" />
 		);
 
 		const expected = 2;
@@ -47,7 +47,7 @@ describe('TimePicker', () => {
 
 	it('should create pickers arranged by order', function () {
 		const subject = mount(
-			<TimePickerBase title="Time" hour={1} minute={1} meridiem={0} meridiems={['am', 'pm']} order={['h', 'm']} open />
+			<TimePickerBase hour={1} meridiem={0} meridiems={['am', 'pm']} minute={1} open order={['h', 'm']} title="Time" />
 		);
 
 		const expected = ['hour', 'minute'];
@@ -58,7 +58,7 @@ describe('TimePicker', () => {
 
 	it('should accept a JavaScript Date for its value prop', function () {
 		const subject = mount(
-			<TimePicker title="Date" value={new Date(2000, 0, 1, 12, 30)} open />
+			<TimePicker open title="Date" value={new Date(2000, 0, 1, 12, 30)} />
 		);
 
 		const minutePicker = subject.find(`.${css.minutesComponents}`).at(0);
@@ -71,7 +71,7 @@ describe('TimePicker', () => {
 
 	it('should accept an updated JavaScript Date for its value prop', function () {
 		const subject = mount(
-			<TimePicker title="Date" value={new Date(2000, 0, 1, 12, 30)} open />
+			<TimePicker open title="Date" value={new Date(2000, 0, 1, 12, 30)} />
 		);
 
 		subject.setProps({
@@ -89,7 +89,7 @@ describe('TimePicker', () => {
 	it('should set "hourAriaLabel" to hour picker', function () {
 		const label = 'custom hour aria-label';
 		const subject = mount(
-			<TimePicker hourAriaLabel={label} title="Date" value={new Date(2000, 0, 1, 12, 30)} open />
+			<TimePicker hourAriaLabel={label} open title="Date" value={new Date(2000, 0, 1, 12, 30)} />
 		);
 
 		const hourPicker = subject.find(`.${css.hourComponents}`).at(0);
@@ -103,7 +103,7 @@ describe('TimePicker', () => {
 	it('should set "meridiemAriaLabel" to meridiem picker', function () {
 		const label = 'custom meridiem aria-label';
 		const subject = mount(
-			<TimePicker meridiemAriaLabel={label} title="Date" value={new Date(2000, 0, 1, 12, 30)} open />
+			<TimePicker meridiemAriaLabel={label} open title="Date" value={new Date(2000, 0, 1, 12, 30)} />
 		);
 
 		const meridiemPicker = subject.find(`.${css.meridiemComponent}`).at(0);
@@ -117,7 +117,7 @@ describe('TimePicker', () => {
 	it('should set "minuteAriaLabel" to minute picker', function () {
 		const label = 'custom minute aria-label';
 		const subject = mount(
-			<TimePicker minuteAriaLabel={label} title="Date" value={new Date(2000, 0, 1, 12, 30)} open />
+			<TimePicker minuteAriaLabel={label} open title="Date" value={new Date(2000, 0, 1, 12, 30)} />
 		);
 
 		const minutePicker = subject.find(`.${css.minutesComponents}`).at(0);
@@ -131,7 +131,7 @@ describe('TimePicker', () => {
 	it('should set "hourLabel" to hour picker', function () {
 		const label = 'custom hour label';
 		const subject = mount(
-			<TimePicker hourLabel={label} title="Date" value={new Date(2000, 0, 1, 12, 30)} open />
+			<TimePicker hourLabel={label} open title="Date" value={new Date(2000, 0, 1, 12, 30)} />
 		);
 
 		const hourPicker = subject.find(`.${css.hourComponents}`).at(0);
@@ -145,7 +145,7 @@ describe('TimePicker', () => {
 	it('should set "meridiemLabel" to meridiem picker', function () {
 		const label = 'custom meridiem label';
 		const subject = mount(
-			<TimePicker meridiemLabel={label} title="Date" value={new Date(2000, 0, 1, 12, 30)} open />
+			<TimePicker meridiemLabel={label} open title="Date" value={new Date(2000, 0, 1, 12, 30)} />
 		);
 
 		const meridiemPicker = subject.find(`.${css.meridiemComponent}`).at(0);
@@ -159,7 +159,7 @@ describe('TimePicker', () => {
 	it('should set "minuteLabel" to minute picker', function () {
 		const label = 'custom minute label';
 		const subject = mount(
-			<TimePicker minuteLabel={label} title="Date" value={new Date(2000, 0, 1, 12, 30)} open />
+			<TimePicker minuteLabel={label} open title="Date" value={new Date(2000, 0, 1, 12, 30)} />
 		);
 
 		const minutePicker = subject.find(`.${css.minutesComponents}`).at(0);
--- packages/moonstone/internal/Picker/tests/Picker-specs.js
+++ packages/moonstone/internal/Picker/tests/Picker-specs.js
@@ -16,7 +16,7 @@ describe('Picker Specs', function () {
 
 	it('should have a default \'value\' of 0', function () {
 		const picker = mount(
-			<Picker index={0} min={0} max={0} />
+			<Picker index={0} max={0} min={0} />
 		);
 
 		const expected = 0;
@@ -28,7 +28,7 @@ describe('Picker Specs', function () {
 	it('should return an object {value: Number} that represents the next value of the Picker component when pressing the increment <span>', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} min={-1} max={1} value={0} index={0} />
+			<Picker index={0} max={1} min={-1} onChange={handleChange} value={0} />
 		);
 
 		increment(picker);
@@ -42,7 +42,7 @@ describe('Picker Specs', function () {
 	it('should return an object {value: Number} that represents the next value of the Picker component when pressing the decrement <span>', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} min={-1} max={1} value={0} index={0} />
+			<Picker index={0} max={1} min={-1} onChange={handleChange} value={0} />
 		);
 
 		decrement(picker);
@@ -56,7 +56,7 @@ describe('Picker Specs', function () {
 	it('should not run the onChange handler when disabled', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} disabled min={0} max={0} value={0} index={0} />
+			<Picker disabled index={0} max={0} min={0} onChange={handleChange} value={0} />
 		);
 
 		increment(picker);
@@ -70,7 +70,7 @@ describe('Picker Specs', function () {
 	it('should wrap to the beginning of the value range if \'wrap\' is true', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} wrap min={-1} max={0} value={0} index={0} />
+			<Picker index={0} max={0} min={-1} onChange={handleChange} value={0} wrap />
 		);
 
 		increment(picker);
@@ -84,7 +84,7 @@ describe('Picker Specs', function () {
 	it('should wrap to the end of the value range if \'wrap\' is true', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} wrap min={0} max={1} value={0} index={0} />
+			<Picker index={0} max={1} min={0} onChange={handleChange} value={0} wrap />
 		);
 
 		decrement(picker);
@@ -98,7 +98,7 @@ describe('Picker Specs', function () {
 	it('should increment by \'step\' value', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} step={3} min={0} max={6} value={0} index={0} />
+			<Picker index={0} max={6} min={0} onChange={handleChange} step={3} value={0} />
 		);
 
 		increment(picker);
@@ -112,7 +112,7 @@ describe('Picker Specs', function () {
 	it('should decrement by \'step\' value', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} step={3} min={0} max={3} value={3} index={0} />
+			<Picker index={0} max={3} min={0} onChange={handleChange} step={3} value={3} />
 		);
 
 		decrement(picker);
@@ -126,7 +126,7 @@ describe('Picker Specs', function () {
 	it('should increment by \'step\' value and wrap successfully', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} wrap step={3} min={0} max={3} value={3} index={0} />
+			<Picker index={0} max={3} min={0} onChange={handleChange} step={3} value={3} wrap />
 		);
 
 		increment(picker);
@@ -140,7 +140,7 @@ describe('Picker Specs', function () {
 	it('should decrement by \'step\' value and wrap successfully', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} wrap step={3} min={0} max={9} value={0} index={0} />
+			<Picker index={0} max={9} min={0} onChange={handleChange} step={3} value={0} wrap />
 		);
 
 		decrement(picker);
@@ -153,7 +153,7 @@ describe('Picker Specs', function () {
 
 	it('should enable the increment button when there is a wrapped value to increment', function () {
 		const picker = mount(
-			<Picker wrap min={0} max={2} value={2} index={0} />
+			<Picker index={0} max={2} min={0} value={2} wrap />
 		);
 
 		const expected = false;
@@ -164,7 +164,7 @@ describe('Picker Specs', function () {
 
 	it('should enable the decrement button when there is a wrapped value to decrement', function () {
 		const picker = mount(
-			<Picker wrap min={0} max={2} value={2} index={0} />
+			<Picker index={0} max={2} min={0} value={2} wrap />
 		);
 
 		const expected = false;
@@ -175,7 +175,7 @@ describe('Picker Specs', function () {
 
 	it('should disable the increment button when there is no value to increment', function () {
 		const picker = mount(
-			<Picker min={0} max={2} value={2} index={0} />
+			<Picker index={0} max={2} min={0} value={2} />
 		);
 
 		const expected = true;
@@ -186,7 +186,7 @@ describe('Picker Specs', function () {
 
 	it('should disable the decrement button when there is no value to decrement', function () {
 		const picker = mount(
-			<Picker min={0} max={2} value={0} index={0} />
+			<Picker index={0} max={2} min={0} value={0} />
 		);
 
 		const expected = true;
@@ -197,7 +197,7 @@ describe('Picker Specs', function () {
 
 	it('should disable the increment and decrement buttons when wrapped and there is a single value', function () {
 		const picker = mount(
-			<Picker wrap min={0} max={0} value={0} index={0} />
+			<Picker index={0} max={0} min={0} value={0} wrap />
 		);
 
 		const expected = true;
@@ -210,7 +210,7 @@ describe('Picker Specs', function () {
 	it('should allow keyboard decrement via left arrow keys when \'joined\' and \'horizontal\'', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} min={-1} max={1} value={0} index={0} joined />
+			<Picker index={0} joined max={1} min={-1} onChange={handleChange} value={0} />
 		);
 
 		const expected = -1;
@@ -223,7 +223,7 @@ describe('Picker Specs', function () {
 	it('should allow keyboard increment via right arrow keys when \'joined\' and \'horizontal\'', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} min={-1} max={1} value={0} index={0} joined />
+			<Picker index={0} joined max={1} min={-1} onChange={handleChange} value={0} />
 		);
 
 		const expected = 1;
@@ -236,7 +236,7 @@ describe('Picker Specs', function () {
 	it('should allow keyboard decrement via down arrow keys when \'joined\' and \'vertical\'', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} min={-1} max={1} value={0} index={0} joined orientation="vertical" />
+			<Picker index={0} joined max={1} min={-1} onChange={handleChange} orientation="vertical" value={0} />
 		);
 
 		const expected = -1;
@@ -249,7 +249,7 @@ describe('Picker Specs', function () {
 	it('should allow keyboard decrement via up arrow keys when \'joined\' and \'vertical\'', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} min={-1} max={1} value={0} index={0} joined orientation="vertical" />
+			<Picker index={0} joined max={1} min={-1} onChange={handleChange} orientation="vertical" value={0} />
 		);
 
 		const expected = 1;
@@ -262,7 +262,7 @@ describe('Picker Specs', function () {
 	it('should not allow keyboard decrement via left arrow keys when \'joined\' and \'vertical\'', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} min={-1} max={1} value={0} index={0} joined orientation="vertical" />
+			<Picker index={0} joined max={1} min={-1} onChange={handleChange} orientation="vertical" value={0} />
 		);
 
 		const expected = false;
@@ -275,7 +275,7 @@ describe('Picker Specs', function () {
 	it('should not allow keyboard increment via right arrow keys when \'joined\' and \'vertical\'', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} min={-1} max={1} value={0} index={0} joined orientation="vertical" />
+			<Picker index={0} joined max={1} min={-1} onChange={handleChange} orientation="vertical" value={0} />
 		);
 
 		const expected = false;
@@ -288,7 +288,7 @@ describe('Picker Specs', function () {
 	it('should not allow keyboard decrement via down arrow keys when \'joined\' and \'horizontal\'', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} min={-1} max={1} value={0} index={0} joined orientation="horizontal" />
+			<Picker index={0} joined max={1} min={-1} onChange={handleChange} orientation="horizontal" value={0}  />
 		);
 
 		const expected = false;
@@ -301,7 +301,7 @@ describe('Picker Specs', function () {
 	it('should not allow keyboard increment via up arrow keys when \'joined\' and \'horizontal\'', function () {
 		const handleChange = sinon.spy();
 		const picker = mount(
-			<Picker onChange={handleChange} min={-1} max={1} value={0} index={0} joined orientation="horizontal" />
+			<Picker index={0} joined max={1} min={-1} onChange={handleChange} orientation="horizontal" value={0} />
 		);
 
 		const expected = false;
@@ -315,7 +315,7 @@ describe('Picker Specs', function () {
 
 		it('should set the aria-label attribute properly in the next icon button', function () {
 			const picker = mount(
-				<Picker index={1} value={1} min={0} max={3}>
+				<Picker index={1} max={3} min={0} value={1}>
 					<PickerItem>1</PickerItem>
 					<PickerItem>2</PickerItem>
 					<PickerItem>3</PickerItem>
@@ -331,7 +331,7 @@ describe('Picker Specs', function () {
 
 		it('should set the aria-label attribute properly in the previous icon button', function () {
 			const picker = mount(
-				<Picker index={1} value={1} min={0} max={3}>
+				<Picker index={1} max={3} min={0} value={1}>
 					<PickerItem>1</PickerItem>
 					<PickerItem>2</PickerItem>
 					<PickerItem>3</PickerItem>
@@ -347,7 +347,7 @@ describe('Picker Specs', function () {
 
 		it('should set the aria-valuetext attribute properly to read it when changing the value', function () {
 			const picker = mount(
-				<Picker index={1} value={1} min={0} max={3}>
+				<Picker index={1} max={3} min={0} value={1}>
 					<PickerItem>1</PickerItem>
 					<PickerItem>2</PickerItem>
 					<PickerItem>3</PickerItem>
@@ -363,7 +363,7 @@ describe('Picker Specs', function () {
 
 		it('should have aria-hidden=true when \'joined\' and not active', function () {
 			const picker = mount(
-				<Picker index={1} value={1} min={0} max={3} joined>
+				<Picker index={1} joined max={3} min={0} value={1}>
 					<PickerItem>1</PickerItem>
 					<PickerItem>2</PickerItem>
 					<PickerItem>3</PickerItem>
@@ -379,7 +379,7 @@ describe('Picker Specs', function () {
 
 		it('should be aria-hidden=false when \'joined\' and active', function () {
 			const picker = mount(
-				<Picker index={1} value={1} min={0} max={3} joined>
+				<Picker index={1} joined max={3} min={0} value={1}>
 					<PickerItem>1</PickerItem>
 					<PickerItem>2</PickerItem>
 					<PickerItem>3</PickerItem>
@@ -398,7 +398,7 @@ describe('Picker Specs', function () {
 		it('should set picker "decrementAriaLabel" to decrement button', () => {
 			const label = 'custom decrement aria-label';
 			const picker = mount(
-				<Picker decrementAriaLabel={label} index={1} value={1} min={0} max={3}>
+				<Picker decrementAriaLabel={label} index={1} max={3} min={0} value={1}>
 					<PickerItem>1</PickerItem>
 					<PickerItem>2</PickerItem>
 					<PickerItem>3</PickerItem>
@@ -415,7 +415,7 @@ describe('Picker Specs', function () {
 		it('should set picker "incrementAriaLabel" to decrement button', () => {
 			const label = 'custom increment aria-label';
 			const picker = mount(
-				<Picker incrementAriaLabel={label} index={1} value={1} min={0} max={3}>
+				<Picker incrementAriaLabel={label} index={1} max={3} min={0} value={1}>
 					<PickerItem>1</PickerItem>
 					<PickerItem>2</PickerItem>
 					<PickerItem>3</PickerItem>
@@ -432,7 +432,7 @@ describe('Picker Specs', function () {
 		it('should set "aria-label" to joined picker', () => {
 			const label = 'custom joined picker aria-label';
 			const picker = mount(
-				<Picker aria-label={label} index={1} value={1} min={0} max={3} joined>
+				<Picker aria-label={label} index={1} joined max={3} min={0} value={1}>
 					<PickerItem>1</PickerItem>
 					<PickerItem>2</PickerItem>
 					<PickerItem>3</PickerItem>
