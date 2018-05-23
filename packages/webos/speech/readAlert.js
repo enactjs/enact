d7faaae9e2718cb53023b22994259e8cbbc557bd
yb.sung <yb.sung@lge.com>
2018-05-23 21:27:49 +0900

    Check audioGuidance option when visibiligy changed
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- packages/webos/speech/readAlert.js
+++ packages/webos/speech/readAlert.js
@@ -4,6 +4,44 @@
 import LS2Request from '../LS2Request';
 import {platform} from '../platform';
 
+let audioGuidance = 'checking';
+
+const checkAudioGuidance = (string, clear, callback) => new LS2Request().send({
+	service: 'luna://com.webos.settingsservice',
+	method: 'getSystemSettings',
+	parameters: {'keys': ['audioGuidance'], 'category': 'option'},
+	onSuccess: function (res) {
+		if (res && res.settings.audioGuidance === 'on') {
+			audioGuidance = 'on';
+			if (callback) {
+				callback(string, clear);
+			}
+		} else {
+			audioGuidance = 'off';
+		}
+	},
+	onFailure: function (err) {
+		console.error('Failed to get system AudioGuidance settings: ' + JSON.stringify(err));
+	}
+});
+
+const readAlertMessage = (string, clear) => new LS2Request().send({
+	service: 'luna://com.webos.service.tts',
+	method: 'speak',
+	parameters: {'text': string, 'clear': clear},
+	onFailure: (err) => {
+		console.error('Failed to readAlertMessage: ' + JSON.stringify(err));
+	}
+});
+
+document.addEventListener('visibilitychange', function () {
+	if (document.visibilityState === 'hidden') {
+		audioGuidance = 'checking';
+	} else if (document.visibilityState === 'visible') {
+		checkAudioGuidance();
+	}
+});
+
 /**
  * Read alert text when accessibility VoiceReadout enabled.
  *
@@ -15,34 +53,17 @@ import {platform} from '../platform';
  */
 const readAlert = (string, clear = true) => {
 	if (platform.tv) {
-		const checkAudioGuidance = (callback) => new LS2Request().send({
-			service: 'luna://com.webos.settingsservice',
-			method: 'getSystemSettings',
-			parameters: {'keys': ['audioGuidance'], 'category': 'option'},
-			onSuccess: function (res) {
-				if (res && res.settings.audioGuidance === 'on') {
-					callback();
-				}
-			},
-			onFailure: function (err) {
-				console.error('Failed to get system AudioGuidance settings: ' + JSON.stringify(err));
-			}
-		});
-
-		const readAlertMessage = () => new LS2Request().send({
-			service: 'luna://com.webos.service.tts',
-			method: 'speak',
-			parameters: {'text': string, 'clear': clear},
-			onFailure: (err) => {
-				console.error('Failed to readAlertMessage: ' + JSON.stringify(err));
-			}
-		});
-
-		checkAudioGuidance(readAlertMessage);
+		if (audioGuidance === 'on') {
+			readAlertMessage(string, clear);
+		} else if (audioGuidance === 'checking') {
+			checkAudioGuidance(string, clear, readAlertMessage);
+		}
 	} else {
 		console.warn('Platform doesn\'t support TTS api.');
 	}
 };
+
+checkAudioGuidance();
 
 export default readAlert;
 export {readAlert};
