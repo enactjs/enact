b69375fb71bf1c6f8c24d1e887c42db113b54045
changgi.lee <changgi.lee@lge.com>
2018-06-18 09:45:13 +0900

    PLAT-61063: Modify comment
    Modify comment
    
    Enact-DCO-1.0-Signed-off-by: Changgi Lee <changgi.lee@lge.com>

--- packages/moonstone/ExpandablePicker/ExpandablePicker.js
+++ packages/moonstone/ExpandablePicker/ExpandablePicker.js
@@ -45,7 +45,7 @@ const ExpandablePickerBase = kind({
 		children: PropTypes.node.isRequired,
 
 		/**
-		 * The `data-webos-voice-group-label` for the Picker.
+		 * The `data-webos-voice-group-label` for LabeledItem and Picker.
 		 *
 		 * @type {String}
 		 * @public
