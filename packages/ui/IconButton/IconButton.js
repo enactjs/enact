61731b4afcc39ce496d92995d6a28ddf56dfb868
Ryan Duffy <ryan.duffy@lge.com>
2018-07-17 13:29:29 -0500

    add back handler support
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- packages/ui/IconButton/IconButton.js
+++ packages/ui/IconButton/IconButton.js
@@ -164,7 +164,7 @@ const IconButtonBase = kind({
 			minWidth: false,
 			children: [
 				<Icon small={small} className={css.icon}>{icon}</Icon>,
-				children
+				...React.Children.toArray(children)
 			]
 		});
 	}
