1a505c3b4bbd68a4a186949545abc341cd3da32e
Blake Stephens <blake.stephens@lge.com>
2018-07-26 13:48:39 -0700

    Properly use the component in the test

--- packages/ui/LabeledIcon/tests/LabeledIcon-specs.js
+++ packages/ui/LabeledIcon/tests/LabeledIcon-specs.js
@@ -6,12 +6,27 @@ import css from '../LabeledIcon.less';
 
 const iconName = 'anIconName';
 const iconLabel = 'A real label';
+const iconComponent = 'div';
 
 describe('LabeledIcon Specs', () => {
+	it('should insert the icon source into the icon when using the prop approach', function () {
+
+		const labeledIcon = mount(
+			<LabeledIcon icon={iconName} iconComponent={iconComponent}>
+				{iconLabel}
+			</LabeledIcon>
+		);
+
+		const expected = iconName;
+		const actual = labeledIcon.find(`.${css.icon}`).first().text();
+
+		expect(actual).to.equal(expected);
+	});
+
 	it('should insert the icon source into the icon slot element', function () {
 
 		const labeledIcon = mount(
-			<LabeledIcon>
+			<LabeledIcon iconComponent={iconComponent}>
 				<icon>{iconName}</icon>
 				{iconLabel}
 			</LabeledIcon>
@@ -25,7 +40,7 @@ describe('LabeledIcon Specs', () => {
 
 	it('should insert custom icon components into the icon slot', function () {
 		const labeledIcon = mount(
-			<LabeledIcon>
+			<LabeledIcon iconComponent={iconComponent}>
 				<icon><CustomIcon>{iconName}</CustomIcon></icon>
 				{iconLabel}
 			</LabeledIcon>
