f9bee72c43854def44f0b4f87c62137d104bfa50
Ryan Duffy <ryan.duffy@lge.com>
2018-07-31 14:08:04 -0700

    Update changeable docs
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- packages/ui/Changeable/Changeable.js
+++ packages/ui/Changeable/Changeable.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/sort-prop-types */
 
 /**
- * Exports the {@link ui/Changeable.Changeable} higher-order component (HOC).
+ * Higher-order component to add state management for a single prop via a single event handler.
  *
  * @module ui/Changeable
  * @exports Changeable
@@ -41,12 +41,22 @@ const defaultConfig = {
 };
 
 /**
- * {@link ui/Changeable.Changeable} is a higher-order component (HOC) that applies a
- * 'Changeable' behavior to its wrapped component.  Its default event and value properties can be
- * configured when applied to a component.
+ * Adds state management to a component for a single prop via a single event callback.
  *
- * If the `prop` is overridden, the property names to set the default value and current value change
- * correspondingly.
+ * Applying `Changeable` to a component will pass two additional props: the current value from state
+ * and an event callback to invoke when the value changes. By default, the value is passed in the
+ * `value` prop and the callback is passed in the `onChange` callback but both are configurable
+ * through the HOC config object.
+ *
+ * If `value` is passed to `Changeable`, the HOC assumes that the value is managed elsewhere and it
+ * will not update its internal state. To set an initial value, use `defaultValue` instead.
+ *
+ * To update the value from the wrapped component, call `onChange` with an object containing a
+ * `value` member with the new value. `Changeable` will update its internal state and pass the
+ * updated value back down to the wrapped component.
+ *
+ * *Note:* If the `prop` is overridden, the property names to set the default value and current
+ * value change correspondingly.
  *
  * @class Changeable
  * @memberof ui/Changeable
@@ -62,27 +72,34 @@ const Changeable = hoc(defaultConfig, (config, Wrapped) => {
 
 		static propTypes = /** @lends ui/Changeable.Changeable.prototype */ {
 			/**
-			 * Event callback to notify that the value has changed. The event object must contain a
-			 * property with the same name as the configured `prop`.
+			 * Called to notify `Changeable` that the value has changed.
 			 *
+			 * The event object must contain a property with the same name as the configured `prop`.
+			 *
+			 * @name onChange
 			 * @type {Function}
 			 * @public
 			 */
 			[change]: PropTypes.func,
 
 			/**
-			 * Default value applied at construction when the value prop is `undefined` or `null`.
+			 * The value set at construction when the value prop is `undefined` or `null`.
 			 *
+			 * This prop is consumed by `Changeable` and not passed onto the wrapped component.
+			 *
+			 * @name defaultValue
 			 * @type {*}
 			 * @public
 			 */
 			[defaultPropKey]: PropTypes.any,
 
 			/**
-			 * Current value. When set at construction, the component is considered "controlled" and
-			 * will only update its internal value when updated by new props. If `undefined`, the
-			 * component is "uncontrolled" and `Changeable` will manage the value using callbacks
-			 * defined by its configuration.
+			 * The current value.
+			 *
+			 * When set at construction, the component is considered "controlled" and will only
+			 * update its internal value when updated by new props. If `undefined`, the component is
+			 * "uncontrolled" and `Changeable` will manage the value using callbacks defined by its
+			 * configuration.
 			 *
 			 * @type {*}
 			 * @public
@@ -90,7 +107,9 @@ const Changeable = hoc(defaultConfig, (config, Wrapped) => {
 			[prop]: PropTypes.any,
 
 			/**
-			 * Controls whether the component is disabled.
+			 * Prevents updates to the internal state of `Changeable`.
+			 *
+			 * `disabled` is forwarded on to the wrapped component.
 			 *
 			 * @type {Boolean}
 			 * @default false
@@ -160,4 +179,6 @@ const Changeable = hoc(defaultConfig, (config, Wrapped) => {
 });
 
 export default Changeable;
-export {Changeable};
+export {
+	Changeable
+};
