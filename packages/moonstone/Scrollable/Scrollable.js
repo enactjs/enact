c6879aa7f0773039c3011e6cbe89983b61d38721
Ryan Duffy <ryan.duffy@lge.com>
2018-08-13 14:10:43 -0700

    Update Scrollable.js

--- packages/moonstone/Scrollable/Scrollable.js
+++ packages/moonstone/Scrollable/Scrollable.js
@@ -546,6 +546,7 @@ class ScrollableBase extends Component {
 		}
 	}
 
+	// Callback for scroller updates; calculate and, if needed, scroll to new position based on focused item.
 	handleScrollerUpdate = () => {
 		if (this.uiRef.scrollToInfo === null && Spotlight.getPointerMode()) {
 			const scrollHeight = this.uiRef.getScrollBounds().scrollHeight;
