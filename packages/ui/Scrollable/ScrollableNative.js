f737c8e28e7f7b37b0b4d344cdb8b9cdb393ae95
Seungcheon Baek <sc.baek@lge.com>
2018-08-14 15:45:38 +0900

    Refactored overscroll logic

--- packages/ui/Scrollable/ScrollableNative.js
+++ packages/ui/Scrollable/ScrollableNative.js
@@ -643,7 +643,7 @@ class ScrollableBaseNative extends Component {
 						delta = this.calculateDistanceByWheel(eventDeltaMode, eventDelta, bounds.clientHeight * scrollWheelPageMultiplierForMaxPixel);
 						needToHideThumb = !delta;
 					} else if (this.overscrollEnabled) {
-						this.checkAndApplyOverscrollEffect('vertical', eventDelta > 0 ? 'after' : 'before', overscrollTypeOnce, 1);
+						this.checkAndApplyOverscrollEffect('vertical', eventDelta > 0 ? 'after' : 'before', overscrollTypeOnce);
 					}
 				} else {
 					if (this.overscrollEnabled && eventDelta < 0 && this.scrollTop <= 0 || eventDelta > 0 && this.scrollTop >= bounds.maxTop) {
@@ -748,7 +748,9 @@ class ScrollableBaseNative extends Component {
 	getEdgeFromPosition = (position, maxPosition) => {
 		if (position <= 0) {
 			return 'before';
-		} else if (position >= maxPosition) {
+		/* If a scroll size or a client size is not integer,
+		   browsers's max scroll position could be smaller than maxPos by 1 pixel.*/
+		} else if (position >= maxPosition - 1) {
 			return 'after';
 		} else {
 			return null;
@@ -787,26 +789,28 @@ class ScrollableBaseNative extends Component {
 		this.setOverscrollStatus(orientation, edge, type === overscrollTypeOnce ? overscrollTypeDone : type, ratio);
 	}
 
-	checkAndApplyOverscrollEffect = (orientation, edge, type, ratio) => {
+	checkAndApplyOverscrollEffect = (orientation, edge, type, ratio = 1) => {
 		const
 			isVertical = (orientation === 'vertical'),
 			curPos = isVertical ? this.scrollTop : this.scrollLeft,
 			maxPos = this.getScrollBounds()[isVertical ? 'maxTop' : 'maxLeft'];
 
-		if (edge === 'before' && curPos <= 0) { // On the beginning edge
-			this.applyOverscrollEffect(orientation, 'before', type, ratio);
-		} else if (edge === 'after' && curPos >= maxPos) { // On the ending edge
-			this.applyOverscrollEffect(orientation, 'after', type, ratio);
+		/* If a scroll size or a client size is not integer,
+		   browsers's max scroll position could be smaller than maxPos by 1 pixel.*/
+		if ((edge === 'before' && curPos <= 0) || (edge === 'after' && curPos >= maxPos - 1)) { // Already on the edge
+			this.applyOverscrollEffect(orientation, edge, type, ratio);
 		} else {
 			this.setOverscrollStatus(orientation, edge, type, ratio);
 		}
 	}
 
 	clearOverscrollEffect = (orientation, edge) => {
-		if (this.props.clearOverscrollEffect) {
-			this.props.clearOverscrollEffect(orientation, edge);
-		} else if (this.getOverscrollStatus(orientation, edge).type !== overscrollTypeNone) {
-			this.applyOverscrollEffect(orientation, edge, overscrollTypeNone, 0);
+		if (this.getOverscrollStatus(orientation, edge).type !== overscrollTypeNone) {
+			if (this.props.clearOverscrollEffect) {
+				this.props.clearOverscrollEffect(orientation, edge);
+			} else {
+				this.applyOverscrollEffect(orientation, edge, overscrollTypeNone, 0);
+			}
 		}
 	}
 
@@ -859,7 +863,7 @@ class ScrollableBaseNative extends Component {
 			if (this.isDragging) {
 				this.applyOverscrollEffectOnDrag(orientation, edge, targetPosition, overscrollTypeHold);
 			} else if (this.getOverscrollStatus(orientation, edge).type === overscrollTypeNone) {
-				this.checkAndApplyOverscrollEffect(orientation, edge, overscrollTypeOnce, 1);
+				this.checkAndApplyOverscrollEffect(orientation, edge, overscrollTypeOnce);
 			}
 		}
 	}
