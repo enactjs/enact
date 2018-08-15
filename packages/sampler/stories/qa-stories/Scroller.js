da9c040dd85df41896fc07325f91ba762019a5a5
Lucie Roy <lucie.roy@lge.com>
2018-08-15 15:08:42 -0700

    Added sample with many ExpandableLists

--- packages/sampler/stories/qa-stories/Scroller.js
+++ packages/sampler/stories/qa-stories/Scroller.js
@@ -2,6 +2,8 @@ import Button from '@enact/moonstone/Button';
 import ExpandableList from '@enact/moonstone/ExpandableList';
 import Scroller from '@enact/moonstone/Scroller';
 import Item from '@enact/moonstone/Item';
+import Divider from '@enact/moonstone/Divider';
+import BodyText from '@enact/moonstone/BodyText';
 import ri from '@enact/ui/resolution';
 import Group from '@enact/ui/Group';
 import React from 'react';
@@ -58,7 +60,7 @@ storiesOf('Scroller', module)
 		'Horizontal scroll',
 		() => (
 			<Scroller
-				direction={select('direction', prop.direction, Scroller, 'horizontal')}
+				direction={select('direction', prop.direction, Scroller, 'vertical')}
 				focusableScrollbar={boolean('focusableScrollbar', Scroller, false)}
 				horizontalScrollbar={select('horizontalScrollbar', prop.horizontalScrollbar, Scroller, 'auto')}
 				onScrollStart={action('onScrollStart')}
@@ -81,4 +83,82 @@ storiesOf('Scroller', module)
 				</div>
 			</Scroller>
 		)
-	);
+	)
+	.add(
+		'With Many ExpandableList',
+		() => (
+			<Scroller
+				direction={select('direction', prop.direction, Scroller, 'vertical')}
+				focusableScrollbar={boolean('focusableScrollbar', Scroller, false)}
+			  horizontalScrollbar={select('horizontalScrollbar', prop.horizontalScrollbar, Scroller, 'auto')}
+				onScrollStart={action('onScrollStart')}
+				onScrollStop={action('onScrollStop')}
+				style={{
+					width: '100%'
+				}}
+			>
+				<Divider>Nothing selected</Divider>
+				<ExpandableList
+					noneText="Nothing Selected"
+					title="Default"
+					closeOnSelect
+				>
+					{['Option 1', 'Option 2', 'Option 3',
+						'Option 4', 'Option 5', 'Option 6',
+						'Option 7', 'Option 8', 'Option 9',
+						'Option 10', 'Option 11', 'Option 12',
+						'Option 13', 'Option 14', 'Option 15',
+						'Option 16', 'Option 17', 'Option 18',
+						'Option 19', 'Option 20'
+					]}
+				</ExpandableList>
+				<br />
+				<Divider>Default selected</Divider>
+				<ExpandableList
+					noneText="Nothing Selected"
+					title="Default"
+					selected={1}
+				>
+					{['Option 1', 'Option 2', 'Option 3']}
+				</ExpandableList>
+				<br />
+				<Divider>Default selected</Divider>
+				<ExpandableList
+					noneText="Nothing Selected"
+					title="Default"
+					selected={1}
+				>
+					{['Option 1', 'Option 2', 'Option 3']}
+				</ExpandableList>
+				<br />
+				<Divider>Default selected</Divider>
+				<ExpandableList
+					noneText="Nothing Selected"
+					title="Default"
+					selected={1}
+				>
+					{['Option 1', 'Option 2', 'Option 3']}
+				</ExpandableList>
+				<br />
+				<Divider>Multitple selected</Divider>
+				<ExpandableList
+					noneText="Nothing Selected"
+					title="multiple"
+					select="multiple"
+					selected={[1, 2]}
+				>
+					{['Option 1', 'Option 2', 'Option 3']}
+				</ExpandableList>
+				<br />
+				<Divider>Long contents selected</Divider>
+				<ExpandableList
+					noneText="Nothing Selected"
+					title="multiple"
+					select="multiple"
+					selected={[18, 19]}
+				>
+					{['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5', 'Option 6', 'Option 7', 'Option 8', 'Option 9', 'Option 10', 'Option 11', 'Option 12', 'Option 13', 'Option 14', 'Option 15', 'Option 16', 'Option 17', 'Option 18', 'Option 19', 'Option 20']}
+				</ExpandableList>
+			</Scroller>
+	  )
+	 );
