101afe664f6c772083098e24315b94e5ea894550
Sangwook-Lee <sangwook1203.lee@lge.com>
2018-09-05 02:00:40 +0900

    ENYO-5604: Tooltip is not repositioned when text changed (#1925)
    * add  function to calculate position when text changed
    
    * updated CHANGELOG
    
    * added test case for qa-sampler
    
    * guard call to setTooltipLayout with showing state
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * fix sample
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- packages/moonstone/TooltipDecorator/TooltipDecorator.js
+++ packages/moonstone/TooltipDecorator/TooltipDecorator.js
@@ -169,6 +169,12 @@ const TooltipDecorator = hoc(defaultConfig, (config, Wrapped) => {
 			};
 		}
 
+		componentDidUpdate (prevProps) {
+			if (this.state.showing && prevProps.tooltipText !== this.props.tooltipText) {
+				this.setTooltipLayout();
+			}
+		}
+
 		componentWillUnmount () {
 			if (currentTooltip === this) {
 				currentTooltip = null;
