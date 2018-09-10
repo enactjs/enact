3a81bf3a7fb2d6f21a859a412ecd150204451cc9
Stephen Choi <stephen.choi@lge.com>
2018-09-10 14:53:58 -0700

    fix keydown handler deletion in ui

--- packages/ui/Scrollable/ScrollableNative.js
+++ packages/ui/Scrollable/ScrollableNative.js
@@ -1280,7 +1280,6 @@ class ScrollableBaseNative extends Component {
 		delete rest.clearOverscrollEffect;
 		delete rest.horizontalScrollbar;
 		delete rest.onFlick;
-		delete rest.onKeyDown;
 		delete rest.onMouseDown;
 		delete rest.onScroll;
 		delete rest.onScrollStart;
