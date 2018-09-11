e422a219f6ef5df9bc2fa68b59a7f7a67b1c922c
sangwook1203.lee <sangwook1203.lee@lge.com>
2018-09-11 10:16:21 +0900

    added qa-sampler

--- packages/sampler/stories/qa/IncrementSlider.js
+++ packages/sampler/stories/qa/IncrementSlider.js
@@ -1,6 +1,37 @@
 import IncrementSliderDelayValue from './components/IncrementSliderDelayValue';
 import React from 'react';
+import IncrementSlider from '@enact/moonstone/IncrementSlider';
+import Button from '@enact/moonstone/Button';
+import ri from '@enact/ui/resolution';
 import {storiesOf} from '@storybook/react';
+
+class IncrementSliderView extends React.Component {
+
+	constructor (props) {
+		super(props);
+		this.state = {
+			value: 0
+		};
+	}
+
+	handleChange = (ev) => {
+		setTimeout(() => {
+			this.setState({value: ev.value});
+		}, 200);
+	}
+
+	render () {
+		return (
+			<div style={{display: 'flex', marginTop: ri.unit(180, 'rem')}}>
+				<div style={{width: '300px'}}>
+					<Button>button</Button>
+					<Button>button</Button>
+				</div>
+				<IncrementSlider style={{flex: 1, width: ri.unit(510, 'rem')}} onChange={this.handleChange} value={this.state.value} />
+			</div>
+		);
+	}
+}
 
 storiesOf('IncrementSlider', module)
 	.add(
@@ -9,6 +40,15 @@ storiesOf('IncrementSlider', module)
 			<div>
 				Focus on one of the IncrementSlider buttons. Every 5 seconds, the value will toggle between 0 and 100. Ensure that focus does not leave the IncrementSlider when this happens.
 				<IncrementSliderDelayValue />
+			</div>
+		)
+	)
+	.add(
+		'spotlight behavior while dragging',
+		() => (
+			<div>
+				While holding down the knob(dargging), move the cursor quickly between knob and SliderButtons.
+				<IncrementSliderView />
 			</div>
 		)
 	);
