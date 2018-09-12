0667f1d884ac534c9b453b2d85342cca3d297c43
yb.sung <yb.sung@lge.com>
2018-09-12 14:46:53 +0900

    Revert "Cleanup"
    This reverts commit dcf1e80b67d7689f08c2ee419ffd957b04929b60.

--- packages/moonstone/Scrollable/ScrollableNative.js
+++ packages/moonstone/Scrollable/ScrollableNative.js
@@ -470,10 +470,11 @@ class ScrollableBaseNative extends Component {
 				endPoint = {
 					x: focusedItemBounds.left + focusedItemBounds.width / 2,
 					y: viewportBounds.top + ((direction === 'up') ? focusedItemBounds.height / 2 - 1 : viewportBounds.height - focusedItemBounds.height / 2 + 1)
-				};
+				},
+				isPointerMode = Spotlight.getPointerMode();
 			let next = null;
 
-			if (Spotlight.getPointerMode()) {
+			if (isPointerMode) {
 				// We need to convert to 5-way key mode to move Spot to another item manually.
 				Spotlight.setPointerMode(false);
 			}
@@ -497,7 +498,7 @@ class ScrollableBaseNative extends Component {
 				this.childRef.scrollToNextItem({direction, focusedItem, reverseDirection: rDirection, spotlightId});
 			}
 
-			if (Spotlight.getPointerMode()) {
+			if (isPointerMode) {
 				// It is not converted to 5-way key mode even though pressing a channel up or down keys in pointer mode.
 				// So we need to convert back to the pointer mode.
 				Spotlight.setPointerMode(true);
