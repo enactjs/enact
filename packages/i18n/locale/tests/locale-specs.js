3629941807a4922239f9fcf253193ce50b80bea8
Roy Sutton <roy.sutton@lge.com>
2018-11-07 13:02:44 -0500

    Update locale tests
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- packages/i18n/locale/tests/locale-specs.js
+++ packages/i18n/locale/tests/locale-specs.js
@@ -1,71 +1,58 @@
 import '../../src/glue.js';
 import {isNonLatinLocale, updateLocale} from '../locale';
 
+const validate = (expected) => (actual) => {
+	expect(actual ? 'non-latin' : 'latin').to.equal(expected);
+};
+
 describe('locale', () => {
 
-	it('should treat "en-US" as latin locale', (done) => {
-		function onLoad (nonLatin) { done(nonLatin); }
-
-		isNonLatinLocale('en-US', {sync: true, onLoad});
+	it('should treat "en-US" as latin locale', () => {
+		isNonLatinLocale('en-US', {sync: true, onLoad: validate('latin')});
 	});
 
-	it('should treat "ja-JP" as non-latin locale', (done) => {
-		function onLoad (nonLatin) { done(!nonLatin); }
-
-		updateLocale('ja-JP');
-
-		// eslint-disable-next-line no-undefined
-		isNonLatinLocale(undefined, {sync: true, onLoad});
+	it('should treat "ja-JP" as non-latin locale', () => {
+		isNonLatinLocale('ja-JP', {sync: true, onLoad: validate('non-latin')});
 	});
 
-	it('should treat "en-US" as non-latin locale with language override', (done) => {
-		function onLoad (nonLatin) { done(!nonLatin); }
-
+	it('should treat "en-US" as non-latin locale with language override', () => {
 		isNonLatinLocale('en-US', {
 			sync: true,
 			nonLatinLanguageOverrides: ['en'],
-			onLoad
+			onLoad: validate('non-latin')
 		});
 	});
 
-	it('should treat "en-US" as non-latin locale with locale override', (done) => {
-		function onLoad (nonLatin) { done(!nonLatin); }
-
+	it('should treat "en-US" as non-latin locale with locale override', () => {
 		isNonLatinLocale('en-US', {
 			sync: true,
 			nonLatinLanguageOverrides: ['en-US'],
-			onLoad
+			onLoad: validate('non-latin')
 		});
 	});
 
-	it('should treat "ja-JP" as latin locale with language override', (done) => {
-		function onLoad (nonLatin) { done(nonLatin); }
-
+	it('should treat "ja-JP" as latin locale with language override', () => {
 		isNonLatinLocale('ja-JP', {
 			sync: true,
 			latinLanguageOverrides: ['ja'],
-			onLoad
+			onLoad: validate('latin')
 		});
 	});
 
-	it('should treat "ja-JP" as latin locale with locale override', (done) => {
-		function onLoad (nonLatin) { done(nonLatin); }
-
+	it('should treat "ja-JP" as latin locale with locale override', () => {
 		isNonLatinLocale('ja-JP', {
 			sync: true,
 			latinLanguageOverrides: ['ja-JP'],
-			onLoad
+			onLoad: validate('latin')
 		});
 	});
 
-	it('should treat "en-US" as latin locale with irrelevant override', (done) => {
-		function onLoad (nonLatin) { done(nonLatin); }
-
+	it('should treat "en-US" as latin locale with irrelevant override', () => {
 		isNonLatinLocale('en-US', {
 			sync: true,
 			latinLanguageOverrides: ['ja'],
 			nonLatinLanguageOverrides: ['es-US'],
-			onLoad
+			onLoad: validate('latin')
 		});
 	});
 });
