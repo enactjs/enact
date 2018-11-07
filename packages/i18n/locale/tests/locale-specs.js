5cd7d91a4ec5b0d68c43cbeec96a1e2201109d9c
Roy Sutton <roy.sutton@lge.com>
2018-11-07 12:20:34 -0500

    Add locale tests
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

---
+++ packages/i18n/locale/tests/locale-specs.js
@@ -0,0 +1,69 @@
+import {isNonLatinLocale, updateLocale} from '../locale';
+
+describe('locale', () => {
+
+	it('should treat "en-US" as latin locale', (done) => {
+		function onLoad (nonLatin) { done(nonLatin); }
+
+		isNonLatinLocale('en-US', {sync: true, onLoad});
+	});
+
+	it.only('should treat "ja-JP" as non-latin locale', (done) => {
+		function onLoad (nonLatin) { done(!nonLatin); }
+
+		updateLocale('ja-JP');
+
+		isNonLatinLocale(undefined, {sync: true, onLoad});
+	});
+
+	it('should treat "en-US" as non-latin locale with language override', (done) => {
+		function onLoad (nonLatin) { done(!nonLatin); }
+
+		isNonLatinLocale('en-US', {
+			sync: true,
+			nonLatinLanguageOverrides: ['en'],
+			onLoad
+		});
+	});
+
+	it('should treat "en-US" as non-latin locale with locale override', (done) => {
+		function onLoad (nonLatin) { done(!nonLatin); }
+
+		isNonLatinLocale('en-US', {
+			sync: true,
+			nonLatinLanguageOverrides: ['en-US'],
+			onLoad
+		});
+	});
+
+	it('should treat "ja-JP" as latin locale with language override', (done) => {
+		function onLoad (nonLatin) { done(nonLatin); }
+
+		isNonLatinLocale('ja-JP', {
+			sync: true,
+			latinLanguageOverrides: ['ja'],
+			onLoad
+		});
+	});
+
+	it('should treat "ja-JP" as latin locale with locale override', (done) => {
+		function onLoad (nonLatin) { done(nonLatin); }
+
+		isNonLatinLocale('ja-JP', {
+			sync: true,
+			latinLanguageOverrides: ['ja-JP'],
+			onLoad
+		});
+	});
+
+	it('should treat "en-US" as latin locale with irrelevant override', (done) => {
+		function onLoad (nonLatin) { done(nonLatin); }
+
+		isNonLatinLocale('en-US', {
+			sync: true,
+			latinLanguageOverrides: ['ja'],
+			nonLatinLanguageOverrides: ['es-US'],
+			onLoad
+		});
+	});
+});
