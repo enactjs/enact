01ea414f318ba6c6e668112dcaad35642c6766a6
Ryan Duffy <ryan.duffy@lge.com>
2018-12-12 08:27:07 -0800

    update spotlight docs
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- packages/spotlight/src/spotlight.js
+++ packages/spotlight/src/spotlight.js
@@ -574,8 +574,9 @@ const Spotlight = (function () {
 		 * Sets the config for spotlight or the specified containerID
 		 *
 		 * @function
-		 * @param {String|Object} param1 Configuration object or container ID
-		 * @param {Object|undefined} param2 Configuration object if container ID supplied in param1
+		 * @param {String|Object} containerIdOrConfig  Configuration object or container ID
+		 * @param {Object}        [config]             Configuration object if container ID supplied
+		 *                                             in `containerIdOrConfig`
 		 * @returns {undefined}
 		 * @public
 		 */
@@ -588,8 +589,9 @@ const Spotlight = (function () {
 		 * object. If no container ID is supplied, a new container ID will be generated.
 		 *
 		 * @function
-		 * @param {String|Object} param1 Configuration object or container ID
-		 * @param {Object|undefined} param2 Configuration object if container ID supplied in param1
+		 * @param {String|Object} containerIdOrConfig  Configuration object or container ID
+		 * @param {Object}        [config]             Configuration object if container ID supplied
+		 *                                             in `containerIdOrConfig`
 		 * @returns {String} The container ID of the container
 		 * @public
 		 */
@@ -760,7 +762,8 @@ const Spotlight = (function () {
 		 * Sets or clears the default container that will receive focus.
 		 *
 		 * @function
-		 * @param {String|undefined} containerId The container ID or a falsy value to clear default container
+		 * @param {String} [containerId] The container ID or a falsy value to clear default
+		 *                               container
 		 * @returns {undefined}
 		 * @public
 		 */
@@ -853,7 +856,7 @@ const Spotlight = (function () {
 		/**
 		 * Returns the currently spotted control.
 		 *
-		 * @returns {Object} The control that currently has focus, if available
+		 * @returns {Node} The control that currently has focus, if available
 		 * @public
 		 */
 		getCurrent: function () {
@@ -864,7 +867,7 @@ const Spotlight = (function () {
 		 * Returns a list of spottable elements wrapped by the supplied container.
 		 *
 		 * @param {String} [containerId] The id of the container used to determine the list of spottable elements
-		 * @returns {NodeList} The spottable elements that are wrapped by the supplied container
+		 * @returns {Node[]} The spottable elements that are wrapped by the supplied container
 		 * @public
 		 */
 		getSpottableDescendants: function (containerId) {
