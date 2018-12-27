3474348bf3161ebdfaecab9d5df7c2a1dc370905
Roy Sutton <roy.sutton@lge.com>
2018-12-27 09:30:31 -0500

    Improve type for arrays
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- packages/ui/Marquee/MarqueeDecorator.js
+++ packages/ui/Marquee/MarqueeDecorator.js
@@ -69,7 +69,7 @@ const defaultConfig = {
 	* Invalidate the distance if any property (like 'inline') changes.
 	* Expects an array of props which on change trigger invalidateMetrics.
 	*
-	* @type {Array}
+	* @type {String[]}
 	* @default ['remeasure']
 	* @memberof ui/Marquee.MarqueeDecorator.defaultConfig
 	*/
@@ -98,7 +98,7 @@ const defaultConfig = {
 	marqueeDirection: (str) => direction(str) === 'rtl' ? 'rtl' : 'ltr'
 };
 
-/**
+/*
  * Checks whether any of the invalidateProps has changed or not
  *
  * @param {String[]} propList An array of invalidateProps
