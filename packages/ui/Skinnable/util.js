2c04596ba03a7339135584231e9c7300f5fa0277
Roy Sutton <roy.sutton@lge.com>
2019-02-01 18:16:21 -0500

    Uncomplicate objectify
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- packages/ui/Skinnable/util.js
+++ packages/ui/Skinnable/util.js
@@ -5,22 +5,20 @@ const objectify = (arg) => {
 	if (!arg) return {};
 
 	if (typeof arg === 'string') {
-		// String case
+		// String case, convert to array for processing
 		arg = arg.split(' ');
-		// Process as array below
-	} else if (typeof arg === 'object' && !(arg instanceof Array)) {
-		// It's just an object already.
-		// return it unaltered
-		return arg;
-	} else if (!(arg instanceof Array)) {
-		return {};
 	}
 
-	// only dealing with arrays now
-	return arg.reduce((obj, a) => {
-		obj[a] = true;
-		return obj;
-	}, {});
+	if (arg instanceof Array) {
+		return arg.reduce((obj, a) => {
+			obj[a] = true;
+			return obj;
+		}, {});
+	} else if (typeof arg === 'object') {
+		return arg;
+	} else {
+		return {};
+	}
 };
 
 const preferDefined = (a, b) => ((a != null) ? a : b);
