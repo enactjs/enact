81bb310bde1b026d7d37813b0a698ee85210064a
haileyr <hangyeolryu@gmail.com>
2019-02-04 17:49:57 -0800

    PLAT-72158: Replace contextTypes with contextType in core/kind (#2065)
    * Updated contextTypes to contextType. and added warning for deprecated  prop.
    
    * updated tests.
    
    * fix lint.
    
    * Updated changelog.
    
    * fixes
    
    * check if config.contextTypes are truthy.
    
    * Added warning module.
    
    * Update CHANGELOG.md
    
    * add back contextTypes until 3.0
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- packages/core/kind/kind.js
+++ packages/core/kind/kind.js
@@ -9,6 +9,7 @@ import React from 'react';
 
 import computed from './computed';
 import styles from './styles';
+import warning from 'warning';
 
 /**
  * @callback RenderFunction
@@ -50,7 +51,7 @@ import styles from './styles';
  * @property {String} name
  * @property {Object.<string, Function>} [propTypes]
  * @property {Object.<string, any>} [defaultProps]
- * @property {Object.<string, Function>} [contextTypes]
+ * @property {Object} [contextType]
  * @property {StylesBlock} [styles]
  * @property {Object.<string, HandlerFunction>} [handlers]
  * @property {Object.<string, ComputedPropFunction>} [computed]
@@ -73,9 +74,7 @@ import styles from './styles';
  *			color: 'green'
  *		},
  *		// expect backgroundColor via context
- *		contextTypes: {
- *			backgroundColor: PropTypes.string
- *		},
+ *		contextType: React.createContext({ backgroundColor }),
  *		// configure styles with the static className to merge with user className
  *		styles: {
  *			// include the CSS modules map so 'button' can be resolved to the local name
@@ -112,6 +111,7 @@ import styles from './styles';
 const kind = (config) => {
 	const {
 		computed: cfgComputed,
+		contextType,
 		contextTypes,
 		defaultProps,
 		handlers,
@@ -120,6 +120,8 @@ const kind = (config) => {
 		render,
 		styles: cfgStyles
 	} = config;
+
+	warning(!contextTypes, `"contextTypes" used by ${name || 'a component'} but is deprecated. Please replace with "contextType" instead.`);
 
 	const renderStyles = cfgStyles ? styles(cfgStyles) : false;
 	const renderComputed = cfgComputed ? computed(cfgComputed) : false;
@@ -137,6 +139,8 @@ const kind = (config) => {
 		static propTypes = propTypes
 
 		static contextTypes = contextTypes
+
+		static contextType = contextType
 
 		static defaultProps = defaultProps
 
