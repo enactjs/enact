fbf2c550b4868b44e9f4d17fcfd93f21bcc3ad19
Ryan Duffy <ryan@tiqtech.com>
2019-02-21 10:49:17 -0800

    PLAT-73547: Update ViewManager internal lifecycle methods (#2115)
    * PLAT-73547: Update ViewManager lifecycle method usage
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * fix up logic when updating a single child
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * Revert "fix up logic when updating a single child"
    
    This reverts commit dd1ce572397ed0c9211c502f32905b93eae14081.
    
    * add back children equality check for updating views case
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * use props.children instead of state.children to determine entering/leaving
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * add test for breadcrumb decorator scenario
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * make the start/end logic sane + add tests
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * minor clean up
    Enact-DCO-1.0-Signed-off-by: Stephen Choi <stephen.choi@lge.com>
    
    * Remove unintentional props
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Update test names
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- packages/ui/ViewManager/ViewManager.js
+++ packages/ui/ViewManager/ViewManager.js
@@ -213,15 +213,18 @@ class ViewManager extends React.Component {
 	}
 
 	render () {
-		const {arranger, childProps, children, duration, end, index, noAnimation, start, enteringDelay, enteringProp, ...rest} = this.props;
+		const {arranger, childProps, children, duration, index, noAnimation, enteringDelay, enteringProp, ...rest} = this.props;
+		let {end = index, start = index} = this.props;
 		const {prevIndex: previousIndex, reverseTransition} = this.state;
 		const childrenList = React.Children.toArray(children);
 
-		const from = (start || start === 0) ? start : index;
-		const to = (end || end === 0) && end >= index ? end : index;
-		const size = to - from + ((noAnimation || !arranger) ? 0 : 1);
+		if (index > end) end = index;
+		if (index < start) start = index;
 
-		const views = childrenList.slice(from, to + 1);
+		const childCount = end - start + 1;
+		const size = (noAnimation || !arranger) ? childCount : childCount + 1;
+
+		const views = childrenList.slice(start, start + childCount);
 		const childFactory = wrapWithView({
 			arranger,
 			duration,
@@ -234,10 +237,12 @@ class ViewManager extends React.Component {
 			childProps
 		});
 
+		delete rest.end;
 		delete rest.reverseTransition;
+		delete rest.start;
 
 		return (
-			<TransitionGroup {...rest} childFactory={childFactory} size={size + 1} currentIndex={index}>
+			<TransitionGroup {...rest} childFactory={childFactory} size={size} currentIndex={index}>
 				{views}
 			</TransitionGroup>
 		);
