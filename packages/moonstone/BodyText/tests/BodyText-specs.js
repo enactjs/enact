f1f37d8db28281eb56390cc6b5f7b38aafd3f4bd
Blake Stephens <blake.stephens@lge.com>
2019-03-01 11:53:10 -0800

    Cleaned up tests
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- packages/moonstone/BodyText/tests/BodyText-specs.js
+++ packages/moonstone/BodyText/tests/BodyText-specs.js
@@ -6,16 +6,16 @@ import css from '../BodyText.module.less';
 
 describe('BodyText Specs', () => {
 
-	// test('should support multi-line content', () => {
-	// 	const subject = mount(
-	// 		<BodyText />
-	// 	);
+	test('should support multi-line content', () => {
+		const subject = mount(
+			<BodyText />
+		);
 
-	// 	const expected = 'p';
-	// 	const actual = subject.getElement().type;
+		const expected = 1;
+		const actual = subject.find('p').length;
 
-	// 	expect(actual).toBe(expected);
-	// });
+		expect(actual).toBe(expected);
+	});
 
 	test('should support single-line marqueeing content when `nowrap` is true', () => {
 		const subject = mount(
@@ -23,26 +23,7 @@ describe('BodyText Specs', () => {
 		);
 
 		const expected = true;
-
-
-
-		//
-		// Enzyme doesn't support : in component displayName
-		//
-
-		// Works when displayName has no colons (changing the name in the component file)
-		// const actual = subject.find('uiMarqueeDecorator').html();
-
-		// Doesn't work when using the real name
-		const actual = subject.find('ui:MarqueeDecorator').html();
-
-
-
-		// const actual = subject.contains(MarqueeDecorator);
-		// const actual = subject.find('.marquee').html();
-		// const actual = subject.instance(MarqueeDecorator);
-		console.log(subject.debug());
-		console.log('actual:', actual);
+		const actual = subject.findWhere(c => c.name() === 'ui:Marquee').exists();
 
 		expect(actual).toBe(expected);
 	});
