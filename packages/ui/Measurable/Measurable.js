e75fd22868e8eeba50368401f72fbcdcc93eb7c9
Blake Stephens <blake.stephens@lge.com>
2019-04-02 14:49:17 -0700

    Added Measurable HOC and Hook
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

---
+++ packages/ui/Measurable/Measurable.js
@@ -0,0 +1,93 @@
+/* eslint-disable react/sort-prop-types */
+
+/**
+ * A higher-order component that adds the ability to measure nodes conveniently.
+ *
+ * @module ui/Measurable
+ * @exports Measurable
+ */
+
+import hoc from '@enact/core/hoc';
+import React, {useState, useCallback} from 'react';
+
+/**
+ * Default config for {@link ui/Measurable.Measurable}.
+ *
+ * @memberof ui/Measurable.Measurable
+ * @hocconfig
+ */
+const defaultConfig = {
+	/**
+	 * Configures the prop name to pass the measurement object.
+	 *
+	 * @type {String}
+	 * @default 'measurement'
+	 * @memberof ui/Measurable.Measurable.defaultConfig
+	 */
+	measurementPropName: 'measurement',
+
+	/**
+	 * Configures the prop name to pass that represents the element to measure.
+	 *
+	 * @type {String}
+	 * @default 'forwardRef'
+	 * @memberof ui/Measurable.Measurable.defaultConfig
+	 */
+	refPropName: 'forwardRef'
+};
+
+function defineMeasurable () {
+	return function () {
+		const [measurement, setMeasurement] = useState();
+		const ref = useCallback(node => {
+			if (node != null) {
+				setMeasurement(node ? node.getBoundingClientRect() : null);
+			}
+		}, []);
+
+		return {
+			ref,
+			measurement
+		};
+	};
+}
+
+const useMeasurable = defineMeasurable();
+
+/**
+ * A higher-order component that adds the ability to measure a referenced node and get that value
+ * back as a prop.
+ *
+ * Applying `Measurable` to a component will pass two additional props: function to fetch the `ref`
+ * and the measurement object (currently this executes `getBoundingClientRect` returning its full
+ * compliment of properties). Both prop names are configurable through the HOC config object.
+ *
+ * Changes to the referenced node will result in new measurements being returned as props. That
+ * being said, it probably is not wise to use this during animation as it may cause unwanted
+ * performance issues.
+ *
+ * @see https://reactjs.org/docs/hooks-faq.html#how-can-i-measure-a-dom-node
+ * @class Measurable
+ * @memberof ui/Measurable
+ * @hoc
+ * @public
+ */
+const Measurable = hoc(defaultConfig, (configHoc, Wrapped) => {
+	return function MeasurableHoc (props) {
+		// Take the config from Measurable and insert it into a fresh instance of defineMeasurable
+		const {ref, measurement} = useMeasurable(props);
+		const measurementProps = {
+			[configHoc.refPropName]: ref,
+			[configHoc.measurementPropName]: measurement
+		};
+
+		return (<Wrapped {...props} {...measurementProps} />);
+	};
+});
+
+export default Measurable;
+export {
+	Measurable,       // HOC
+	useMeasurable,    // Hook
+	defineMeasurable  // Hook definition method
+};
