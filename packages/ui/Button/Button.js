5d01dbefddc4758f6431cbb801549c8db432268c
Blake Stephens <blake.stephens@lge.com>
2019-04-03 13:55:24 -0700

    Fixed Selected Button color, added docs.
    Just took a (bad) guess at the light-skin selected background color. We'll likely need to update this later.
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- packages/ui/Button/Button.js
+++ packages/ui/Button/Button.js
@@ -39,6 +39,7 @@ const ButtonBase = kind({
 		 * * `button` - The root component class
 		 * * `bg` - The background node of the button
 		 * * `client` - The content node of the button
+		 * * `hasIcon` - Applied when there is an `icon` present
 		 * * `icon` - The icon node, when `icon` is set
 		 * * `minWidth` - Applied when `minWidth` prop is `true`
 		 * * `pressed` - Applied when `pressed` prop is `true`
