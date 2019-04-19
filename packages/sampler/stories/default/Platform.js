404738c0ff396902ce28ca8befe61684fc5a00b1
Roy Sutton <roy.sutton@lge.com>
2019-04-19 12:16:01 -0400

    Add key
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- packages/sampler/stories/default/Platform.js
+++ packages/sampler/stories/default/Platform.js
@@ -15,7 +15,7 @@ function logObject (object) {
 			} else if (value === true) {
 				value = 'true';
 			}
-			return <div>{key}: {value}</div>
+			return <div key={key}>{key}: {value}</div>
 		}
 	);
 }
