eac288d95e0ba878e2f165655dcad6bcd0e5b6b4
Blake Stephens <blake.stephens@lge.com>
2019-05-17 16:00:41 -0700

    ENYO-5980: Updated sample to statically set button sizes and only include relevant knobs (#2298)
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- packages/sampler/stories/qa/Button.js
+++ packages/sampler/stories/qa/Button.js
@@ -1,6 +1,6 @@
 import Button, {ButtonBase} from '@enact/moonstone/Button';
 import IconButton from '@enact/moonstone/IconButton';
-import Divider from '@enact/moonstone/Divider';
+import Heading from '@enact/moonstone/Heading';
 import UIButton, {ButtonBase as UIButtonBase} from '@enact/ui/Button';
 import {icons} from '@enact/moonstone/Icon';
 import React from 'react';
@@ -96,54 +96,39 @@ storiesOf('Button', module)
 		'with tap area displayed',
 		() => (
 			<div>
-				<Divider>Button</Divider>
+				<Heading>Button</Heading>
 				<Button
 					className={css.tapArea}
 					onClick={action('onClick')}
-					backgroundOpacity={select('backgroundOpacity', prop.backgroundOpacity, Config)}
-					color={select('color', prop.color, Config)}
 					disabled={boolean('disabled', Config)}
-					icon={select('icon', prop.icons, Config)}
-					minWidth={storybookBoolean('minWidth', true) ? void 0 : false}
-					selected={boolean('selected', Config)}
-					size={select('size', ['small', 'large'], Config)}
+					size="large"
 				>
 					Normal Button
 				</Button>
 				<Button
 					className={css.tapArea}
 					onClick={action('onClick')}
-					backgroundOpacity={select('backgroundOpacity', prop.backgroundOpacity, Config)}
-					color={select('color', prop.color, Config)}
 					disabled={boolean('disabled', Config)}
-					icon={select('icon', prop.icons, Config)}
-					minWidth={storybookBoolean('minWidth', true) ? void 0 : false}
-					selected={boolean('selected', Config)}
-					size={select('size', ['small', 'large'], Config)}
+					size="small"
 				>
 					Small Button
 				</Button>
-				<Divider>IconButton</Divider>
+				<Heading>IconButton</Heading>
 				<IconButton
-					backgroundOpacity={select('backgroundOpacity', prop.backgroundOpacity, Config)}
 					className={css.tapArea}
-					color={select('color', prop.color, Config)}
 					disabled={boolean('disabled', Config)}
 					onClick={action('onClick')}
-					selected={boolean('selected', Config)}
+					size="large"
 				>
-					{select('icon', prop.icons, Config) || '☃'}
+					star
 				</IconButton>
 				<IconButton
-					backgroundOpacity={select('backgroundOpacity', prop.backgroundOpacity, Config)}
 					className={css.tapArea}
-					color={select('color', prop.color, Config)}
 					disabled={boolean('disabled', Config)}
 					onClick={action('onClick')}
-					small
-					selected={boolean('selected', Config)}
+					size="small"
 				>
-					{select('icon', prop.icons, Config) || '☃'}
+					star
 				</IconButton>
 			</div>
 		)
