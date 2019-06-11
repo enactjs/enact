c16a6e43944da584a68cca3245d5745997f1dabb
Stephen Choi <stephen.choi@lge.com>
2019-06-11 10:24:59 -0700

    Fix more test

--- packages/moonstone/Divider/tests/Divider-specs.js
+++ packages/moonstone/Divider/tests/Divider-specs.js
@@ -4,18 +4,6 @@ import Divider from '../Divider';
 
 describe('Divider Specs', () => {
 
-	test('should capitalize its content', () => {
-		const content = 'uncapped';
-		const divider = mount(
-			<Divider>{content}</Divider>
-		);
-
-		const expected = content.charAt(0).toUpperCase();
-		const actual = divider.text().charAt(0);
-
-		expect(actual).toBe(expected);
-	});
-
 	test('should render a Divider with content', () => {
 		const content = 'Hello Divider!';
 
