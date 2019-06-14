5818a2c5a9830636a6242d5e31d2a40040371a1b
Stephen Choi <stephen.choi@lge.com>
2019-06-14 14:53:42 -0700

    clean up test case

--- packages/moonstone/Heading/tests/Heading-specs.js
+++ packages/moonstone/Heading/tests/Heading-specs.js
@@ -4,19 +4,6 @@ import Heading from '../Heading';
 
 describe('Heading Specs', () => {
 
-	// Temporarily skipping until the theme-wide uppercase status can be officially determined.
-	test.skip('should capitalize its content', () => {
-		const content = 'uncapped';
-		const heading = mount(
-			<Heading>{content}</Heading>
-		);
-
-		const expected = content.charAt(0).toUpperCase();
-		const actual = heading.text().charAt(0);
-
-		expect(actual).toBe(expected);
-	});
-
 	test('should render a Heading with content', () => {
 		const content = 'Hello Heading!';
 
