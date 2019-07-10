c2e2a9fe8f0137a8efff66a7cee4d8661eb9e86c
Teck <teck.liew@lge.com>
2019-07-10 14:42:16 -0700

    revert moonstone/Video docs

--- packages/moonstone/VideoPlayer/Video.js
+++ packages/moonstone/VideoPlayer/Video.js
@@ -1,23 +1,27 @@
 import Video from '@enact/ui/Video';
 
 /**
- * Provides support for more advanced video configurations for `Video`.
+ * Provides support for more advanced video configurations for `VideoPlayer`.
  *
  * Custom Video Tag
  *
  * ```
- * <Video mediaComponent="custom-video-element">
- *   <source src="path/to/source.mp4" />
- * </Video>
+ * <VideoPlayer>
+ *   <Video mediaComponent="custom-video-element">
+ *     <source src="path/to/source.mp4" />
+ *   </Video>
+ * </VideoPlayer>
  * ```
  *
  * Preload Video Source
  *
  * ```
- * <Video>
- *   <source src="path/to/source.mp4" />
- *   <source src="path/to/preload-source.mp4" slot="preloadSource" />
- * </Video>
+ * <VideoPlayer>
+ *   <Video>
+ *     <source src="path/to/source.mp4" />
+ *     <source src="path/to/preload-source.mp4" slot="preloadSource" />
+ *   </Video>
+ * </VideoPlayer>
  * ```
  *
  * @class Video
