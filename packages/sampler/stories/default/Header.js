797242faa6b501760bc778785c8162eca4efa39f
Ryan Duffy <ryan.duffy@lge.com>
2019-07-25 09:10:21 -0700

    Revert "hack header sample to test"
    This reverts commit 1acfdeb397697d09531b86bd579fb969fe76ed46.

--- packages/sampler/stories/default/Header.js
+++ packages/sampler/stories/default/Header.js
@@ -1,4 +1,4 @@
-import {Header, HeaderBase, Panels, Panel} from '@enact/moonstone/Panels';
+import {Header, HeaderBase} from '@enact/moonstone/Panels';
 import Button from '@enact/moonstone/Button';
 import IconButton from '@enact/moonstone/IconButton';
 import Input from '@enact/moonstone/Input';
@@ -29,30 +29,29 @@ storiesOf('Moonstone', module)
 	.add(
 		'Header',
 		context => {
-			context.noPanels = true;
+			context.noHeader = true;
+
+			const noCloseButton = boolean('noCloseButton', {displayName: 'Panels'});
+			context.panelsProps = {noCloseButton: noCloseButton || false};
 
 			const headerInput = boolean('headerInput', Config) ? <Input placeholder="placeholder text" /> : null;
 			const childrenSelection = select('children', ['no buttons', '1 button', '2 buttons'], Config);
 			const children = prop.children[childrenSelection];
 
 			return (
-				<Panels noCloseButton={boolean('noCloseButton', {displayName: 'Panels'}) || false} style={{position: 'fixed'}}>
-					<Panel>
-						<Header
-							title={text('title', Config, 'The Matrix')}
-							titleBelow={text('titleBelow', Config, 'Free your mind')}
-							subTitleBelow={text('subTitleBelow', Config, 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.')}
-							type={select('type', prop.type, Config)}
-							centered={boolean('centered', Config)}
-							fullBleed={boolean('fullBleed', Config)}
-							headerInput={headerInput}
-							hideLine={boolean('hideLine', Config)}
-							marqueeOn={select('marqueeOn', prop.marqueeOn, Config)}
-						>
-							{children}
-						</Header>
-					</Panel>
-				</Panels>
+				<Header
+					title={text('title', Config, 'The Matrix')}
+					titleBelow={text('titleBelow', Config, 'Free your mind')}
+					subTitleBelow={text('subTitleBelow', Config, 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.')}
+					type={select('type', prop.type, Config)}
+					centered={boolean('centered', Config)}
+					fullBleed={boolean('fullBleed', Config)}
+					headerInput={headerInput}
+					hideLine={boolean('hideLine', Config)}
+					marqueeOn={select('marqueeOn', prop.marqueeOn, Config)}
+				>
+					{children}
+				</Header>
 			);
 		},
 		{
