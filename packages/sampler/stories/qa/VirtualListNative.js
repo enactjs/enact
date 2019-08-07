4db1e60f9a196780cd4070670ac3b257b31e7743
Ryan Duffy <ryan.duffy@lge.com>
2019-08-07 10:32:31 -0700

    inline the code
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- packages/sampler/stories/qa/VirtualListNative.js
+++ packages/sampler/stories/qa/VirtualListNative.js
@@ -1,37 +1,23 @@
 import React from 'react';
-import {Column, Cell} from '@enact/ui/Layout';
 import Item from '@enact/moonstone/Item';
-import {Panel} from '@enact/moonstone/Panels';
 import {VirtualListNative} from '@enact/moonstone/VirtualList';
+import {Column, Cell} from '@enact/ui/Layout';
 
 import {storiesOf} from '@storybook/react';
 
-let items = [];
-for (let i = 0; i < 10; i++) {
-	const
-		count = ('00' + i).slice(-3),
-		text = `Item ${count}`;
-
-	items.push({text});
-}
-
-class VirtualListWithExtraItems extends React.PureComponent {
-	constructor (props) {
-		super(props);
-	}
-
-	renderItem = ({index, ...rest}) => {
-		const text = `Item ${('00' + index).slice(-3)}`;
-		return (<Item {...rest}>{text}</Item>);
-	};
-
-	render () {
-		return (
+storiesOf('VirtualListNative', module)
+	.add(
+		'with extra items',
+		() => (
 			<Column>
 				<Cell
 					component={VirtualListNative}
 					focusableScrollbar
-					itemRenderer={this.renderItem}
+					// eslint-disable-next-line react/jsx-no-bind
+					itemRenderer={({index, ...rest}) => {
+						const text = `Item ${('00' + index).slice(-3)}`;
+						return (<Item {...rest}>{text}</Item>);
+					}}
 					dataSize={10}
 					direction="vertical"
 					horizontalScrollbar="hidden"
@@ -43,14 +29,5 @@ class VirtualListWithExtraItems extends React.PureComponent {
 				<Cell shrink component={Item}>extra item2</Cell>
 				<Cell shrink component={Item}>extra item3</Cell>
 			</Column>
-		);
-	}
-}
-
-storiesOf('VirtualListNative', module)
-	.add(
-		'with extra items',
-		() => (
-			<VirtualListWithExtraItems />
 		)
 	);
