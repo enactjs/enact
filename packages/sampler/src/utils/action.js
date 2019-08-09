e95ef838e87c37d3ff317311ccac5fec24fa1e71
Ryan Duffy <ryan.duffy@lge.com>
2019-08-09 07:13:04 -0700

    improve inline comment
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- packages/sampler/src/utils/action.js
+++ packages/sampler/src/utils/action.js
@@ -3,7 +3,8 @@ import {action} from '@storybook/addon-actions';
 const safeAction = (name, include = ['type']) => {
 	const handler = action(name);
 	return (ev) => {
-		// If it's a React synthetic event, extract the type member and drop the rest
+		// Ducktyping for React synthetic event to extract the specified members to improve
+		// serialization perf
 		if (ev.nativeEvent) {
 			ev = Object.keys(ev).filter(key => include.includes(key)).reduce((acc, key) => {
 				acc[key] = ev[key];
