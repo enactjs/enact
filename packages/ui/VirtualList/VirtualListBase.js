28e60b706884ea802c43da2169a0d4be508be3bc
YunBum Sung <yb.sung@lge.com>
2019-09-16 10:26:31 +0900

    Add initial value to calculate the scroll size
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- packages/ui/VirtualList/VirtualListBase.js
+++ packages/ui/VirtualList/VirtualListBase.js
@@ -1062,7 +1062,7 @@ const VirtualListBaseFactory = (type) => {
 
 		getVirtualScrollDimension = () => {
 			if (this.props.itemSizes) {
-				return this.props.itemSizes.reduce((total, size) => total + size);
+				return this.props.itemSizes.reduce((total, size) => total + size, 0);
 			} else {
 				const
 					{dimensionToExtent, primary, curDataSize} = this,
