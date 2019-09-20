0e9a7ca8935ea5379bbfe605902e9d2e4784fde9
YunBum Sung <yb.sung@lge.com>
2019-09-20 12:31:45 +0900

    Cleanup
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- packages/moonstone/Scrollable/ScrollableNative.js
+++ packages/moonstone/Scrollable/ScrollableNative.js
@@ -797,15 +797,13 @@ class ScrollableBaseNative extends Component {
 				const position = {x, y};
 				const {current: {containerRef: {current}}} = this.uiRef;
 				const elemFromPoint = document.elementFromPoint(x, y);
-				if (elemFromPoint && elemFromPoint.closest) {
-					const target =
-						getIntersectingElement(elemFromPoint.closest(`.${spottableClass}`), current) ||
-						getTargetInViewByDirectionFromPosition(direction, position, current) ||
-						getTargetInViewByDirectionFromPosition(reverseDirections[direction], position, current);
+				const target =
+					elemFromPoint && elemFromPoint.closest && getIntersectingElement(elemFromPoint.closest(`.${spottableClass}`), current) ||
+					getTargetInViewByDirectionFromPosition(direction, position, current) ||
+					getTargetInViewByDirectionFromPosition(reverseDirections[direction], position, current);
 
-					if (target) {
-						Spotlight.focus(target);
-					}
+				if (target) {
+					Spotlight.focus(target);
 				}
 			}
 			this.pointToFocus = null;
