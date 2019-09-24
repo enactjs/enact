f26a190897a466294156f9bd2676706ed74cfe51
Lucie Roy <lucie.roy@lge.com>
2019-09-24 13:27:20 -0700

    Add a sample woth long RTL text

--- packages/sampler/stories/qa/Header.js
+++ packages/sampler/stories/qa/Header.js
@@ -16,12 +16,13 @@ const inputData = {
 	shortTitle: 'Enact',
 	shortTitleBelow: 'An app framework',
 	shortSubTitleBelow: 'Built atop React',
-	shortRtlTitle: 'כתוביות למט',
+	shortRtlTitle: 'כתוביות למט ',
 	shortRtlTitleBelow: 'כתוביות למט',
 
 	longTitle: 'Core, The building blocks of an Enact application. Moonstone, our TV-centric UI library.',
 	longTitleBelow: 'An app development framework built atop React that’s easy to use, performant and customizable. The goal of Enact is to provide the building blocks for creating robust and maintainable applications.',
-	longSubTitleBelow: 'With over 50 components to choose from, Moonstone provides a solid base for creating applications designed for large screens. The Enact team welcomes contributions from anyone motivated to help out.'
+	longSubTitleBelow: 'With over 50 components to choose from, Moonstone provides a solid base for creating applications designed for large screens. The Enact team welcomes contributions from anyone motivated to help out.',
+	longRtlTitle: 'כתוביות למט כתוביות למט סביבה לימודית מתוקשבת מבית מטח שינויים מרחיקי לכת": כנס מט"ח מציג את המהפכות בעולם החינוך'
 };
 
 const headerComponents = <Button>Header Button</Button>;
@@ -159,6 +160,23 @@ storiesOf('Header', module)
 					type="compact"
 					title={text('title', Config, inputData.shortRtlTitle)}
 					titleBelow={text('titleBelow', Config, inputData.shortRtlTitleBelow)}
+					marqueeOn={select('marqueeOn', prop.marqueeOn, Config)}
+				>
+					{addHeaderComponents ? headerComponents : null}
+				</Header>
+			);
+		}
+	)
+	.add(
+		'RTL text, long title',
+		context => {
+			context.noHeader = true;
+			const addHeaderComponents = boolean('add headerComponents', Config);
+			return (
+				<Header
+					title={text('title', Config, inputData. longRtlTitle)}
+					titleBelow={text('titleBelow', Config, inputData.shortRtlTitleBelow)}
+					subTitleBelow={text('subTitleBelow', Config, inputData.shortRtlTitleBelow)}
 					marqueeOn={select('marqueeOn', prop.marqueeOn, Config)}
 				>
 					{addHeaderComponents ? headerComponents : null}
