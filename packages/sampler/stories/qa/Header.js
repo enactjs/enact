8854baf48a754fa535fb63661bc6ba9c951c45ab
Lucie <lucie.roy@lge.com>
2019-09-30 11:17:18 -0700

    ENYO 6287: Add QA Header sample for RTL with long string (#2580)
    * Add a sample woth long RTL text
    
    * modified rtl text to arabic both short and long strings
    
    * updated short strings to Arabic characters
    
    Reviewed-By: Blake Stephens (blake.stephens@lge.com)
    Integrated-By: Ryan Duffy (ryan.duffy@lge.com)

--- packages/sampler/stories/qa/Header.js
+++ packages/sampler/stories/qa/Header.js
@@ -16,12 +16,13 @@ const inputData = {
 	shortTitle: 'Enact',
 	shortTitleBelow: 'An app framework',
 	shortSubTitleBelow: 'Built atop React',
-	shortRtlTitle: 'כתוביות למט',
-	shortRtlTitleBelow: 'כתוביות למט',
+	shortRtlTitle: 'غينيا واستمر',
+	shortRtlTitleBelow: 'غينيا واستمر',
 
 	longTitle: 'Core, The building blocks of an Enact application. Moonstone, our TV-centric UI library.',
 	longTitleBelow: 'An app development framework built atop React that’s easy to use, performant and customizable. The goal of Enact is to provide the building blocks for creating robust and maintainable applications.',
-	longSubTitleBelow: 'With over 50 components to choose from, Moonstone provides a solid base for creating applications designed for large screens. The Enact team welcomes contributions from anyone motivated to help out.'
+	longSubTitleBelow: 'With over 50 components to choose from, Moonstone provides a solid base for creating applications designed for large screens. The Enact team welcomes contributions from anyone motivated to help out.',
+	longRtlTitle: 'غينيا واستمر العصبة ضرب قد. وباءت الأمريكي الأوربيين هو به،, هو العالم، الثقيلة بال. مع وايرلندا الأوروبيّون كان, قد بحق أسابيع العظمى واعتلاء. انه كل وإقامة المواد.'
 };
 
 const headerComponents = <Button>Header Button</Button>;
@@ -159,6 +160,23 @@ storiesOf('Header', module)
 					type="compact"
 					title={text('title', Config, inputData.shortRtlTitle)}
 					titleBelow={text('titleBelow', Config, inputData.shortRtlTitleBelow)}
+					marqueeOn={select('marqueeOn', prop.marqueeOn, Config)}
+				>
+					{addHeaderComponents ? headerComponents : null}
+				</Header>
+			);
+		}
+	)
+	.add(
+		'RTL text, long title',
+		context => {
+			context.noHeader = true;
+			const addHeaderComponents = boolean('add headerComponents', Config);
+			return (
+				<Header
+					title={text('title', Config, inputData. longRtlTitle)}
+					titleBelow={text('titleBelow', Config, inputData.shortRtlTitleBelow)}
+					subTitleBelow={text('subTitleBelow', Config, inputData.shortRtlTitleBelow)}
 					marqueeOn={select('marqueeOn', prop.marqueeOn, Config)}
 				>
 					{addHeaderComponents ? headerComponents : null}
