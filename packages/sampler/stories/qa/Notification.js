d68f321026f08d59ef675ad7fbfa9fe7fcde418b
Lucie <lucie.roy@lge.com>
2019-11-12 10:54:27 -0800

    ENYO-6353: Add QA sample for Notification with Sinhala language (#2647)
    * added new sample to test Sinhala language in Notification popup
    
    * modified just created  sample
    
    * Move sample to Notification
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Add Sinhala to locale list
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Describe Sinhala better
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Fix lint error
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * align locales
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    Reviewed-By: Roy Sutton (roy.sutton@lge.com)
    Integrated-By: Ryan Duffy (ryan.duffy@lge.com)

--- packages/sampler/stories/qa/Notification.js
+++ packages/sampler/stories/qa/Notification.js
@@ -52,11 +52,45 @@ class StatefulNotification extends React.Component {
 		);
 	}
 }
+// si-LK - sinhala language
+const sinhala = 'සේවය නඩත්තු කිරීම හෝ වැඩි දියුණු කිරීම සඳහා කලා ගැලරිය සේවයට එකතු කිරීම, නවීකරණය කිරීම, පිවිසීම අක්‍රිය කිරීම හෝ අවසන් කිරීම යනාදිය තම පූර්ණ අභිමතය පරිදි සිදු කිරීමට LG Electronics Inc. හට හිමිකම් ඇත.කලා ගැලරිය සේවාව ලද හැකි වන්නේ ඔබ ඉහත නියමයන්ට එකඟ වුවහොත් පමණි.';
+
+class LongButtonsSinhala extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			thirdButton: false
+		};
+	}
+	toggleState = () => {
+		this.setState(({thirdButton}) => ({thirdButton: !thirdButton}));
+	}
+
+	render () {
+		return (
+			<Notification open>
+				<span>{sinhala}</span>
+				<buttons>
+					<Button onClick={this.toggleState}>Click here to toggle the third button</Button>
+					<Button onClick={this.toggleState}>Click here to toggle the third button</Button>
+					{this.state.thirdButton ? <Button onClick={this.toggleState}>Click here to toggle the third button</Button> : null}
+				</buttons>
+			</Notification>
+		);
+	}
+}
+
 
 storiesOf('Notification', module)
 	.add(
 		'with dynamic content',
 		() => (
 			<StatefulNotification />
+		)
+	)
+	.add(
+		'with long buttons and Sinhala',
+		() => (
+			<LongButtonsSinhala />
 		)
 	);
