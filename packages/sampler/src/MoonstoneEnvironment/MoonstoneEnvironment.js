558c3b157e84cc718ddf5172af02d859f2ff71f9
Roy Sutton <roy.sutton@lge.com>
2019-11-11 19:50:04 -0500

    Describe Sinhala better
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- packages/sampler/src/MoonstoneEnvironment/MoonstoneEnvironment.js
+++ packages/sampler/src/MoonstoneEnvironment/MoonstoneEnvironment.js
@@ -79,7 +79,7 @@ const locales = {
 	'ta-IN - Tamil, custom Indian font':                    'ta-IN',
 	'ja-JP - Japanese, custom Japanese font':               'ja-JP',
 	'en-JP - English, custom Japanese font':                'en-JP',
-	'si-LK - Sinhala, fallback font':                		'si-LK'
+	'si-LK - Sinhala, external font family with different line metrics': 'si-LK'
 };
 
 // This mapping/remapping is necessary to support objects being used as select-knob values, since
