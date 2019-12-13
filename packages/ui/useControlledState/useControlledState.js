a07b987a96a625a38bffa51d3ad418e86140eb69
Ryan Duffy <ryan.duffy@lge.com>
2019-12-13 13:56:44 -0800

    improve memo for useControlledState
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- packages/ui/useControlledState/useControlledState.js
+++ packages/ui/useControlledState/useControlledState.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 // Generate a handler that hides the controlled value from users, supports functional callbacks,
 // and is memoized by the onChange provided by useState
-function createHandler (onChange) {
-	return (value) => {
+function createHandler () {
+	return (onChange) => (value) => {
 		onChange(prevState => ({
 			value: typeof value === 'function' ? value(prevState.value) : value,
 			controlled: prevState.controlled
@@ -30,7 +30,7 @@ function useControlledState (defaultValue, propValue, controlled) {
 
 	return [
 		calcValue(defaultValue, propValue, state.value, state.controlled),
-		React.useMemo(createHandler(onChange), [onChange])
+		React.useMemo(createHandler, [onChange])(onChange)
 	];
 }
 
