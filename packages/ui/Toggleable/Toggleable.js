a58a4278de3bbbede3f88bd1c65e9b8a939b235d
Ryan Duffy <ryan.duffy@lge.com>
2020-01-21 12:29:31 -0800

    refactor useToggle for explicit API
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- packages/ui/Toggleable/Toggleable.js
+++ packages/ui/Toggleable/Toggleable.js
@@ -5,13 +5,98 @@
  * @exports Toggleable
  */
 
+import {forward} from '@enact/core/handle';
 import hoc from '@enact/core/hoc';
 import {cap} from '@enact/core/util';
 import PropTypes from 'prop-types';
 import React from 'react';
 import warning from 'warning';
 
-import {configureToggle, defaultConfig, useToggle} from './useToggle';
+import {useToggle} from './useToggle';
+
+/**
+ * Default config for `Toggleable`.
+ *
+ * @memberof ui/Toggleable.Toggleable
+ * @hocconfig
+ */
+const defaultConfig = {
+	/**
+	 * Configures the event name that activates the component.
+	 *
+	 * **Note**: When using `activate`/`deactivate`, the event payload will only forward the original
+	 * event and not include toggled `prop` value. Use `toggle` to receive toggled value from the
+	 * event payload.
+	 *
+	 * Example:
+	 * ```
+	 * const ToggleItem = Toggleable({activate: 'onFocus', deactivate: 'onBlur'}, Item);
+	 *
+	 * handleEvent = (ev) => {
+	 * 	// do something with `ev.selected` here
+	 * }
+	 *
+	 * <ToggleItem onToggle={handleEvent}>This is a toggle item</Item>
+	 *
+	 * @type {String}
+	 * @memberof ui/Toggleable.Toggleable.defaultConfig
+	 */
+	activate: null,
+
+	/**
+	 * Configures the event name that deactivates the component.
+	 *
+	 * **Note**: When using `activate`/`deactivate`, the event payload will only forward the original
+	 * event and not include toggled `prop` value. Use `toggle` to receive toggled value from the
+	 * event payload.
+	 *
+	 * Example:
+	 * ```
+	 * const ToggleItem = Toggleable({activate: 'onFocus', deactivate: 'onBlur'}, Item);
+	 *
+	 * handleEvent = (ev) => {
+	 * 	// do something with `ev.selected` here
+	 * }
+	 *
+	 * <ToggleItem onToggle={handleEvent}>This is a toggle item</Item>
+	 * ```
+	 * @type {String}
+	 * @memberof ui/Toggleable.Toggleable.defaultConfig
+	 */
+	deactivate: null,
+
+	/**
+	 * Configures additional props to attach to the event that is sent when toggled.
+	 *
+	 * @type {String[]}
+	 * @default []
+	 * @memberof ui/Toggleable.Toggleable.defaultConfig
+	 */
+	eventProps: [],
+
+	/**
+	 * Configures the property that is passed to the wrapped component when toggled.
+	 *
+	 * @type {String}
+	 * @default 'selected'
+	 * @memberof ui/Toggleable.Toggleable.defaultConfig
+	 */
+	prop: 'selected',
+
+	/**
+	 * Configures the event name that toggles the component.
+	 *
+	 * The payload includes a toggled Boolean value of `prop`.
+	 *
+	 * **Note**: The payload will override the original event. If a native event is set, then the native
+	 * event payload will be lost.
+	 *
+	 * @type {String}
+	 * @default 'onToggle'
+	 * @memberof ui/Toggleable.Toggleable.defaultConfig
+	 */
+	toggle: 'onToggle'
+};
 
 /**
  * A higher-order component that applies a 'toggleable' behavior to its wrapped component.
@@ -34,21 +119,43 @@ import {configureToggle, defaultConfig, useToggle} from './useToggle';
  * @public
  */
 const ToggleableHOC = hoc(defaultConfig, (config, Wrapped) => {
-	const fn = useToggle.configure(config);
+	const {prop, toggle, activate, deactivate, toggleProp} = config;
 
 	function Toggleable (props) {
-		const updated = {
-			...props,
-			...fn(props)
-		};
-
 		const defaultPropKey = 'default' + cap(config.prop || defaultConfig.prop);
+		const updated = {...props};
+		const hook = useToggle({
+			disabled: props.disabled,
+			selected: props[prop],
+			defaultSelected: props[defaultPropKey],
+			onToggle: props[toggle]
+		});
 
 		warning(
-			!(config.prop in props && defaultPropKey in props),
-			`Do not specify both '${config.prop}' and '${defaultPropKey}' for Toggleable instances.
-			'${defaultPropKey}' will be ignored unless '${config.prop}' is 'null' or 'undefined'.`
+			!(prop in props && defaultPropKey in props),
+			`Do not specify both '${prop}' and '${defaultPropKey}' for Toggleable instances.
+			'${defaultPropKey}' will be ignored unless '${prop}' is 'null' or 'undefined'.`
 		);
+
+		if (prop) updated[prop] = hook.selected;
+
+		if (toggleProp || toggle) {
+			updated[toggleProp || toggle] = (ev) => {
+				if (hook.onToggle()) forward(toggleProp, ev, props);
+			};
+		}
+
+		if (activate) {
+			updated[activate] = (ev) => {
+				if (hook.onActivate()) forward(activate, ev, props);
+			};
+		}
+
+		if (deactivate) {
+			updated[deactivate] = (ev) => {
+				if (hook.onDeactivate()) forward(deactivate, ev, props);
+			};
+		}
 
 		delete updated[defaultPropKey];
 
@@ -114,7 +221,6 @@ const ToggleableHOC = hoc(defaultConfig, (config, Wrapped) => {
 
 export default ToggleableHOC;
 export {
-	configureToggle,
 	ToggleableHOC as Toggleable,
 	useToggle
 };
--- packages/ui/Toggleable/useToggle.js
+++ packages/ui/Toggleable/useToggle.js
@@ -1,162 +1,44 @@
 
 import {adaptEvent, forProp, forward, handle, not} from '@enact/core/handle';
-import {cap} from '@enact/core/util';
-import {pick} from 'ramda';
 
 import useControlledState from '../useControlledState';
 
-/**
- * Default config for `Toggleable`.
- *
- * @memberof ui/Toggleable.Toggleable
- * @hocconfig
- */
-const defaultConfig = {
-	/**
-	 * Configures the event name that activates the component.
-	 *
-	 * **Note**: When using `activate`/`deactivate`, the event payload will only forward the original
-	 * event and not include toggled `prop` value. Use `toggle` to receive toggled value from the
-	 * event payload.
-	 *
-	 * Example:
-	 * ```
-	 * const ToggleItem = Toggleable({activate: 'onFocus', deactivate: 'onBlur'}, Item);
-	 *
-	 * handleEvent = (ev) => {
-	 * 	// do something with `ev.selected` here
-	 * }
-	 *
-	 * <ToggleItem onToggle={handleEvent}>This is a toggle item</Item>
-	 *
-	 * @type {String}
-	 * @memberof ui/Toggleable.Toggleable.defaultConfig
-	 */
-	activate: null,
-
-	/**
-	 * Configures the event name that deactivates the component.
-	 *
-	 * **Note**: When using `activate`/`deactivate`, the event payload will only forward the original
-	 * event and not include toggled `prop` value. Use `toggle` to receive toggled value from the
-	 * event payload.
-	 *
-	 * Example:
-	 * ```
-	 * const ToggleItem = Toggleable({activate: 'onFocus', deactivate: 'onBlur'}, Item);
-	 *
-	 * handleEvent = (ev) => {
-	 * 	// do something with `ev.selected` here
-	 * }
-	 *
-	 * <ToggleItem onToggle={handleEvent}>This is a toggle item</Item>
-	 * ```
-	 * @type {String}
-	 * @memberof ui/Toggleable.Toggleable.defaultConfig
-	 */
-	deactivate: null,
-
-	/**
-	 * Configures additional props to attach to the event that is sent when toggled.
-	 *
-	 * @type {String[]}
-	 * @default []
-	 * @memberof ui/Toggleable.Toggleable.defaultConfig
-	 */
-	eventProps: [],
-
-	/**
-	 * Configures the property that is passed to the wrapped component when toggled.
-	 *
-	 * @type {String}
-	 * @default 'selected'
-	 * @memberof ui/Toggleable.Toggleable.defaultConfig
-	 */
-	prop: 'selected',
-
-	/**
-	 * Configures the event name that toggles the component.
-	 *
-	 * The payload includes a toggled Boolean value of `prop`.
-	 *
-	 * **Note**: The payload will override the original event. If a native event is set, then the native
-	 * event payload will be lost.
-	 *
-	 * @type {String}
-	 * @default 'onToggle'
-	 * @memberof ui/Toggleable.Toggleable.defaultConfig
-	 */
-	toggle: 'onToggle'
-};
-
-const configureToggle = (config) => {
-	const {activate, deactivate, eventProps, prop, toggle, toggleProp} = {...defaultConfig, ...config};
-	const defaultPropKey = 'default' + cap(prop);
-
-	const isEnabled = not(forProp('disabled', true));
-	const handleToggle = handle(
-		isEnabled,
-		forward(toggleProp),
-		adaptEvent(
-			(ev, props, value) => ({
-				...pick(eventProps, props),
-				[prop]: !value
-			}),
-			forward(toggle)
-		),
-		(ev, props, {value, onToggle}) => onToggle(!value)
-	).named('handleToggle');
-
-	const handleActivate = handle(
-		isEnabled,
-		adaptEvent(
-			(ev, props) => ({
-				...pick(eventProps, props),
-				[prop]: true
-			}),
-			forward(activate)
-		),
-		(ev, props, {onToggle}) => onToggle(true)
-	).named('handleActivate');
-
-	const handleDeactivate = handle(
-		isEnabled,
-		adaptEvent(
-			(ev, props) => ({
-				...pick(eventProps, props),
-				[prop]: false
-			}),
-			forward(deactivate)
-		),
-		(ev, props, {onToggle}) => onToggle(false)
-	).named('handleActivate');
-
-	// eslint-disable-next-line no-shadow
-	function useToggle (props) {
-		const [value, onToggle] = useControlledState(props[defaultPropKey], props[prop], prop in props);
-		const context = {
-			value,
-			onToggle
-		};
-
-		const updated = {};
-		if (prop) updated[prop] = Boolean(value);
-		if (toggleProp || toggle) updated[toggleProp || toggle] = (ev) => handleToggle(ev, props, context);
-		if (activate) updated[activate] = (ev) => handleActivate(ev, props, context);
-		if (deactivate) updated[deactivate] = (ev) => handleDeactivate(ev, props, context);
-
-		return updated;
+const isEnabled = not(forProp('disabled', true));
+const handleEvent = (adapter) => handle(
+	isEnabled,
+	adaptEvent(
+		(ev, props, context) => ({
+			selected: adapter(ev, props, context)
+		}),
+		forward('onToggle')
+	),
+	(ev, props, context) => {
+		context.onToggle(adapter(ev, props, context));
+		return true;
 	}
+);
 
-	return useToggle;
-};
+const handleToggle = handleEvent((ev, props, {value}) => !value).named('handleToggle');
+const handleActivate = handleEvent(() => true).named('handleActivate');
+const handleDeactivate = handleEvent(() => false).named('handleDeactivate');
 
-const useToggle = configureToggle();
-useToggle.configure = configureToggle;
+// eslint-disable-next-line no-shadow
+function useToggle (props) {
+	const [value, onToggle] = useControlledState(props.defaultSelected, props.selected, typeof props.selected !== 'undefined');
+	const context = {
+		value,
+		onToggle
+	};
+
+	return {
+		onActivate: (ev) => handleActivate(ev, props, context),
+		onDeactivate: (ev) => handleDeactivate(ev, props, context),
+		onToggle: (ev) => handleToggle(ev, props, context),
+		selected: Boolean(value)
+	};
+}
 
 export default useToggle;
 export {
-	configureToggle,
-	defaultConfig,
 	useToggle
 };
