77e770201a35efef0e57145502c6978fc03c18c4
YunBum Sung <yb.sung@lge.com>
2020-02-10 15:34:55 +0900

    Cleanup
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- packages/ui/VirtualList/tests/VirtualList-specs.js
+++ packages/ui/VirtualList/tests/VirtualList-specs.js
@@ -1,5 +1,5 @@
 import {configure, mount} from 'enzyme';
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 
 import VirtualList from '../VirtualList';
