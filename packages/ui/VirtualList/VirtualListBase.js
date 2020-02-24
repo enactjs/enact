e2a92e32da8b130c5c0170de9fff0f45a8af6433
YunBum Sung <yb.sung@lge.com>
2020-02-25 07:58:39 +0900

    Rename after refreshing
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

---
+++ packages/ui/VirtualList/VirtualListBase.js
@@ -0,0 +1,10 @@
+import {UiVirtualListBase, UiVirtualListBaseNative} from './UiVirtualListBase';
+import {gridListItemSizeShape, itemSizesShape} from './VirtualListBasic';
+
+export default UiVirtualListBase;
+export {
+	gridListItemSizeShape,
+	itemSizesShape,
+	UiVirtualListBase as VirtualListBase,
+	UiVirtualListBaseNative as VirtualListBaseNative
+};
