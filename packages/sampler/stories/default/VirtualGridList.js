c068634d2a6002cef941a8fc467eb5a37e12b5ab
YunBum Sung <yb.sung@lge.com>
2020-03-11 10:03:18 +0900

    Update GridListItem size
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- packages/sampler/stories/default/VirtualGridList.js
+++ packages/sampler/stories/default/VirtualGridList.js
@@ -27,6 +27,7 @@ const
 				{...rest}
 				caption={text}
 				source={source}
+				style={{width: '100%'}}
 				subCaption={subText}
 			/>
 		);
@@ -67,7 +68,7 @@ storiesOf('UI', module)
 				horizontalScrollbar={select('horizontalScrollbar', prop.scrollbarOption, VirtualGridListConfig)}
 				itemRenderer={uiRenderItem}
 				itemSize={{
-					minWidth: ri.scale(number('minWidth', VirtualGridListConfig, 100)),
+					minWidth: ri.scale(number('minWidth', VirtualGridListConfig, 180)),
 					minHeight: ri.scale(number('minHeight', VirtualGridListConfig, 270))
 				}}
 				noScrollByWheel={boolean('noScrollByWheel', VirtualGridListConfig)}
