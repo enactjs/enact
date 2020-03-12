2886776e830af023e8c83d774aa3a2e385b4ba38
Mikyung Kim <mikyung27.kim@lge.com>
2020-03-12 17:41:40 +0900

    Added console log

--- packages/ui/useScroll/Scrollbar.js
+++ packages/ui/useScroll/Scrollbar.js
@@ -41,6 +41,7 @@ const setCSSVariable = (element, variable, value) => {
  * @private
  */
 const ScrollbarBase = memo(forwardRef((props, ref) => {
+	console.log("ui/ScrollbarBase start==============================");
 	// Refs
 	const uiScrollbarContainerRef = useRef();
 	const thumbRef = useRef();
@@ -64,7 +65,9 @@ const ScrollbarBase = memo(forwardRef((props, ref) => {
 	}
 
 	useEffect(() => {
+		console.log("ui/ScrollbarBase useEffect1 nothing");
 		return () => {
+			console.log("ui/ScrollbarBase useEffect1 cleanup hideThumbjob stop");
 			hideThumbJob.current.stop();
 		};
 	}, []);
@@ -95,6 +98,7 @@ const ScrollbarBase = memo(forwardRef((props, ref) => {
 		}
 	}));
 
+	console.log("ui/ScrollbarBase render");
 	return (
 		<div {...rest} className={containerClassName} ref={uiScrollbarContainerRef}>
 			{childRenderer({thumbRef})}
@@ -197,6 +201,7 @@ const Scrollbar = forwardRef((props, ref) => {
 		};
 	}, [scrollbarBaseRef]);
 
+	console.log("ui/Scrollbar render");
 	return (
 		<ScrollbarBase
 			{...props}
