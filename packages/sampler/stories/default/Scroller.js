3c2dbc652d8a1f6b843009eefc6938f759e83f17
jeonghee27 <jeonghee27.ahn@lge.com>
2020-03-31 18:14:31 +0900

    ENYO-6440: Apply ri-scale on sampler and qa-sampler Enact-DCO-1.0-Signed-off-by: Jeonghee Ahn jeonghee27.ahn@lge.com

--- packages/sampler/stories/default/Scroller.js
+++ packages/sampler/stories/default/Scroller.js
@@ -31,15 +31,15 @@ storiesOf('UI', module)
 			>
 				<div
 					style={{
-						height: ri.unit(1002, 'rem'),
-						width: ri.unit(2001, 'rem')
+						height: ri.scaleToRem(2002),
+						width: ri.scaleToRem(4001)
 					}}
 				>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
 					Aenean id blandit nunc. Donec lacinia nisi vitae mi dictum, eget pulvinar nunc tincidunt. Integer vehicula tempus rutrum. Sed efficitur neque in arcu dignissim cursus.
 					<div
 						style={{
-							marginTop: ri.unit(801, 'rem')
+							marginTop: ri.scaleToRem(1602)
 						}}
 					>
 						Mauris blandit sollicitudin mattis. Fusce commodo arcu vitae risus consectetur sollicitudin. Aliquam eget posuere orci. Cras pellentesque lobortis sapien non lacinia.
