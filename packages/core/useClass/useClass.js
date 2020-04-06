394df354d435a4b93297d1f3400489e57d66490c
Ryan Duffy <ryan.duffy@lge.com>
2020-04-06 14:07:32 -0700

    improve docs for useClass
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- packages/core/useClass/useClass.js
+++ packages/core/useClass/useClass.js
@@ -4,8 +4,27 @@ import React from 'react';
  * Creates one instance of the class, `Ctor` with the provided `args`, for the life of the
  * component.
  *
+ * ```
+ * class MyClass {
+ *   constructor ({value}) {
+ *     this.value = value;
+ *   }
+ *
+ *   handleEvent = (ev) => {
+ *     if (ev.key === 'Enter') {
+ *       // do something with this.value
+ *     }
+ *   }
+ * }
+ *
+ * function Component (props) {
+ *   const inst = useClass(MyClass, props.value);
+ *   return <div onClick={inst.handleEvent} />
+ * }
+ * ```
+ *
  * @param {Function} Ctor Class constructor
- * @param  {...any} args Arguments to pass to the constructor
+ * @param  {...any}  args Arguments to pass to the constructor
  * @private
  */
 function useClass (Ctor, ...args) {
