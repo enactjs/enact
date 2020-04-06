0ee88f802e1c5fd826c66039026627dff03502ac
Ryan Duffy <ryan.duffy@lge.com>
2020-04-06 12:02:12 -0700

    add docs for useI18nContext
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- packages/i18n/I18nDecorator/useI18nContext.js
+++ packages/i18n/I18nDecorator/useI18nContext.js
@@ -2,6 +2,24 @@ import React from 'react';
 
 const I18nContext = React.createContext(null);
 
+/**
+ * Object returned by `useI18nContext`
+ *
+ * @typedef {Object} useI18nContextInterface
+ * @memberof i18n/I18nDecorator
+ * @property {Boolean}  loaded       `true` when external resource files have been loaded
+ * @property {Boolean}  rtl          `true` for locales with right-to-left reading order
+ * @property {Function} updateLocale Updates the locale
+ * @property {String}   locale       Current locale
+ * @private
+ */
+
+/**
+ * Retrieves the current locale and a method to update the current locale
+ *
+ * @returns {useI18nContextInterface}
+ * @private
+ */
 function useI18nContext () {
 	// This isn't adding much value but does allow a layer of abstraction so we don't have to export
 	// the I18nContext object and can add private API if needed later.
