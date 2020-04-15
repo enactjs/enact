2a74bc11eed107189016364c685e4b5e2d649358
Ryan Duffy <ryan.duffy@lge.com>
2020-04-15 16:45:18 -0700

    docs
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- packages/ui/AnnounceDecorator/useAnnounce.js
+++ packages/ui/AnnounceDecorator/useAnnounce.js
@@ -2,6 +2,34 @@ import React from 'react';
 
 import Announce from './Announce';
 
+/**
+ * Object returned by `useAnnounce`
+ *
+ * @typedef {Object} useAnnounceInterface
+ * @property {Function} announce Called to alert the user of behavior for accessibility
+ * @property {Node}     children An additional element which must be added to the component tree to
+ *                               support notifying the user
+ * @private
+ */
+
+/**
+ * Provides a method to alert the user of behavior for accessibility.
+ *
+ * ```
+ * 	function Component () {
+ *		const {announce, children} = useAnnounce();
+ *
+ *		return (
+ *			<Base onClick={() => announce('Unusually important notification for accessibility')}>
+ *				{children}
+ *			</Base>
+ *		);
+ *	}
+ * ```
+ *
+ * @returns {useAnnounceInterface}
+ * @private
+ */
 function useAnnouce () {
 	const ref = React.useRef(null);
 	const announce = React.useCallback(message => {
