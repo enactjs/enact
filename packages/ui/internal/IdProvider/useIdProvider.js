66af18b0f86ba851614fec7c2d41678ac47f312e
YunBum Sung <yb.sung@lge.com>
2020-04-20 15:06:31 +0900

    Migrate IdProvider to use hooks
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

---
+++ packages/ui/internal/IdProvider/useIdProvider.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+let GlobalId = 0;
+const ID_KEY = '$$ID$$';
+
+/**
+ * Configuration for `useIdProvider`
+ *
+ * @typedef {Object} useIdProviderConfig
+ * @memberof ui/IdProvider
+ * @property {String} [prefix] Optional prefix for the identifier.
+ * @private
+ */
+
+/**
+ * Object returned by `useIdProvider`
+ *
+ * @typedef {Object} useIdProviderInterface
+ * @memberof ui/IdProvider
+ * @property {Function} [generateId] The function to generate id.
+ * @private
+ */
+
+/**
+ * Generate globally-unique identifiers
+ *
+ * @param {useIdProviderConfig} config Configuration options
+ * @returns {useIdProviderInterface}
+ * @private
+ */
+const useIdProvider = ({prefix}) => {
+	const ids = React.useRef({});
+
+	React.useEffect(() => {
+		// Call the onUnmount handler for each generated id (note: not the key)
+		for (const key in ids.current) {
+			const {id, onUnmount} = ids.current[key];
+
+			if (typeof onUnmount === 'function') {
+				onUnmount(id);
+			}
+		}
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+	const generateId = React.useCallback((key = ID_KEY, idPrefix = prefix, onUnmount) => {
+		// if an id has been generated for the key, return it
+		if (key in ids.current) {
+			return ids.current[key].id;
+		}
+
+		// otherwise generate a new id (with an optional prefix), cache it, and return it
+		const id = `${idPrefix}${++GlobalId}`;
+		ids.current[typeof key === 'undefined' ? `generated-${id}` : key] = {
+			id,
+			onUnmount
+		};
+
+		return id;
+	}, [prefix]);
+
+	return {
+		generateId
+	};
+};
+
+export default useIdProvider;
+export {
+	useIdProvider
+};
