3b1e94c80119b653fad1e9c22000ec7527b18a00
Ryan Duffy <ryan.duffy@lge.com>
2020-04-30 10:49:50 -0700

    refactor into class, add unit tests
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- packages/ui/internal/IdProvider/useId.js
+++ packages/ui/internal/IdProvider/useId.js
@@ -1,7 +1,7 @@
+import useClass from '@enact/core/useClass';
 import React from 'react';
 
-let GlobalId = 0;
-const ID_KEY = '$$ID$$';
+import Provider from './Provider';
 
 /**
  * Configuration for `useIdProvider`
@@ -29,42 +29,14 @@ const ID_KEY = '$$ID$$';
  * @private
  */
 const useIdProvider = ({prefix}) => {
-	const ids = React.useRef({});
-	// The ref value 'ids.current' will likely have changed by the time.
-	// So it was copiped from 'ids.current' to the `idsCurrent`.
-	const idsCurrent = ids.current;
+	const provider = useClass(Provider, prefix);
 
 	React.useEffect(() => {
-		return () => {
-			// Call the onUnmount handler for each generated id (note: not the key)
-			for (const key in idsCurrent) {
-				const {id, onUnmount} = idsCurrent[key];
-
-				if (typeof onUnmount === 'function') {
-					onUnmount(id);
-				}
-			}
-		};
-	}, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-	const generateId = React.useCallback((key = ID_KEY, idPrefix = prefix, onUnmount) => {
-		// if an id has been generated for the key, return it
-		if (key in ids.current) {
-			return ids.current[key].id;
-		}
-
-		// otherwise generate a new id (with an optional prefix), cache it, and return it
-		const id = `${idPrefix}${++GlobalId}`;
-		ids.current[typeof key === 'undefined' ? `generated-${id}` : key] = {
-			id,
-			onUnmount
-		};
-
-		return id;
-	}, [prefix]);
+		return () => provider.unload();
+	}, [provider]);
 
 	return {
-		generateId
+		generateId: provider.generate
 	};
 };
 
