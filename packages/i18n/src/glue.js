e9a6fa4931523c96aaf69a36b9e0d0f3f173525f
Jason Robitaille <jason.robitaille@lge.com>
2020-05-13 16:10:28 -0700

    ENYO-6436: Only use iLib loader when necessary. (#2768)
    * ENYO-6436: Only use iLib loader when necessary.
    
    * Use ILIB_NO_ASSETS constant instead of ILIB_DISABLE

--- packages/i18n/src/glue.js
+++ packages/i18n/src/glue.js
@@ -1,3 +1,4 @@
+/* global ILIB_NO_ASSETS */
 /*
  * glue.js - glue code to fit ilib into enyo
  *
@@ -24,7 +25,13 @@ import './dates';
 import Loader from './Loader';
 import {updateLocale} from '../locale';
 
-ilib.setLoaderCallback(new Loader());
+// if iLib data is pre-provided, or ILIB_NO_ASSETS is set, skip iLib loader usage
+// otherwise use our local Loader.js file
+if (!global.ilibData &&
+		!(typeof ILIB_NO_ASSETS !== 'undefined' && ILIB_NO_ASSETS === true)) {
+	ilib.setLoaderCallback(new Loader());
+}
+
 
 if (typeof window === 'object' && typeof window.UILocale !== 'undefined') {
 	// this is a hack until GF-1581 is fixed
