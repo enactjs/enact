016ba38ee67e94c4a819cf2a161201a4ecf01e4d
Ryan Duffy <ryan.duffy@lge.com>
2020-06-11 15:29:28 -0700

    PLAT-109453: Add useChainRefs (#2782)
    * add useChainRefs
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * add core dependency
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * add docs
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * linting
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

---
+++ packages/core/useChainRefs/useChainRefs.js
@@ -0,0 +1,56 @@
+/**
+ * Updates multiple reference callbacks or objects
+ *
+ * @module core/useChainRefs
+ * @exports useChainRefs
+ * @exports chainRefs
+ * @private
+ */
+
+import React from 'react';
+import warning from 'warning';
+
+// Safely handles functional and object refs (and ignores invalid refs)
+function updateRef (ref, node) {
+	if (ref) {
+		if (typeof ref === 'function') {
+			ref(node);
+		} else if (ref.hasOwnProperty('current')) {
+			ref.current = node;
+		} else {
+			// warn for a truthy ref that isn't a function or is an object without `current`
+			warning(ref, `Invalid ref "${ref}" passed to useChainRefs.`);
+		}
+	}
+}
+
+/**
+ * Creates a reference callback that updates each of the provided references
+ *
+ * @param {Object|Function} ...handlers  List of references to be updated.
+ * @returns {Function}                   A callback that updates each reference
+ * @public
+ */
+function chainRefs (...refs) {
+	return (node) => {
+		refs.forEach(ref => updateRef(ref, node));
+	};
+}
+
+/**
+ * Creates a memoized reference callback that updates each of the provided references
+ *
+ * @param {Object|Function} ...handlers  List of references to be updated.
+ * @returns {Function}                   A memoized callback that updates each reference
+ * @public
+ */
+function useChainRefs (...refs) {
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	return React.useCallback(chainRefs(...refs), refs);
+}
+
+export default useChainRefs;
+export {
+	useChainRefs,
+	chainRefs
+};
