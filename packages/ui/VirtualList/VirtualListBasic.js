a0c2ec17474227efb506a0e9bf9354f9e7399ab8
jeonghee27 <jeonghee27.ahn@lge.com>
2020-06-11 17:37:33 +0900

    Support positioning for items wrapped in a div Enact-DCO-1.0-Signed-off-by: Jeonghee Ahn <jeonghee27.ahn@lge.com>

--- packages/ui/VirtualList/VirtualListBasic.js
+++ packages/ui/VirtualList/VirtualListBasic.js
@@ -303,6 +303,7 @@ class VirtualListBasic extends Component {
 		super(props);
 
 		this.contentRef = React.createRef();
+		this.itemContainerRefs = [];
 
 		if (props.clientSize) {
 			this.calculateMetrics(props);
@@ -954,10 +955,10 @@ class VirtualListBasic extends Component {
 	// For individually sized item
 	applyItemPositionToDOMElement (index) {
 		const
-			{direction, itemRefs, rtl} = this.props,
+			{direction, rtl} = this.props,
 			{numOfItems} = this.state,
 			{itemPositions} = this,
-			childNode = itemRefs.current[index % numOfItems];
+			childNode = this.itemContainerRefs[index % numOfItems];
 
 		if (childNode && itemPositions[index]) {
 			const position = itemPositions[index].position;
@@ -1068,6 +1069,11 @@ class VirtualListBasic extends Component {
 					itemRefs.current[key] = (parseInt(itemNode.dataset.index) === index) ?
 						itemNode :
 						ref.querySelector(`[data-index="${index}"]`);
+
+					if (itemRefs.current[key]) {
+						this.itemContainerRefs[key] = (this.contentRef.current === itemRefs.current[key].parentElement) ?
+							itemRefs.current[key] : itemRefs.current[key].parentElement;
+					}
 				}
 			};
 
