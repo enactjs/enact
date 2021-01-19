f5ac8f9c66297eadeb73ed50739aa8210746ee96
Jeongran Jang <jeongran.jang@lge.com>
2021-01-19 10:48:12 +0900

    PLAT-129634: Register existing addons (#2890)
    * Register addon-actions and addon-knobs
      Enact-DCO-1.0-Signed-off-by: Jeongran Jang (jeongran.jang@lge.com)
    
    * Set up decorators and parameters for configuration
      Enact-DCO-1.0-Signed-off-by: Jeongran Jang (jeongran.jang@lge.com)
    
    * Add new line at the end of file
      Enact-DCO-1.0-Signed-off-by: Jeongran Jang (jeongran.jang@lge.com)

--- packages/sampler/.storybook/preview.js
+++ packages/sampler/.storybook/preview.js
@@ -1,3 +1,14 @@
-import Environment from '../src/Environment'
+import {addDecorator} from '@storybook/react';
+import {configureActions} from '@enact/storybook-utils/addons/actions';
+import {withKnobs} from '@enact/storybook-utils/addons/knobs';
 
+import Environment from '../src/Environment';
+
+configureActions();
+addDecorator(withKnobs);
+export const parameters = {
+	knobs: {
+		timestamps: true,
+	}
+};
 export const decorators = [Environment];
