3eb475eee3a4d03bd05ae6742b5bd8efde2d1af7
jeonghee27 <jeonghee27.ahn@lge.com>
2021-05-27 18:08:16 +0900

    apply review Enact-DCO-1.0-Signed-off-by: Jeonghee Ahn (jeonghee27.ahn@lge.com)

--- packages/spotlight/src/spotlight.js
+++ packages/spotlight/src/spotlight.js
@@ -730,10 +730,12 @@ const Spotlight = (function () {
 		 * @param {String|Node} [elem] The spotlight ID or selector for either a spottable
 		 *  component or a spotlight container, or spottable node. If not supplied, the default
 		 *  container will be focused.
+		 * @param {Object} [conatinerOption] A object containing `enterTo`.(optional)
+		 *  It will be passed to the getTargetByContainer when `elem` is container.
 		 * @returns {Boolean} `true` if focus successful, `false` if not.
 		 * @public
 		 */
-		focus: function (elem, enterTo) {
+		focus: function (elem, conatinerOption = {}) {
 			let target = elem;
 			let wasContainerId = false;
 
@@ -741,7 +743,7 @@ const Spotlight = (function () {
 				target = getTargetByContainer();
 			} else if (typeof elem === 'string') {
 				if (getContainerConfig(elem)) {
-					target = getTargetByContainer(elem, enterTo);
+					target = getTargetByContainer(elem, conatinerOption.enterTo);
 					wasContainerId = true;
 				} else if (/^[\w\d-]+$/.test(elem)) {
 					// support component IDs consisting of alphanumeric, dash, or underscore
@@ -750,7 +752,7 @@ const Spotlight = (function () {
 					target = getTargetBySelector(elem);
 				}
 			} else if (isContainer(elem)) {
-				target = getTargetByContainer(getContainerId(elem), enterTo);
+				target = getTargetByContainer(getContainerId(elem), conatinerOption.enterTo);
 			}
 
 			const nextContainerIds = getContainersForNode(target);
