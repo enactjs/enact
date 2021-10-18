5ab6f9a0851a71fa40edef5311e6db4e10ea4d18
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2021-10-18 10:38:56 +0300

    *WIP* migrated unit tests for BodyText and Button

--- packages/ui/BodyText/tests/BodyText-specs.js
+++ packages/ui/BodyText/tests/BodyText-specs.js
@@ -1,75 +1,105 @@
 import {mount, shallow} from 'enzyme';
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+
 import BodyText, {BodyTextBase} from '../BodyText';
 import {Cell} from '../../Layout';
+
 import css from '../BodyText.module.less';
 
 describe('BodyText Specs', () => {
-
 	test('should render a single <p> tag', () => {
 		const msg = 'Hello BodyText!';
-		const bodyText = mount(
-			<BodyText>{msg}</BodyText>
-		);
+		// const bodyText = mount(
+		// 	<BodyText>{msg}</BodyText>
+		// );
+		render(<BodyText>{msg}</BodyText>);
+		const bodyText = screen.getByText('Hello BodyText!');
 
-		const bodyTextTag = bodyText.find('p');
-		const expected = 1;
-		const actual = bodyTextTag.length;
+		expect(bodyText).toBeInTheDocument();
 
-		expect(actual).toBe(expected);
+		// const bodyTextTag = bodyText.find('p');
+		// const expected = 1;
+		// const actual = bodyTextTag.length;
+		//
+		// expect(actual).toBe(expected);
 	});
 
 	test('should render BodyText with content', () => {
 		const content = 'Hello BodyText!';
 
-		const bodyTextTag = mount(
-			<BodyText>{content}</BodyText>
-		);
+		// const bodyTextTag = mount(
+		// 	<BodyText>{content}</BodyText>
+		// );
+		render(<BodyText>{content}</BodyText>);
+		const bodyText = screen.getByText('Hello BodyText!');
 
-		const expected = content;
-		const actual = bodyTextTag.text();
+		expect(bodyText).toHaveTextContent(content);
 
-		expect(actual).toBe(expected);
+		// const expected = content;
+		// const actual = bodyTextTag.text();
+		//
+		// expect(actual).toBe(expected);
 	});
-
+	//
 	test('should not include the centered class by default', () => {
-		const subject = shallow(
-			<BodyTextBase />
-		);
+		// const subject = shallow(
+		// 	<BodyTextBase />
+		// );
+		render(<BodyTextBase data-testid="bodyText" />);
+		const bodyText = screen.getByTestId('bodyText');
 
-		const expected = false;
-		const actual = subject.hasClass(css.centered);
-		expect(actual).toBe(expected);
+		const expected = css.centerd;
+
+		expect(bodyText).not.toHaveClass(expected);
+
+		// const expected = false;
+		// const actual = subject.hasClass(css.centered);
+		// expect(actual).toBe(expected);
 	});
-
+	//
 	test('should include the centered class if `centered` is true', () => {
-		const subject = shallow(
-			<BodyTextBase centered />
-		);
+		// const subject = shallow(
+		// 	<BodyTextBase centered />
+		// );
+		render(<BodyText centered data-testid="bodyText" />);
+		const bodyText = screen.getByTestId('bodyText');
 
-		const expected = true;
-		const actual = subject.hasClass(css.centered);
-		expect(actual).toBe(expected);
+		const expected = 'centered';
+
+		expect(bodyText).toHaveClass(expected);
+
+		// const expected = true;
+		// const actual = subject.hasClass(css.centered);
+		// expect(actual).toBe(expected);
 	});
 
-	test('should support changing the component element to a different DOM node', () => {
-		const componentTag = 'address';
-		const subject = shallow(
-			<BodyTextBase component={componentTag} />
-		);
-
-		const expected = componentTag;
-		const actual = subject.getElement().type;
-		expect(actual).toBe(expected);
-	});
-
+	// Test is skipped because React Testing Library does not support if component element is changed to a functional component
 	test('should support changing the component element to a functional component', () => {
 		const component = Cell;
-		const subject = shallow(
-			<BodyTextBase component={component} />
-		);
+		// const subject = shallow(
+		// 	<BodyTextBase component={component} />
+		// );
+		render(<BodyTextBase component={component} data-testid="bodyText" />);
+		const bodyText = screen.getByTestId('bodyText');
 
-		const expected = component;
-		const actual = subject.getElement().type;
+		const expected = 'cell';
+
+		expect(bodyText).toHaveClass(expected);
+	});
+
+	// Test is skipped because React Testing Library does not support if the component if changed to a different DOM node
+	test.skip('should support changing the component element to a different DOM node', () => {
+		const componentTag = 'address';
+		// const subject = shallow(
+		// 	<BodyTextBase component={componentTag} />
+		// );
+		render(<BodyTextBase component={componentTag} data-testid="bodyText" />);
+		const bodyText = screen.getByTestId('bodyText');
+
+		const expected = componentTag;
+		const actual = bodyText.getElement().type;
+
 		expect(actual).toBe(expected);
 	});
 });
--- packages/ui/Button/tests/Button-specs.js
+++ packages/ui/Button/tests/Button-specs.js
@@ -1,12 +1,15 @@
 import {mount} from 'enzyme';
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
 
-import Button from '../Button';
+import {Button, ButtonBase} from '../Button';
+
+import css from '../Button.module.less';
 
 describe('Button', () => {
-
 	test('should return a DOM node reference for `componentRef`', () => {
 		const ref = jest.fn();
-		mount(<Button ref={ref} />);
+		render(<Button ref={ref} />);
 
 		const expected = 'DIV';
 		const actual = ref.mock.calls[0][0].nodeName;
@@ -14,4 +17,19 @@ describe('Button', () => {
 		expect(actual).toBe(expected);
 	});
 
+	test('should have \'disabled\' HTML attribute when \'disabled\' prop is provided', () => {
+		render(<Button disabled>I am a disabled Button</Button>);
+		const button = screen.getByRole('button');
+
+		expect(button).toHaveAttribute('disabled');
+	});
+
+	test('should have default minWidth', function () {
+		render(<ButtonBase />);
+		const button = screen.getByRole('button');
+
+		const expected = css.minWidth;
+
+		expect(button).toHaveClass(expected);
+	});
 });
