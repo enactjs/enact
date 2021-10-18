d18bfce82d462410548314fc71f59ca46f041213
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2021-10-18 11:03:38 +0300

    fix lint

--- packages/ui/Spinner/tests/Spinner-specs.js
+++ packages/ui/Spinner/tests/Spinner-specs.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import {render, screen} from '@testing-library/react'
+import {render, screen} from '@testing-library/react';
 
 import {SpinnerBase} from '../Spinner';
 
