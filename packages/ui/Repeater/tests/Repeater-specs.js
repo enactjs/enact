a6ea875242e1f1bc4253fce4026e34892e161abd
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-10-19 16:55:58 +0300

    Migrated unit tests for Repeater and ResolutionDecorator

--- packages/ui/Repeater/tests/Repeater-specs.js
+++ packages/ui/Repeater/tests/Repeater-specs.js
@@ -1,4 +1,6 @@
-import {mount, shallow} from 'enzyme';
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+
 import Repeater, {RepeaterBase} from '../Repeater';
 
 describe('Repeater Specs', () => {
@@ -9,156 +11,125 @@ describe('Repeater Specs', () => {
 	const CustomType = (props) => <div>{props.content}</div>;
 
 	test('should have a root span element', () => {
-		const subject = shallow(
-			<RepeaterBase childComponent="div">{stringItems}</RepeaterBase>
-		);
+		render(<RepeaterBase childComponent="div" data-testid="repeater">{stringItems}</RepeaterBase>);
 
 		const expected = 'span';
-		const actual = subject.first().name();
+		const rootElemementName = screen.getByTestId("repeater").nodeName.toLowerCase();
 
-		expect(actual).toBe(expected);
+		expect(rootElemementName).toBe(expected);
 	});
 
 	test('should accept a nodeName as root element', () => {
-		const subject = shallow(
-			<RepeaterBase component="div" childComponent="div">{stringItems}</RepeaterBase>
-		);
+		render(<RepeaterBase childComponent="div" component="div" data-testid="repeater">{stringItems}</RepeaterBase>);
 
 		const expected = 'div';
-		const actual = subject.first().name();
+		const rootElemementName = screen.getByTestId("repeater").nodeName.toLowerCase();
 
-		expect(actual).toBe(expected);
+		expect(rootElemementName).toBe(expected);
 	});
 
 	test('should accept a function as root element', () => {
-		const subject = shallow(
-			<RepeaterBase component={CustomRootType} childComponent="div">{stringItems}</RepeaterBase>
-		);
+		render(<RepeaterBase childComponent="div" component={CustomRootType} data-testid="repeater">{stringItems}</RepeaterBase>);
 
-		const expected = 'CustomRootType';
-		const actual = subject.first().name();
+		const expected = 'div';
+		const rootElemementName = screen.getByTestId("repeater").nodeName.toLowerCase();
 
-		expect(actual).toBe(expected);
+		expect(rootElemementName).toBe(expected);
 	});
 
 	test('should accept a nodeName as childComponent', () => {
-		const subject = shallow(
-			<RepeaterBase childComponent="div">{stringItems}</RepeaterBase>
-		);
+		render(<RepeaterBase childComponent="div" data-testid="repeater">{stringItems}</RepeaterBase>);
 
 		const expected = 3;
-		const actual = subject.first().children().length;
+		const actual = screen.getByTestId("repeater").children.length;
 
 		expect(actual).toBe(expected);
 	});
 
 	test('should accept a function as childComponent', () => {
-		const subject = shallow(
-			<RepeaterBase childComponent={CustomType}>{stringItems}</RepeaterBase>
-		);
+		render(<RepeaterBase childComponent={CustomType} data-testid="repeater">{stringItems}</RepeaterBase>);
 
 		const expected = 3;
-		const actual = subject.first().children().length;
+		const actual = screen.getByTestId("repeater").children.length;
 
 		expect(actual).toBe(expected);
 	});
 
-	test(
-		'should create a number of children matching the length of items',
-		() => {
-			const subject = shallow(
-				<RepeaterBase childComponent="div">{stringItems}</RepeaterBase>
-			);
+	test('should create a number of children matching the length of items',() => {
+		render(<RepeaterBase childComponent="div" data-testid="repeater">{stringItems}</RepeaterBase>);
 
-			const expected = stringItems.length;
-			const actual = subject.first().children().length;
+		const expected = stringItems.length;
+		const actual = screen.getByTestId("repeater").children.length;
 
-			expect(actual).toBe(expected);
-		}
-	);
+		expect(actual).toBe(expected);
+	});
 
 	test('should support an array of objects as items', () => {
-		const subject = shallow(
-			<RepeaterBase childComponent={CustomType}>{objItems}</RepeaterBase>
-		);
+		render(<RepeaterBase childComponent={CustomType} data-testid="repeater">{objItems}</RepeaterBase>);
 
 		const expected = objItems.length;
-		const actual = subject.first().children().length;
+		const actual = screen.getByTestId("repeater").children.length;
 
 		expect(actual).toBe(expected);
 	});
 
 	test('should support passing itemProps to children', () => {
-		const subject = shallow(
-			<RepeaterBase childComponent="div" itemProps={{title: 'test'}}>{stringItems}</RepeaterBase>
-		);
+		render(<RepeaterBase childComponent="div" data-testid="repeater" itemProps={{title: 'test'}}>{stringItems}</RepeaterBase>);
 
 		const expected = 'test';
-		const actual = subject.first().childAt(0).prop('title');
+		const actual = screen.getByTestId("repeater").children.item(0);
 
-		expect(actual).toBe(expected);
+		expect(actual).toHaveAttribute("title", expected);
 	});
 
 	test('should pass index to each child', () => {
-		const subject = shallow(
-			<RepeaterBase childComponent="div">{stringItems}</RepeaterBase>
-		);
+		render(<RepeaterBase childComponent="div" data-testid="repeater">{stringItems}</RepeaterBase>);
 
-		const expected = 0;
-		const actual = subject.first().childAt(0).prop('data-index');
+		const expected = "0";
+		const actual = screen.getByTestId("repeater").children.item(0);
 
-		expect(actual).toBe(expected);
+		expect(actual).toHaveAttribute("data-index", expected);
 	});
 
 	test('should pass data to each child', () => {
-		const subject = shallow(
-			<RepeaterBase childComponent="div" childProp="data-str">{stringItems}</RepeaterBase>
-		);
+		render(<RepeaterBase childComponent="div" childProp="data-str" data-testid="repeater">{stringItems}</RepeaterBase>);
 
 		const expected = stringItems[0];
-		const actual = subject.first().childAt(0).prop('data-str');
+		const actual = screen.getByTestId("repeater").children.item(0);
 
-		expect(actual).toBe(expected);
+		expect(actual).toHaveAttribute("data-str", expected);
 	});
 
 	test('should pass item as children to each child', () => {
-		const subject = shallow(
-			<RepeaterBase childComponent="div">{stringItems}</RepeaterBase>
-		);
+		render(<RepeaterBase childComponent="div" data-testid="repeater">{stringItems}</RepeaterBase>);
 
 		const expected = stringItems[0];
-		const actual = subject.first().childAt(0).prop('children');
+		const actual = screen.getByTestId("repeater").children.item(0);
 
-		expect(actual).toBe(expected);
+		expect(actual).toHaveTextContent(expected);
 	});
 
 	test('should set role to list by default', () => {
-		const subject = shallow(
-			<RepeaterBase childComponent="div">{stringItems}</RepeaterBase>
-		);
+		render(<RepeaterBase childComponent="div" data-testid="repeater">{stringItems}</RepeaterBase>);
 
 		const expected = 'list';
-		const actual = subject.prop('role');
+		const listElement = screen.getByTestId("repeater");
 
-		expect(actual).toBe(expected);
+		expect(listElement).toHaveAttribute("role", expected);
 	});
 
 	test('should allow role to be overridden', () => {
-		const subject = shallow(
-			<RepeaterBase childComponent="div" role="listbox">{stringItems}</RepeaterBase>
-		);
+		render(<RepeaterBase childComponent="div" data-testid="repeater" role="listbox">{stringItems}</RepeaterBase>);
 
 		const expected = 'listbox';
-		const actual = subject.prop('role');
+		const listElement = screen.getByTestId("repeater");
 
-		expect(actual).toBe(expected);
+		expect(listElement).toHaveAttribute("role", expected);
 	});
 
 	test('should return a DOM node reference for `componentRef``', () => {
 		const ref = jest.fn();
-		mount(
-			<Repeater component="div" childComponent="div" ref={ref}>{stringItems}</Repeater>
-		);
+		render(<Repeater childComponent="div" component="div" data-testid="repeater" ref={ref}>{stringItems}</Repeater>);
 
 		const expected = 'DIV';
 		const actual = ref.mock.calls[0][0].nodeName;
