fba58e874ad17d45a412f0cda3a6d765a14692de
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2021-11-01 16:06:41 +0200

    code review fixes

--- packages/core/useHandlers/tests/useHandlers-specs.js
+++ packages/core/useHandlers/tests/useHandlers-specs.js
@@ -4,7 +4,7 @@ import {render} from '@testing-library/react';
 import useHandlers from '../useHandlers';
 
 describe('useHandlers', () => {
-	let data = [];
+	let data = {};
 
 	const context = {
 		value: 1
@@ -17,20 +17,20 @@ describe('useHandlers', () => {
 			}
 		}, props, context);
 
-		data.push(handlers);
+		data = {
+			handlers
+		}
 
 		return (
 			<div {...props} data-testid="divComponent" />
 		);
 	}
 
-	afterEach(() => data.splice(0, data.length));
-
 	// Sanity test for Component moreso than useHandlers test
 	test('should include handlers in props', () => {
 		render(<Component />);
 
-		const actual = data[0].testEvent;
+		const actual = data.handlers.testEvent;
 
 		expect(actual).toBeDefined();
 	});
@@ -38,11 +38,11 @@ describe('useHandlers', () => {
 	test('should have the same reference across renders', () => {
 		const {rerender} = render(<Component />);
 
-		const expected = data[0].testEvent;
+		const expected = data.handlers.testEvent;
 
 		rerender(<Component />);
 
-		const actual =  data[1].testEvent;
+		const actual = data.handlers.testEvent;
 
 		expect(actual).toBe(expected);
 	});
@@ -53,7 +53,7 @@ describe('useHandlers', () => {
 
 		rerender(<Component>{'updated'}</Component>);
 
-		data[0].testEvent(spy);
+		data.handlers.testEvent(spy);
 
 		expect(spy).toHaveBeenCalled();
 	});
@@ -64,7 +64,7 @@ describe('useHandlers', () => {
 
 		rerender(<Component>{'updated'}</Component>);
 
-		data[0].testEvent(spy);
+		data.handlers.testEvent(spy);
 
 		const expected = {children: 'updated'};
 		const actual = spy.mock.calls[0][0];
@@ -76,7 +76,7 @@ describe('useHandlers', () => {
 		const spy = jest.fn();
 		render(<Component />);
 
-		data[0].testEvent(spy);
+		data.handlers.testEvent(spy);
 
 		// defined a "global" context to ease testability but this isn't representative of the
 		// expected use case of this feature.
@@ -90,7 +90,7 @@ describe('useHandlers', () => {
 		const spy = jest.fn().mockImplementation(() => 'ok');
 		render(<Component />);
 
-		const returnValue = data[0].testEvent(spy);
+		const returnValue = data.handlers.testEvent(spy);
 
 		// defined a "global" context to ease testability but this isn't representative of the
 		// expected use case of this feature.
