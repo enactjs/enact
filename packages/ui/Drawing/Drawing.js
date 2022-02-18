a2d74184198a763af09cfe378abcb56c8e037eec
Vladut Clapou <vladut.clapou@lgepartner.com>
2022-02-18 11:00:30 +0200

    Draw point functionality added

--- packages/ui/Drawing/Drawing.js
+++ packages/ui/Drawing/Drawing.js
@@ -214,6 +214,8 @@ const DrawingBase = kind({
 			contextRef.current.beginPath(); // start a canvas path
 			contextRef.current.moveTo(offsetX, offsetY); // move the starting point to initial position
 			points.push({x: offsetX, y: offsetY});
+			contextRef.current.lineTo(offsetX, offsetY); // draw a single point
+			contextRef.current.stroke();
 			beginPointRef.current = {x: offsetX, y: offsetY};
 			setIsDrawing(true);
 		}
