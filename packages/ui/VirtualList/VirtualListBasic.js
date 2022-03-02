6856f4eb340141e9841640a9b0e828c8144c6815
Seungcheon Baek <sc.baek@lge.com>
2022-03-02 17:19:51 +0900

    WRN-13462: Use standard scrollTo option for lists and scrollers
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- packages/ui/VirtualList/VirtualListBasic.js
+++ packages/ui/VirtualList/VirtualListBasic.js
@@ -442,7 +442,7 @@ class VirtualListBasic extends Component {
 			this.updateScrollPosition({
 				x: xMax > x ? x : xMax,
 				y: yMax > y ? y : yMax
-			});
+			}, 'instant');
 
 			deferScrollTo = true;
 		} else if (this.hasDataSizeChanged) {
@@ -453,7 +453,7 @@ class VirtualListBasic extends Component {
 
 			deferScrollTo = true;
 		} else if (prevProps.rtl !== this.props.rtl) {
-			this.updateScrollPosition(this.getXY(this.scrollPosition, 0));
+			this.updateScrollPosition(this.getXY(this.scrollPosition, 0), 'instant');
 		}
 
 		const maxPos = this.isPrimaryDirectionVertical ? this.scrollBounds.maxTop : this.scrollBounds.maxLeft;
@@ -505,12 +505,12 @@ class VirtualListBasic extends Component {
 	itemPositions = [];
 	indexToScrollIntoView = -1;
 
-	updateScrollPosition = ({x, y}, rtl = this.props.rtl) => {
+	updateScrollPosition = ({x, y}, behavior) => {
 		if (this.props.scrollMode === 'native') {
-			this.scrollToPosition(x, y, rtl);
+			this.scrollToPosition(x, y, behavior);
 		} else {
 			this.setScrollPositionTarget (x, y);
-			this.setScrollPosition(x, y, rtl);
+			this.setScrollPosition(x, y);
 		}
 	};
 
@@ -676,9 +676,7 @@ class VirtualListBasic extends Component {
 		if (scrollMode === 'translate' && this.contentRef.current) {
 			this.contentRef.current.style.transform = null;
 		} else if (scrollMode === 'native' && node) {
-			node.style.scrollBehavior = null;
-			this.updateScrollPosition(this.getXY(this.scrollPosition, 0));
-			node.style.scrollBehavior = 'smooth';
+			this.updateScrollPosition(this.getXY(this.scrollPosition, 0), 'instant');
 		}
 	}
 
@@ -847,13 +845,17 @@ class VirtualListBasic extends Component {
 	}
 
 	// scrollMode 'native' only
-	scrollToPosition (x, y, rtl = this.props.rtl) {
-		if (this.props.scrollContentRef.current && this.props.scrollContentRef.current.scrollTo) {
-			if (rtl) {
-				x = (platform.ios || platform.safari || platform.chrome >= 85 || platform.androidChrome >= 85 ) ? -x : this.scrollBounds.maxLeft - x;
-			}
+	getRtlPositionX = (x) => {
+		if (this.props.rtl) {
+			return (platform.ios || platform.safari || platform.chrome >= 85 || platform.androidChrome >= 85) ? -x : this.scrollBounds.maxLeft - x;
+		}
+		return x;
+	};
 
-			this.props.scrollContentRef.current.scrollTo(x, y);
+	// scrollMode 'native' only
+	scrollToPosition (left, top, behavior) { // TARGET FUNCTION
+		if (this.props.scrollContentRef.current && this.props.scrollContentRef.current.scrollTo) {
+			this.props.scrollContentRef.current.scrollTo({left: this.getRtlPositionX(left), top, behavior});
 		}
 	}
 
@@ -868,7 +870,7 @@ class VirtualListBasic extends Component {
 
 	// scrollMode 'translate' only
 	setScrollPositionTarget (x, y) {
-		// The `x`, `y` as parameters in scrollToPosition() are the position when stopping scrolling.
+		// The `left`, `top` as parameters in scrollToPosition() are the position when stopping scrolling.
 		// But the `x`, `y` as parameters in setScrollPosition() are the position between current position and the position stopping scrolling.
 		// To know the position when stopping scrolling properly, `x` and `y` are passed and cached in `this.scrollPositionTarget`.
 		if (this.isPrimaryDirectionVertical) {
@@ -879,7 +881,8 @@ class VirtualListBasic extends Component {
 	}
 
 	// scrollMode 'translate' only
-	setScrollPosition (x, y, rtl = this.props.rtl) {
+	setScrollPosition (x, y) {
+		const rtl = this.props.rtl;
 		if (this.contentRef.current) {
 			this.contentRef.current.style.transform = `translate3d(${rtl ? x : -x}px, -${y}px, 0)`;
 			this.didScroll(x, y);
