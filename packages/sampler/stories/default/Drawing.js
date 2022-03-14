0fc74a842255bf71700249a520ca477269e747bb
adrian-cocoara-lgp <63335823+adrian-cocoara-lgp@users.noreply.github.com>
2022-03-14 18:09:33 +0200

    WRN-18651: Make canvas size configurable (#3028)
    * change width/height dynamically
    
    * fix lint
    
    * code review fixes

--- packages/sampler/stories/default/Drawing.js
+++ packages/sampler/stories/default/Drawing.js
@@ -1,6 +1,6 @@
 import Drawing, {DrawingBase} from '@enact/ui/Drawing';
 import {mergeComponentMetadata} from '@enact/storybook-utils';
-import {range, select} from '@enact/storybook-utils/addons/controls';
+import {number, range, select} from '@enact/storybook-utils/addons/controls';
 
 const DrawingConfig = mergeComponentMetadata('Drawing', Drawing, DrawingBase);
 
@@ -14,6 +14,8 @@ export const _Drawing = (args) => (
 		brushColor={args['brushColor']}
 		brushSize={args['brushSize']}
 		canvasColor={args['canvasColor']}
+		canvasHeight={args['canvasHeight']}
+		canvasWidth={args['canvasWidth']}
 		drawingTool={args['drawingTool']}
 		fillColor={args['fillColor']}
 	/>
@@ -22,6 +24,8 @@ export const _Drawing = (args) => (
 select('brushColor', _Drawing, ['red', 'green', 'blue'], DrawingConfig);
 range('brushSize', _Drawing, DrawingConfig, {min: 1, max: 50}, 4);
 select('canvasColor', _Drawing, ['black', 'white'], DrawingConfig);
+number('canvasHeight', _Drawing, DrawingConfig, 500);
+number('canvasWidth', _Drawing, DrawingConfig, 800);
 select('drawingTool', _Drawing, ['brush', 'fill', 'triangle', 'rectangle', 'circle', 'erase'], DrawingConfig);
 select('fillColor', _Drawing, ['red', 'green', 'blue'], DrawingConfig);
 
