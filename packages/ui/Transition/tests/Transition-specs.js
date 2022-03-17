5b910eba778e970c8332877fdcabb4ddca8bddb8
노시정/책임연구원/SW Platform(연)webOS개발실 webOS Framework Task(sj.ro@lge.com) <sj.ro@lge.com>
2022-03-18 08:53:11 +0900

    fix the lint error in transition-specs.js Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)

--- packages/ui/Transition/tests/Transition-specs.js
+++ packages/ui/Transition/tests/Transition-specs.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import {fireEvent, render, screen} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 
 import Transition, {TransitionBase} from '../Transition';
 
