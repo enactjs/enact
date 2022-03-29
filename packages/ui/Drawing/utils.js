869a50e63c0f6531ee93efcd13f69ef8bf79cd16
Daniel Stoian <daniel.stoian@lgepartner.com>
2022-03-29 18:03:03 +0300

    Scaled fill functionality

--- packages/ui/Drawing/utils.js
+++ packages/ui/Drawing/utils.js
@@ -8,6 +8,40 @@ const forAllNeighbors = (point, fn) => {
 	fn({x: point.x - 1, y: point.y});
 };
 
+function deltaE(rgbA, rgbB) {
+	let labA = rgb2lab(rgbA);
+	let labB = rgb2lab(rgbB);
+	let deltaL = labA[0] - labB[0];
+	let deltaA = labA[1] - labB[1];
+	let deltaB = labA[2] - labB[2];
+	let c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
+	let c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
+	let deltaC = c1 - c2;
+	let deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+	deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
+	let sc = 1.0 + 0.045 * c1;
+	let sh = 1.0 + 0.015 * c1;
+	let deltaLKlsl = deltaL / (1.0);
+	let deltaCkcsc = deltaC / (sc);
+	let deltaHkhsh = deltaH / (sh);
+	let i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
+	return i < 0 ? 0 : Math.sqrt(i);
+}
+
+function rgb2lab(rgb){
+	let r = rgb[0] / 255, g = rgb[1] / 255, b = rgb[2] / 255, x, y, z;
+	r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+	g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+	b = (b > 0.04045) ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+	x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+	y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
+	z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+	x = (x > 0.008856) ? Math.pow(x, 1/3) : (7.787 * x) + 16/116;
+	y = (y > 0.008856) ? Math.pow(y, 1/3) : (7.787 * y) + 16/116;
+	z = (z > 0.008856) ? Math.pow(z, 1/3) : (7.787 * z) + 16/116;
+	return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)]
+}
+
 /*
  * Given two color, returns true when they are similar.
  */
@@ -21,17 +55,27 @@ const isSimilarColor = (color1, color2) => {
 //console.log(color1, color2)
 	const color1HexString = parseInt(color1, 10).toString(16);
 	const color2HexString = parseInt(color2, 10).toString(16);
+//console.log(color1HexString, color2HexString)
+	const delta = deltaE([parseInt(color1HexString.substring(6), 16), parseInt(color1HexString.substring(4,6), 16), parseInt(color1HexString.substring(2,4),16)], [parseInt(color2HexString.substring(6), 16), parseInt(color2HexString.substring(4,6), 16), parseInt(color2HexString.substring(2,4),16)])
 
-	const dr = parseInt(color1HexString.substring(6), 16) - parseInt(color2HexString.substring(6), 16);
-	const dg = parseInt(color1HexString.substring(4,6), 16) - parseInt(color2HexString.substring(4,6), 16);
-	const db = parseInt(color1HexString.substring(2,4), 16) - parseInt(color2HexString.substring(2,4), 16);
-	const da = parseInt(color1HexString.substring(0,2), 16) - parseInt(color2HexString.substring(0,2), 16);
+
+	// const dr = parseInt(color1HexString.substring(6), 16) - parseInt(color2HexString.substring(6), 16);
+	// const dg = parseInt(color1HexString.substring(4,6), 16) - parseInt(color2HexString.substring(4,6), 16);
+	// const db = parseInt(color1HexString.substring(2,4), 16) - parseInt(color2HexString.substring(2,4), 16);
+	// const da = parseInt(color1HexString.substring(0,2), 16) - parseInt(color2HexString.substring(0,2), 16);
 
 	// check if the analyzed pixel has a similar color as the starting point in order to handle the anti-aliasing of the drawn border
-	if (dr * dr + dg * dg + db * db + da * da > 128 * 128) {
-		return false;
+	// if (dr * dr + dg * dg + db * db + da * da < 4) {
+	// 	//console.log(dr * dr + dg * dg + db * db + da * da)
+	// 	return true;
+	// }
+	// return false;
+
+	if (delta < 20) {
+		//console.log(dr * dr + dg * dg + db * db + da * da)
+		return true;
 	}
-	return true;
+	return false;
 
 // 		// get red/green/blue int values of hex1
 // 		let r1 = parseInt(color1HexString.substring(6), 16);
@@ -97,25 +141,20 @@ const fillDrawing = (contextRef, event, fillColor) => {
 	const targetColor = getPixelValue(pixelData, startPos.x, startPos.y);
 
 	if (targetColor !== fillColorHexNumber) {
-		const pixelList = [startPos];
-		while (pixelList.length) {
-			const pos = pixelList.pop();
+		const pixelList = [startPos.x, startPos.y];
+		while (pixelList.length > 0) {
+			const y = pixelList.pop();
+			const x = pixelList.pop();
 
-			const currentColor = getPixelValue(pixelData, pos.x, pos.y);
-			if (isSimilarColor(currentColor, targetColor) && currentColor !== -1) {
-				pixelData.data[pos.y * pixelData.width + pos.x] = fillColorHexNumber;
-				// pixelsToCheck.push(x + 1, y);
-				// pixelsToCheck.push(x - 1, y);
-				// pixelsToCheck.push(x, y + 1);
-				// pixelsToCheck.push(x, y - 1);
-				forAllNeighbors(pos, function (neighbor) {
-					if (neighbor.x >= 0 && neighbor.x < imageData.width &&
-						neighbor.y >= 0 && neighbor.y < imageData.height &&
-						isSimilarColor(getPixelValue(pixelData, neighbor.x, neighbor.y), targetColor))
-						{
-							pixelList.push(neighbor);
-					}
-				});
+			const currentColor = getPixelValue(pixelData, x, y);
+			if(currentColor !== -1 && targetColor !== -1) {
+				if (targetColor === currentColor) {
+					pixelData.data[y * pixelData.width + x] = fillColorHexNumber;
+					pixelList.push(x + 1, y);
+					pixelList.push(x - 1, y);
+					pixelList.push(x, y + 1);
+					pixelList.push(x, y - 1);
+				}
 			}
 		}
 
