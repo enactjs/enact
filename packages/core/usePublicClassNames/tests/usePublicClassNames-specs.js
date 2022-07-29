67f4e4fcefa818132865cc71904b133de276195c
Mikyung Kim <mikyung27.kim@lge.com>
2022-07-29 09:50:07 +0900

    WRO-3625: Add usePublicClassNames
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

---
+++ packages/core/usePublicClassNames/tests/usePublicClassNames-specs.js
@@ -0,0 +1,50 @@
+import {renderHook} from '@testing-library/react';
+
+import usePublicClassNames from '../usePublicClassNames';
+
+describe('usePublicClassNames', () => {
+	const componentCss = {
+		test: 'test-class',
+		a: 'a-class',
+		b: 'b-class'
+	};
+	const customCss = {
+		test: 'test-class-custom',
+		a: 'a-class-custom',
+		b: 'b-class-custom',
+		myClass: 'my-class'
+	};
+
+	test('should return `componentCss` if `customCss` is undefined', () => {
+		const testComponentCss = {test: 'test-class'};
+		const {result} = renderHook(() => usePublicClassNames({componentCss: testComponentCss, customCss: null, publicClassNames: true}));
+
+		expect(result.current).toEqual(testComponentCss);
+	});
+
+	test('should return a merged css has all keys from `componentCss` and merged values when `publicClassNames` is true', () => {
+		const {result} = renderHook(() => usePublicClassNames({componentCss, customCss, publicClassNames: true}));
+
+		const expected = {
+			test: 'test-class test-class-custom',
+			a: 'a-class a-class-custom',
+			b: 'b-class b-class-custom'
+		};
+
+		expect(result.current.test).toBe(expected.test);
+		expect(result.current.a).toBe(expected.a);
+		expect(result.current.b).toBe(expected.b);
+	});
+
+	test('should return a merged css has all keys from `componentCss` and merged values when `publicClassNames` is an array of strings', () => {
+		const {result} = renderHook(() => usePublicClassNames({componentCss, customCss, publicClassNames: ['a', 'b']}));
+
+		const expected = {
+			a: 'a-class a-class-custom',
+			b: 'b-class b-class-custom'
+		};
+
+		expect(result.current.a).toBe(expected.a);
+		expect(result.current.b).toBe(expected.b);
+	});
+});
---
+++ packages/core/usePublicClassNames/usePublicClassNames.js
@@ -0,0 +1,37 @@
+import {mergeClassNameMaps} from '../util';
+
+/**
+ * A hook for supporting `publicClassNames` to functional components.
+ * It returns merged CSS of given two CSS objects according to `publicClassNames` option.
+ *
+ * @param   {Object.<string, string>}    [componentCss]     The CSS of the component
+ * @param   {Object.<string, string>}    [customCss]        The supplied collection of CSS class names to the
+ *                                                          corresponding internal elements and states of the component
+ * @param   {Boolean|String|String[]}    [publicClassNames] The keys of public class names of the component
+ *                                                          If this value is `true`, all of the keys from the component
+ *                                                          CSS will become public class names.
+ * @returns {Object}                                        A merged CSS
+ * @private
+ */
+function usePublicClassNames ({componentCss, customCss, publicClassNames}) {
+	let allowedClassNames = publicClassNames;
+	let mergedCss = componentCss;
+
+	if (componentCss && allowedClassNames === true) {
+		allowedClassNames = Object.keys(componentCss);
+	} else if (typeof allowedClassNames === 'string') {
+		allowedClassNames = allowedClassNames.split(/\s+/);
+	}
+
+	// if the config includes a css map, merge them together now
+	if (componentCss && allowedClassNames && customCss) {
+		mergedCss = mergeClassNameMaps(componentCss, customCss, allowedClassNames);
+	}
+
+	return mergedCss;
+}
+
+export default usePublicClassNames;
+export {
+	usePublicClassNames
+};
