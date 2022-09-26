51dd4e02c4a43cfb63fdfb0bfa7466446d4e118d
Daniel Stoian <daniel.stoian@lgepartner.com>
2022-09-26 17:24:06 +0300

    added unit tests for ui/Slider

--- packages/ui/Slider/tests/Slider-specs.js
+++ packages/ui/Slider/tests/Slider-specs.js
@@ -1,5 +1,7 @@
-import {render} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
 
+import Knob from '../Knob';
 import Slider from '../Slider';
 
 describe('Slider', () => {
@@ -15,5 +17,42 @@ describe('Slider', () => {
 		const actual = ref.mock.calls[0][0].nodeName;
 
 		expect(actual).toBe(expected);
+	});
+
+	test('should set knob proportion to 0 when \'defaultValue\' is smaller than min value', () => {
+		jest.spyOn(console, 'warn').mockImplementation(() => {});
+		render(<Slider data-testid="slider" defaultValue={-10} max={100} min={0} progressBarComponent={ProgressBar} />);
+
+		const slider = screen.getByTestId('slider');
+		const expected = '0';
+
+		expect(slider).toHaveStyle({'--ui-slider-proportion-end-knob': expected});
+	});
+
+	test('should set knob proportion to 1 when \'defaultValue\' is bigger than max value', () => {
+		jest.spyOn(console, 'warn').mockImplementation(() => {});
+		render(<Slider data-testid="slider" defaultValue={110} max={100} min={0} progressBarComponent={ProgressBar} />);
+
+		const slider = screen.getByTestId('slider');
+		const expected = '1';
+
+		expect(slider).toHaveStyle({'--ui-slider-proportion-end-knob': expected});
+	});
+
+	test('should set knob proportion to 0.5 when \'defaultValue\' is half of the range between min and max value', () => {
+		render(<Slider data-testid="slider" defaultValue={50} max={100} min={0} progressBarComponent={ProgressBar} />);
+
+		const slider = screen.getByTestId('slider');
+		const expected = '0.5';
+
+		expect(slider).toHaveStyle({'--ui-slider-proportion-end-knob': expected});
+	});
+
+	test('should render a knob', () => {
+		render(<Knob role="knob" />);
+
+		const knob = screen.queryByRole('knob');
+
+		expect(knob).toBeInTheDocument();
 	});
 });
