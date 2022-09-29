f6910be7fa11c03eba43b6ebebd89e6656213d67
Stanca Pop <stanca.pop@lgepartner.com>
2022-09-29 16:03:37 +0300

    added tests for measurable

---
+++ packages/ui/Measurable/tests/Measurable-specs.js
@@ -0,0 +1,29 @@
+import {render} from '@testing-library/react';
+
+import Measurable from '../Measurable';
+
+describe('Measurable', () => {
+    let data;
+
+    const DivComponent = (props) => {
+        data = props;
+
+        return <div />;
+    };
+
+    const MeasurableComponent = Measurable({refProp: 'controlsRef', measurementProp: 'controlsMeasurements'}, DivComponent);
+
+    describe('config', () => {
+        test('should pass \'controlsMeasurements\' prop to the wrapped component ', () => {
+            render(<MeasurableComponent />);
+
+            expect(data).toHaveProperty('controlsMeasurements');
+        });
+
+        test('should pass \'controlsRef\' prop to the wrapped component ', () => {
+            render(<MeasurableComponent />);
+
+            expect(data).toHaveProperty('controlsRef');
+        });
+    });
+});
