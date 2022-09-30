17e779b5e8c89907e5fbf45b03887e19d04acaef
Stanca Pop <stanca.pop@lgepartner.com>
2022-09-30 13:11:05 +0300

    added unit test for placeholder

---
+++ packages/ui/Placeholder/tests/Placeholder-specs.js
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+
+import PlaceholderDecorator from '../../Placeholder';
+
+describe('Placeholder', () => {
+	const Component = PlaceholderDecorator('div');
+
+	describe('config', () => {
+		test('should configure the default style of the placeholder element', () => {
+			render(
+				<div data-testid="wrapper">
+					<Component placeholderComponent={'span'} />
+				</div>
+			);
+
+			const expectedAttribute = 'style';
+			const expectedValue = 'height: 0px; width: auto;';
+			const actual = screen.getByTestId('wrapper').children[0];
+
+			expect(actual).toHaveAttribute(expectedAttribute, expectedValue);
+		});
+	});
+});
