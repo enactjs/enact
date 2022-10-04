dac63c693782888d0b80d7cd0216705f0169be23
Daniel Stoian <daniel.stoian@lgepartner.com>
2022-10-04 16:51:01 +0300

    increased code coverage for ui/measurable

--- packages/ui/Measurable/tests/Measurable-specs.js
+++ packages/ui/Measurable/tests/Measurable-specs.js
@@ -1,29 +1,102 @@
-import {render} from '@testing-library/react';
+import {act, render} from '@testing-library/react';
 
-import Measurable from '../Measurable';
+import Measurable, {useMeasurable} from '../Measurable';
+
+let data;
+
+const DivComponent = (props) => {
+	data = props;
+
+	return <div ref={props.controlsRef} />;
+};
+
+const MeasurableComponent = Measurable({refProp: 'controlsRef', measurementProp: 'controlsMeasurements'}, DivComponent);
+
+const UseMeasurableDecorator = (Wrapped) => {
+	return function UseMeasurableDecorator (props) { // eslint-disable-line no-shadow
+		const {ref: controlsRef, measurement: {width: contentWidth = 0} = {}} = useMeasurable();
+
+		const measurableProps = {
+			controlsRef,
+			contentSize: contentWidth
+		};
+
+		return <Wrapped {...props} {...measurableProps} />;
+	};
+};
+
+const UseMeasurableComponent = UseMeasurableDecorator(DivComponent);
 
 describe('Measurable', () => {
-	let data;
+	test('should pass \'controlsMeasurements\' prop to the wrapped component', () => {
+		let listener = () => {};
+		global.ResizeObserver = class {
+			constructor (ls) {
+				listener = ls;
+			}
+			observe () {}
+			unobserve () {}
+			disconnect () {}
+		};
 
-	const DivComponent = (props) => {
-		data = props;
+		render(<MeasurableComponent />);
 
-		return <div />;
-	};
-
-	const MeasurableComponent = Measurable({refProp: 'controlsRef', measurementProp: 'controlsMeasurements'}, DivComponent);
-
-	describe('config', () => {
-		test('should pass \'controlsMeasurements\' prop to the wrapped component', () => {
-			render(<MeasurableComponent />);
-
-			expect(data).toHaveProperty('controlsMeasurements');
+		act(() => {
+			listener([
+				{
+					target: {
+						clientWidth: 100,
+						scrollWidth: 200,
+						clientHeight: 100,
+						scrollHeight: 200
+					}
+				}
+			]);
 		});
 
-		test('should pass \'controlsRef\' prop to the wrapped component', () => {
-			render(<MeasurableComponent />);
+		expect(data).toHaveProperty('controlsMeasurements');
 
-			expect(data).toHaveProperty('controlsRef');
+		global.ResizeObserver = null;
+	});
+
+	test('should pass \'controlsRef\' prop to the wrapped component', () => {
+		let listener = () => {};
+		global.MutationObserver = class {
+			constructor (ls) {
+				listener = ls;
+			}
+			disconnect () {}
+			observe () {}
+		};
+
+		render(<MeasurableComponent />);
+
+		act(() => {
+			listener([
+				{
+					addedNodes : [],
+					attributeName :"class",
+					attributeNamespace:null,
+					nextSibling:null,
+					oldValue:null,
+					previousSibling:null,
+					removedNodes: [],
+					target: 'div.Button_button__XZ8WH',
+					type:"attributes"
+				}
+			]);
 		});
+
+		expect(data).toHaveProperty('controlsRef');
+
+		global.MutationObserver = null;
+	});
+});
+
+describe('useMeasurable', () => {
+	test('should pass \'controlsRef\' prop to the wrapped component', () => {
+		render(<UseMeasurableComponent />);
+
+		expect(data).toHaveProperty('controlsRef');
 	});
 });
