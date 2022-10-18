62f9e1b2d651244fcd172251e64c104a5c6c4f9e
Seungcheon Baek <sc.baek@lge.com>
2022-10-18 16:28:50 +0900

    Add 'describe' to unit test for better readability
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- packages/core/internal/prop-types/tests/prop-types-specs.js
+++ packages/core/internal/prop-types/tests/prop-types-specs.js
@@ -3,7 +3,7 @@ import {Component, createRef} from 'react';
 import EnactPropTypes from '../prop-types';
 
 
-describe('prop-types (except EnactPropTypes.deprecated)', () => {
+describe('prop-types', () => {
 	const DummyElement = (<div />);
 	const DummyRenderable = () => DummyElement;
 	const dummyString = 'Some String';
@@ -29,240 +29,247 @@ describe('prop-types (except EnactPropTypes.deprecated)', () => {
 		}
 	}
 
-	class RequiredComponent extends Component {
-		static displayName = 'RequiredComponent';
-
-		static propTypes = {
-			typeRequired: EnactPropTypes.renderable.isRequired
-		};
-
-		render ({...rest} = {}) {
-			delete rest.typeRequired;
-			return (<div>Test</div>);
-		}
-	}
-
+	let consoleWarnMock = null;
 	let consoleErrorMock = null;
 
 	beforeEach(() => {
+		consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation();
 		consoleErrorMock = jest.spyOn(console, 'error').mockImplementation();
 	});
 
 	afterEach(() => {
+		consoleWarnMock.mockRestore();
 		consoleErrorMock.mockRestore();
 	});
 
-	test('should not call console.error when required prop value is given', () => {
-		render(<RequiredComponent typeRequired={DummyRenderable} />);
+	describe('no prop', () => {
+		test('should not call console.error when no prop is given', () => {
+			render(<TestComponent />);
 
-		expect(consoleErrorMock).not.toHaveBeenCalled();
+			expect(consoleErrorMock).not.toHaveBeenCalled();
+		});
 	});
 
-	test('should call console.error when prop value is missing for isRequired', () => {
-		render(<RequiredComponent />);
+	describe('component', () => {
+		test('should not call console.error for EnactPropTypes.component if renderable value is given', () => {
+			render(
+				<TestComponent
+					typeComponent={DummyRenderable}
+				/>
+			);
 
-		expect(consoleErrorMock).toHaveBeenCalled();
+			expect(consoleErrorMock).not.toHaveBeenCalled();
+		});
+
+		test('should call console.error for EnactPropTypes.component if string value is given', () => {
+			render(
+				<TestComponent
+					typeComponent={dummyString}
+				/>
+			);
+
+			expect(consoleErrorMock).toHaveBeenCalled();
+		});
+
+		test('should call console.error for EnactPropTypes.component if element value is given', () => {
+			render(
+				<TestComponent
+					typeComponent={DummyElement}
+				/>
+			);
+
+			expect(consoleErrorMock).toHaveBeenCalled();
+		});
 	});
 
-	test('should not call console.error when no prop is given', () => {
-		render(<TestComponent />);
+	describe('componentOverride', () => {
+		test('should not call console.error for EnactPropTypes.componentOverride if renderable value is given', () => {
+			render(
+				<TestComponent
+					typeComponentOverride={DummyRenderable}
+				/>
+			);
 
-		expect(consoleErrorMock).not.toHaveBeenCalled();
+			expect(consoleErrorMock).not.toHaveBeenCalled();
+		});
+
+		test('should call console.error for EnactPropTypes.componentOverride if string value is given', () => {
+			render(
+				<TestComponent
+					typeComponentOverride={dummyString}
+				/>
+			);
+
+			expect(consoleErrorMock).toHaveBeenCalled();
+		});
+
+		test('should not call console.error for EnactPropTypes.componentOverride if element value is given', () => {
+			render(
+				<TestComponent
+					typeComponentOverride={DummyElement}
+				/>
+			);
+
+			expect(consoleErrorMock).not.toHaveBeenCalled();
+		});
 	});
 
-	test('should not call console.error for EnactPropTypes.component if renderable value is given', () => {
-		render(
-			<TestComponent
-				typeComponent={DummyRenderable}
-			/>
-		);
+	describe('ref', () => {
+		test('should not call console.error for EnactPropTypes.ref if ref value is given', () => {
+			const ref = createRef();
 
-		expect(consoleErrorMock).not.toHaveBeenCalled();
+			render(
+				<TestComponent
+					typeRef={ref}
+				/>
+			);
+
+			expect(consoleErrorMock).not.toHaveBeenCalled();
+		});
+
+		test('should call console.error for EnactPropTypes.ref if ref value is given', () => {
+			const ref = dummyString;
+
+			render(
+				<TestComponent
+					typeRef={ref}
+				/>
+			);
+
+			expect(consoleErrorMock).toHaveBeenCalled();
+		});
 	});
 
-	test('should call console.error for EnactPropTypes.component if string value is given', () => {
-		render(
-			<TestComponent
-				typeComponent={dummyString}
-			/>
-		);
+	describe('renderable', () => {
+		test('should not call console.error for EnactPropTypes.renderable if renderable value is given', () => {
+			render(
+				<TestComponent
+					typeRenderable={DummyRenderable}
+				/>
+			);
 
-		expect(consoleErrorMock).toHaveBeenCalled();
+			expect(consoleErrorMock).not.toHaveBeenCalled();
+		});
+
+		test('should not call console.error for EnactPropTypes.renderable if string value is given', () => {
+			render(
+				<TestComponent
+					typeRenderable={dummyString}
+				/>
+			);
+
+			expect(consoleErrorMock).not.toHaveBeenCalled();
+		});
+
+		test('should call console.error for EnactPropTypes.renderable if element value is given', () => {
+			render(
+				<TestComponent
+					typeRenderable={DummyElement}
+				/>
+			);
+
+			expect(consoleErrorMock).toHaveBeenCalled();
+		});
 	});
 
-	test('should call console.error for EnactPropTypes.component if element value is given', () => {
-		render(
-			<TestComponent
-				typeComponent={DummyElement}
-			/>
-		);
+	describe('renderableOverride', () => {
+		test('should not call console.error for EnactPropTypes.renderableOverride if renderable value is given', () => {
+			render(
+				<TestComponent
+					typeRenderableOverride={DummyRenderable}
+				/>
+			);
 
-		expect(consoleErrorMock).toHaveBeenCalled();
+			expect(consoleErrorMock).not.toHaveBeenCalled();
+		});
+
+		test('should not call console.error for EnactPropTypes.renderableOverride if string value is given', () => {
+			render(
+				<TestComponent
+					typeRenderableOverride={dummyString}
+				/>
+			);
+
+			expect(consoleErrorMock).not.toHaveBeenCalled();
+		});
+
+		test('should not call console.error for EnactPropTypes.renderableOverride if element value is given', () => {
+			render(
+				<TestComponent
+					typeRenderableOverride={DummyElement}
+				/>
+			);
+
+			expect(consoleErrorMock).not.toHaveBeenCalled();
+		});
 	});
 
-	test('should not call console.error for EnactPropTypes.componentOverride if renderable value is given', () => {
-		render(
-			<TestComponent
-				typeComponentOverride={DummyRenderable}
-			/>
-		);
-
-		expect(consoleErrorMock).not.toHaveBeenCalled();
-	});
-
-	test('should call console.error for EnactPropTypes.componentOverride if string value is given', () => {
-		render(
-			<TestComponent
-				typeComponentOverride={dummyString}
-			/>
-		);
-
-		expect(consoleErrorMock).toHaveBeenCalled();
-	});
-
-	test('should not call console.error for EnactPropTypes.componentOverride if element value is given', () => {
-		render(
-			<TestComponent
-				typeComponentOverride={DummyElement}
-			/>
-		);
-
-		expect(consoleErrorMock).not.toHaveBeenCalled();
-	});
-
-	test('should not call console.error for EnactPropTypes.ref if ref value is given', () => {
-		const ref = createRef();
-
-		render(
-			<TestComponent
-				typeRef={ref}
-			/>
-		);
-
-		expect(consoleErrorMock).not.toHaveBeenCalled();
-	});
-
-	test('should call console.error for EnactPropTypes.ref if ref value is given', () => {
-		const ref = dummyString;
-
-		render(
-			<TestComponent
-				typeRef={ref}
-			/>
-		);
-
-		expect(consoleErrorMock).toHaveBeenCalled();
-	});
-
-	test('should not call console.error for EnactPropTypes.renderable if renderable value is given', () => {
-		render(
-			<TestComponent
-				typeRenderable={DummyRenderable}
-			/>
-		);
-
-		expect(consoleErrorMock).not.toHaveBeenCalled();
-	});
-
-	test('should not call console.error for EnactPropTypes.renderable if string value is given', () => {
-		render(
-			<TestComponent
-				typeRenderable={dummyString}
-			/>
-		);
-
-		expect(consoleErrorMock).not.toHaveBeenCalled();
-	});
-
-	test('should call console.error for EnactPropTypes.renderable if element value is given', () => {
-		render(
-			<TestComponent
-				typeRenderable={DummyElement}
-			/>
-		);
-
-		expect(consoleErrorMock).toHaveBeenCalled();
-	});
-
-	test('should not call console.error for EnactPropTypes.renderableOverride if renderable value is given', () => {
-		render(
-			<TestComponent
-				typeRenderableOverride={DummyRenderable}
-			/>
-		);
-
-		expect(consoleErrorMock).not.toHaveBeenCalled();
-	});
-
-	test('should not call console.error for EnactPropTypes.renderableOverride if string value is given', () => {
-		render(
-			<TestComponent
-				typeRenderableOverride={dummyString}
-			/>
-		);
-
-		expect(consoleErrorMock).not.toHaveBeenCalled();
-	});
-
-	test('should not call console.error for EnactPropTypes.renderableOverride if element value is given', () => {
-		render(
-			<TestComponent
-				typeRenderableOverride={DummyElement}
-			/>
-		);
-
-		expect(consoleErrorMock).not.toHaveBeenCalled();
-	});
-});
-
-describe('prop-types (only EnactPropTypes.deprecated)', () => {
-	let consoleWarnMock = null;
-
-	beforeEach(() => {
-		consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation();
-	});
-
-	afterEach(() => {
-		consoleWarnMock.mockRestore();
-	});
-
-	test('should call console.warn when prop value is given for EnactPropTypes.deprecated', () => {
-		const ref = createRef();
-
-		class DeprecatedComponent extends Component {
-			static displayName = 'DeprecatedComponent';
+	describe('isRequired', () => {
+		class RequiredComponent extends Component {
+			static displayName = 'RequiredComponent';
 
 			static propTypes = {
-				typeDeprecated: EnactPropTypes.deprecated(EnactPropTypes.ref)
+				typeRequired: EnactPropTypes.renderable.isRequired
 			};
 
 			render ({...rest} = {}) {
-				delete rest.typeDeprecated;
+				delete rest.typeRequired;
 				return (<div>Test</div>);
 			}
 		}
 
-		render(<DeprecatedComponent typeDeprecated={ref} />);
+		test('should not call console.error when required prop value is given', () => {
+			render(<RequiredComponent typeRequired={DummyRenderable} />);
 
-		expect(consoleWarnMock).toHaveBeenCalled();
+			expect(consoleErrorMock).not.toHaveBeenCalled();
+		});
+
+		test('should call console.error when prop value is missing for isRequired', () => {
+			render(<RequiredComponent />);
+
+			expect(consoleErrorMock).toHaveBeenCalled();
+		});
 	});
 
-	test('should call console.warn even when prop value is not given for EnactPropTypes.deprecated', () => {
-		class DeprecatedComponent extends Component {
-			static displayName = 'DeprecatedComponent';
+	describe('deprecated', () => {
+		test('should call console.warn when prop value is given for EnactPropTypes.deprecated', () => {
+			const ref = createRef();
 
-			static propTypes = {
-				typeDeprecated: EnactPropTypes.deprecated(EnactPropTypes.ref)
-			};
+			class DeprecatedComponent extends Component {
+				static displayName = 'DeprecatedComponent';
 
-			render ({...rest} = {}) {
-				delete rest.typeDeprecated;
-				return (<div>Test</div>);
+				static propTypes = {
+					typeDeprecated: EnactPropTypes.deprecated(EnactPropTypes.ref)
+				};
+
+				render ({...rest} = {}) {
+					delete rest.typeDeprecated;
+					return (<div>Test</div>);
+				}
 			}
-		}
 
-		render(<DeprecatedComponent />);
+			render(<DeprecatedComponent typeDeprecated={ref} />);
 
-		expect(consoleWarnMock).toHaveBeenCalled();
+			expect(consoleWarnMock).toHaveBeenCalled();
+		});
+
+		test('should call console.warn even when prop value is not given for EnactPropTypes.deprecated', () => {
+			class DeprecatedComponent extends Component {
+				static displayName = 'DeprecatedComponent';
+
+				static propTypes = {
+					typeDeprecated: EnactPropTypes.deprecated(EnactPropTypes.ref)
+				};
+
+				render ({...rest} = {}) {
+					delete rest.typeDeprecated;
+					return (<div>Test</div>);
+				}
+			}
+
+			render(<DeprecatedComponent />);
+
+			expect(consoleWarnMock).toHaveBeenCalled();
+		});
 	});
 });
