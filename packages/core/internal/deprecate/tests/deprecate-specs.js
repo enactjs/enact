b2a2de60ff4470f4756efae4eefd62ac85ef107b
Seungcheon Baek <sc.baek@lge.com>
2022-10-18 16:00:21 +0900

    Refine unit test code
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- packages/core/internal/deprecate/tests/deprecate-specs.js
+++ packages/core/internal/deprecate/tests/deprecate-specs.js
@@ -11,32 +11,31 @@ describe('deprecate', () => {
 		alwaysWarn: true
 	};
 	const expectedMessage = 'DEPRECATED: DeprecatedThing since 0.0.0. Will be removed in 0.0.1. Replaced by ValidThing. No more information.';
+	let consoleWarnMock = null;
+
+	beforeEach(() => {
+		consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation();
+	});
+
+	afterEach(() => {
+		consoleWarnMock.mockRestore();
+	});
 
 	test('should call console.warn with a warning message for deprecated function', () => {
-		const consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation();
 		const WarningThing = deprecate(Thing, config);
 
 		const expected = Thing();
 		const actual = WarningThing();
 
 		expect(actual).toBe(expected);
-		// eslint-disable-next-line no-console
-		expect(console.warn).toHaveBeenCalledWith(expect.stringContaining(expectedMessage));
-
-		consoleWarnMock.mockRestore();
+		expect(consoleWarnMock).toHaveBeenCalledWith(expect.stringContaining(expectedMessage));
 	});
 
 	test('should call console.warn with a warning message for stand-alone usage', () => {
-		const consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation();
-
 		const expected = config;
 		const actual = deprecate(config);
 
 		expect(actual).toBe(expected);
-		// eslint-disable-next-line no-console
-		expect(console.warn).toHaveBeenCalledWith(expect.stringContaining(expectedMessage));
-
-		consoleWarnMock.mockRestore();
+		expect(consoleWarnMock).toHaveBeenCalledWith(expect.stringContaining(expectedMessage));
 	});
-
 });
--- packages/core/internal/prop-types/tests/prop-types-specs.js
+++ packages/core/internal/prop-types/tests/prop-types-specs.js
@@ -2,18 +2,8 @@ import {render} from '@testing-library/react';
 import {Component, createRef} from 'react';
 import EnactPropTypes from '../prop-types';
 
-/*
-const EnactPropTypes = {
-	component,
-	componentOverride,
-	ref,
-	deprecated,
-	renderable,
-	renderableOverride
-};
-*/
 
-describe('prop-types', () => {
+describe('prop-types (except EnactPropTypes.deprecated)', () => {
 	const DummyElement = (<div />);
 	const DummyRenderable = () => DummyElement;
 	const dummyString = 'Some String';
@@ -39,9 +29,217 @@ describe('prop-types', () => {
 		}
 	}
 
-	test('should call console.warn when prop value is given for EnactPropTypes.deprecated', () => {
-		const consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation();
+	let consoleErrorMock = null;
 
+	beforeEach(() => {
+		consoleErrorMock = jest.spyOn(console, 'error').mockImplementation();
+	});
+
+	afterEach(() => {
+		consoleErrorMock.mockRestore();
+	});
+
+	test('should not call console.error when required prop value is given', () => {
+		class RequiredComponent extends Component {
+			static displayName = 'RequiredComponent';
+
+			static propTypes = {
+				typeRequired: EnactPropTypes.renderable.isRequired
+			};
+
+			render ({...rest} = {}) {
+				delete rest.typeRequired;
+				return (<div>Test</div>);
+			}
+		}
+
+		render(<RequiredComponent typeRequired={DummyRenderable} />);
+
+		expect(consoleErrorMock).not.toHaveBeenCalled();
+	});
+
+	test('should call console.error when prop value is missing for isRequired', () => {
+		class RequiredComponent extends Component {
+			static displayName = 'RequiredComponent';
+
+			static propTypes = {
+				typeRequired: EnactPropTypes.renderable.isRequired
+			};
+
+			render ({...rest} = {}) {
+				delete rest.typeRequired;
+				return (<div>Test</div>);
+			}
+		}
+
+		render(<RequiredComponent />);
+
+		expect(consoleErrorMock).toHaveBeenCalled();
+	});
+
+	test('should not call console.error when no prop is given', () => {
+		render(<TestComponent />);
+
+		expect(consoleErrorMock).not.toHaveBeenCalled();
+	});
+
+	test('should not call console.error for EnactPropTypes.component if renderable value is given', () => {
+		render(
+			<TestComponent
+				typeComponent={DummyRenderable}
+			/>
+		);
+
+		expect(consoleErrorMock).not.toHaveBeenCalled();
+	});
+
+	test('should call console.error for EnactPropTypes.component if string value is given', () => {
+		render(
+			<TestComponent
+				typeComponent={dummyString}
+			/>
+		);
+
+		expect(consoleErrorMock).toHaveBeenCalled();
+	});
+
+	test('should call console.error for EnactPropTypes.component if element value is given', () => {
+		render(
+			<TestComponent
+				typeComponent={DummyElement}
+			/>
+		);
+
+		expect(consoleErrorMock).toHaveBeenCalled();
+	});
+
+	test('should not call console.error for EnactPropTypes.componentOverride if renderable value is given', () => {
+		render(
+			<TestComponent
+				typeComponentOverride={DummyRenderable}
+			/>
+		);
+
+		expect(consoleErrorMock).not.toHaveBeenCalled();
+	});
+
+	test('should call console.error for EnactPropTypes.componentOverride if string value is given', () => {
+		render(
+			<TestComponent
+				typeComponentOverride={dummyString}
+			/>
+		);
+
+		expect(consoleErrorMock).toHaveBeenCalled();
+	});
+
+	test('should not call console.error for EnactPropTypes.componentOverride if element value is given', () => {
+		render(
+			<TestComponent
+				typeComponentOverride={DummyElement}
+			/>
+		);
+
+		expect(consoleErrorMock).not.toHaveBeenCalled();
+	});
+
+	test('should not call console.error for EnactPropTypes.ref if ref value is given', () => {
+		const ref = createRef();
+
+		render(
+			<TestComponent
+				typeRef={ref}
+			/>
+		);
+
+		expect(consoleErrorMock).not.toHaveBeenCalled();
+	});
+
+	test('should call console.error for EnactPropTypes.ref if ref value is given', () => {
+		const ref = dummyString;
+
+		render(
+			<TestComponent
+				typeRef={ref}
+			/>
+		);
+
+		expect(consoleErrorMock).toHaveBeenCalled();
+	});
+
+	test('should not call console.error for EnactPropTypes.renderable if renderable value is given', () => {
+		render(
+			<TestComponent
+				typeRenderable={DummyRenderable}
+			/>
+		);
+
+		expect(consoleErrorMock).not.toHaveBeenCalled();
+	});
+
+	test('should not call console.error for EnactPropTypes.renderable if string value is given', () => {
+		render(
+			<TestComponent
+				typeRenderable={dummyString}
+			/>
+		);
+
+		expect(consoleErrorMock).not.toHaveBeenCalled();
+	});
+
+	test('should call console.error for EnactPropTypes.renderable if element value is given', () => {
+		render(
+			<TestComponent
+				typeRenderable={DummyElement}
+			/>
+		);
+
+		expect(consoleErrorMock).toHaveBeenCalled();
+	});
+
+	test('should not call console.error for EnactPropTypes.renderableOverride if renderable value is given', () => {
+		render(
+			<TestComponent
+				typeRenderableOverride={DummyRenderable}
+			/>
+		);
+
+		expect(consoleErrorMock).not.toHaveBeenCalled();
+	});
+
+	test('should not call console.error for EnactPropTypes.renderableOverride if string value is given', () => {
+		render(
+			<TestComponent
+				typeRenderableOverride={dummyString}
+			/>
+		);
+
+		expect(consoleErrorMock).not.toHaveBeenCalled();
+	});
+
+	test('should not call console.error for EnactPropTypes.renderableOverride if element value is given', () => {
+		render(
+			<TestComponent
+				typeRenderableOverride={DummyElement}
+			/>
+		);
+
+		expect(consoleErrorMock).not.toHaveBeenCalled();
+	});
+});
+
+describe('prop-types (only EnactPropTypes.deprecated)', () => {
+	let consoleWarnMock = null;
+
+	beforeEach(() => {
+		consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation();
+	});
+
+	afterEach(() => {
+		consoleWarnMock.mockRestore();
+	});
+
+	test('should call console.warn when prop value is given for EnactPropTypes.deprecated', () => {
 		const ref = createRef();
 
 		class DeprecatedComponent extends Component {
@@ -59,15 +257,10 @@ describe('prop-types', () => {
 
 		render(<DeprecatedComponent typeDeprecated={ref} />);
 
-		// eslint-disable-next-line no-console
-		expect(console.warn).toHaveBeenCalled();
-
-		consoleWarnMock.mockRestore();
+		expect(consoleWarnMock).toHaveBeenCalled();
 	});
 
 	test('should call console.warn even when prop value is not given for EnactPropTypes.deprecated', () => {
-		const consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation();
-
 		class DeprecatedComponent extends Component {
 			static displayName = 'DeprecatedComponent';
 
@@ -83,283 +276,6 @@ describe('prop-types', () => {
 
 		render(<DeprecatedComponent />);
 
-		// eslint-disable-next-line no-console
-		expect(console.warn).toHaveBeenCalled();
-
-		consoleWarnMock.mockRestore();
+		expect(consoleWarnMock).toHaveBeenCalled();
 	});
-
-	test('should not call console.error when required prop value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		class RequiredComponent extends Component {
-			static displayName = 'RequiredComponent';
-
-			static propTypes = {
-				typeRequired: EnactPropTypes.renderable.isRequired
-			};
-
-			render ({...rest} = {}) {
-				delete rest.typeRequired;
-				return (<div>Test</div>);
-			}
-		}
-
-		render(<RequiredComponent typeRequired={DummyRenderable} />);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).not.toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should call console.error when prop value is missing for isRequired', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		class RequiredComponent extends Component {
-			static displayName = 'RequiredComponent';
-
-			static propTypes = {
-				typeRequired: EnactPropTypes.renderable.isRequired
-			};
-
-			render ({...rest} = {}) {
-				delete rest.typeRequired;
-				return (<div>Test</div>);
-			}
-		}
-
-		render(<RequiredComponent />);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should not call console.error when no prop is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		render(<TestComponent />);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).not.toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should not call console.error for EnactPropTypes.component if renderable value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		render(
-			<TestComponent
-				typeComponent={DummyRenderable}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).not.toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should call console.error for EnactPropTypes.component if string value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		render(
-			<TestComponent
-				typeComponent={dummyString}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should call console.error for EnactPropTypes.component if element value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		render(
-			<TestComponent
-				typeComponent={DummyElement}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should not call console.error for EnactPropTypes.componentOverride if renderable value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		render(
-			<TestComponent
-				typeComponentOverride={DummyRenderable}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).not.toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should call console.error for EnactPropTypes.componentOverride if string value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		render(
-			<TestComponent
-				typeComponentOverride={dummyString}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should not call console.error for EnactPropTypes.componentOverride if element value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		render(
-			<TestComponent
-				typeComponentOverride={DummyElement}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).not.toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should not call console.error for EnactPropTypes.ref if ref value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		const ref = createRef();
-
-		render(
-			<TestComponent
-				typeRef={ref}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).not.toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should call console.error for EnactPropTypes.ref if ref value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		const ref = dummyString;
-
-		render(
-			<TestComponent
-				typeRef={ref}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should not call console.error for EnactPropTypes.renderable if renderable value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		render(
-			<TestComponent
-				typeRenderable={DummyRenderable}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).not.toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should not call console.error for EnactPropTypes.renderable if string value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		render(
-			<TestComponent
-				typeRenderable={dummyString}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).not.toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should call console.error for EnactPropTypes.renderable if element value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		render(
-			<TestComponent
-				typeRenderable={DummyElement}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should not call console.error for EnactPropTypes.renderableOverride if renderable value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		render(
-			<TestComponent
-				typeRenderableOverride={DummyRenderable}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).not.toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should not call console.error for EnactPropTypes.renderableOverride if string value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		render(
-			<TestComponent
-				typeRenderableOverride={dummyString}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).not.toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
-	test('should not call console.error for EnactPropTypes.renderableOverride if element value is given', () => {
-		const consoleErrMock = jest.spyOn(console, 'error').mockImplementation();
-
-		render(
-			<TestComponent
-				typeRenderableOverride={DummyElement}
-			/>
-		);
-
-		// eslint-disable-next-line no-console
-		expect(console.error).not.toHaveBeenCalled();
-
-		consoleErrMock.mockRestore();
-	});
-
 });
