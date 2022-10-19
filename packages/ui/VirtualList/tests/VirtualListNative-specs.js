d3cdfc44feab8e50d8cc61da09c2e4aa4cfcca8e
jeonghee27 <jeonghee27.ahn@lge.com>
2022-10-19 14:06:09 +0900

    Add test files for scrollmode="native"

---
+++ packages/ui/VirtualList/tests/VirtualListNative-specs.js
@@ -0,0 +1,337 @@
+import '@testing-library/jest-dom';
+import {act, render, screen} from '@testing-library/react';
+
+import VirtualList from '../VirtualList';
+
+describe('VirtualListNative', () => {
+	let
+		clientSize,
+		dataSize,
+		getScrollTo,
+		handlerOnScroll,
+		handlerOnScrollStart,
+		handlerOnScrollStop,
+		items,
+		myScrollTo,
+		onScrollCount,
+		onScrollStartCount,
+		onScrollStopCount,
+		renderItem,
+		resultScrollTop;
+
+	beforeEach(() => {
+		clientSize = {clientWidth: 1280, clientHeight: 720};
+		dataSize = 100;
+		items = [];
+		onScrollCount = 0;
+		onScrollStartCount = 0;
+		onScrollStopCount = 0;
+		resultScrollTop = 0;
+
+		getScrollTo = (scrollTo) => {
+			myScrollTo = scrollTo;
+		};
+		handlerOnScroll = () => {
+			onScrollCount++;
+		};
+		handlerOnScrollStart = () => {
+			onScrollStartCount++;
+		};
+		handlerOnScrollStop = (done, testCase) => (e) => {
+			onScrollStopCount++;
+			resultScrollTop = e.scrollTop;
+
+			testCase();
+			done();
+		};
+		renderItem = ({index, ...rest}) => {	// eslint-disable-line enact/display-name
+			return (
+				<div {...rest} id={'item' + index}>
+					{items[index].name}
+				</div>
+			);
+		};
+
+		for (let i = 0; i < dataSize; i++) {
+			items.push({name: 'Account ' + i});
+		}
+	});
+
+	afterEach(() => {
+		clientSize = null;
+		dataSize = null;
+		getScrollTo = null;
+		handlerOnScroll = null;
+		handlerOnScrollStart = null;
+		handlerOnScrollStop = null;
+		items = null;
+		myScrollTo = null;
+		onScrollCount = null;
+		onScrollStartCount = null;
+		onScrollStopCount = null;
+		renderItem = null;
+		resultScrollTop = null;
+	});
+
+	test('should render a list of \'items\'', () => {
+		render(
+			<VirtualList
+				clientSize={clientSize}
+				dataSize={dataSize}
+				itemRenderer={renderItem}
+				itemSize={30}
+				scrollMode="native"
+			/>
+		);
+
+		const expected = 'Account 0';
+		const actual = screen.getByRole('list').children.item(0).textContent;
+
+		expect(actual).toBe(expected);
+	});
+
+	test('should render overhang items when clientSize is not given', () => {
+		render(
+			<VirtualList
+				dataSize={dataSize}
+				itemRenderer={renderItem}
+				itemSize={30}
+				scrollMode="native"
+			/>
+		);
+
+		const expected = 3;
+		const actual = screen.getByRole('list').children.length;
+
+		expect(actual).toBe(expected);
+	});
+
+	test('should render (clientHeight / itemHeight + overhang) items', () => {
+		render(
+			<VirtualList
+				clientSize={clientSize}
+				dataSize={dataSize}
+				itemRenderer={renderItem}
+				itemSize={30}
+				scrollMode="native"
+			/>
+		);
+
+		const expected = 27; // 720 / 30 + 3
+		const actual = screen.getByRole('list').children.length;
+
+		expect(actual).toBe(expected);
+	});
+
+	test('should re-render clientHeight / itemHeight + overhang) items after changing client size', () => {
+		const {rerender} = render(
+			<VirtualList
+				clientSize={clientSize}
+				dataSize={dataSize}
+				itemRenderer={renderItem}
+				itemSize={30}
+				scrollMode="native"
+			/>
+		);
+
+		const newClientSize = {clientWidth: 1280, clientHeight: 360};
+
+		rerender(
+			<VirtualList
+				clientSize={newClientSize}
+				dataSize={dataSize}
+				itemRenderer={renderItem}
+				itemSize={30}
+				scrollMode="native"
+			/>
+		);
+
+		const expected = 15; // 360 / 30 + 3
+		const actual = screen.getByRole('list').children.length;
+
+		expect(actual).toBe(expected);
+	});
+
+	describe('ScrollTo', () => {
+		test.skip('should scroll to the specific item of a given index with scrollTo', (done) => {
+			const onScrollStop = handlerOnScrollStop(done, () => {
+				const expected = 300;
+				const actual = resultScrollTop;
+
+				expect(actual).toBe(expected);
+			});
+
+			render(
+				<VirtualList
+					cbScrollTo={getScrollTo}
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={30}
+					onScrollStop={onScrollStop}
+					scrollMode="native"
+				/>
+			);
+
+			act(() => myScrollTo({index: 10, animate: false}));
+		});
+
+		test.skip('should scroll to the given \'x\' position with scrollTo', (done) => {
+			const onScrollStop = handlerOnScrollStop(done, () => {
+				const expected = 1;
+				const actual = onScrollStopCount;
+
+				expect(actual).toBe(expected);
+			});
+
+			render(
+				<VirtualList
+					cbScrollTo={getScrollTo}
+					clientSize={clientSize}
+					dataSize={dataSize}
+					direction="horizontal"
+					itemRenderer={renderItem}
+					itemSize={30}
+					onScrollStop={onScrollStop}
+					scrollMode="native"
+				/>
+			);
+
+			act(() => myScrollTo({position: {x: 100}, animate: false}));
+		});
+
+		test.skip('should scroll to the given \'y\' position with scrollTo', (done) => {
+			const onScrollStop = handlerOnScrollStop(done, () => {
+				const expected = 100;
+				const actual = resultScrollTop;
+
+				expect(actual).toBe(expected);
+			});
+
+			render(
+				<VirtualList
+					cbScrollTo={getScrollTo}
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={30}
+					onScrollStop={onScrollStop}
+					scrollMode="native"
+				/>
+			);
+
+			act(() => myScrollTo({position: {y: 100}, animate: false}));
+		});
+
+		describe('scroll events', () => {
+			test.skip('should call onScrollStart once', () => {
+				render(
+					<VirtualList
+						cbScrollTo={getScrollTo}
+						clientSize={clientSize}
+						dataSize={dataSize}
+						itemRenderer={renderItem}
+						itemSize={30}
+						onScrollStart={handlerOnScrollStart}
+						scrollMode="native"
+					/>
+				);
+
+				act(() => myScrollTo({position: {y: 100}, animate: false}));
+
+				const expected = 1;
+				const actual = onScrollStartCount;
+
+				expect(actual).toBe(expected);
+			});
+
+			test.skip('should call onScroll once', () => {
+				render(
+					<VirtualList
+						cbScrollTo={getScrollTo}
+						clientSize={clientSize}
+						dataSize={dataSize}
+						itemRenderer={renderItem}
+						itemSize={30}
+						onScroll={handlerOnScroll}
+						scrollMode="native"
+					/>
+				);
+
+				act(() => myScrollTo({position: {y: 100}, animate: false}));
+
+				const expected = 1;
+				const actual = onScrollCount;
+
+				expect(actual).toBe(expected);
+			});
+
+			test.skip('should call onScrollStop once', (done) => {
+				const onScrollStop = handlerOnScrollStop(done, () => {
+					const expected = 1;
+					const actual = onScrollStopCount;
+
+					expect(actual).toBe(expected);
+				});
+
+				render(
+					<VirtualList
+						cbScrollTo={getScrollTo}
+						clientSize={clientSize}
+						dataSize={dataSize}
+						itemRenderer={renderItem}
+						itemSize={30}
+						onScrollStop={onScrollStop}
+						scrollMode="native"
+					/>
+				);
+
+				act(() => myScrollTo({position: {y: 100}, animate: false}));
+			});
+		});
+	});
+
+	describe('Adding an item', () => {
+		test('should render an added item named \'Password 0\' as the first item', (done) => {
+			const itemArray = [{name: 'A'}, {name: 'B'}, {name: 'C'}];
+			const renderItemArray = ({index, ...rest}) => {
+				return (
+					<div {...rest} id={'item' + index}>
+						{itemArray[index].name}
+					</div>
+				);
+			};
+
+			const {rerender} = render(
+				<VirtualList
+					clientSize={clientSize}
+					dataSize={itemArray.length}
+					itemRenderer={renderItemArray}
+					itemSize={30}
+					scrollMode="native"
+				/>
+			);
+
+			itemArray.unshift({name: 'Password 0'});
+			rerender(
+				<VirtualList
+					clientSize={clientSize}
+					dataSize={itemArray.length}
+					itemRenderer={renderItemArray}
+					itemSize={30}
+					scrollMode="native"
+				/>
+			);
+
+			jest.useFakeTimers();
+
+			act(() => jest.advanceTimersByTime(0));
+			const expected = itemArray[0].name;
+			const actual = screen.getByRole('list').children.item(0).textContent;
+
+			expect(actual).toBe(expected);
+			done();
+			jest.useRealTimers();
+		});
+	});
+});
