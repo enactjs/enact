b0117581c6bec79b138d6280a3c7af33c19768ec
jeonghee27 <jeonghee27.ahn@lge.com>
2022-10-24 16:33:56 +0900

    Add VirtualList testcases Enact-DCO-1.0-Signed-off-by: Jeonghee Ahn (jeonghee27.ahn@lge.com)

--- packages/ui/VirtualList/tests/VirtualGridList-specs.js
+++ packages/ui/VirtualList/tests/VirtualGridList-specs.js
@@ -1,8 +1,37 @@
 import '@testing-library/jest-dom';
-import {act, render, screen} from '@testing-library/react';
+import {act, fireEvent, render, screen} from '@testing-library/react';
 
 import {VirtualGridList} from '../VirtualList';
 import {ImageItem as UiImageItem} from '../../ImageItem';
+
+const activate = (list) => fireEvent.keyUp(list, {keyCode: 13});
+const keyDown = (keyCode) => (slider) => fireEvent.keyDown(slider, {keyCode});
+
+const downKeyDown = keyDown(40);
+
+const getElementClientCenter = (element) => {
+	const {left, top, width, height} = element.getBoundingClientRect();
+	return {x: left + width / 2, y: top + height / 2};
+};
+
+const drag = async (element, {delta, steps = 1}) => {
+	const from = getElementClientCenter(element);
+	const to = {x: from.x + delta.x, y: from.y + delta.y};
+	const step = {x: (to.x - from.x) / steps, y: (to.y - from.y) / steps};
+	const current = {clientX: from.x, clientY: from.y};
+
+	fireEvent.mouseEnter(element, current);
+	fireEvent.mouseOver(element, current);
+	fireEvent.mouseMove(element, current);
+	fireEvent.mouseDown(element, current);
+	for (let i = 0; i < steps; i++) {
+		current.clientX += step.x;
+		current.clientY += step.y;
+		act(() => jest.advanceTimersByTime(1000 / steps));
+		fireEvent.mouseMove(element, current);
+	}
+	fireEvent.mouseUp(element, current);
+};
 
 describe('VirtualGridList', () => {
 	let
@@ -20,6 +49,7 @@ describe('VirtualGridList', () => {
 		onScrollStopCount,
 		renderItem,
 		resultScrollTop,
+		startScrollTop,
 		svgGenerator;
 
 	beforeEach(() => {
@@ -31,6 +61,7 @@ describe('VirtualGridList', () => {
 		onScrollStartCount = 0;
 		onScrollStopCount = 0;
 		resultScrollTop = 0;
+		startScrollTop = 0;
 
 		getScrollTo = (scrollTo) => {
 			myScrollTo = scrollTo;
@@ -38,7 +69,8 @@ describe('VirtualGridList', () => {
 		handlerOnScroll = () => {
 			onScrollCount++;
 		};
-		handlerOnScrollStart = () => {
+		handlerOnScrollStart = (e) => {
+			resultScrollTop = e.scrollTop;
 			onScrollStartCount++;
 		};
 		handlerOnScrollStop = (done, testCase) => (e) => {
@@ -92,12 +124,14 @@ describe('VirtualGridList', () => {
 		handlerOnScrollStart = null;
 		handlerOnScrollStop = null;
 		items = null;
+		itemSize = null;
 		myScrollTo = null;
 		onScrollCount = null;
 		onScrollStartCount = null;
 		onScrollStopCount = null;
 		renderItem = null;
 		resultScrollTop = null;
+		startScrollTop = null;
 	});
 
 	test('should render a list of \'items\'', () => {
@@ -228,68 +262,319 @@ describe('VirtualGridList', () => {
 			act(() => myScrollTo({position: {y: 100}, animate: false}));
 		});
 
-		describe('scroll events', () => {
-			test('should call onScrollStart once', () => {
-				render(
-					<VirtualGridList
-						cbScrollTo={getScrollTo}
-						clientSize={clientSize}
-						dataSize={dataSize}
-						itemRenderer={renderItem}
-						itemSize={itemSize}
-						onScrollStart={handlerOnScrollStart}
-					/>
-				);
-
-				act(() => myScrollTo({position: {y: 100}, animate: false}));
-
-				const expected = 1;
-				const actual = onScrollStartCount;
+		test('should scroll to the given align with scrollTo', (done) => {
+			const onScrollStop = handlerOnScrollStop(done, () => {
+				const expected = Math.ceil(dataSize / 7 ) * (itemSize.minHeight + 3) - clientSize.clientHeight; // 1280/180 = 7
+				const actual = resultScrollTop;
 
 				expect(actual).toBe(expected);
 			});
 
-			test('should call onScroll once', () => {
-				render(
-					<VirtualGridList
-						cbScrollTo={getScrollTo}
-						clientSize={clientSize}
-						dataSize={dataSize}
-						itemRenderer={renderItem}
-						itemSize={itemSize}
-						onScroll={handlerOnScroll}
-					/>
-				);
+			render(
+				<VirtualGridList
+					cbScrollTo={getScrollTo}
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={itemSize}
+					onScrollStop={onScrollStop}
+				/>
+			);
 
-				act(() => myScrollTo({position: {y: 100}, animate: false}));
+			act(() => myScrollTo({align: 'bottom', animate: false}));
+		});
 
-				const expected = 1;
-				const actual = onScrollCount;
+		test('should scroll to the given align with scrollTo after changing dataSize', (done) => {
+			const newDataSize = 50;
+			const onScrollStop = handlerOnScrollStop(done, () => {
+				const expected = Math.ceil(newDataSize / 7 ) * (itemSize.minHeight + 3) - clientSize.clientHeight;
+				const actual = resultScrollTop;
 
 				expect(actual).toBe(expected);
 			});
 
-			test('should call onScrollStop once', (done) => {
-				const onScrollStop = handlerOnScrollStop(done, () => {
-					const expected = 1;
-					const actual = onScrollStopCount;
+			const {rerender} = render(
+				<VirtualGridList
+					cbScrollTo={getScrollTo}
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={itemSize}
+					onScrollStop={onScrollStop}
+				/>
+			);
 
-					expect(actual).toBe(expected);
-				});
+			rerender(
+				<VirtualGridList
+					cbScrollTo={getScrollTo}
+					clientSize={clientSize}
+					dataSize={newDataSize}
+					itemRenderer={renderItem}
+					itemSize={itemSize}
+					onScrollStop={onScrollStop}
+				/>
+			);
 
-				render(
-					<VirtualGridList
-						cbScrollTo={getScrollTo}
-						clientSize={clientSize}
-						dataSize={dataSize}
-						itemRenderer={renderItem}
-						itemSize={itemSize}
-						onScrollStop={onScrollStop}
-					/>
-				);
+			act(() => myScrollTo({align: 'bottom', animate: false}));
+		});
 
-				act(() => myScrollTo({position: {y: 100}, animate: false}));
+		test('should scroll to the given align with scrollTo after changing itemSize', (done) => {
+			const newItemSize = {minWidth: 180, minHeight: 300};
+			const onScrollStop = handlerOnScrollStop(done, () => {
+				const expected = Math.ceil(dataSize / 7 ) * (newItemSize.minHeight + 3) - clientSize.clientHeight;
+				const actual = resultScrollTop;
+
+				expect(actual).toBe(expected);
 			});
+
+			const {rerender} = render(
+				<VirtualGridList
+					cbScrollTo={getScrollTo}
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={itemSize}
+					onScrollStop={onScrollStop}
+				/>
+			);
+
+			rerender(
+				<VirtualGridList
+					cbScrollTo={getScrollTo}
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={newItemSize}
+					onScrollStop={onScrollStop}
+				/>
+			);
+
+			act(() => myScrollTo({align: 'bottom', animate: false}));
+		});
+
+		test('should scroll to the given align with scrollTo after changing spacing', (done) => {
+			const newSpacing = 1;
+			const onScrollStop = handlerOnScrollStop(done, () => {
+				const expected = Math.ceil(dataSize / 7 ) * (itemSize.minHeight + newSpacing + 3) - clientSize.clientHeight - newSpacing;
+				const actual = resultScrollTop;
+
+				expect(actual).toBe(expected);
+			});
+
+			const {rerender} = render(
+				<VirtualGridList
+					cbScrollTo={getScrollTo}
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={itemSize}
+					onScrollStop={onScrollStop}
+				/>
+			);
+
+			rerender(
+				<VirtualGridList
+					cbScrollTo={getScrollTo}
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={itemSize}
+					onScrollStop={onScrollStop}
+					spacing={newSpacing}
+				/>
+			);
+
+			act(() => myScrollTo({align: 'bottom', animate: false}));
+		});
+	});
+
+	describe('scroll events', () => {
+		test('should call onScrollStart once', () => {
+			render(
+				<VirtualGridList
+					cbScrollTo={getScrollTo}
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={itemSize}
+					onScrollStart={handlerOnScrollStart}
+				/>
+			);
+
+			act(() => myScrollTo({position: {y: 100}, animate: false}));
+
+			const expected = 1;
+			const actual = onScrollStartCount;
+
+			expect(actual).toBe(expected);
+		});
+
+		test('should call onScroll once', () => {
+			render(
+				<VirtualGridList
+					cbScrollTo={getScrollTo}
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={itemSize}
+					onScroll={handlerOnScroll}
+				/>
+			);
+
+			act(() => myScrollTo({position: {y: 100}, animate: false}));
+
+			const expected = 1;
+			const actual = onScrollCount;
+
+			expect(actual).toBe(expected);
+		});
+
+		test('should call onScrollStop once', (done) => {
+			const onScrollStop = handlerOnScrollStop(done, () => {
+				const expected = 1;
+				const actual = onScrollStopCount;
+
+				expect(actual).toBe(expected);
+			});
+
+			render(
+				<VirtualGridList
+					cbScrollTo={getScrollTo}
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={itemSize}
+					onScrollStop={onScrollStop}
+				/>
+			);
+
+			act(() => myScrollTo({position: {y: 100}, animate: false}));
+		});
+	});
+
+	describe('Scroll by event', () => {
+		beforeEach(() => {
+			jest.useFakeTimers();
+		});
+
+		afterEach(() => {
+			jest.useRealTimers();
+		});
+
+		test('should scroll to the by wheel', (done) => {
+			const fn = jest.fn();
+
+			const onScrollStop = handlerOnScrollStop(done, () => {
+				fn();
+				expect(startScrollTop).toBe(0);
+				expect(onScrollStartCount).toBe(1);
+				expect(resultScrollTop).toBeGreaterThan(0);
+			});
+
+			render(
+				<VirtualGridList
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={itemSize}
+					onScrollStop={onScrollStop}
+					onScrollStart={handlerOnScrollStart}
+				/>
+			);
+
+			const list = screen.getByRole('list');
+			fireEvent.wheel(list, {deltaY: 100});
+
+			act(() => jest.advanceTimersByTime(1000)); // Wait onScrollStop
+
+			expect(fn).toBeCalled();
+		});
+
+		test('should not scroll to the by wheel when `noScrollByWheel` prop is true', (done) => {
+			const fn = jest.fn();
+
+			render(
+				<VirtualGridList
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={itemSize}
+					noScrollByWheel
+					onScrollStop={fn}
+					onScrollStart={handlerOnScrollStart}
+				/>
+			);
+
+			const list = screen.getByRole('list');
+			fireEvent.wheel(list, {deltaY: 100});
+
+			act(() => jest.advanceTimersByTime(1000)); // Wait onScrollStop
+
+			expect(fn).not.toHaveBeenCalled();
+
+			done();
+		});
+
+		test('should scroll to the by drag', async () => {
+			const fn = jest.fn();
+
+			const onScrollStop = (e) => {
+				fn();
+				expect(startScrollTop).toBe(0);
+				expect(onScrollStartCount).toBe(1);
+
+				resultScrollTop = e.scrollTop;
+				expect(resultScrollTop).toBeGreaterThan(0);
+			};
+
+			render(
+				<VirtualGridList
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={itemSize}
+					onScrollStop={onScrollStop}
+					onScrollStart={handlerOnScrollStart}
+				/>
+			);
+
+			const list = screen.getByRole('list');
+
+			activate(list);
+			await drag(list, {delta: {x: 0, y: -300}});
+
+			act(() => jest.advanceTimersByTime(1000)); // Wait onScrollStop
+
+			expect(fn).toBeCalled();
+		});
+
+		test('should not scroll to the by key', (done) => {
+			const fn = jest.fn();
+
+			render(
+				<VirtualGridList
+					clientSize={clientSize}
+					dataSize={dataSize}
+					itemRenderer={renderItem}
+					itemSize={itemSize}
+					onScrollStop={fn}
+					onScrollStart={handlerOnScrollStart}
+				/>
+			);
+
+			const list = screen.getByRole('list');
+
+			activate(list);
+
+			for (let i = 0; i < 50; i++) {
+				downKeyDown(list);
+			}
+
+			act(() => jest.advanceTimersByTime(1000)); // Wait onScrollStop
+
+			expect(fn).not.toHaveBeenCalled();
+
+			done();
 		});
 	});
 
