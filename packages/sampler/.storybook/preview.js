e15ccc5da923d4185caf1a3a29cce0e8a5b048e6
Stanca <63341832+stanca-pop-lgp@users.noreply.github.com>
2022-11-02 12:23:22 +0200

    Update preview.js

--- packages/sampler/.storybook/preview.js
+++ packages/sampler/.storybook/preview.js
@@ -43,7 +43,7 @@ export const parameters = {
 	docs: {
 		container: DocsContainer,
 		inlineStories: false,
-		iframeHeight: ri.scaleToRem(900),
+		iframeHeight: ri.scaleToRem(300),
 		page: () => (
 			<>
 				<Title />
