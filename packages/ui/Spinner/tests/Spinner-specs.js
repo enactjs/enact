8238ecce965af777239a38a4242c8fc5fe8b5048
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2022-11-21 14:15:41 +0200

    fixed path of FloatingLayerDecorator for spinner unit test

--- packages/ui/Spinner/tests/Spinner-specs.js
+++ packages/ui/Spinner/tests/Spinner-specs.js
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 import {render, screen} from '@testing-library/react';
 
 import {SpinnerBase} from '../Spinner';
-import {FloatingLayerDecorator} from '../../FloatingLayer';
+import {FloatingLayerDecorator} from '../../FloatingLayer/FloatingLayerDecorator';
 
 describe('Spinner Specs', () => {
 	test('should have centered class when centered prop equals true', () => {
