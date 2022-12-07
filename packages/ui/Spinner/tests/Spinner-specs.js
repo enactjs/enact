893685fa75580192cf3e47e77f0e7213355bc296
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2022-12-07 10:51:20 +0200

    code review fixes

--- packages/ui/Spinner/tests/Spinner-specs.js
+++ packages/ui/Spinner/tests/Spinner-specs.js
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 import {render, screen} from '@testing-library/react';
 
 import {SpinnerBase} from '../Spinner';
-import {FloatingLayerDecorator} from '../../FloatingLayer/FloatingLayerDecorator';
+import {FloatingLayerDecorator} from '../../FloatingLayer';
 
 describe('Spinner Specs', () => {
 	test('should have centered class when centered prop equals true', () => {
