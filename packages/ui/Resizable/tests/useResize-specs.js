d81cdac36d86eb3fb38217c36f1ff2b55be7e55a
Juwon Jeong <juwon.jeong@lge.com>
2023-05-19 17:14:06 +0900

    WRP-862: Refactor Resizable to be hook-based implementation (#3102)
    * WRO-5710: Added useResizable hook
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Cleanup useResizable code
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Add useResizable cleanup(unmount) function
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Add useResizable unit test
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Fix lint warnings
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Fix lint warnings
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Rename useResizable to useResize
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Refactoring Resizable to use useResize
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Fix typo
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Fix useResize
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Fix Resizable unit test
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Change .travis.yml to avoid travis error
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Apply review to fix documentation of useResize
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Fix typo
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Add temporal code to .travis.yml  to fix travis error
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Apply review
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Add temporal code to fix travis error
    
    * Fix travis node version to 19
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Apply review
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)

---
+++ packages/ui/Resizable/tests/useResize-specs.js
@@ -0,0 +1,38 @@
+import Registry from '@enact/core/internal/Registry';
+import {render} from '@testing-library/react';
+
+import {ResizeContext} from '../Resizable';
+import useResize from '../useResize';
+
+describe('useResize', () => {
+	let data;
+
+	const DivComponent = (props) => {
+		data = props;
+
+		return <div data-testid="component" />;
+	};
+
+	const ResizeButtonWithHook = (props) => {
+		const handlers = useResize(props, {resize: 'onClick'});
+		return <DivComponent {...handlers}>{props.children}</DivComponent>;
+	};
+
+	describe('config', () => {
+		test('should pass \'onClick\' handler to the wrapped component', () => {
+			const resizeRegistry = Registry.create();
+			render(
+				<ResizeContext.Provider value={resizeRegistry.register}>
+					<ResizeButtonWithHook />
+				</ResizeContext.Provider>
+			);
+
+			expect(data).toHaveProperty('onClick');
+
+			const expected = 'function';
+			const actual = typeof data.onClick;
+
+			expect(actual).toBe(expected);
+		});
+	});
+});
