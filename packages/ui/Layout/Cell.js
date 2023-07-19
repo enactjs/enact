ad7dd7e77426cb902331f29087f525da124615cc
alexandru-morariu-lgp <84004128+alexandrumorariu@users.noreply.github.com>
2023-07-19 16:58:24 +0300

    Quick fix

--- packages/ui/Layout/Cell.js
+++ packages/ui/Layout/Cell.js
@@ -60,7 +60,7 @@ const CellBase = kind({
 		component:  EnactPropTypes.renderable,
 
 		/**
-		 * Customizes the component used in the `Cell` by mapping the supplied collection of CSS class names to the corresponding internal elements and states of this component..
+		 * Customizes the component used in the `Cell` by mapping the supplied collection of CSS class names to the corresponding internal elements and states of this component.
 		 *
 		 * When using {@link ui/Layout.Cell}, the `css` prop is forwarded to this component
 		 * as `componentCss`.
