9e8c3a2c3c8ac0f4609031ed10c49b457f94182c
SJ RO <sj.ro@lge.com>
2023-07-28 17:34:12 +0900

    fix the comments and link Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)

--- packages/spotlight/SpotlightContainerDecorator/SpotlightContainerDecorator.js
+++ packages/spotlight/SpotlightContainerDecorator/SpotlightContainerDecorator.js
@@ -75,19 +75,19 @@ const defaultConfig = {
 	 * Directs which element receives focus when gaining focus from another container. If
 	 * `'default-element'`, the default focused item will be selected. If `'last-focused'`, the
 	 * container will focus the last focused item; if the container has never had focus, the default
-	 * element will receive focus. If `null`, the default 5-way behavior will be applied.
+	 * element will receive focus. If `''`, the default 5-way behavior will be applied.
 	 *
 	 * @type {String}
-	 * @default null
+	 * @default ''
 	 * @memberof spotlight/SpotlightContainerDecorator.SpotlightContainerDecorator.defaultConfig
 	 * @public
 	 */
-	enterTo: null,
+	enterTo: '',
 
 	/**
 	 * Directs which element receives focus when losing focus in the current container using 5-way direction keys.
 	 * If `{down: '#left'}`, the focus to move to the DOM element whose id value is `left` when pressing the 5-way down.
-	 * If `{left: '', right: ''}, the focus cannot leave the current container with 5-way left and right.
+	 * If `{left: '', right: ''}`, the focus cannot leave the current container with 5-way left and right.
 	 * If `null`, the default 5-way behavior will be applied.
 	 *
 	 * @type {Object}
