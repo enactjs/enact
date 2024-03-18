05671bd8e52cdbaecc2c5ec6df87e40d8936180f
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2024-03-18 16:59:53 +0200

    code review fixes

--- packages/spotlight/src/tests/inputType-specs.js
+++ packages/spotlight/src/tests/inputType-specs.js
@@ -1,6 +1,24 @@
 import {getInputType, setInputType} from '../inputType';
 
 describe('inputType', () => {
+	test('should have a default input type as \'key\'', () => {
+		setInputType('none');
+
+		const expected = 'key';
+		const actual = getInputType();
+
+		expect(actual).toBe(expected);
+	});
+
+	test('should ignore input type in internal variable when invalid type is set', () => {
+		setInputType('none');
+
+		const expected = 'key';
+		const actual = getInputType();
+
+		expect(actual).toBe(expected);
+	});
+
 	test('should set input type properly in internal variable', () => {
 		setInputType('touch');
 
