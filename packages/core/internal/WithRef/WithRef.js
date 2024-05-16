eee59517207dc9c29b8e9f0b7a67bbadce146f7c
Seungcheon Baek <5037955+SeungcheonBaek@users.noreply.github.com>
2024-05-16 16:36:51 +0900

    Implement `WithRef` private internal higher order component
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

---
+++ packages/core/internal/WithRef/WithRef.js
@@ -0,0 +1,30 @@
+import {forwardRef, useId, useImperativeHandle, useRef} from 'react';
+
+const WithRef = (WrappedComponent) => {
+	const HoC = forwardRef(function ({['data-withref-id']: givenId, referrerName, ...rest}, ref) {
+		const divRef = useRef();
+		const generatedId = useId();
+		const id = givenId || generatedId;
+
+		useImperativeHandle(ref, () => {
+			const node = divRef.current;
+			const attributeSelector = `[data-withref-id="${node.getAttribute('data-withref-target')}"]`;
+			const selector = `:scope ${attributeSelector}, :scope :has(${attributeSelector})`;
+			return node.parentElement.querySelector(selector) || null;
+		}, []);
+
+		return (
+			<>
+				<WrappedComponent {...rest} data-withref-id={id} />
+				<div data-withref-target={id} data-withref-referrer={referrerName} ref={divRef} style={{display: 'none'}} />
+			</>
+		);
+	});
+
+	HoC.displayName = 'WithRef';
+
+	return HoC;
+};
+
+export default WithRef;
+export {WithRef};
