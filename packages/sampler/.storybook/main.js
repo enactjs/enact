ba4d25b3cf1d1f634be325984b81fcaa300eeee8
Daniel Stoian <daniel.stoian@lgepartner.com>
2024-06-14 12:36:58 +0300

    added back storybook/addon-docs

--- packages/sampler/.storybook/main.js
+++ packages/sampler/.storybook/main.js
@@ -33,7 +33,8 @@ module.exports = {
 	addons: [
 		'@enact/storybook-utils/addons/actions',
 		'@enact/storybook-utils/addons/controls',
-		'@enact/storybook-utils/addons/toolbars'
+		'@enact/storybook-utils/addons/toolbars',
+		'@storybook/addon-docs'
 	],
 	webpackFinal: async (config, {configType}) => {
 		return webpack(config, configType, __dirname);
