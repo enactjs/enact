1f85566a1cacb1528449532f369b23ff302d9a38
Juwon Jeong <juwon.jeong@lge.com>
2025-03-06 15:16:09 +0900

    Added sampler
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)

---
+++ packages/sampler/stories/default/Card.js
@@ -0,0 +1,34 @@
+import {boolean, select, text} from '@enact/storybook-utils/addons/controls';
+import Card from '@enact/ui/Card';
+import ri from '@enact/ui/resolution';
+
+Card.displayName = 'Card';
+
+export default {
+	title: 'UI/Card',
+	component: 'Card'
+};
+
+export const _Card = (args) => (
+	<Card
+		orientation={args['orientation']}
+		src={args['src']}
+		textOverlay={args['textOverlay']}
+		style={{
+			width: ri.scaleToRem(180),
+			height: ri.scaleToRem(120)
+		}}
+	>
+		{'Card'}
+	</Card>
+);
+
+select('orientation', _Card, ['horizontal', 'vertical'], Card, 'vertical');
+boolean('textOverlay', _Card, Card);
+text('src', _Card, Card, "https://placehold.co/300x300/69cdff/ffffff/png?text=Image");
+
+_Card.parameters = {
+	info: {
+		text: 'Basic usage of Card'
+	}
+};
