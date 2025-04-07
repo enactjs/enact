a9bcc2899119547f4548d99dce7d6e63a7edbca4
myelyn.kim <myelyn.kim@lge.com>
2025-04-07 15:29:16 +0900

    Added reset method to spotlight to reseting an instance of Spotlight Accelerator
    Enact-DCO-1.0-Signed-off-by: Hyelyn Kim (myelyn.kim@lge.com)

--- packages/spotlight/src/spotlight.js
+++ packages/spotlight/src/spotlight.js
@@ -967,7 +967,16 @@ const Spotlight = (function () {
 		 * @param {Object} position `x` and `y` coordinates for the pointer
 		 * @private
 		 */
-		focusNextFromPoint: spotNextFromPoint
+		focusNextFromPoint: spotNextFromPoint,
+
+		/**
+		 * Resets spotlight accelerate
+		 *
+		 * @private
+		 */
+		resetSpotlightAccelerate: function () {
+			SpotlightAccelerator.reset();
+		}
 	};
 
 	return exports;
