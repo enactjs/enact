d566900f9135ed1ee46efd6e1acf37072ffdaa8e
Juwon Jeong <juwon.jeong@lge.com>
2025-04-09 16:02:45 +0900

    Fixed typo
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)

--- packages/spotlight/src/spotlight.js
+++ packages/spotlight/src/spotlight.js
@@ -970,7 +970,7 @@ const Spotlight = (function () {
 		focusNextFromPoint: spotNextFromPoint,
 
 		/**
-		 * Resets spotlight accelerate
+		 * Resets spotlight accelerator
 		 *
 		 * @private
 		 */
