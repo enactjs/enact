47bf3f8f369e5b8997714cbfb29455f8cc1257a9
ion-andrusciac-lgp <ion.andrusciac@lgepartner.com>
2025-09-04 17:11:46 +0300

    Added `unit-tests`

---
+++ packages/ui/Scroller/tests/ScrollerBasic-specs.js
@@ -0,0 +1,41 @@
+import '@testing-library/jest-dom';
+
+import {ScrollerBasic} from '../Scroller';
+
+describe('ScrollBasic', () => {
+	let scrollContentRef;
+
+	beforeEach(() => {
+		scrollContentRef = {
+			current: {
+				scrollLeft: 0,
+				scrollTop: 0,
+				scrollTo: jest.fn()
+			}
+		};
+	});
+
+	test(
+		'should call scrollTo on scrollToPosition',
+		async () => {
+			const instance = new ScrollerBasic({scrollContentRef, direction: 'both'});
+
+			instance.scrollToPosition(30, 40, 'smooth');
+			expect(scrollContentRef.current.scrollTo).toHaveBeenCalledWith({left: 30, top: 40, behavior: 'smooth'});
+
+			instance.scrollToPosition(30, 40, 'instant');
+			expect(scrollContentRef.current.scrollTo).toHaveBeenCalledWith({left: 30, top: 40, behavior: 'instant'});
+		}
+	);
+
+	test(
+		'should call requestAnimationFrame on animateScroll',
+		async () => {
+			const requestAnimationFrame = jest.spyOn(window, 'requestAnimationFrame').mockImplementation(() => {});
+			const instance = new ScrollerBasic({scrollContentRef, direction: 'both'});
+
+			instance.animateScroll(30, 40, scrollContentRef.current);
+			expect(requestAnimationFrame).toHaveBeenCalled();
+		}
+	);
+});
