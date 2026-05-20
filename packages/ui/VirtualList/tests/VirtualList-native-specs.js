8fa92b034641a3f2106c38cacbea316be1f46d68
ion-andrusciac-lgp <ion.andrusciac@lgepartner.com>
2026-05-20 14:34:26 +0300

    Added `unit tests`

--- packages/ui/VirtualList/tests/VirtualList-native-specs.js
+++ packages/ui/VirtualList/tests/VirtualList-native-specs.js
@@ -1,7 +1,15 @@
 import '@testing-library/jest-dom';
 import {act, render, screen} from '@testing-library/react';
 
-import VirtualList from '../VirtualList';
+import VirtualList, {VirtualListBasic} from '../VirtualList';
+
+const mockPlatform = {};
+
+jest.mock('@enact/core/platform', () => ({
+	get platform () {
+		return mockPlatform;
+	}
+}));
 
 describe('VirtualList with native scrollMode', () => {
 	let
@@ -176,6 +184,89 @@ describe('VirtualList with native scrollMode', () => {
 			expect(actual).toBe(expected);
 			done();
 			jest.useRealTimers();
+		});
+	});
+
+	describe('Animating scroll', () => {
+		let instance, node, scrollContentRef;
+
+		beforeEach(() => {
+			node = {
+				scrollTo: jest.fn(),
+				scrollBy: jest.fn(),
+				scrollLeft: 0,
+				scrollTop: 0
+			};
+			scrollContentRef = {current: node};
+			instance = new VirtualListBasic({scrollContentRef, clientSize, itemSize, itemRenderer: renderItem, scrollMode: 'native'});
+
+			jest.spyOn(window, 'requestAnimationFrame').mockImplementation((cb) => {
+				instance.rafCallback = cb;
+				return 123;
+			});
+			jest.spyOn(window, 'cancelAnimationFrame').mockImplementation(jest.fn());
+			jest.spyOn(performance, 'now').mockReturnValue(0);
+		});
+
+		test('should call native scrollTo when behavior is instant', () => {
+			instance.scrollToPosition(0, 300, 'instant');
+
+			expect(node.scrollTo).toHaveBeenCalledWith({left: 0, top: 300, behavior: 'instant'});
+		});
+
+		test('should call native scrollTo when NOT on Chrome and behavior is smooth', () => {
+			mockPlatform.chrome = false;
+			instance.scrollToPosition(0, 300, 'smooth');
+
+			expect(node.scrollTo).toHaveBeenCalledWith({left: 0, top: 300, behavior: 'smooth'});
+		});
+
+		test('should call animateScroll when on Chrome and behavior is smooth', () => {
+			mockPlatform.chrome = 88;
+			instance.animateScroll = jest.fn();
+			instance.scrollToPosition(0, 300, 'smooth');
+
+			expect(instance.animateScroll).toHaveBeenCalledWith(0, 300, node);
+		});
+
+		test('should cancel animation when content is scrolled by pageKey', () => {
+			instance.scrollToPosition(0, 600, 'smooth');
+			node.lastInputType = 'pageKey';
+			instance.scrollToPosition(0, 200, 'smooth');
+
+			expect(window.cancelAnimationFrame).toHaveBeenCalled();
+		});
+
+		test('should initiate animation loop using requestAnimationFrame', () => {
+			instance.animateScroll(0, 300, node);
+
+			expect(window.requestAnimationFrame).toHaveBeenCalled();
+		});
+
+		test('should scroll incrementally on each frame', () => {
+			instance.animateScroll(0, 300, node);
+			instance.rafCallback();
+
+			expect(node.scrollBy).toHaveBeenCalledWith(expect.objectContaining({behavior: 'instant'}));
+			expect(node.scrolling).toBe(true);
+		});
+
+		test('should cancel animation and stop when target is reached', () => {
+			node.scrollTop = 300;
+			instance.animateScroll(0, 310, node);
+			instance.rafCallback(600);
+
+			expect(window.cancelAnimationFrame).toHaveBeenCalled();
+			expect(node.scrolling).toBe(false);
+		});
+
+		test('should fallback to smooth scroll if animation exceeds duration (timeout)', () => {
+			instance.animateScroll(0, 300, node);
+			instance.rafCallback(600);
+			node.scrollTop = 250;
+
+			expect(node.scrollTo).toHaveBeenCalledWith({left: 0, top: 300, behavior: 'smooth'});
+			expect(window.cancelAnimationFrame).toHaveBeenCalled();
 		});
 	});
 });
