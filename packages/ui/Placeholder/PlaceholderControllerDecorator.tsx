313e721586a2c4282f509ac773eee233b79c951b
alexandru-morariu-lgp <84004128+alexandrumorariu@users.noreply.github.com>
2026-09-09 10:08:38 +0300

    Review fixes

--- packages/ui/Placeholder/PlaceholderControllerDecorator.tsx
+++ packages/ui/Placeholder/PlaceholderControllerDecorator.tsx
@@ -96,8 +96,8 @@ const PlaceholderControllerDecorator = hoc(defaultConfig, (config, Wrapped) => {
 			} else {
 				this.node = this.nodeRef.current;
 				this.bounds = {
-					height: this.node!.offsetHeight,
-					width: this.node!.offsetWidth
+					height: this.node?.offsetHeight ?? 0,
+					width: this.node?.offsetWidth ?? 0
 				};
 			}
 		}
